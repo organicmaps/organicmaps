4b958d35beb127130c0f448aa1260c3ae5b8f707
Alex Zolotarev <deathbaba@gmail.com>
2011-01-15 06:31:53 +0200

    Updated string_utils tests

--- base/base_tests/string_utils_test.cpp
+++ base/base_tests/string_utils_test.cpp
@@ -17,3 +17,30 @@ UNIT_TEST(make_lower_case)
   utils::make_lower_case(s);
   TEST_EQUAL(s, "this_is_lower", ());
 }
+
+UNIT_TEST(to_double)
+{
+  string s;
+  double d;
+
+  s = "0.123";
+  TEST(utils::to_double(s, d), ());
+  TEST_ALMOST_EQUAL(0.123, d, ());
+
+  s = "1.";
+  TEST(utils::to_double(s, d), ());
+  TEST_ALMOST_EQUAL(1.0, d, ());
+
+  s = "0";
+  TEST(utils::to_double(s, d), ());
+  TEST_ALMOST_EQUAL(0., d, ());
+
+  s = "5.6843418860808e-14";
+  TEST(utils::to_double(s, d), ());
+  TEST_ALMOST_EQUAL(5.6843418860808e-14, d, ());
+
+  s = "-2";
+  TEST(utils::to_double(s, d), ());
+  TEST_ALMOST_EQUAL(-2.0, d, ());
+
+}
