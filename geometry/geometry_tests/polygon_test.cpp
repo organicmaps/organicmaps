859862235121a661d1dd882dcfb9627d57dda236
vng <viktor.govako@gmail.com>
2011-01-16 13:33:58 +0200

    - Fix 'strip' algorithm; - Add IsPolygonCCW() function; - Add more tests;

--- geometry/geometry_tests/polygon_test.cpp
+++ geometry/geometry_tests/polygon_test.cpp
@@ -1,11 +1,16 @@
 #include "../../testing/testing.hpp"
+
 #include "../polygon.hpp"
 #include "../point2d.hpp"
+
 #include "../../base/macros.hpp"
+
+#include "../../std/algorithm.hpp"
+
 
 namespace
 {
-typedef m2::PointD P;
+  typedef m2::PointD P;
 }
 
 UNIT_TEST(IsSegmentInCone)
@@ -29,33 +34,102 @@ UNIT_TEST(IsSegmentInCone)
   TEST(!IsSegmentInCone(P(0,0), P(-1,-3), P(-1,1), P(1,1)), ());
 }
 
+namespace
+{
+  template <typename IterT>
+  void TestDiagonalVisible(IterT beg, IterT end, IterT i0, IterT i1, bool res)
+  {
+    TEST_EQUAL ( IsDiagonalVisible(beg, end, i0, i1), res, () );
+    TEST_EQUAL ( IsDiagonalVisible(beg, end, i1, i0), res, () );
+  }
+}
+
 UNIT_TEST(IsDiagonalVisible)
 {
-  P polyA [] = { P(0,0), P(3,0), P(3,2), P(2,2), P(2,1), P(0,1) };
-  vector<P> poly(&polyA[0], &polyA[0] + ARRAY_SIZE(polyA));
-  // TODO: Reverse directions.
+  P poly [] = { P(0, 0), P(3, 0), P(3, 2), P(2, 2), P(2, 1), P(0, 1) };
+  P const * b = poly;
+  P const * e = poly + ARRAY_SIZE(poly);
 
-  TEST(!IsDiagonalVisible(poly.begin(), poly.end(), poly.begin() + 0, poly.begin() + 2), ());
-  TEST(!IsDiagonalVisible(poly.begin(), poly.end(), poly.begin() + 0, poly.begin() + 3), ());
-  TEST( IsDiagonalVisible(poly.begin(), poly.end(), poly.begin() + 0, poly.begin() + 4), ());
-  TEST(!IsDiagonalVisible(poly.begin(), poly.end(), poly.begin() + 5, poly.begin() + 3), ());
-  TEST(!IsDiagonalVisible(poly.begin(), poly.end(), poly.begin() + 5, poly.begin() + 2), ());
-  TEST( IsDiagonalVisible(poly.begin(), poly.end(), poly.begin() + 5, poly.begin() + 1), ());
-  TEST( IsDiagonalVisible(poly.begin(), poly.end(), poly.begin() + 1, poly.begin() + 5), ());
+  TestDiagonalVisible(b, e, b + 0, b + 1, true);
+  TestDiagonalVisible(b, e, b + 0, b + 2, false);
+  TestDiagonalVisible(b, e, b + 0, b + 3, false);
+  TestDiagonalVisible(b, e, b + 0, b + 4, true);
+  TestDiagonalVisible(b, e, b + 0, b + 5, true);
+  TestDiagonalVisible(b, e, b + 5, b + 4, true);
+  TestDiagonalVisible(b, e, b + 5, b + 3, false);
+  TestDiagonalVisible(b, e, b + 5, b + 2, false);
+  TestDiagonalVisible(b, e, b + 5, b + 1, true);
+}
+
+namespace 
+{
+  void TestFindStrip(P const * beg, size_t n)
+  {
+    size_t const i = FindSingleStrip(n, IsDiagonalVisibleFunctor<P const *>(beg, beg + n));
+    TEST_LESS(i, n, ());
+
+    vector<size_t> test;
+    MakeSingleStripFromIndex(i, n, MakeBackInsertFunctor(test));
+
+    sort(test.begin(), test.end());
+    unique(test.begin(), test.end());
+
+    TEST_EQUAL(test.size(), n, ());
+  }
+
+  void TestFindStripMulti(P const * beg, size_t n)
+  {
+    for (size_t i = 3; i <= n; ++i)
+      TestFindStrip(beg, i);
+  }
 }
 
 UNIT_TEST(FindSingleStrip)
 {
   {
-    P poly [] = { P(0,0), P(3,0), P(3,2), P(2,2), P(2,1), P(0,1) };
-    size_t const n = ARRAY_SIZE(poly);
-    TEST_NOT_EQUAL(
-          FindSingleStrip(n, IsDiagonalVisibleFunctor<P const *>(&poly[0], &poly[0] + n)), n, ());
+    P poly[] = { P(0, 0), P(3, 0), P(3, 2), P(2, 2), P(2, 1), P(0, 1) };
+    TestFindStripMulti(poly, ARRAY_SIZE(poly));
   }
+
   {
-    P poly [] = { P(0,0), P(2, -1), P(3,-1), P(3,2), P(2,2), P(2,1), P(0,1) };
-    size_t const n = ARRAY_SIZE(poly);
-    TEST_EQUAL(
-          FindSingleStrip(n, IsDiagonalVisibleFunctor<P const *>(&poly[0], &poly[0] + n)), n, ());
+    P poly[] = { P(0, 0), P(2, -1), P(3, -1), P(3, 2), P(2, 2), P(2, 1), P(0, 1) };
+    TestFindStripMulti(poly, ARRAY_SIZE(poly));
   }
+
+  {
+    // Minsk, Bobryiskaya str., 7
+    P poly[] = {
+      P(53.8926922, 27.5460021),
+      P(53.8926539, 27.5461821),
+      P(53.8926164, 27.5461591),
+      P(53.8925455, 27.5464921),
+      P(53.8925817, 27.5465143),
+      P(53.8925441, 27.5466909),
+      P(53.8923762, 27.5465881),
+      P(53.8925229, 27.5458984)
+    };
+    TestFindStrip(poly, ARRAY_SIZE(poly));
+  }
+}
+
+namespace 
+{
+  template <typename IterT> void TestPolygonCCW(IterT beg, IterT end)
+  {
+    TEST(IsPolygonCCW(beg, end), ());
+    reverse(beg, end);
+    TEST(!IsPolygonCCW(beg, end), ())
+  }
+}
+
+UNIT_TEST(IsPolygonCCW)
+{
+  P arr1[] = { P(1, 1), P(2, 0), P(3, 2) };
+  TestPolygonCCW(arr1, arr1 + ARRAY_SIZE(arr1));
+
+  P arr2[] = { P(0, 0), P(1, 0), P(0, 1) };
+  TestPolygonCCW(arr2, arr2 + ARRAY_SIZE(arr2));
+
+  P arr3[] = { P(0, 1), P(1, 1), P(1, 0), P(2, 0), P(2, 1), P(1, 1), P(1, 2), P(0, 2) };
+  TestPolygonCCW(arr3, arr3 + ARRAY_SIZE(arr3));
 }
--- geometry/polygon.hpp
+++ geometry/polygon.hpp
@@ -5,33 +5,75 @@
 #include "../base/math.hpp"
 #include "../base/stl_add.hpp"
 
+#include "../std/iterator.hpp"
+
+
+template <typename IsVisibleF>
+bool FindSingleStripForIndex(size_t i, size_t n, IsVisibleF isVisible)
+{
+  // Searching for a strip only in a single direction, because the opposite direction
+  // is traversed from the last vertex of the possible strip.
+  size_t a = my::PrevModN(i, n);
+  size_t b = my::NextModN(i, n);
+  for (size_t j = 2; j < n; ++j)
+  {
+    ASSERT_NOT_EQUAL ( a, b, () );
+    if (!isVisible(a, b))
+      return false;
+    if (j & 1)
+      a = my::PrevModN(a, n);
+    else
+      b = my::NextModN(b, n);
+  }
+
+  ASSERT_EQUAL ( a, b, () );
+  return true;
+}
+
 // If polygon with n vertices is a single strip, return the start index of the strip or n otherwise.
 template <typename IsVisibleF>
-size_t FindSingleStrip(size_t const n, IsVisibleF isVisible)
+size_t FindSingleStrip(size_t n, IsVisibleF isVisible)
 {
   for (size_t i = 0; i < n; ++i)
   {
-    // Searching for a strip only in a single direction, because the opposite direction
-    // is traversed from the last vertex of the possible strip.
-    size_t a = my::PrevModN(i, n);
-    size_t b = my::NextModN(i, n);
-    for (size_t j = 2; j < n; ++j)
-    {
-      ASSERT_NOT_EQUAL(a, b, ());
-      if (!isVisible(a, b))
-        break;
-      if (j & 1)
-        a = my::PrevModN(a, n);
-      else
-        b = my::NextModN(b, n);
-    }
-    if (a == b)
+    if (FindSingleStripForIndex(i, n, isVisible))
       return i;
   }
+
   return n;
 }
 
-// Is segment (v, v1) in cone (vPrev, v, vNext)? Orientation CCW.
+#ifdef DEBUG
+template <typename IterT> bool TestPolygonPreconditions(IterT beg, IterT end)
+{
+  ASSERT_GREATER ( distance(beg, end), 2, () );
+  ASSERT ( !AlmostEqual(*beg, *(--end)), () );
+  return true;
+}
+#endif
+
+/// Is polygon [beg, end) has CCW orientation.
+template <typename IterT> bool IsPolygonCCW(IterT beg, IterT end)
+{
+  ASSERT ( TestPolygonPreconditions(beg, end), () );
+
+  double minY = numeric_limits<double>::max();
+  IterT iRes;
+  for (IterT i = beg; i != end; ++i)
+  {
+    if ((*i).y < minY || ((*i).y == minY && (*i).x < (*iRes).x))
+    {
+      iRes = i;
+      minY = (*i).y;
+    }
+  }
+
+  return CrossProduct(*iRes - *PrevIterInCycle(iRes, beg, end),
+                      *NextIterInCycle(iRes, beg, end) - *iRes) > 0;
+}
+
+/// Is segment (v, v1) in cone (vPrev, v, vNext)?
+/// @precondition Orientation CCW!!!
 template <typename PointT> bool IsSegmentInCone(PointT v, PointT v1, PointT vPrev, PointT vNext)
 {
   PointT const diff = v1 - v;
@@ -52,12 +94,21 @@ template <typename PointT> bool IsSegmentInCone(PointT v, PointT v1, PointT vPre
   }
 }
 
-// Is diagonal (i0, i1) visible in polygon [beg, end).
+/// Is diagonal (i0, i1) visible in polygon [beg, end).
+/// @precondition Orientation CCW!!
 template <typename IterT>
-bool IsDiagonalVisible(IterT const beg, IterT const end, IterT const i0, IterT const i1)
+bool IsDiagonalVisible(IterT beg, IterT end, IterT i0, IterT i1)
 {
-  // TODO: Orientation CCW!!
-  if (!IsSegmentInCone(*i0, *i1, *PrevIterInCycle(i0, beg, end), *NextIterInCycle(i0, beg, end)))
+  ASSERT ( IsPolygonCCW(beg, end), () );
+  ASSERT ( TestPolygonPreconditions(beg, end), () );
+  ASSERT ( i0 != i1, () );
+
+  IterT const prev = PrevIterInCycle(i0, beg, end);
+  IterT const next = NextIterInCycle(i0, beg, end);
+  if (prev == i1 || next == i1)
+    return true;
+
+  if (!IsSegmentInCone(*i0, *i1, *prev, *next))
     return false;
 
   for (IterT j0 = beg, j1 = PrevIterInCycle(beg, beg, end); j0 != end; j1 = j0++)
@@ -69,14 +120,49 @@ bool IsDiagonalVisible(IterT const beg, IterT const end, IterT const i0, IterT c
 
 template <typename IterT> class IsDiagonalVisibleFunctor
 {
+  IterT m_Beg, m_End;
 public:
-  IsDiagonalVisibleFunctor(IterT const beg, IterT const end) : m_Beg(beg), m_End(end) {}
+  IsDiagonalVisibleFunctor(IterT beg, IterT end) : m_Beg(beg), m_End(end) {}
 
   bool operator () (size_t a, size_t b) const
   {
-    return IsDiagonalVisible(m_Beg, m_End, m_Beg + a, m_End + b);
+    return IsDiagonalVisible(m_Beg, m_End, m_Beg + a, m_Beg + b);
   }
-
-private:
-  IterT m_Beg, m_End;
 };
+
+namespace detail
+{
+  template <typename F> class StripEmitter
+  {
+    F & m_f;
+    int m_order;
+
+  public:
+    StripEmitter(F & f) : m_f(f), m_order(0) {}
+
+    bool operator () (size_t a, size_t b)
+    {
+      if (m_order == 0)
+      {
+        m_f(b);
+        m_f(a);
+        m_order = 1;
+      }
+      else
+      {
+        m_f(m_order == 1 ? b : a);
+        m_order = -m_order;
+      }
+      return true;
+    }
+  };
+}
+
+/// Make single strip for the range of points [beg, end), started with index = i.
+template <typename F> 
+void MakeSingleStripFromIndex(size_t i, size_t n, F f)
+{
+  ASSERT_LESS ( i, n, () );
+  f(i);
+  FindSingleStripForIndex(i, n, detail::StripEmitter<F>(f));
+}
