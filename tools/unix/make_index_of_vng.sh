f67523f3e7f5b42b6feacefc1362192d16837501
vng <viktor.govako@gmail.com>
2011-01-18 20:04:44 +0200

    Fix my shell file for maps generation.

--- tools/unix/make_index_of_vng.sh
+++ tools/unix/make_index_of_vng.sh
@@ -15,5 +15,6 @@ then
   PV=pv
 fi
 
-$PV ../../../omim-maps/$2.osm.bz2 | bzip2 -d | $INDEXER_TOOL --generate_intermediate_data=true --generate_final_data=false --use_light_nodes=true --generate_index=false --intermediate_data_path=$TMPDIR
-$PV ../../../omim-maps/$2.osm.bz2 | bzip2 -d | $INDEXER_TOOL --generate_intermediate_data=false --generate_final_data=true --use_light_nodes=true --generate_index=true --sort_features=true --intermediate_data_path=$TMPDIR --output=$2 --bucketing_level=0
+$PV ../../../omim-maps/$2.osm.bz2 | bzip2 -d | $INDEXER_TOOL --preprocess_xml=true --use_light_nodes=true --intermediate_data_path=$TMPDIR
+
+$PV ../../../omim-maps/$2.osm.bz2 | bzip2 -d | $INDEXER_TOOL --use_light_nodes=true --generate_features=true --generate_geometry=true --generate_index=true --sort_features=true --intermediate_data_path=$TMPDIR --output=$2 --bucketing_level=0
