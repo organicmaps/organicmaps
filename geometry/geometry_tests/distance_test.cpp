6292d9f06d5741cfe749ad199006ba6e299cb520
Alex Zolotarev <deathbaba@gmail.com>
2011-01-19 01:55:59 +0200

    Updated distance tests

--- geometry/geometry_tests/distance_test.cpp
+++ geometry/geometry_tests/distance_test.cpp
@@ -2,15 +2,32 @@
 #include "../distance.hpp"
 #include "../point2d.hpp"
 
-UNIT_TEST(DistanceToLineSquare2D)
+template <class PointT>
+void FloatingPointsTest()
 {
-  mn::DistanceToLineSquare<m2::PointD> d(m2::PointD(-1, 3), m2::PointD(2, 1));
-  TEST_ALMOST_EQUAL(d(m2::PointD(-1, 3)), 0.0, ());
-  TEST_ALMOST_EQUAL(d(m2::PointD(2, 1)), 0.0, ());
-  TEST_ALMOST_EQUAL(d(m2::PointD(-0.5, 0.5)), 3.25, ());
-  TEST_ALMOST_EQUAL(d(m2::PointD(-0.5, 0.5)), 3.25, ());
-  TEST_ALMOST_EQUAL(d(m2::PointD(3.5, 0.0)), 3.25, ());
-  TEST_ALMOST_EQUAL(d(m2::PointD(4.0, 4.0)), 13.0, ());
-  TEST_ALMOST_EQUAL(d(m2::PointD(0.5, 2.0)), 0.0, ());
-  TEST_ALMOST_EQUAL(d(m2::PointD(0.0, 1.25)), 0.5 * 0.5 + 0.75 * 0.75, ());
+  mn::DistanceToLineSquare<PointT> d(PointT(-1, 3), PointT(2, 1));
+  TEST_ALMOST_EQUAL(d(PointT(-1, 3)), 0.0, ());
+  TEST_ALMOST_EQUAL(d(PointT(2, 1)), 0.0, ());
+  TEST_ALMOST_EQUAL(d(PointT(-0.5, 0.5)), 3.25, ());
+  TEST_ALMOST_EQUAL(d(PointT(-0.5, 0.5)), 3.25, ());
+  TEST_ALMOST_EQUAL(d(PointT(3.5, 0.0)), 3.25, ());
+  TEST_ALMOST_EQUAL(d(PointT(4.0, 4.0)), 13.0, ());
+  TEST_ALMOST_EQUAL(d(PointT(0.5, 2.0)), 0.0, ());
+  TEST_ALMOST_EQUAL(d(PointT(0.0, 1.25)), 0.5 * 0.5 + 0.75 * 0.75, ());
+}
+
+UNIT_TEST(DistanceToLineSquare2D_Floating)
+{
+  FloatingPointsTest<m2::PointD>();
+  FloatingPointsTest<m2::PointF>();
+}
+
+#include "../../base/logging.hpp"
+
+UNIT_TEST(DistanceToLineSquare2D_Integer)
+{
+  mn::DistanceToLineSquare<m2::PointI> dI(m2::PointI(-1, 3), m2::PointI(2, 1));
+  TEST_ALMOST_EQUAL(dI(m2::PointI(-1, 3)), 0.0, ());
+  TEST_ALMOST_EQUAL(dI(m2::PointI(2, 1)), 0.0, ());
+  TEST_ALMOST_EQUAL(dI(m2::PointI(4, 4)), 13.0, ());
 }
