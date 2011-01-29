ec88c9bc0e5b6bf84be66a3f1c1c9f551725ac8c
Yury Melnichek <melnichek@gmail.com>
2011-01-29 16:57:48 +0100

    Add maxPoint to the geometry coding routines.

--- indexer/geometry_coding.hpp
+++ indexer/geometry_coding.hpp
@@ -7,26 +7,32 @@
 
 void EncodePolyline(vector<m2::PointU> const & points,
                     m2::PointU const & basePoint,
+                    m2::PointU const & maxPoint,
                     vector<char> & serialOutput);
 
 void DecodePolyline(char const * pBeg, char const * pEnd,
                     m2::PointU const & basePoint,
+                    m2::PointU const & maxPoint,
                     vector<m2::PointU> & points);
 
 void EncodeTriangles(vector<m2::PointU> const & points,
                      vector<tuple<uint32_t, uint32_t, uint32_t> > const & triangles,
                      m2::PointU const & basePoint,
+                     m2::PointU const & maxPoint,
                      vector<char> & serialOutput);
 
 void DecodeTriangles(char const * pBeg, char const * pEnd,
                      m2::PointU const & basePoint,
+                     m2::PointU const & maxPoint,
                      vector<m2::PointU> & points,
                      vector<tuple<uint32_t, uint32_t, uint32_t> > & triangles);
 
 void EncodeTriangleStrip(vector<m2::PointU> const & points,
                          m2::PointU const & basePoint,
+                         m2::PointU const & maxPoint,
                          vector<char> & serialOutput);
 
 void DecodeTriangleStrip(char const * pBeg, char const * pEnd,
                          m2::PointU const & basePoint,
+                         m2::PointU const & maxPoint,
                          vector<m2::PointU> & points);
