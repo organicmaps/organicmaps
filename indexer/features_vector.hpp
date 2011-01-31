bc5062fb4de7320a68b7f9fb6f528869522125c3
Yury Melnichek <melnichek@gmail.com>
2011-01-31 20:05:13 +0100

    Comment out some logging.

--- indexer/features_vector.hpp
+++ indexer/features_vector.hpp
@@ -30,7 +30,7 @@ public:
   {
     FileReader r = dataR.m_cont.GetReader(HEADER_FILE_TAG);
     m_source.m_base = ReadPrimitiveFromPos<int64_t>(r, 0);
-    LOG(LINFO, ("OFFSET = ", m_source.m_base));
+    // LOG(LINFO, ("OFFSET = ", m_source.m_base));
   }
 
   void Get(uint64_t pos, FeatureType & feature) const
