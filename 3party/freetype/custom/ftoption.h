107e292a94dbc899f1652c93652f9c5c64735e4c
Alex Zolotarev <deathbaba@gmail.com>
2011-02-13 01:07:28 +0100

    Freetype customization for arm non-thumb builds

--- 3party/freetype/custom/ftoption.h
+++ 3party/freetype/custom/ftoption.h
@@ -121,8 +121,9 @@ FT_BEGIN_HEADER
   /* performance-critical functions (e.g. FT_MulFix).  You should only do  */
   /* that to verify that the assembler function works properly, or to      */
   /* execute benchmark tests of the various implementations.               */
-/* #define FT_CONFIG_OPTION_NO_ASSEMBLER */
-
+#if defined( __arm__ ) && !defined( __thumb__ )
+#define FT_CONFIG_OPTION_NO_ASSEMBLER
+#endif
 
   /*************************************************************************/
   /*                                                                       */
