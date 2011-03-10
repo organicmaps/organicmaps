fac83110e21bf93d37333f08f1eb3b36cda9b531
Yury Melnichek <melnichek@gmail.com>
2011-03-10 23:57:56 +0100

    Better MY_PATH evaluation in ensure_mkspecs.sh

--- tools/unix/ensure_mkspecs.sh
+++ tools/unix/ensure_mkspecs.sh
@@ -1,3 +1,4 @@
-MY_PATH=`echo $0 | grep -o '.*/'`
+#!/bin/bash
+MY_PATH=`dirname $(stat -f %N $PWD"/"$0)`
 sudo ln -sf $MY_PATH/../mkspecs/iphone* /usr/local/Qt4.7/mkspecs/
 sudo ln -sf $MY_PATH/../mkspecs/common/iphone-* /usr/local/Qt4.7/mkspecs/common/
