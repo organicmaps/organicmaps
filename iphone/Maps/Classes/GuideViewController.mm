3e4133e109a46e4100e55b8b4fb1defb8f9d89f7
Yury Melnichek <melnichek@gmail.com>
2011-03-19 01:36:30 +0100

    [iPhone] GuideViewController orientation.

--- iphone/Maps/Classes/GuideViewController.mm
+++ iphone/Maps/Classes/GuideViewController.mm
@@ -63,7 +63,7 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    return YES;
 }
 
 @end
