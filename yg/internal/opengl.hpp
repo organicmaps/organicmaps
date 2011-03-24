46865415e0706f0ad0e683651d3d6ea50d0c63ca
Alex Zolotarev <deathbaba@gmail.com>
2011-03-24 23:10:54 +0000

    [iOS] Disabled opengles 2.0 with macro

--- yg/internal/opengl.hpp
+++ yg/internal/opengl.hpp
@@ -3,14 +3,17 @@
 
 #if defined(OMIM_OS_WINDOWS)
   #include "opengl_win32.hpp"
+
 #elif defined(OMIM_OS_BADA)
   #include <FGraphicsOpengl.h>
   using namespace Osp::Graphics::Opengl;
   #define OMIM_GL_ES
+
 #elif (defined(OMIM_OS_MAC) || defined(OMIM_OS_IPHONE))
   #include <TargetConditionals.h>
 
   #ifdef OMIM_OS_IPHONE
+    #define USE_OPENGLES20_IF_AVAILABLE 0
     #include <OpenGLES/ES1/gl.h>
     #include <OpenGLES/ES1/glext.h>
     #define OMIM_GL_ES
