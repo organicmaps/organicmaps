d0e84da10df5e059efa54d31cfb305abed49cfd2
Alex Zolotarev <deathbaba@gmail.com>
2011-03-24 23:11:14 +0000

    Fixed OS names

--- std/target_os.hpp
+++ std/target_os.hpp
@@ -2,13 +2,13 @@
 
 #if defined(_BADA_SIMULATOR) || defined(_BADA_DEVICE)
   #define OMIM_OS_BADA
-  #define OMIM_OS_NAME "Bada"
+  #define OMIM_OS_NAME "bada"
 
 #elif defined(__APPLE__)
   #include <TargetConditionals.h>
   #if (TARGET_OS_IPHONE > 0)
     #define OMIM_OS_IPHONE
-    #define OMIM_OS_NAME "iOS"
+    #define OMIM_OS_NAME "ios"
     #if (TARGET_IPHONE_SIMULATOR > 0)
       #define OMIM_OS_IPHONE_SIMULATOR
     #else
@@ -16,20 +16,19 @@
     #endif
   #else
     #define OMIM_OS_MAC
-    #define OMIM_OS_NAME "MacOS"
+    #define OMIM_OS_NAME "mac"
   #endif
 
 #elif defined(_WIN32)
   #define OMIM_OS_WINDOWS
+  #define OMIM_OS_NAME "win"
 
   #ifdef __MINGW32__
     #define OMIM_OS_WINDOWS_MINGW
-    #define OMIM_OS_NAME "MinGW"
   #else
     #define OMIM_OS_WINDOWS_NATIVE
-    #define OMIM_OS_NAME "Win"
   #endif
 #else
   #define OMIM_OS_LINUX
-  #define OMIM_OS_NAME "Linux"
+  #define OMIM_OS_NAME "linux"
 #endif
