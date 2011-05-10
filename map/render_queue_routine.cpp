6ec7e0ae5a0484d04551eb42dae2b42eb573049a
vng <viktor.govako@gmail.com>
2011-05-10 23:51:40 +0300

    Comment out 9x drawing.

--- map/render_queue_routine.cpp
+++ map/render_queue_routine.cpp
@@ -174,8 +174,9 @@ void RenderQueueRoutine::getUpdateAreas(
   }
   else
   {
-//    areas.push_back(newRect);
+    areas.push_back(newRect);
 
+    /*
     int rectW = (newRect.SizeX() + 9) / 5;
     int rectH = (newRect.SizeY() + 9) / 5;
     m2::RectI r( 2 * rectW, 2 * rectH, 3 * rectW, 3 * rectH);
@@ -204,7 +205,7 @@ void RenderQueueRoutine::getUpdateAreas(
     areas.push_back(m2::Offset(r, 0, 2*rectH));
     areas.push_back(m2::Offset(r, -rectW, 2*rectH));
     areas.push_back(m2::Offset(r, -2*rectW, 2*rectH));
-
+    */
   }
 }
 
