84487559ff2835d6043cb880233fc810c19e81a1
vng <viktor.govako@gmail.com>
2011-05-23 20:27:46 +0300

    Add world generation command files for Windows.

---
+++ tools/win/gen_index_world.bat
@@ -0,0 +1,3 @@
+call set_vars.bat %1 %2
+
+%GENERATOR_TOOL% --generate_index=true --intermediate_data_path=D:\Temp\ --output=World --bucketing_level=0
---
+++ tools/win/gen_world.bat
@@ -0,0 +1,4 @@
+call process_xml.bat %1 %2
+call gen_features_world.bat %1 %2
+call gen_geometry_world.bat %1 %2
+call gen_index_world.bat %1 %2
