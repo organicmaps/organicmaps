aba4488f301058e561af2d5c83edd987f0b42232
Alex Zolotarev <deathbaba@gmail.com>
2011-06-01 02:38:41 +0200

    [mac] Updated deployment script

--- tools/unix/create_mac_image.sh
+++ tools/unix/create_mac_image.sh
@@ -3,4 +3,4 @@
 cd ../../../omim-build-release/out/release
 umount /Volumes/MapsWithMe
 rm MapsWithMe.dmg
-macdeployqt MapsWithMe.app -dmg
+~/QtSDK/Desktop/Qt/473/gcc/bin/macdeployqt MapsWithMe.app -dmg
