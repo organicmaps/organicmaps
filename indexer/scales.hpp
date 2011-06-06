a4e947935d772f58de7b7b9d06134159a0b79b26
Alex Zolotarev <deathbaba@gmail.com>
2011-06-06 04:20:31 +0200

    For VNG: to finish methods implementation

--- indexer/scales.hpp
+++ indexer/scales.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../geometry/rect2d.hpp"
+#include "../geometry/point2d.hpp"
 
 namespace scales
 {
@@ -9,7 +10,11 @@ namespace scales
 
   double GetM2PFactor(int level);
   int GetScaleLevel(double ratio);
+  /// @return such ration, that GetScaleLevel(ration) == level
+  double GetRationForLevel(int level);
   int GetScaleLevel(m2::RectD const & r);
+  /// @return such rect, that GetScaleLevel(rect) == level
+  m2::RectD GetRectForLevel(int level, m2::PointD const & center, double widthToHeightRatio);
   double GetEpsilonForLevel(int level);
   double GetEpsilonForSimplify(int level);
   bool IsGoodForLevel(int level, m2::RectD const & r);
