7ec6b27014a07e96888303a7dd83015996f060b1
Alex Zolotarev <deathbaba@gmail.com>
2011-06-12 20:57:42 +0300

    Removed wstring completely, strings::UniString should be used instead

--- std/string.hpp
+++ std/string.hpp
@@ -12,12 +12,6 @@ using std::basic_string;
 using std::string;
 using std::getline;
 
-#if defined(OMIM_OS_BADA) || defined(OMIM_OS_ANDROID)
-typedef std::basic_string<wchar_t> wstring;
-#else
-using std::wstring;
-#endif
-
 #ifdef DEBUG_NEW
 #define new DEBUG_NEW
 #endif
