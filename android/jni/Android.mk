33d868612bf244d7c7afce53d5dfe944bd9ebdde
Alex Zolotarev <deathbaba@gmail.com>
2011-06-20 23:03:45 +0300

    [android] Linked static libs to mapswithme.so

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -4,6 +4,9 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := mapswithme
 LOCAL_SRC_FILES := main_native.cpp
-LOCAL_LDLIBS    := -llog
+LOCAL_LDLIBS    := -llog -L../../omim-android-release/out/release \
+  	-lwords -lmap -lstorage -lversion -lsearch -lindexer -lyg -lplatform \
+  	-lgeometry -lcoding -lbase -lexpat -lfreetype -lfribidi -lzlib -lbzip2 \
+  	-ljansson -ltomcrypt
 
 include $(BUILD_SHARED_LIBRARY)
---
+++ android/jni/Application.mk
@@ -0,0 +1,2 @@
+APP_STL := stlport_static
+APP_CFLAGS += -I../3party/boost
