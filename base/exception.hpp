9bce8acff51674edd91ce78c64a7c902d71e57fd
vng <viktor.govako@gmail.com>
2011-07-02 19:59:45 +0300

    Fix RootException message formatting.

--- base/exception.hpp
+++ base/exception.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include "internal/message.hpp"
 #include "macros.hpp"
-#include "../std/algorithm.hpp"
+
 #include "../std/exception.hpp"
 #include "../std/string.hpp"
 
@@ -18,18 +18,21 @@ public:
 
   virtual char const * what() const throw()
   {
+    size_t const count = m_Msg.size();
+
     string asciiMsg;
-    for (size_t i = 0; i < m_Msg.size(); ++i)
+    asciiMsg.resize(count);
+
+    for (size_t i = 0; i < count; ++i)
     {
       if (static_cast<unsigned char>(m_Msg[i]) < 128)
-      {
-        asciiMsg += char(m_Msg[i]);
-      } else
-      {
-        asciiMsg += '?';
-      }
+        asciiMsg[i] = char(m_Msg[i]);
+      else
+        asciiMsg[i] = '?';
     }
-    static string msg = string(m_What) + ", \"" + asciiMsg + "\"";
+
+    static string msg;
+    msg = string(m_What) + ", \"" + asciiMsg + "\"";
     return msg.c_str();
   }
 
