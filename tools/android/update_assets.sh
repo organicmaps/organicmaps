bbfac69edc26f17b78eb58e6b91dca70a756b6da
Alex Zolotarev <deathbaba@gmail.com>
2011-07-03 03:53:37 +0300

    [android] Added utility scripts

---
+++ tools/android/update_assets.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e -x -u
+
+SRC=../../data
+DST=../../android/assets
+
+# Remove old links
+rm $DST/*
+
+files=(01_dejavusans.ttf 02_wqy-microhei.ttf 03_jomolhari-id-a3d.ttf 04_padauk.ttf \
+       05_khmeros.ttf 06_code2000.ttf World.mwm about-travelguide-iphone.html \
+       basic.skn basic_highres.skn categories.txt classificator.txt countries.txt \
+       drawing_rules.bin fonts_blacklist.txt fonts_whitelist.txt languages.txt \
+       maps.update symbols_24.png symbols_48.png unicode_blocks.txt visibility.txt)
+
+for item in ${files[*]}
+do
+  ln -s $SRC/$item $DST/$item
+done
