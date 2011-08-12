299d31ecc5f348368e840d3e36ca6397b9ea8d45
Yury Melnichek <melnichek@malinovka.local>
2011-08-12 20:43:30 +0300

    Add script that runs all tests on Unix.

---
+++ tools/unix/run_tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e -u
+
+if [ $# -lt 1 ]; then
+  echo "Usage:"
+  echo "  $0 {debug|release}"
+  exit 115
+fi
+
+set -x
+
+find ../omim-build-$1/out/$1 -name "*_tests" | \
+    grep -v /tmp/ | \
+    awk '{ print $0" &&" } END { print "echo REALLY ALL TESTS PASSED" }' | \
+    sh -x
