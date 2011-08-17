b63811bab4cb45ade99b398cda1f2359fdc8b5da
vng <viktor.govako@gmail.com>
2011-08-17 13:17:17 +0300

    [win] Cut out search index generation into separate bat file.

--- tools/win/gen_index_world.bat
+++ tools/win/gen_index_world.bat
@@ -1,3 +1,3 @@
 call set_vars.bat %1 %2
 
-%GENERATOR_TOOL% --generate_index=true --generate_search_index=true --intermediate_data_path=D:\Temp\ --output=World --bucketing_level=0
+%GENERATOR_TOOL% --generate_index=true --intermediate_data_path=D:\Temp\ --output=World --bucketing_level=0
---
+++ tools/win/gen_search_index.bat
@@ -0,0 +1,3 @@
+call set_vars.bat %1 %2
+
+%GENERATOR_TOOL% --generate_search_index=true --output=%2
