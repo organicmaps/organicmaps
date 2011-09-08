0c3352b86446d6a2d148029dd6c7351c712d6555
vng <viktor.govako@gmail.com>
2011-09-08 15:06:01 +0300

    [win] Remove old build-mwm-toolchain options.

--- tools/win/gen_features_world.bat
+++ tools/win/gen_features_world.bat
@@ -1,3 +1,3 @@
 call set_vars.bat %1 %2
 
-%UNZIP_TOOL% -dc %OSM_FILE% | %GENERATOR_TOOL% --generate_features=true --use_light_nodes=true --split_by_polygons=true --generate_world_scale=9 --merge_coastlines=true --intermediate_data_path=D:\Temp\ --bucketing_level=0
+%UNZIP_TOOL% -dc %OSM_FILE% | %GENERATOR_TOOL% --generate_features=true --use_light_nodes=true --split_by_polygons=true --generate_world=true --intermediate_data_path=D:\Temp\
--- tools/win/gen_geometry_world.bat
+++ tools/win/gen_geometry_world.bat
@@ -1,3 +1,3 @@
 call set_vars.bat %1 %2
 
-%GENERATOR_TOOL% --generate_geometry=true --output=World --bucketing_level=0
+%GENERATOR_TOOL% --generate_geometry=true --generate_world=true --output=World
