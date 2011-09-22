85a3771dbfcc3fe8193ccaa88f6eead7f99b4de6
vng <viktor.govako@gmail.com>
2011-09-22 16:56:28 +0300

    Fix index initialization (FilesContainerR).

--- indexer/index.cpp
+++ indexer/index.cpp
@@ -5,10 +5,10 @@
 
 namespace
 {
-FilesContainerR * CreateFileContainer(string const & fileName)
-{
-  return new FilesContainerR(fileName);
-}
+  FilesContainerR * CreateFileContainer(string const & fileName)
+  {
+    return new FilesContainerR(GetPlatform().GetReader(fileName));
+  }
 }  // unnamed namespace
 
 Index::Index() : MwmSet(bind(&Index::FillInMwmInfo, this, _1, _2), &CreateFileContainer)
@@ -19,8 +19,11 @@ void Index::FillInMwmInfo(string const & fileName, MwmInfo & info)
 {
   IndexFactory factory;
   factory.Load(FilesContainerR(GetPlatform().GetReader(fileName)));
+
   feature::DataHeader const & h = factory.GetHeader();
   info.m_limitRect = h.GetBounds();
-  info.m_minScale = static_cast<uint8_t>(h.GetScaleRange().first);
-  info.m_maxScale = static_cast<uint8_t>(h.GetScaleRange().second);
+
+  pair<int, int> const scaleR = h.GetScaleRange();
+  info.m_minScale = static_cast<uint8_t>(scaleR.first);
+  info.m_maxScale = static_cast<uint8_t>(scaleR.second);
 }
