02bb18b2dabd83ab01c97647da7cd369f7d4c9f6
Alex Zolotarev <deathbaba@gmail.com>
2011-10-08 21:21:56 +0300

    [android] Fixed scripts and documentation

--- tools/android/update_assets.sh
+++ tools/android/update_assets.sh
@@ -8,11 +8,11 @@ DST=../../android/assets
 rm $DST/*
 
 files=(01_dejavusans.ttf 02_wqy-microhei.ttf 03_jomolhari-id-a3d.ttf 04_padauk.ttf \
-       05_khmeros.ttf 06_code2000.ttf World.mwm about-travelguide-iphone.html \
-       basic.skn basic_highres.skn categories.txt classificator.txt countries.txt \
+       05_khmeros.ttf 06_code2000.ttf World.mwm WorldCoasts.mwm about-travelguide-iphone.html \
+       basic.skn basic_highres.skn categories.txt classificator.txt types.txt countries.txt \
        drawing_rules.bin fonts_blacklist.txt fonts_whitelist.txt languages.txt \
        maps.update symbols_24.png symbols_48.png unicode_blocks.txt visibility.txt \
-       types.txt )
+       )
 
 for item in ${files[*]}
 do
