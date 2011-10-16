6d7c8d56f8e282f0dcd31ea17cc99d5773e78019
Alex Zolotarev <deathbaba@gmail.com>
2011-10-17 01:48:15 +0300

    [ios] Code cleanup

--- iphone/Common/WebViewController.mm
+++ iphone/Common/WebViewController.mm
@@ -32,22 +32,6 @@
   return self;
 }
 
-- (void)dealloc
-{
-  [super dealloc];
-}
-
-- (void)didReceiveMemoryWarning
-{
-  // Releases the view if it doesn't have a superview.
-  [super didReceiveMemoryWarning];
-
-  // Release any cached data, images, etc that aren't in use.
-}
-
-#pragma mark - View lifecycle
-
-// Implement loadView to create a view hierarchy programmatically, without using a nib.
 - (void)loadView
 {
   CGRect frame = [[UIScreen mainScreen] applicationFrame];
@@ -65,24 +49,8 @@
   [webView release];
 }
 
-/*
- // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
- - (void)viewDidLoad
- {
- [super viewDidLoad];
- }
- */
-
-- (void)viewDidUnload
-{
-  [super viewDidUnload];
-  // Release any retained subviews of the main view.
-  self.view = nil;
-}
-
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-  // Return YES for supported orientations
   return YES;
 }
 
