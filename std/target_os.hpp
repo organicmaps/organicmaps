90230383277b1470d36ed6c3f2208f0e1316d44e
Alex Zolotarev <deathbaba@gmail.com>
2011-10-21 18:35:28 +0300

    Added defines OMIM_OS_DESKTOP and OMIM_OS_MOBILE

--- std/target_os.hpp
+++ std/target_os.hpp
@@ -3,10 +3,12 @@
 #if defined(ANDROID)
   #define OMIM_OS_ANDROID
   #define OMIM_OS_NAME "android"
+  #define OMIM_OS_MOBILE
 
 #elif defined(_BADA_SIMULATOR) || defined(_BADA_DEVICE)
   #define OMIM_OS_BADA
   #define OMIM_OS_NAME "bada"
+  #define OMIM_OS_MOBILE
 
 #elif defined(__APPLE__)
   #include <TargetConditionals.h>
@@ -18,21 +20,27 @@
     #else
       #define OMIM_OS_IPHONE_DEVICE
     #endif
+    #define OMIM_OS_MOBILE
+
   #else
     #define OMIM_OS_MAC
     #define OMIM_OS_NAME "mac"
+    #define OMIM_OS_DESKTOP
   #endif
 
 #elif defined(_WIN32)
   #define OMIM_OS_WINDOWS
   #define OMIM_OS_NAME "win"
+  #define OMIM_OS_DESKTOP
 
   #ifdef __MINGW32__
     #define OMIM_OS_WINDOWS_MINGW
   #else
     #define OMIM_OS_WINDOWS_NATIVE
   #endif
+
 #else
   #define OMIM_OS_LINUX
   #define OMIM_OS_NAME "linux"
+  #define OMIM_OS_DESKTOP
 #endif
