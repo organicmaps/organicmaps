1fa5a1f1a63d5cb1989840f11dd2122b334730c8
vng <viktor.govako@gmail.com>
2011-11-24 19:05:19 +0300

    Delete drawing_rules.bin.

--- tools/android/update_assets.sh
+++ tools/android/update_assets.sh
@@ -10,7 +10,7 @@ rm $DST/*
 files=(01_dejavusans.ttf 02_wqy-microhei.ttf 03_jomolhari-id-a3d.ttf 04_padauk.ttf \
        05_khmeros.ttf 06_code2000.ttf World.mwm WorldCoasts.mwm about-travelguide-iphone.html \
        basic_ldpi.skn basic_mdpi.skn basic_hdpi.skn categories.txt classificator.txt types.txt countries.txt \
-       drawing_rules.bin fonts_blacklist.txt fonts_whitelist.txt languages.txt \
+       fonts_blacklist.txt fonts_whitelist.txt languages.txt \
        maps.update symbols_ldpi.png symbols_mdpi.png symbols_hdpi.png unicode_blocks.txt visibility.txt \
        drules_proto.txt)
 
