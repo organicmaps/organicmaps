7006338a6dce142148e0a52b852dfae9534cfad8
Alex Zolotarev <deathbaba@gmail.com>
2011-11-28 17:18:59 +0300

    [android] Removed unused file link

--- tools/android/update_assets.sh
+++ tools/android/update_assets.sh
@@ -11,7 +11,7 @@ files=(01_dejavusans.ttf 02_wqy-microhei.ttf 03_jomolhari-id-a3d.ttf 04_padauk.t
        05_khmeros.ttf 06_code2000.ttf World.mwm WorldCoasts.mwm about-travelguide-iphone.html \
        basic_ldpi.skn basic_mdpi.skn basic_hdpi.skn categories.txt classificator.txt types.txt countries.txt \
        fonts_blacklist.txt fonts_whitelist.txt languages.txt \
-       maps.update symbols_ldpi.png symbols_mdpi.png symbols_hdpi.png unicode_blocks.txt visibility.txt \
+       symbols_ldpi.png symbols_mdpi.png symbols_hdpi.png unicode_blocks.txt visibility.txt \
        drules_proto.txt drules_proto.bin)
 
 for item in ${files[*]}
