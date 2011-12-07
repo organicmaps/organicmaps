57a0a30a8c0576a111dc24a72efa724cef32b850
Alex Zolotarev <deathbaba@gmail.com>
2011-12-07 20:18:25 +0300

    Added 3rd server to default list

--- defines.hpp
+++ defines.hpp
@@ -30,7 +30,7 @@
 
 #ifdef OMIM_PRODUCTION
   #define URL_SERVERS_LIST "http://active.servers.url"
-  #define DEFAULT_SERVERS_JSON "[\"http://1st.default.server/\",\"http://2nd.default.server/\"]"
+  #define DEFAULT_SERVERS_JSON "[\"http://1st.default.server/\",\"http://2nd.default.server/\",\"http://3rd.default.server/\"]"
 #else
   #define URL_SERVERS_LIST "http://metaserver-dev.mapswithme.com/server_data/active_servers"
   #define DEFAULT_SERVERS_JSON "[\"http://svobodu404popugajam.mapswithme.com:34568/maps/\",\"http://svobodu404popugajam.mapswithme.com:34568/maps/\"]"
