b934b270a69fb40ea0a9828817ef0e35b0de965f
rachytski <siarhei.rachytski@gmail.com>
2012-01-07 03:51:30 +0400

    regenerated skins for different resolution and added basic_xhdpi.skn and symbols_xhdpi.png

--- tools/android/update_assets.sh
+++ tools/android/update_assets.sh
@@ -9,10 +9,10 @@ rm $DST/*
 
 files=(01_dejavusans.ttf 02_wqy-microhei.ttf 03_jomolhari-id-a3d.ttf 04_padauk.ttf \
        05_khmeros.ttf 06_code2000.ttf World.mwm WorldCoasts.mwm about-travelguide-iphone.html \
-       basic_ldpi.skn basic_mdpi.skn basic_hdpi.skn categories.txt classificator.txt types.txt countries.txt \
-       fonts_blacklist.txt fonts_whitelist.txt languages.txt \
-       symbols_ldpi.png symbols_mdpi.png symbols_hdpi.png unicode_blocks.txt visibility.txt \
-       drules_proto.txt drules_proto.bin)
+       basic_ldpi.skn basic_mdpi.skn basic_hdpi.skn basic_xhdpi.skn categories.txt classificator.txt 
+       types.txt countries.txt fonts_blacklist.txt fonts_whitelist.txt languages.txt \
+       symbols_ldpi.png symbols_mdpi.png symbols_hdpi.png symbols_xhdpi.png unicode_blocks.txt \ 
+       visibility.txt drules_proto.txt drules_proto.bin)
 
 for item in ${files[*]}
 do
--- tools/unix/generate_symbols.sh
+++ tools/unix/generate_symbols.sh
@@ -4,16 +4,20 @@ MY_PATH=`dirname $0`
 BINARY_PATH="$MY_PATH/../../../tools_only-build-release/out/release/skin_generator"
 DATA_PATH="$MY_PATH/../../data"
 
-"$BINARY_PATH" --symbolWidth 24 --symbolHeight 24 \
+"$BINARY_PATH" --symbolWidth 18 --symbolHeight 18 \
     --symbolsDir "$DATA_PATH/styles/symbols" \
     --skinName "$DATA_PATH/basic" --skinSuffix="ldpi"
 
-"$BINARY_PATH" --symbolWidth 36 --symbolHeight 36 \
+"$BINARY_PATH" --symbolWidth 24 --symbolHeight 24 \
     --symbolsDir "$DATA_PATH/styles/symbols" \
     --skinName "$DATA_PATH/basic" --skinSuffix="mdpi"
 
-"$BINARY_PATH" --symbolWidth 48 --symbolHeight 48 \
+"$BINARY_PATH" --symbolWidth 36 --symbolHeight 36 \
     --symbolsDir "$DATA_PATH/styles/symbols" \
     --skinName "$DATA_PATH/basic" --skinSuffix="hdpi"
+
+"$BINARY_PATH" --symbolWidth 48 --symbolHeight 48 \
+    --symbolsDir "$DATA_PATH/styles/symbols" \
+    --skinName "$DATA_PATH/basic" --skinSuffix="xhdpi"
 
 
