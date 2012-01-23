dab815a85b254d6a6e8e4e346d460aa343866017
vng <viktor.govako@gmail.com>
2012-01-23 16:59:51 +0300

    Test code for linear and area objects dumping.

--- generator/feature_sorter.cpp
+++ generator/feature_sorter.cpp
@@ -411,6 +411,28 @@ namespace feature
       return scales::IsGoodForLevel(level, r);
     }
 
+    /*
+    class DoPrintTypes
+    {
+      Classificator const & m_c;
+
+    public:
+      DoPrintTypes() : m_c(classif())
+      {
+        LOG(LINFO, ("Start"));
+      }
+      ~DoPrintTypes()
+      {
+        LOG(LINFO, ("Finish"));
+      }
+
+      void operator() (uint32_t t)
+      {
+        LOG(LINFO, (m_c.GetFullObjectName(t)));
+      }
+    };
+    */
+
   public:
     void operator() (FeatureBuilder2 & fb)
     {
@@ -418,6 +440,15 @@ namespace feature
 
       bool const isLine = fb.IsLine();
       bool const isArea = fb.IsArea();
+
+      // Dump features with linear and area types.
+      /*
+      if (isLine && isArea)
+      {
+        DoPrintTypes doPrint;
+        fb.GetFeatureBase().ForEachTypeRef(doPrint);
+      }
+      */
 
       for (int i = m_header.GetScalesCount()-1; i >= 0; --i)
       {
