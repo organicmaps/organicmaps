ea3abd04a0aad2bfe12cfb23ca42519c8b98f2a9
Alex Zolotarev <deathbaba@gmail.com>
2012-01-25 08:21:28 +0300

    [android] Enabled back TilingRenderPolicy by default

--- map/render_policy.cpp
+++ map/render_policy.cpp
@@ -172,7 +172,7 @@ RenderPolicy * CreateRenderPolicy(VideoTimer * videoTimer,
   else
   {
 #ifdef OMIM_OS_ANDROID
-    return new RenderPolicyST(videoTimer, useDefaultFB, rmParams, primaryRC);
+    return new TilingRenderPolicyST(videoTimer, useDefaultFB, rmParams, primaryRC);
 #endif
 #ifdef OMIM_OS_IPHONE
     return new RenderPolicyMT(videoTimer, useDefaultFB, rmParams, primaryRC);
