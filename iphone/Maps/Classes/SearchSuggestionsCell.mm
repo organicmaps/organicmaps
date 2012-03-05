752a2cf976b0086b77f7619061b70052790ace73
Alex Zolotarev <deathbaba@gmail.com>
2012-03-05 02:44:46 +0100

    [ios] Removed unused code

--- iphone/Maps/Classes/SearchSuggestionsCell.mm
+++ iphone/Maps/Classes/SearchSuggestionsCell.mm
@@ -70,42 +70,4 @@
   }
 }
 
-//- (void)layoutSubviews
-//{
-//  [super layoutSubviews];
-//  CGRect r = self.contentView.bounds;
-//  // Leave some experimentally choosen paddings
-//  CGFloat const KPaddingX = 10.0;
-//  CGFloat const KPaddingBottom = 1.0;
-//
-//  r.origin.x += KPaddingX;
-//  r.size.width -= 2 * KPaddingX;
-//  r.size.height -= KPaddingBottom;
-//
-//  // Labels on the right should always fit and be visible, but not more than half of the cell
-//  CGFloat const w = r.size.width;
-//  CGFloat const h = r.size.height;
-//  CGFloat const yDelim = (int)(r.origin.y + h / 5 * 3);
-//
-//  CGFloat xTopDelim = (int)(r.origin.x + w / 2);
-//  CGFloat xBottomDelim = xTopDelim;
-//  if (featureType.text.length)
-//  {
-//    CGSize const typeTextSize = [featureType.text sizeWithFont:featureType.font];
-//    if (xTopDelim + typeTextSize.width < r.origin.x + w)
-//      xTopDelim = r.origin.x + w - typeTextSize.width - KPaddingX;
-//  }
-//  featureName.frame = CGRectMake(r.origin.x, r.origin.y, xTopDelim - r.origin.x, yDelim - r.origin.y);
-//  featureType.frame = CGRectMake(xTopDelim, r.origin.y, r.origin.x + w - xTopDelim, yDelim - r.origin.y);
-//
-//  if (featureDistance.text.length)
-//  {
-//    CGSize const distanceTextSize = [featureDistance.text sizeWithFont:featureDistance.font];
-//    if (xBottomDelim + distanceTextSize.width < r.origin.x + w)
-//      xBottomDelim = r.origin.x + w - distanceTextSize.width - KPaddingX;
-//  }
-//  featureCountry.frame = CGRectMake(r.origin.x, yDelim, xBottomDelim - r.origin.x, r.origin.y + h - yDelim);
-//  featureDistance.frame = CGRectMake(xBottomDelim, yDelim, r.origin.x + w - xBottomDelim, r.origin.y + h - yDelim);
-//}
-
 @end
