c04d3668b77f661788e5784ea259f9a5c27cdf45
rachytski <siarhei.rachytski@gmail.com>
2012-04-11 11:20:14 +0400

    [android] added packed_polygons.bin to assets.

--- tools/android/update_assets.sh
+++ tools/android/update_assets.sh
@@ -13,7 +13,7 @@ files=(01_dejavusans.ttf 02_wqy-microhei.ttf 03_jomolhari-id-a3d.ttf 04_padauk.t
        basic_ldpi.skn basic_mdpi.skn basic_hdpi.skn basic_xhdpi.skn categories.txt classificator.txt 
        types.txt fonts_blacklist.txt fonts_whitelist.txt languages.txt \
        symbols_ldpi.png symbols_mdpi.png symbols_hdpi.png symbols_xhdpi.png unicode_blocks.txt \ 
-       visibility.txt drules_proto.txt drules_proto.bin)
+       visibility.txt drules_proto.txt drules_proto.bin packed_polygons.bin)
 
 for item in ${files[*]}
 do
