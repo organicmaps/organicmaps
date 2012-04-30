8d5b26990ec987b5c352cc1bc2e96711dfffa115
Yury Melnichek <melnichek@gmail.com>
2012-04-30 09:22:42 +0200

    Remove unused sha224, sha384, sha512 functions because they are linked into code.

--- coding/sha2.cpp
+++ coding/sha2.cpp
@@ -7,22 +7,6 @@
 
 namespace sha2
 {
-  string digest224(char const * data, size_t dataSize, bool returnAsHexString)
-  {
-    hash_state md;
-    unsigned char out[224/8] = { 0 };
-    if (CRYPT_OK == sha224_init(&md)
-        && CRYPT_OK == sha224_process(&md, reinterpret_cast<unsigned char const *>(data), dataSize)
-        && CRYPT_OK == sha224_done(&md, out))
-    {
-      if (returnAsHexString)
-        return ToHex(string(reinterpret_cast<char const *>(out), ARRAY_SIZE(out)));
-      else
-        return string(reinterpret_cast<char const *>(out), ARRAY_SIZE(out));
-    }
-    return string();
-  }
-
   string digest256(char const * data, size_t dataSize, bool returnAsHexString)
   {
     hash_state md;
@@ -38,37 +22,4 @@ namespace sha2
     }
     return string();
   }
-
-  string digest384(char const * data, size_t dataSize, bool returnAsHexString)
-  {
-    hash_state md;
-    unsigned char out[384/8] = { 0 };
-    if (CRYPT_OK == sha384_init(&md)
-        && CRYPT_OK == sha384_process(&md, reinterpret_cast<unsigned char const *>(data), dataSize)
-        && CRYPT_OK == sha384_done(&md, out))
-    {
-      if (returnAsHexString)
-        return ToHex(string(reinterpret_cast<char const *>(out), ARRAY_SIZE(out)));
-      else
-        return string(reinterpret_cast<char const *>(out), ARRAY_SIZE(out));
-    }
-    return string();
-  }
-
-  string digest512(char const * data, size_t dataSize, bool returnAsHexString)
-  {
-    hash_state md;
-    unsigned char out[512/8] = { 0 };
-    if (CRYPT_OK == sha512_init(&md)
-        && CRYPT_OK == sha512_process(&md, reinterpret_cast<unsigned char const *>(data), dataSize)
-        && CRYPT_OK == sha512_done(&md, out))
-    {
-      if (returnAsHexString)
-        return ToHex(string(reinterpret_cast<char const *>(out), ARRAY_SIZE(out)));
-      else
-        return string(reinterpret_cast<char const *>(out), ARRAY_SIZE(out));
-    }
-    return string();
-  }
 }
-
