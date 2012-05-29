2f4ef343b48af266285587fca3bf9375aea3719d
Yury Melnichek <melnichek@gmail.com>
2012-05-29 07:02:39 +0200

    [guide] User memory mapping when grepping.

--- crawler/extract-image-urls.sh
+++ crawler/extract-image-urls.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e -u -x
 
-grep --ignore-case --only-matching --no-filename '<img[^/]*src=\"[^">]*"' -r --include=*.opt . \
+grep --ignore-case --only-matching --no-filename --mmap '<img[^/]*src=\"[^">]*"' -r --include=*.opt . \
   | sed 's/<img.*src="//g' \
   | sed 's/"$//g' \
   | sort -u \
