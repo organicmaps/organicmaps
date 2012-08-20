dd9e0287a3a0cca11674c44919b46906725af98a
rachytski <siarhei.rachytski@gmail.com>
2012-08-20 12:43:51 +0300

    added anim/Task.cpp

---
+++ anim/task.cpp
@@ -0,0 +1,58 @@
+#include "task.hpp"
+
+namespace anim
+{
+  Task::Task()
+    : m_State(EStarted)
+  {}
+
+  Task::~Task()
+  {}
+
+  Task::EState Task::State() const
+  {
+    return m_State;
+  }
+
+  void Task::SetState(EState State)
+  {
+    m_State = State;
+  }
+
+  void Task::OnStart(double ts)
+  {
+    SetState(EInProgress);
+  }
+
+  void Task::OnStep(double ts)
+  {
+  }
+
+  void Task::OnCancel(double ts)
+  {
+  }
+
+  void Task::OnEnd(double ts)
+  {
+  }
+
+  void Task::Cancel()
+  {
+    SetState(ECancelled);
+  }
+
+  void Task::End()
+  {
+    SetState(EEnded);
+  }
+
+  bool Task::IsEnded() const
+  {
+    return State() == EEnded;
+  }
+
+  bool Task::IsCancelled() const
+  {
+    return State() == ECancelled;
+  }
+}
