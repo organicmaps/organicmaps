05366f6155549d084aaf5ad2f9beabbf4480271b
Alex Zolotarev <deathbaba@gmail.com>
2012-11-18 22:52:45 +0300

    [android] Added separate folder for Samsung Lite with new package com.mapswithme.maps.samsung

--- tools/android/update_assets.sh
+++ tools/android/update_assets.sh
@@ -1,3 +1,4 @@
 ./update_assets_for_version.sh ../../android/MapsWithMePro/assets
 ./update_assets_for_version.sh ../../android/MapsWithMeLite/assets
+./update_assets_for_version.sh ../../android/MapsWithMeLite.Samsung/assets
 ./update_flags.sh
