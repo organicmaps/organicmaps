72fc2ef47539861d4856108f18777452bf4cfbf9
AlexeiVitenko <AlexeiVitenko@gmail.com>
2013-01-07 00:55:34 +0300

    [android] [bookmarks] add util method for camel case

--- android/src/com/mapswithme/util/Utils.java
+++ android/src/com/mapswithme/util/Utils.java
@@ -50,4 +50,41 @@ public class Utils
     activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
     return value.getDimension(metrics);
   }
+
+
+  public static String toTitleCase(String str)
+  {
+    if (str == null)
+    {
+        return null;
+    }
+    boolean space = true;
+    StringBuilder builder = new StringBuilder(str);
+    final int len = builder.length();
+
+    for (int i=0; i < len; ++i)
+    {
+      char c = builder.charAt(i);
+      if (space)
+      {
+        if (!Character.isWhitespace(c))
+        {
+          // Convert to title case and switch out of whitespace mode.
+          builder.setCharAt(i, Character.toTitleCase(c));
+          space = false;
+        }
+      }
+      else if (Character.isWhitespace(c))
+      {
+        space = true;
+      }
+      else
+      {
+        builder.setCharAt(i, Character.toLowerCase(c));
+      }
+    }
+
+    return builder.toString();
+  }
+
 }
