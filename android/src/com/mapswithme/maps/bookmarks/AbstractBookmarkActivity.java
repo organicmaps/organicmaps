8a59247c0422283b194f78af6a34eb29e4213cad
AlexeiVitenko <AlexeiVitenko@gmail.com>
2013-01-14 08:05:31 +0300

    [android] [bookmarks] add back button to actionBar for api >= 11

--- android/src/com/mapswithme/maps/bookmarks/AbstractBookmarkActivity.java
+++ android/src/com/mapswithme/maps/bookmarks/AbstractBookmarkActivity.java
@@ -2,17 +2,37 @@ package com.mapswithme.maps.bookmarks;
 
 import com.mapswithme.maps.bookmarks.data.BookmarkManager;
 
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public abstract class AbstractBookmarkActivity extends Activity
 {
   protected BookmarkManager mManager;
 
+  @SuppressLint("NewApi")
   @Override
   protected void onCreate(Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
     mManager = BookmarkManager.getBookmarkManager(getApplicationContext());
+    if (Integer.parseInt(android.os.Build.VERSION.SDK) >= 11)
+    {
+      getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item)
+  {
+    if (item.getItemId() == android.R.id.home)
+    {
+      onBackPressed();
+      return true;
+    }
+    else
+      return super.onOptionsItemSelected(item);
   }
 }
--- android/src/com/mapswithme/maps/bookmarks/AbstractBookmarkListActivity.java
+++ android/src/com/mapswithme/maps/bookmarks/AbstractBookmarkListActivity.java
@@ -2,17 +2,36 @@ package com.mapswithme.maps.bookmarks;
 
 import com.mapswithme.maps.bookmarks.data.BookmarkManager;
 
+import android.annotation.SuppressLint;
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public abstract class AbstractBookmarkListActivity extends ListActivity
 {
   protected BookmarkManager mManager;
 
+  @SuppressLint("NewApi")
   @Override
   protected void onCreate(Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
     mManager = BookmarkManager.getBookmarkManager(getApplicationContext());
+    if (Integer.parseInt(android.os.Build.VERSION.SDK) >= 11)
+    {
+      getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item)
+  {
+    if (item.getItemId() == android.R.id.home)
+    {
+      onBackPressed();
+      return true;
+    }
+    else
+      return super.onOptionsItemSelected(item);
   }
 }
