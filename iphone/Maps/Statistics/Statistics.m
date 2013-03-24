5ded0b00d1fe15a8ec432bf7f10d5c6a4a2410b0
Alex Zolotarev <alex@mapswithme.com>
2013-03-24 19:58:29 +0100

    Address comments.

--- iphone/Maps/Statistics/Statistics.m
+++ iphone/Maps/Statistics/Statistics.m
@@ -6,19 +6,17 @@
 
 - (void) startSession
 {
-  NSLog(@"Stats: Start session.");
   [Flurry startSession:
       [[NSBundle mainBundle] objectForInfoDictionaryKey:@"FlurryKey"]];
 }
 
 - (void) stopSession
 {
-  NSLog(@"Stats: Stop session.");
 }
 
 + (Statistics *) instance
 {
-  static Statistics *instance = nil;
+  static Statistics* instance = nil;
   static dispatch_once_t onceToken;
   dispatch_once(&onceToken, ^{
     instance = [[Statistics alloc] init];
