c9a0c07cf1f5c649cd23ea4d22c27d39861b7ec8
Dmitry Kunin <dkunin@mapswith.me>
2013-05-03 14:31:46 +0300

    [ui] icons size reduced

--- tools/unix/generate_symbols.sh
+++ tools/unix/generate_symbols.sh
@@ -8,15 +8,15 @@ DATA_PATH="$MY_PATH/../../data"
     --symbolsDir "$DATA_PATH/styles/symbols" \
     --skinName "$DATA_PATH/resources-ldpi/basic" --skinSuffix=""
 
-"$BINARY_PATH" --symbolWidth 24 --symbolHeight 24 \
+"$BINARY_PATH" --symbolWidth 16 --symbolHeight 16 \
     --symbolsDir "$DATA_PATH/styles/symbols" \
     --skinName "$DATA_PATH/resources-mdpi/basic" --skinSuffix=""
 
-"$BINARY_PATH" --symbolWidth 36 --symbolHeight 36 \
+"$BINARY_PATH" --symbolWidth 24 --symbolHeight 24 \
     --symbolsDir "$DATA_PATH/styles/symbols" \
     --skinName "$DATA_PATH/resources-hdpi/basic" --skinSuffix=""
 
-"$BINARY_PATH" --symbolWidth 48 --symbolHeight 48 \
+"$BINARY_PATH" --symbolWidth 36 --symbolHeight 36 \
     --symbolsDir "$DATA_PATH/styles/symbols" \
     --skinName "$DATA_PATH/resources-xhdpi/basic" --skinSuffix=""
 
