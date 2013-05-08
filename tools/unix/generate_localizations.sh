0163f9c16502c68acc13f781379a6530018663d5
Yury Melnichek <melnichek@gmail.com>
2013-05-08 19:27:36 +0200

    [tools] Add a script to generate localizations.

---
+++ tools/unix/generate_localizations.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e -u -x
+
+# TODO: Add "--untagged --tags android" when tags are properly set.
+./tools/twine/twine generate-all-string-files ./strings.txt ./android/res/
+./tools/twine/twine generate-all-string-files ./strings.txt ./iphone/Maps/
