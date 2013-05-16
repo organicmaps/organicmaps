13a9ebd0044cb7bba525ea74e2fe0ffbd5753080
vng <viktor.govako@gmail.com>
2013-05-17 02:21:37 +0300

    [iOS] Added NSException logging.

--- iphone/Maps/main.mm
+++ iphone/Maps/main.mm
@@ -3,10 +3,11 @@
 #import <Foundation/NSThread.h>
 
 #include "../../platform/platform.hpp"
+
 #include "../../base/logging.hpp"
 
-//////////////////////////////////////////////////////////////////////////////////
-/// Used to trick iOs and enable multithreading support with non-native pthreads
+
+/// Used to trick iOs and enable multithreading support with non-native pthreads.
 @interface Dummy : NSObject 
 - (void) dummyThread: (id) obj;
 @end
@@ -14,11 +15,22 @@
 @implementation Dummy
 - (void) dummyThread: (id) obj {}
 @end
-///////////////////////////////////////////////////////////////////////////////////
+
+
+/// Used to log NSExceptions.
+static void exceptionHandler(NSException * ex)
+{
+  NSArray *stack = [ex callStackSymbols];
+  NSLog(@"%@", ex.name);
+  NSLog(@"%@", ex.reason);
+  NSLog(@"Stack trace: %@", stack);
+}
 
 int main(int argc, char * argv[])
 {
   LOG(LINFO, ("MapsWithMe started, detected CPU cores:", GetPlatform().CpuCores()));
+
+  NSSetUncaughtExceptionHandler(&exceptionHandler);
 
   NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
