1de9912e0f72cf20aeca315f095ca030637abca6
Alex Zolotarev <alex@mapswithme.com>
2013-07-03 21:58:07 +0300

    [ios][api] Restored accidently missed API files

---
+++ api/iOS/src/MapsWithMeAPI.h
@@ -0,0 +1,63 @@
+/*******************************************************************************
+
+ Copyright (c) 2013, MapsWithMe GmbH
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ ******************************************************************************/
+
+#import <Foundation/Foundation.h>
+
+#if __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_4_3
+  #error "MapsWithMe supports iOS >= 4.3 only"
+#endif
+
+// Wrapper for a pin on a map
+@interface MWMPin : NSObject
+
+// [required] pin latitude and longitude
+@property (nonatomic, assign) double lat;
+@property (nonatomic, assign) double lon;
+// [optional] pin title
+@property (nonatomic, retain) NSString * optionalTitle;
+// [optional] passed back to the app when pin is clicked, OR, if it's a valid url,
+// it will be opened from MapsWithMe after selecting "More Details..." for the pin
+@property (nonatomic, retain) NSString * optionalId;
+- (id) initWithLat:(double)lat lon:(double)lon title:(NSString *)title id:(NSString *)pinId;
+@end
+
+
+// MapsWithMe API interface
+@interface MWMApi : NSObject
+
+// returns YES if url is received from MapsWithMe and can be parsed
++ (BOOL) isMapsWithMeUrl:(NSURL *)url;
+// returns nil if received url is invalid or not from MapsWithMe
++ (MWMPin *) pinFromUrl:(NSURL *)url;
+// returns NO if MapsWithMe is not installed or outdated version doesn't support API calls
++ (BOOL) isApiSupported;
++ (BOOL) showLat:(double)lat lon:(double)lon title:(NSString *)optionalTitle id:(NSString *)optionalId;
++ (BOOL) showPin:(MWMPin *)pin;
++ (BOOL) showPins:(NSArray *)pins;
+
+@end
