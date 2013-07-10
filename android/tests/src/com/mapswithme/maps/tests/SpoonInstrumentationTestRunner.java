8f9e141e875f937ae5e85f2fa8da9c1d4df0e773
Dmitry Kunin <dkunin@mapswith.me>
2013-07-10 14:19:50 +0300

    [android, tests] Refactoring, libs.

--- android/tests/src/com/mapswithme/maps/tests/SpoonInstrumentationTestRunner.java
+++ android/tests/src/com/mapswithme/maps/tests/SpoonInstrumentationTestRunner.java
@@ -12,27 +12,40 @@ import static android.os.PowerManager.FULL_WAKE_LOCK;
 import static android.os.PowerManager.ON_AFTER_RELEASE;
 
 /**
- * Test runner which disables the keyguard and wake up the screen during execution. Requires test
- * application have {@code android.permission.DISABLE_KEYGUARD} permission declared.
+ * Test runner which disables the keyguard and wake up the screen during
+ * execution. Requires test application have
+ * {@code android.permission.DISABLE_KEYGUARD} permission declared.
  **/
-public class SpoonInstrumentationTestRunner extends InstrumentationTestRunner {
-  @Override public void onStart() {
-    runOnMainSync(new Runnable() {
-      @Override public void run() {
+public class SpoonInstrumentationTestRunner extends InstrumentationTestRunner
+{
+  @Override
+  public void onStart()
+  {
+    runOnMainSync(new Runnable()
+    {
+      @SuppressWarnings("deprecation")
+      @Override
+      public void run()
+      {
         Application app = (Application) getTargetContext().getApplicationContext();
         String simpleName = SpoonInstrumentationTestRunner.class.getSimpleName();
-
-        // Unlock the device so that the tests can input keystrokes.
-        ((KeyguardManager) app.getSystemService(KEYGUARD_SERVICE)) //
-            .newKeyguardLock(simpleName) //
-            .disableKeyguard();
-        // Wake up the screen.
-        ((PowerManager) app.getSystemService(POWER_SERVICE)) //
-            .newWakeLock(FULL_WAKE_LOCK | ACQUIRE_CAUSES_WAKEUP | ON_AFTER_RELEASE, simpleName) //
-            .acquire();
+        try
+        {
+          // Unlock the device so that the tests can input keystrokes.
+          ((KeyguardManager) app.getSystemService(KEYGUARD_SERVICE)) //
+              .newKeyguardLock(simpleName) //
+              .disableKeyguard();
+          // Wake up the screen.
+          ((PowerManager) app.getSystemService(POWER_SERVICE)) //
+              .newWakeLock(FULL_WAKE_LOCK | ACQUIRE_CAUSES_WAKEUP | ON_AFTER_RELEASE, simpleName) //
+              .acquire();
+        }
+        catch (SecurityException e)
+        {
+          // because of kinlde we could just have this exception thrown
+        }
       }
     });
-
     super.onStart();
   }
 }
