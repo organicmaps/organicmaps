8dd7fdb88ea4642f36c03266d466d689daf564c0
Dmitry Kunin <dkunin@mapswith.me>
2013-07-10 18:55:19 +0300

    [android, test] Search, Settings

--- android/tests/src/com/mapswithme/maps/tests/SearchActivityTest.java
+++ android/tests/src/com/mapswithme/maps/tests/SearchActivityTest.java
@@ -1,37 +1,104 @@
 package com.mapswithme.maps.tests;
 
-import com.mapswithme.maps.SearchActivity;
-import com.squareup.spoon.Spoon;
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
 
 import android.app.Instrumentation;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.jayway.android.robotium.solo.Solo;
+import com.mapswithme.maps.R;
+import com.mapswithme.maps.SearchActivity;
+import com.squareup.spoon.Spoon;
 
 public class SearchActivityTest extends ActivityInstrumentationTestCase2<SearchActivity>
 {
-
-  Instrumentation instrumentation;
-  SearchActivity activity;
 
   public SearchActivityTest()
   {
     super(SearchActivity.class);
   }
 
-
-  @Override
-  protected void setUp() throws Exception
+  public void testView_Spinner()
   {
-    super.setUp();
+    final SearchActivity activity = getActivity();
+    final Spinner spinner = ((Spinner) activity.findViewById(R.id.search_modes_spinner));
+    final EditText searchField = (EditText) activity.findViewById(R.id.search_string);
+    final Solo solo = new Solo(getInstrumentation(), getActivity());
+    solo.clearEditText(searchField);
 
-    instrumentation = getInstrumentation();
-    activity = getActivity();
+    Spoon.screenshot(activity, "initial_state");
+    solo.clickOnView(spinner);
+
+    final CountDownLatch cdl = new CountDownLatch(1);
+    spinner.postDelayed(new Runnable()
+    {
+      @Override
+      public void run()
+      {
+        cdl.countDown();
+      }
+    }, 3 * 1000);
+    try
+    {
+      cdl.await();
+    }
+    catch (InterruptedException e)
+    {
+      fail("Didn't wait for screenshot");
+    }
+
+    Spoon.screenshot(activity, "spinner_clicked");
   }
 
-
-  public void testSetUp_Search()
+  public void testSearch_Search()
   {
-    assertNotNull(activity);
-    Spoon.screenshot(activity, "search");
+    final SearchActivity activity = getActivity();
+    final Solo solo = new Solo(getInstrumentation(), getActivity());
+    final Spinner spinner = ((Spinner) activity.findViewById(R.id.search_modes_spinner));
+
+    final EditText searchField = (EditText) activity.findViewById(R.id.search_string);
+    final String[] searchTerms = { "fo", "food", "ресторан", "Ми", "Минск", "Моск", "San", "London", "Cinema" };
+
+    for (String term : searchTerms)
+      for (int i = 0; i < 3; i++)
+      {
+        final int index = i;
+        getInstrumentation().runOnMainSync(new Runnable()
+        {
+          @Override
+          public void run()
+          {
+            spinner.setSelection(index);
+          }
+        });
+        getInstrumentation().waitForIdleSync();
+
+        solo.clearEditText(searchField);
+        solo.enterText(searchField, term);
+
+        final CountDownLatch cdl = new CountDownLatch(1);
+        searchField.postDelayed(new Runnable()
+        {
+          @Override
+          public void run()
+          {
+            cdl.countDown();
+          }
+        }, 1500);
+        try
+        {
+          cdl.await();
+        }
+        catch (InterruptedException e)
+        {
+          fail("Didn't wait for screenshot");
+        }
+
+        Spoon.screenshot(activity, "search_" + term.hashCode());
+      }
   }
 
 }
--- android/tests/src/com/mapswithme/maps/tests/SettingsActivityTest.java
+++ android/tests/src/com/mapswithme/maps/tests/SettingsActivityTest.java
@@ -1,9 +1,12 @@
 package com.mapswithme.maps.tests;
 
-import com.mapswithme.maps.settings.SettingsActivity;
-import com.squareup.spoon.Spoon;
-
+import android.preference.CheckBoxPreference;
 import android.test.ActivityInstrumentationTestCase2;
+
+import com.mapswithme.maps.R;
+import com.mapswithme.maps.settings.SettingsActivity;
+import com.mapswithme.util.statistics.Statistics;
+import com.squareup.spoon.Spoon;
 
 public class SettingsActivityTest extends ActivityInstrumentationTestCase2<SettingsActivity>
 {
@@ -26,5 +29,14 @@ public class SettingsActivityTest extends ActivityInstrumentationTestCase2<Setti
   {
     assertNotNull(activity);
     Spoon.screenshot(activity, "settings");
+
+  }
+
+  @SuppressWarnings("deprecation")
+  public void testStat_CheckBox()
+  {
+   CheckBoxPreference pref =  (CheckBoxPreference)activity.findPreference(activity.getString(R.string.pref_allow_stat));
+   assertNotNull(pref);
+   assertEquals(Statistics.INSTANCE.isStatisticsEnabled(activity), pref.isChecked());
   }
 }
