d8fcede10c11f23a84d8ec661e584697a99ebdb8
Kirill Zhdanovich <kzhdanovich@gmail.com>
2013-08-22 19:26:34 +0300

    [core] country file name tests added

--- storage/storage_tests/storage_tests.cpp
+++ storage/storage_tests/storage_tests.cpp
@@ -12,9 +12,11 @@ UNIT_TEST(StorageTest_Smoke)
 
   TIndex const i1 = st.FindIndexByFile("USA_Georgia");
   TEST(i1.IsValid(), ());
+  TEST_EQUAL(st.CountryFileName(i1), "USA_Georgia", ());
 
   TIndex const i2 = st.FindIndexByFile("Georgia");
   TEST(i2.IsValid(), ());
+  TEST_EQUAL(st.CountryFileName(i2), "Georgia", ());
 
   TEST_NOT_EQUAL(i1, i2, ());
 }
