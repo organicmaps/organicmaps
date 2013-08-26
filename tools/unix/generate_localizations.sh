1298fd62036ffad44ce4c9152cb5c3168d568ea4
vng <viktor.govako@gmail.com>
2013-08-26 20:39:27 +0300

    Remove dummy strings.

--- tools/unix/generate_localizations.sh
+++ tools/unix/generate_localizations.sh
@@ -5,3 +5,9 @@ set -e -u -x
 # TODO: Add "--untagged --tags android" when tags are properly set.
 ./tools/twine/twine generate-all-string-files ./strings.txt ./android/res/
 ./tools/twine/twine generate-all-string-files ./strings.txt ./iphone/Maps/
+
+# Delete dummy string files
+rm ./android/res/values-sw600dp/strings.xml
+rm ./android/res/values-sw720dp-land/strings.xml
+rm ./android/res/values-sw720dp/strings.xml
+rm ./android/res/values-v11/strings.xml
