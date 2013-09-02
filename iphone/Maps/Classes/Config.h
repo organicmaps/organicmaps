64c0e5db847cc636d890fb6e2de18b104ad4596e
Kirill Zhdanovich <kzhdanovich@gmail.com>
2013-09-02 11:49:42 +0300

    [iOS] precompiled header added to project. Config file created.

---
+++ iphone/Maps/Classes/Config.h
@@ -0,0 +1,6 @@
+
+
+#define isIPad (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+#define isIPhone (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
+
+#define APP [UIApplication sharedApplication]
