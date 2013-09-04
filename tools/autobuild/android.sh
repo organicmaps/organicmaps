d6fca19b716dc36408b7dbc59040ec28c8c87971
Alex Zolotarev <alex@mapswithme.com>
2013-09-04 17:11:13 +0300

    [android] Added jni support to YoPme

--- tools/autobuild/android.sh
+++ tools/autobuild/android.sh
@@ -6,7 +6,7 @@ set -e -u -x
 LOCAL_DIRNAME="${PWD}/$(dirname "$0")"
 
 if [[ $# < 1 ]]; then
-  echo "Usage: $0 <debug|release|production> [clean]"
+  echo "Usage: $0 <debug|release|production> [armeabi|armeabi-v7a|mips|x86] [android-PLATFORM_NUMBER]"
   exit 1
 fi
 CONFIGURATION="$1"
@@ -21,16 +21,19 @@ SHADOW_DIR_BASE="$LOCAL_DIRNAME/../../../omim-android"
 # Try to read ndk root path from android/local.properties file
 export NDK_ROOT=$(GetNdkRoot) || ( echo "Can't read NDK root path from android/local.properties"; exit 1 )
 export NDK_HOST=$(GetNdkHost) || ( echo "Can't get your OS type, please check tools/autobuild/ndk_helper.sh script"; exit 1 )
+if [[ $# > 2 ]] ; then
+  export NDK_PLATFORM=$3
+fi
 
-NDK_ABI_LIST=(armeabi armeabi-v7a mips x86)
+if [[ $# > 1 ]] ; then
+  NDK_ABI_LIST=$2
+else
+  NDK_ABI_LIST=(armeabi armeabi-v7a mips x86)
+fi
+
 
 for abi in "${NDK_ABI_LIST[@]}"; do
   SHADOW_DIR="${SHADOW_DIR_BASE}-${CONFIGURATION}-${abi}"
-  if [[ $# > 1 && "$2" == "clean" ]] ; then
-    echo "Cleaning $CONFIGURATION-$abi configuration..."
-    rm -rf "$SHADOW_DIR"
-  else
-    export NDK_ABI="$abi"
-    BuildQt "$SHADOW_DIR" "$MKSPEC" "$QMAKE_PARAMS" || ( echo "ERROR while building $abi config"; exit 1 )
-  fi
+  export NDK_ABI="$abi"
+  BuildQt "$SHADOW_DIR" "$MKSPEC" "$QMAKE_PARAMS" 1>&2 || ( echo "ERROR while building $abi config"; exit 1 )
 done
