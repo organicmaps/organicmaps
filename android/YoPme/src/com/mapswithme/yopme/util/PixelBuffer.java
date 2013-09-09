e7959ce3233e4538e21143d180b1329bd0d9549e
ExMix <rahuba.youri@mapswithme.com>
2013-09-09 18:49:45 +0300

    correct pixel buffer for EGL

--- android/YoPme/src/com/mapswithme/yopme/util/PixelBuffer.java
+++ android/YoPme/src/com/mapswithme/yopme/util/PixelBuffer.java
@@ -4,27 +4,21 @@ import java.nio.IntBuffer;
 import java.util.Arrays;
 import java.util.Comparator;
 
-import javax.microedition.khronos.egl.*;
-import javax.microedition.khronos.opengles.GL11;
-
 import android.graphics.Bitmap;
-import android.opengl.GLES10;
+import android.opengl.EGL14;
+import android.opengl.EGLConfig;
+import android.opengl.EGLContext;
+import android.opengl.EGLDisplay;
+import android.opengl.EGLSurface;
+import android.opengl.GLES20;
 import android.util.Log;
-
-import static javax.microedition.khronos.egl.EGL11.*;
 
 public class PixelBuffer
 {
 	private final String TAG = "PixelBuffer";
-	//private static final int EGL_RENDERABLE_TYPE = 0x3040;
-  	//private static final int EGL_OPENGL_ES2_BIT = 0x0004;
-  	private static final int EGL_CONTEXT_CLIENT_VERSION = 0x3098;
-  	
-	private final EGL10 mEgl = (EGL10)EGLContext.getEGL();
 	private EGLDisplay mDisplay;
 	private EGLContext mContext;
 	private EGLSurface mSurface;
-	private GL11 mGL = null;
 	
 	private int mWidth;
 	private int mHeight;
@@ -37,96 +31,98 @@ public class PixelBuffer
 	
 	public void init() 
 	{
-		mDisplay = mEgl.eglGetDisplay(EGL11.EGL_DEFAULT_DISPLAY);
-		if (mDisplay == EGL11.EGL_NO_DISPLAY)
-			throw new EglOperationException("EGL error : No display", mEgl.eglGetError());
+    mDisplay = EGL14.eglGetDisplay(EGL14.EGL_DEFAULT_DISPLAY);
+    if (mDisplay == EGL14.EGL_NO_DISPLAY)
+      throw new EglOperationException("EGL error : No display", EGL14.eglGetError());
 		
-		int[] version = new int[2];
-		if (!mEgl.eglInitialize(mDisplay, version))
-			throw new EglOperationException("EGL error : initialize fault", mEgl.eglGetError());
+    int[] versionMajor = new int[4];
+    versionMajor[0] = 1;
+    int[] versionMinor = new int[4];
+    versionMinor[0] = 4;
+    if (!EGL14.eglInitialize(mDisplay, versionMajor, 0, versionMinor, 0))
+      throw new EglOperationException("EGL error : initialization has failed", EGL14.eglGetError());
 		
 		EGLConfig[] configs = getConfigs();
 		for (int i = 0; i < configs.length; ++i)
 		{
 			mSurface = createSurface(configs[i]);
-			if (mSurface == EGL_NO_SURFACE)
+      if (mSurface == EGL14.EGL_NO_SURFACE)
 				continue;
 			
 			mContext = createContext(configs[i]);
-			if (mContext == EGL_NO_CONTEXT)
+      if (mContext == EGL14.EGL_NO_CONTEXT)
 			{
-				mEgl.eglDestroySurface(mDisplay, mSurface);
+        EGL14.eglDestroySurface(mDisplay, mSurface);
 				continue;
 			}
 			
 			break;
 		}
 		
-		if (mSurface == EGL_NO_SURFACE)
-			throw new EglOperationException("EGL error : Surface not created", mEgl.eglGetError());
+    if (mSurface == EGL14.EGL_NO_SURFACE)
+      throw new EglOperationException("EGL error : Surface was not created", EGL14.eglGetError());
 		
-		if (mContext == EGL_NO_CONTEXT)
-			throw new EglOperationException("EGL error : Context not created", mEgl.eglGetError());
+    if (mContext == EGL14.EGL_NO_CONTEXT)
+      throw new EglOperationException("EGL error : Context was not created", EGL14.eglGetError());
 		
 		Log.d(TAG, "Egl inited");
 	}
 	
 	public void terminate()
 	{
-		if (mDisplay != EGL_NO_DISPLAY)
+    if (mDisplay != EGL14.EGL_NO_DISPLAY)
 		{
-			mEgl.eglMakeCurrent(mDisplay, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
-			if (mContext != EGL_NO_CONTEXT)
-				mEgl.eglDestroyContext(mDisplay, mContext);
-			if (mSurface != EGL_NO_SURFACE)
-				mEgl.eglDestroySurface(mDisplay, mSurface);
+      EGL14.eglMakeCurrent(mDisplay, EGL14.EGL_NO_SURFACE, EGL14.EGL_NO_SURFACE, EGL14.EGL_NO_CONTEXT);
+      if (mContext != EGL14.EGL_NO_CONTEXT)
+        EGL14.eglDestroyContext(mDisplay, mContext);
+      if (mSurface != EGL14.EGL_NO_SURFACE)
+        EGL14.eglDestroySurface(mDisplay, mSurface);
 			
-			mEgl.eglTerminate(mDisplay);
+      EGL14.eglTerminate(mDisplay);
 		}
 		
-		mDisplay = EGL_NO_DISPLAY;
-		mSurface = EGL_NO_SURFACE;
-		mContext = EGL_NO_CONTEXT;
+    mDisplay = EGL14.EGL_NO_DISPLAY;
+    mSurface = EGL14.EGL_NO_SURFACE;
+    mContext = EGL14.EGL_NO_CONTEXT;
 		Log.d(TAG, "Egl terminated");
 	}
 	
 	public void attachToThread()
 	{
 		Log.d(TAG, "Pixel buffer attached");
-		if (!mEgl.eglMakeCurrent(mDisplay, mSurface, mSurface, mContext))
-			throw new EglOperationException("EGL error : Context was not bind to thread", mEgl.eglGetError());
-		
-		mGL = (GL11)mContext.getGL();
+    if (!EGL14.eglMakeCurrent(mDisplay, mSurface, mSurface, mContext))
+    {
+      Log.d(TAG, "Throwed");
+      throw new EglOperationException("EGL error : Context was not binded to thread", EGL14.eglGetError());
+    }
 	}
 	
 	public void detachFromThread()
 	{
 		Log.d(TAG, "Pixel buffer detached");
-		if (!mEgl.eglMakeCurrent(mDisplay, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT))
-			throw new EglOperationException("EGL error : Context was not bind to thread", mEgl.eglGetError());
-		
-		mGL = null;
+    if (!EGL14.eglMakeCurrent(mDisplay, EGL14.EGL_NO_SURFACE, EGL14.EGL_NO_SURFACE, EGL14.EGL_NO_CONTEXT))
+      throw new EglOperationException("EGL error : Context was not binded to the thread", EGL14.eglGetError());
 	}
 	
 	public Bitmap readBitmap()
 	{
-		if (mEgl == null)
-			return Bitmap.createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_4444);
+    GLES20.glPixelStorei(GLES20.GL_PACK_ALIGNMENT, 1);
+    GLES20.glPixelStorei(GLES20.GL_UNPACK_ALIGNMENT, 1);
 		
-		IntBuffer ib = IntBuffer.allocate(mWidth * mHeight);
-        IntBuffer ibt = IntBuffer.allocate(mWidth * mHeight);
-        mGL.glReadPixels(0, 0, mWidth, mHeight, GLES10.GL_RGBA, GLES10.GL_UNSIGNED_BYTE, ib);
+    Log.d(TAG, "Read bitmap. Width = " + mWidth + ", Heigth = " + mHeight);
+    IntBuffer ib = IntBuffer.allocate(mWidth * mHeight);
+    GLES20.glReadPixels(0, 0, mWidth, mHeight, GLES20.GL_RGBA, GLES20.GL_UNSIGNED_BYTE, ib);
+    
+    // Convert upside down mirror-reversed image to right-side up normal image.
+    IntBuffer ibt = IntBuffer.allocate(ib.capacity());
+    for (int i = 0; i < mHeight; ++i)
+      for (int j = 0; j < mWidth; ++j)
+        ibt.put((mHeight-i-1)*mWidth + j, ib.get(i*mWidth + j));
 
-        // Convert upside down mirror-reversed image to right-side up normal image.
-        for (int i = 0; i < mHeight; i++) {     
-            for (int j = 0; j < mWidth; j++) {
-                ibt.put((mHeight-i-1)*mWidth + j, ib.get(i*mWidth + j));
-            }
-        }                  
-
-        Bitmap bmp = Bitmap.createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_8888);
-        bmp.copyPixelsFromBuffer(ibt);
-        return bmp;
+    ib = null;
+    Bitmap bmp = Bitmap.createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_8888);
+    bmp.copyPixelsFromBuffer(ibt);
+    return bmp;
 	}
 	
 	class ConfigSorter implements Comparator<EGLConfig>
@@ -140,13 +136,16 @@ public class PixelBuffer
 		private int getWeight(EGLConfig config)
 		{
 			int[] value = new int[1];
-			mEgl.eglGetConfigAttrib(mDisplay, config, EGL_CONFIG_CAVEAT, value);
+      EGL14.eglGetConfigAttrib(mDisplay, config, EGL14.EGL_CONFIG_CAVEAT, value, 0);
 			
 			switch (value[0]) 
 			{
-			case EGL_NONE: 					return 0;
-			case EGL_SLOW_CONFIG:   		return 1;
-			case EGL_NON_CONFORMANT_CONFIG: return 2;
+        case EGL14.EGL_NONE:
+          return 0;
+        case EGL14.EGL_SLOW_CONFIG:
+          return 1;
+        case EGL14.EGL_NON_CONFORMANT_CONFIG:
+          return 2;
 			}
 			
 			return 0;
@@ -155,39 +154,31 @@ public class PixelBuffer
 	
 	private EGLContext createContext(EGLConfig config)
 	{
-		int[] contextAttributes = new int[] {EGL_CONTEXT_CLIENT_VERSION, 2, EGL_NONE};
-		return mEgl.eglCreateContext(mDisplay, config, EGL_NO_CONTEXT, contextAttributes);
+    int[] contextAttributes = new int[] { EGL14.EGL_CONTEXT_CLIENT_VERSION, 2, EGL14.EGL_NONE };
+    return EGL14.eglCreateContext(mDisplay, config, EGL14.EGL_NO_CONTEXT, contextAttributes, 0);
 	}
 	
 	private EGLSurface createSurface(EGLConfig config)
 	{
-		int[] surfaceAttribs = new int[] { EGL_HEIGHT, mHeight,
-										   EGL_WIDTH , mWidth,
-										   EGL_NONE};
-		
-		return mEgl.eglCreatePbufferSurface(mDisplay, config, surfaceAttribs);
+    int[] surfaceAttribs = new int[] { EGL14.EGL_WIDTH, mWidth, EGL14.EGL_HEIGHT, mHeight, EGL14.EGL_NONE };
+    return EGL14.eglCreatePbufferSurface(mDisplay, config, surfaceAttribs, 0);
 	}
 	
 	private EGLConfig[] getConfigs()
 	{
 		EGLConfig[] configs = new EGLConfig[40];
 		int[] numConfigs = new int[] { 0 };
+    int[] configAttributes = { EGL14.EGL_RED_SIZE, 5, EGL14.EGL_GREEN_SIZE, 6, EGL14.EGL_BLUE_SIZE, 5,
+        EGL14.EGL_STENCIL_SIZE, 0, EGL14.EGL_DEPTH_SIZE, 16, EGL14.EGL_RENDERABLE_TYPE, EGL14.EGL_OPENGL_ES2_BIT,
+        EGL14.EGL_NONE };
 		
-		int[] configAttributes = { EGL_RED_SIZE, 		EGL_DONT_CARE,
-								   EGL_GREEN_SIZE, 		EGL_DONT_CARE,
-								   EGL_BLUE_SIZE,		EGL_DONT_CARE,
-								   //EGL_STENCIL_SIZE, 	0,
-								   //EGL_DEPTH_SIZE,	   16,
-								   //EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
-								   EGL_NONE };
-		
-		if (!mEgl.eglChooseConfig(mDisplay, configAttributes, configs, 40, numConfigs))
-			throw new EglOperationException("EGL error : config not choosed", mEgl.eglGetError());
+    if (!EGL14.eglChooseConfig(mDisplay, configAttributes, 0, configs, 0, configs.length, numConfigs, 0))
+      throw new EglOperationException("EGL error : ChooseConfig has failed", EGL14.eglGetError());
 		
 		if (numConfigs[0] == 0)
-			throw new EglOperationException("EGL error : config not founded", mEgl.eglGetError());
+      throw new EglOperationException("EGL error : No approproate config found", EGL14.eglGetError());
 		
-		Log.d(TAG, "Config numbers = " + numConfigs[0]);
+    Log.d(TAG, "Found " + numConfigs[0] + " EGL configs");
 		
 		EGLConfig[] result = Arrays.copyOf(configs, numConfigs[0]);
 		Arrays.sort(result, new ConfigSorter());
