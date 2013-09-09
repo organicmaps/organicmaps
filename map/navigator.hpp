49063b134a80b0c1ec1a7d592d2449ec443db62b
ExMix <rahuba.youri@mapswithme.com>
2013-09-09 18:47:19 +0300

    ScaleProcessor need for yopme part.

--- map/navigator.hpp
+++ map/navigator.hpp
@@ -66,6 +66,8 @@ public:
   /// so we should take it into an account
   m2::PointD ShiftPoint(m2::PointD const & pt) const;
 
+  ScalesProcessor const & GetScaleProcessor() const;
+
 private:
   ScalesProcessor const & m_scales;
 
