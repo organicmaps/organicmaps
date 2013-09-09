bf13980bbae912c21e2e937be64af9cc543ffe88
ExMix <rahuba.youri@mapswithme.com>
2013-09-09 18:43:55 +0300

    update assets for yopme

--- tools/android/update_assets.sh
+++ tools/android/update_assets.sh
@@ -1,4 +1,5 @@
 ./update_assets_for_version.sh ../../android/MapsWithMePro/assets
 ./update_assets_for_version.sh ../../android/MapsWithMeLite/assets
 ./update_assets_for_version.sh ../../android/MapsWithMeLite.Samsung/assets
+./update_assets_for_version.sh ../../android/YoPme/assets
 ./update_flags.sh
