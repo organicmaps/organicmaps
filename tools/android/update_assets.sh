0cf47ad311bca8c489318c93eccbd04b94598145
vng <viktor.govako@gmail.com>
2013-09-11 13:28:02 +0300

    [yopme] Black and White styles.

--- tools/android/update_assets.sh
+++ tools/android/update_assets.sh
@@ -1,5 +1,11 @@
 ./update_assets_for_version.sh ../../android/MapsWithMePro/assets
 ./update_assets_for_version.sh ../../android/MapsWithMeLite/assets
 ./update_assets_for_version.sh ../../android/MapsWithMeLite.Samsung/assets
+
 ./update_assets_for_version.sh ../../android/YoPme/assets
+SRC=../../../data
+DST=../../android/YoPme/assets
+rm -rf $DST/drules_proto.bin
+ln -s $SRC/drules_proto-bw.bin $DST/drules_proto.bin
+
 ./update_flags.sh
