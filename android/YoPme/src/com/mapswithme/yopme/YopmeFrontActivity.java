ebee007be5d44a07dd127607cff1e150c93ca782
vng <viktor.govako@gmail.com>
2013-09-15 21:32:59 +0300

    Revert "[yop] Gyro filter."

--- android/YoPme/src/com/mapswithme/yopme/YopmeFrontActivity.java
+++ android/YoPme/src/com/mapswithme/yopme/YopmeFrontActivity.java
@@ -12,15 +12,9 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.text.SpannableString;
 import android.text.util.Linkify;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -31,7 +25,7 @@ import android.widget.PopupMenu.OnMenuItemClickListener;
 import android.widget.TextView;
 
 public class YopmeFrontActivity extends Activity
-                                implements OnClickListener, SensorEventListener
+                                implements OnClickListener
 {
   private TextView   mSelectedLocation;
   private View mMenu;
@@ -63,10 +57,6 @@ public class YopmeFrontActivity extends Activity
     actionBar.setDisplayShowCustomEnabled(true);
     actionBar.setCustomView(R.layout.action_bar_view);
 
-    // sencors set up
-    sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-    accel = sm.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-
     setUpView();
 
     //restore
@@ -85,20 +75,6 @@ public class YopmeFrontActivity extends Activity
 
     if (isIntroNeeded())
       showIntro();
-  }
-
-  @Override
-  protected void onPause()
-  {
-    super.onPause();
-    sm.unregisterListener(this);
-  }
-
-  @Override
-  protected void onResume()
-  {
-    super.onResume();
-    sm.registerListener(this, accel, SensorManager.SENSOR_DELAY_NORMAL);
   }
 
   @Override
@@ -137,12 +113,6 @@ public class YopmeFrontActivity extends Activity
   @Override
   public void onClick(View v)
   {
-    if (!mClickable)
-    {
-      Log.d("SENSOR", "SKIPING CLICK");
-      return;
-    }
-
     if (R.id.me == v.getId())
     {
       BackscreenActivity.startInMode(getApplicationContext(), Mode.LOCATION, null, MapDataProvider.COMFORT_ZOOM);
@@ -238,33 +208,5 @@ public class YopmeFrontActivity extends Activity
   private void markIntroShown()
   {
     getSharedPreferences(PREFS, 0).edit().putBoolean(KEY_INTRO, false).apply();
-  }
-
-  @Override
-  public void onAccuracyChanged(Sensor sensor, int accuracy)
-  {
-  }
-
-  private long mLastTimeStamp;
-  private boolean mClickable = true;
-  private SensorManager sm;
-  private Sensor accel;
-
-  @Override
-  public void onSensorChanged(SensorEvent event)
-  {
-    // in nanos
-    final long interval = 1000;
-    // in radians/second
-    final float lowerBound = 1.0f;
-
-    final float yRotationSpeed = event.values[1];
-    if (Math.abs(yRotationSpeed) > lowerBound)
-    {
-      mLastTimeStamp = event.timestamp;
-      mClickable = false;
-    }
-    else if (event.timestamp - mLastTimeStamp > interval * 1000000)
-      mClickable = true;
   }
 }
