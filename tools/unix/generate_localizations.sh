aa6aba35376cb7f8a2503e0bb1fbdb38388c0cb1
vng <viktor.govako@gmail.com>
2013-09-23 12:44:36 +0300

    [tools] No need in additional rm in twine 0.4.+

--- tools/unix/generate_localizations.sh
+++ tools/unix/generate_localizations.sh
@@ -6,8 +6,3 @@ set -e -u -x
 ./tools/twine/twine generate-all-string-files ./strings.txt ./android/res/
 ./tools/twine/twine generate-all-string-files ./strings.txt ./iphone/Maps/
 
-# Delete dummy string files
-rm ./android/res/values-sw600dp/strings.xml
-rm ./android/res/values-sw720dp-land/strings.xml
-rm ./android/res/values-sw720dp/strings.xml
-rm ./android/res/values-v11/strings.xml
