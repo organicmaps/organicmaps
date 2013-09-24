bbb1663ee540c42ff7e7713ba59eaa392f3fd5be
ExMix <rahuba.youri@mapswithme.com>
2013-09-24 19:01:13 +0300

    [drape] index buffer always use uint16_t for store index value and for upload need to use conform signature

--- drape/index_buffer.cpp
+++ drape/index_buffer.cpp
@@ -4,3 +4,8 @@ IndexBuffer::IndexBuffer(uint16_t capacity)
   : GLBuffer(GLBuffer::IndexBuffer, sizeof(uint16_t), capacity)
 {
 }
+
+void IndexBuffer::UploadData(uint16_t * data, uint16_t size)
+{
+  GLBuffer::UploadData((void *)data, size);
+}
--- drape/index_buffer.hpp
+++ drape/index_buffer.hpp
@@ -6,4 +6,6 @@ class IndexBuffer : public GLBuffer
 {
 public:
   IndexBuffer(uint16_t capacity);
+
+  void UploadData(uint16_t * data, uint16_t size);
 };
