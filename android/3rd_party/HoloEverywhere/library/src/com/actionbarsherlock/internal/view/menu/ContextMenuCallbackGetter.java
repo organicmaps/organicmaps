af1cc929bce1fd89f25d87d6e5ffc81fbf113e8d
Dmitry Kunin <dkunin@mapswith.me>
2013-11-12 16:53:13 +0300

    [AND] Merge ext libs. Removed unused sources.
    interm
    
    interm

--- android/3rd_party/HoloEverywhere/library/src/com/actionbarsherlock/internal/view/menu/ContextMenuCallbackGetter.java
+++
@@ -1,8 +0,0 @@
-
-package com.actionbarsherlock.internal.view.menu;
-
-import android.view.View.OnCreateContextMenuListener;
-
-public interface ContextMenuCallbackGetter {
-    public OnCreateContextMenuListener getOnCreateContextMenuListener();
-}
--- android/3rd_party/HoloEverywhere/library/src/org/holoeverywhere/util/Pool.java
+++
@@ -1,8 +0,0 @@
-
-package org.holoeverywhere.util;
-
-public interface Pool<T extends Poolable<T>> {
-    public abstract T acquire();
-
-    public abstract void release(T element);
-}
