a2a286ad99dfdab036fd53ef693659c4b36ed099
Igor Khmurets <subzero@mapswithme.com>
2013-11-14 12:35:50 +0300

    [ios] Added UIViewController (Navigation) category

---
+++ iphone/Maps/Classes/UIViewController+Navigation.h
@@ -0,0 +1,10 @@
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (Navigation)
+
+- (void)showBackButton;
+
+@property (nonatomic, readonly) UIStoryboard * mainStoryboard;
+
+@end
---
+++ iphone/Maps/Classes/UIViewController+Navigation.m
@@ -0,0 +1,33 @@
+
+#import "UIViewController+Navigation.h"
+#import "UIKitCategories.h"
+
+@implementation UIViewController (Navigation)
+
+- (void)showBackButton
+{
+  UIButton * backButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 56, 44)];
+  [backButton addTarget:self action:@selector(backButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+  [backButton setImage:[UIImage imageNamed:@"nav-bar-button-back"] forState:UIControlStateNormal];
+  UIBarButtonItem *space = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFixedSpace target:nil action:nil];
+  if (SYSTEM_VERSION_IS_LESS_THAN(@"7")) {
+    space.width = -8;
+  } else {
+    space.width = -16;
+  }
+  UIBarButtonItem * item = [[UIBarButtonItem alloc] initWithCustomView:backButton];
+  self.navigationItem.leftBarButtonItems = @[space, item];
+}
+
+- (void)backButtonPressed:(id)sender
+{
+  [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (UIStoryboard *)mainStoryboard
+{
+  NSString * name = UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad ? @"Main_iPad" : @"Main_iPhone";
+  return [UIStoryboard storyboardWithName:name bundle:nil];
+}
+
+@end
