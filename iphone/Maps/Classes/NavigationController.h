0ba70b423fe6a0c9cbc0f2c7a6b7481139f6574b
Igor Khmurets <subzero@mapswithme.com>
2013-11-21 19:35:24 +0300

    [ios] Added custom NavigationController

---
+++ iphone/Maps/Classes/NavigationController.h
@@ -0,0 +1,6 @@
+
+#import <UIKit/UIKit.h>
+
+@interface NavigationController : UINavigationController
+
+@end
---
+++ iphone/Maps/Classes/NavigationController.m
@@ -0,0 +1,45 @@
+
+#import "NavigationController.h"
+#import "MapsAppDelegate.h"
+#import "UIViewController+Navigation.h"
+
+@interface NavigationController () <UINavigationControllerDelegate>
+
+@end
+
+@implementation NavigationController
+
+- (UIStatusBarStyle)preferredStatusBarStyle
+{
+  return [self.viewControllers count] > 1 ? UIStatusBarStyleLightContent : UIStatusBarStyleDefault;
+}
+
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
+
+  self.delegate = self;
+}
+
+- (void)navigationController:(UINavigationController *)navigationController willShowViewController:(UIViewController *)viewController animated:(BOOL)animated
+{
+  if ([viewController isMemberOfClass:[MapViewController class]])
+  {
+    MapViewController * mapViewController = [MapsAppDelegate theApp].m_mapViewController;
+    [navigationController setNavigationBarHidden:![mapViewController shouldShowNavBar] animated:YES];
+  }
+  else
+  {
+    [navigationController setNavigationBarHidden:NO animated:animated];
+  }
+  if ([navigationController.viewControllers count] > 1)
+    [viewController showBackButton];
+}
+
+- (void)didReceiveMemoryWarning
+{
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
