eda264b75096e63a0a01f045ca843a3c73a11032
Igor Khmurets <subzero@mapswithme.com>
2013-11-28 18:10:50 +0300

    [ios] UIKitCategories +colorWithColorCode: fix

--- iphone/Maps/Categories/UIKitCategories.m
+++ iphone/Maps/Categories/UIKitCategories.m
@@ -21,11 +21,6 @@
 + (UIColor *)colorWithColorCode:(NSString *)hexString
 {
   NSString * cleanString = [hexString stringByReplacingOccurrencesOfString:@"#" withString:@""];
-  if (cleanString.length == 3)
-    cleanString = [NSString stringWithFormat:@"%@%@%@%@%@%@",
-                   [cleanString substringWithRange:NSMakeRange(0, 1)], [cleanString substringWithRange:NSMakeRange(0, 1)],
-                   [cleanString substringWithRange:NSMakeRange(1, 1)], [cleanString substringWithRange:NSMakeRange(1, 1)],
-                   [cleanString substringWithRange:NSMakeRange(2, 1)], [cleanString substringWithRange:NSMakeRange(2, 1)]];
 
   if (cleanString.length == 6)
     cleanString = [cleanString stringByAppendingString:@"ff"];
