813c79501e3e68d58359953a2941e933af16dd53
Igor Khmurets <subzero@mapswithme.com>
2013-12-05 21:15:59 +0300

    [ios] Now you can share bookmarks using Gmail.app if you did no set up Mail.app

--- iphone/Maps/Classes/ShareActionSheet.mm
+++ iphone/Maps/Classes/ShareActionSheet.mm
@@ -11,15 +11,22 @@
 +(void)showShareActionSheetInView:(id)view withObject:(id)del
 {
   UIActionSheet * as = [[UIActionSheet alloc] initWithTitle:NSLocalizedString(@"share", nil) delegate:del cancelButtonTitle:nil  destructiveButtonTitle:nil otherButtonTitles:nil];
+
   if ([MessageComposeViewController canSendText])
     [as addButtonWithTitle:NSLocalizedString(@"message", nil)];
-  if ([MailComposeViewController canSendMail])
+  if ([MailComposeViewController canSendMail] || [self canUseGmailApp])
     [as addButtonWithTitle:NSLocalizedString(@"email", nil)];
   [as addButtonWithTitle:NSLocalizedString(@"copy_link", nil)];
   [as addButtonWithTitle:NSLocalizedString(@"cancel", nil)];
   [as setCancelButtonIndex:as.numberOfButtons - 1];
   [as showInView:view];
   [as release];
+}
+
++ (BOOL)canUseGmailApp
+{
+  // This is not official google api
+  return [[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"googlegmail://"]];
 }
 
 +(void)resolveActionSheetChoice:(UIActionSheet *)as buttonIndex:(NSInteger)buttonIndex text:(NSString *)text
@@ -45,23 +52,39 @@
 
 +(void)sendEmailWith:(NSString *)textFieldText andUrl:(NSString *)shortUrl view:(id)view delegate:(id)del gX:(double)gX gY:(double)gY myPosition:(BOOL)myPos
 {
-  MailComposeViewController * mailVC = [[[MailComposeViewController alloc] init] autorelease];
+
+  NSString * subject;
+  NSString * body;
   NSString * httpGe0Url = [shortUrl stringByReplacingCharactersInRange:NSMakeRange(0, 6) withString:@"http://ge0.me/"];
   if (myPos)
   {
     search::AddressInfo info;
     GetFramework().GetAddressInfoForGlobalPoint(m2::PointD(gX, gY), info);
     NSString * nameAndAddress = [NSString stringWithUTF8String:info.FormatNameAndAddress().c_str()];
-    [mailVC setMessageBody:[NSString stringWithFormat:NSLocalizedString(@"my_position_share_email", nil), nameAndAddress, shortUrl, httpGe0Url] isHTML:NO];
-    [mailVC setSubject:NSLocalizedString(@"my_position_share_email_subject", nil)];
+    body = [NSString stringWithFormat:NSLocalizedString(@"my_position_share_email", nil), nameAndAddress, shortUrl, httpGe0Url];
+    subject = NSLocalizedString(@"my_position_share_email_subject", nil);
   }
   else
   {
-    [mailVC setMessageBody:[NSString stringWithFormat:NSLocalizedString(@"bookmark_share_email", nil), textFieldText, shortUrl, httpGe0Url] isHTML:NO];
-    [mailVC setSubject:NSLocalizedString(@"bookmark_share_email_subject", nil)];
+    body = [NSString stringWithFormat:NSLocalizedString(@"bookmark_share_email", nil), textFieldText, shortUrl, httpGe0Url];
+    subject = NSLocalizedString(@"bookmark_share_email_subject", nil);
   }
-  mailVC.mailComposeDelegate = del;
-  [view presentModalViewController:mailVC animated:YES];
+
+  if ([MailComposeViewController canSendMail])
+  {
+    MailComposeViewController * mailVC = [[[MailComposeViewController alloc] init] autorelease];
+    [mailVC setMessageBody:body isHTML:NO];
+    [mailVC setSubject:subject];
+    mailVC.mailComposeDelegate = del;
+    [view presentModalViewController:mailVC animated:YES];
+  }
+  else if ([self canUseGmailApp])
+  {
+    subject = [subject stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    body = [body stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    NSString *urlString = [NSString stringWithFormat:@"googlegmail:///co?subject=%@&body=%@", subject, body];
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:urlString]];
+  }
 }
 
 +(void)sendMessageWithUrl:(NSString *)shortUrl view:(id)view delegate:(id)del myPosition:(BOOL)myPos
