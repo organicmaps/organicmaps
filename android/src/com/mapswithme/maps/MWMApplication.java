f12f598c20045a2eebf555873393152ff86c79ff
Alex Zolotarev <alex@mapswithme.com>
2013-12-06 11:30:03 +0300

    Revert "[AND] Never say never to MapsWithMe"
    This reverts commit 8d02a576830de2346891417bce91ba2bab577263.

--- android/src/com/mapswithme/maps/MWMApplication.java
+++ android/src/com/mapswithme/maps/MWMApplication.java
@@ -297,6 +297,7 @@ public class MWMApplication extends android.app.Application implements MapStorag
 
   static public final int OK = 0;
   static public final int LATER = 1;
+  static public final int NEVER = 2;
   public native void submitDialogResult(int dlg, int res);
   /// @}
 
