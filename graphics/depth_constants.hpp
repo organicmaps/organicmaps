a5998765104831ec20d3654b6cdd6cc367f1b8fc
ExMix <rahuba.youri@mapswithme.com>
2013-12-15 15:26:07 +0300

    fix depth layout of gui elements

--- graphics/depth_constants.hpp
+++ graphics/depth_constants.hpp
@@ -7,17 +7,17 @@ namespace graphics
   static const int debugDepth = maxDepth;
   static const int benchmarkDepth = maxDepth;
 
+  static const int compassDepth = maxDepth;
+  static const int rulerDepth = maxDepth;
+  static const int countryStatusDepth = maxDepth - 10;
+
   /// @todo 100 is a temporary solution fo iOS.
   /// Need to review logic of gui elements, glyphs and symbols caching
   /// (display_list_cache.dpp). Depth is hardcoded there.
-
   static const int balloonContentInc = 100;
-  static const int balloonBaseDepth = maxDepth - balloonContentInc;
+  static const int balloonBaseDepth = countryStatusDepth - (balloonContentInc + 10);
 
-  static const int compassDepth = balloonBaseDepth - 10;
-  static const int rulerDepth = compassDepth;
-  static const int locationDepth = rulerDepth - 10;
+  static const int locationDepth = balloonBaseDepth - 10;
   static const int poiAndBookmarkDepth = locationDepth - 10;
   static const int tracksDepth = poiAndBookmarkDepth - 10;
-  static const int countryStatusDepth = poiAndBookmarkDepth - 10;
 }
