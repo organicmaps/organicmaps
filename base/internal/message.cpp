6e2731d4a41f05842e705521b747143d48e5abd6
Alex Zolotarev <alex@mapswithme.com>
2014-01-27 14:37:38 +0300

    Logger fix - print original UTF-8 strings

--- base/internal/message.cpp
+++ base/internal/message.cpp
@@ -2,21 +2,22 @@
 
 string DebugPrint(string const & t)
 {
-  string res;
-  res.push_back('\'');
-  for (string::const_iterator it = t.begin(); it != t.end(); ++it)
-  {
-    static char const toHex[] = "0123456789abcdef";
-    unsigned char const c = static_cast<unsigned char>(*it);
-    if (c >= ' ' && c <= '~')
-      res.push_back(*it);
-    else
-    {
-      res.push_back('\\');
-      res.push_back(toHex[c >> 4]);
-      res.push_back(toHex[c & 0xf]);
-    }
-  }
-  res.push_back('\'');
-  return res;
+//  string res;
+//  res.push_back('\'');
+//  for (string::const_iterator it = t.begin(); it != t.end(); ++it)
+//  {
+//    static char const toHex[] = "0123456789abcdef";
+//    unsigned char const c = static_cast<unsigned char>(*it);
+//    if (c >= ' ' && c <= '~')
+//      res.push_back(*it);
+//    else
+//    {
+//      res.push_back('\\');
+//      res.push_back(toHex[c >> 4]);
+//      res.push_back(toHex[c & 0xf]);
+//    }
+//  }
+//  res.push_back('\'');
+  // Simply print UTF-8 string. Our computers (finally) supports it.
+  return t;
 }
