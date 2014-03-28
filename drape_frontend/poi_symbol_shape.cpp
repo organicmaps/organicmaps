3b6d445bdba4767b22911c8682b005bc91677642
ExMix <rahuba.youri@mapswithme.com>
2014-03-28 11:47:17 +0300

    [drape] texture coodinates fix enable blending for poi

--- drape_frontend/poi_symbol_shape.cpp
+++ drape_frontend/poi_symbol_shape.cpp
@@ -23,6 +23,8 @@ namespace df
     GLState state(gpu::TEXTURING_PROGRAM, 0);
     state.SetTextureSet(region.m_textureSet);
 
+    state.SetBlending(Blending(true));
+
     m2::PointF halfSize(region.m_pixelSize.x / 2.0, region.m_pixelSize.y / 2.0);
     m2::RectF const & texRect = region.m_stRect;
     float depth = m_params.m_depth;
@@ -30,10 +32,10 @@ namespace df
 
     float stream[]     =
     //   x       y       z      normal.x    normal.y        s              t          textureIndex
-    { m_pt.x, m_pt.y, depth, -halfSize.x,  halfSize.y, texRect.minX(), texRect.minY(), texture,
-      m_pt.x, m_pt.y, depth, -halfSize.x, -halfSize.y, texRect.minX(), texRect.maxX(), texture,
-      m_pt.x, m_pt.y, depth,  halfSize.x,  halfSize.y, texRect.maxX(), texRect.minY(), texture,
-      m_pt.x, m_pt.y, depth,  halfSize.x, -halfSize.y, texRect.maxX(), texRect.maxY(), texture };
+    { m_pt.x, m_pt.y, depth, -halfSize.x,  halfSize.y, texRect.minX(), texRect.maxY(), texture,
+      m_pt.x, m_pt.y, depth, -halfSize.x, -halfSize.y, texRect.minX(), texRect.minY(), texture,
+      m_pt.x, m_pt.y, depth,  halfSize.x,  halfSize.y, texRect.maxX(), texRect.maxY(), texture,
+      m_pt.x, m_pt.y, depth,  halfSize.x, -halfSize.y, texRect.maxX(), texRect.minY(), texture};
 
     AttributeProvider provider(1, 4);
     BindingInfo info(3);
@@ -43,21 +45,21 @@ namespace df
     posDecl.m_componentCount = 3;
     posDecl.m_componentType = GLConst::GLFloatType;
     posDecl.m_offset = 0;
-    posDecl.m_stride = (3 + 2 + 2) * sizeof(float);
+    posDecl.m_stride = 8 * sizeof(float);
 
     BindingDecl & normalDecl = info.GetBindingDecl(1);
     normalDecl.m_attributeName = "a_normal";
     normalDecl.m_componentCount = 2;
     normalDecl.m_componentType = GLConst::GLFloatType;
     normalDecl.m_offset = 3 * sizeof(float);
-    normalDecl.m_stride = (3 + 2 + 2) * sizeof(float);
+    normalDecl.m_stride = 8 * sizeof(float);
 
     BindingDecl & texDecl = info.GetBindingDecl(2);
     texDecl.m_attributeName = "a_texCoords";
-    texDecl.m_componentCount = 3;
+    texDecl.m_componentCount = 2;
     texDecl.m_componentType = GLConst::GLFloatType;
-    texDecl.m_offset = 3 * sizeof(float);
-    texDecl.m_stride = (3 + 2 + 2) * sizeof(float);
+    texDecl.m_offset = (3 + 2) * sizeof(float);
+    texDecl.m_stride = 8 * sizeof(float);
 
     provider.InitStream(0, info, MakeStackRefPointer<void>(stream));
     batcher->InsertTriangleStrip(state, MakeStackRefPointer(&provider));
