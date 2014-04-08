e896a5e68dd78c966aa717f620b718a4adaa63c0
Sergey Pisarchik <pisarchik@mapswithme.com>
2014-04-08 10:34:11 +0300

    [Tizen] remove some unused files made chmod +x update_assets.sh

--- tizen/scripts/update_assets.sh
+++ tizen/scripts/update_assets.sh
@@ -1,5 +1,2 @@
+#!/bin/bash
 ./update_assets_for_version.sh ../RunUnitTests/res
-# ./update_assets_for_version.sh ../../android/MapsWithMeLite/assets
-# ./update_assets_for_version.sh ../../android/MapsWithMeLite.Samsung/assets
-
-# ./update_flags.sh
