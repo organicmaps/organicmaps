d3a047d7184c53789c497ca23a7be78cd5f0d8eb
Sergey Pisarchik <pisarchik@mapswithme.com>
2014-04-08 17:44:03 +0300

    [Tizen] Removed defines

--- tizen/inc/FIo.hpp
+++ tizen/inc/FIo.hpp
@@ -1,8 +1,5 @@
 #pragma once
-#include "../../std/target_os.hpp"
-#ifdef OMIM_OS_TIZEN
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wignored-qualifiers"
   #include <FIo.h>
 #pragma clang diagnostic pop
-#endif
