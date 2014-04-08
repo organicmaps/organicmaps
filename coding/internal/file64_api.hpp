f4c1dbe5d882af944d57edd876eacbcff627dd02
Sergey Pisarchik <pisarchik@mapswithme.com>
2014-04-08 17:39:08 +0300

    [Tizen] Uncomment Static_assert

--- coding/internal/file64_api.hpp
+++ coding/internal/file64_api.hpp
@@ -7,7 +7,7 @@
   #define ftell64 _ftelli64
 
 #elif defined(OMIM_OS_TIZEN)
-  //STATIC_ASSERT(sizeof(_off64_t) == 8);
+  STATIC_ASSERT(sizeof(__off64_t) == 8);
   #define fseek64 fseeko64
   #define ftell64 ftello64
 
