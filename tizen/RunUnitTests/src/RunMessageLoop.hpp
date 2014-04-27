20d5dacab64faa095b0e8ca56ca19ac85d84cac9
Sergey Pisarchik <pisarchik@mapswithme.com>
2014-04-27 17:49:29 +0300

    [Tizen] Added some files to fix some unit tests errors.

---
+++ tizen/RunUnitTests/src/RunMessageLoop.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+void RunMessageLoop();
+
+void EndMessageLoop();
---
+++ tizen/scripts/update_assets_for_unit_tests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -x -u
+
+SRC=../../../data
+DST=$1
+
+# Remove old links
+#rm -rf $DST
+#mkdir $DST
+
+files=(about.html resources-ldpi resources-mdpi resources-hdpi resources-xhdpi resources-xxhdpi categories.txt classificator.txt
+       types.txt fonts_blacklist.txt fonts_whitelist.txt languages.txt unicode_blocks.txt \
+       drules_proto.bin packed_polygons.bin countries.txt World.mwm WorldCoasts.mwm 00_roboto_regular.ttf 01_dejavusans.ttf 02_wqy-microhei.ttf
+       03_jomolhari-id-a3d.ttf 04_padauk.ttf 05_khmeros.ttf 06_code2000.ttf
+       minsk-pass.mwm)
+
+for item in ${files[*]}
+do
+  ln -s $SRC/$item $DST/$item
+done
