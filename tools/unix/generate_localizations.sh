982832d263cd69b67f4e4a8b6f2bcc0ba7374edf
Sergey Pisarchik <pisarchik@mapswithme.com>
2014-05-16 11:54:39 +0300

    [tools] Explicitly specify format in generate_localizations.sh

--- tools/unix/generate_localizations.sh
+++ tools/unix/generate_localizations.sh
@@ -3,6 +3,6 @@
 set -e -u -x
 
 # TODO: Add "--untagged --tags android" when tags are properly set.
-./tools/twine/twine generate-all-string-files ./strings.txt ./android/res/
-./tools/twine/twine generate-all-string-files ./strings.txt ./iphone/Maps/
+./tools/twine/twine --format android generate-all-string-files ./strings.txt ./android/res/
+./tools/twine/twine --format apple generate-all-string-files ./strings.txt ./iphone/Maps/
 
