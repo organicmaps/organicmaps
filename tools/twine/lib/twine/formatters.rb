dc94522ce25015c7542036162446ae624ed658dd
Sergey Pisarchik <pisarchik@mapswithme.com>
2014-05-16 16:10:20 +0300

    [tools] Add tizen support to twine.

--- tools/twine/lib/twine/formatters.rb
+++ tools/twine/lib/twine/formatters.rb
@@ -5,9 +5,10 @@ require 'twine/formatters/flash'
 require 'twine/formatters/gettext'
 require 'twine/formatters/jquery'
 require 'twine/formatters/django'
+require 'twine/formatters/tizen'
 
 module Twine
   module Formatters
-    FORMATTERS = [Formatters::Apple, Formatters::Android, Formatters::Gettext, Formatters::JQuery, Formatters::Flash, Formatters::Django]
+    FORMATTERS = [Formatters::Apple, Formatters::Android, Formatters::Gettext, Formatters::JQuery, Formatters::Flash, Formatters::Django, Formatters::Tizen]
   end
 end
