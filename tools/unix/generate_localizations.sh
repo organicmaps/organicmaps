0823d0b580c6eb62c6e443a3915dafdf224618a3
Sergey Pisarchik <pisarchik@mapswithme.com>
2014-05-17 11:17:50 +0300

    [Tizen] Add resource strings generation

--- tools/unix/generate_localizations.sh
+++ tools/unix/generate_localizations.sh
@@ -5,4 +5,5 @@ set -e -u -x
 # TODO: Add "--untagged --tags android" when tags are properly set.
 ./tools/twine/twine --format android generate-all-string-files ./strings.txt ./android/res/
 ./tools/twine/twine --format apple generate-all-string-files ./strings.txt ./iphone/Maps/
+./tools/twine/twine --format tizen generate-all-string-files ./strings.txt ./tizen/MapsWithMe/res/
 
