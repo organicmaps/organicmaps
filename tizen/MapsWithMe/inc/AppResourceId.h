f00d3368b64a46c5656052d009ef45ab7e77b719
Sergey Pisarchik <pisarchik@mapswithme.com>
2014-05-21 22:17:09 +0300

    [Tizen] Add resources.

--- tizen/MapsWithMe/inc/AppResourceId.h
+++ tizen/MapsWithMe/inc/AppResourceId.h
@@ -6,6 +6,7 @@
 #endif // _APP_RESOURCE_ID_H_
 extern const wchar_t* IDC_ABOUT_BUTTON;
 extern const wchar_t* IDC_CLOSE_BUTTON;
+extern const wchar_t* IDC_DOWNLOAD_LISTVIEW;
 extern const wchar_t* IDC_ENABLE_SCALE_BUTTONS_CB;
 extern const wchar_t* IDC_FOOTS_CHECKBUTTON;
 extern const wchar_t* IDC_MEASUREMENT_NOTE;
@@ -15,9 +16,16 @@ extern const wchar_t* IDC_VERSION_LABEL;
 extern const wchar_t* IDC_WEB;
 extern const wchar_t* IDF_ABOUT_FORM;
 extern const wchar_t* IDF_ABOUT_FORM1;
+extern const wchar_t* IDF_DOWNLOAD_FORM;
 extern const wchar_t* IDS_ABOUT;
+extern const wchar_t* IDS_ARE_YOU_SURE;
 extern const wchar_t* IDS_CANCEL;
+extern const wchar_t* IDS_CANCEL_DOWNLOAD;
+extern const wchar_t* IDS_DELETE;
+extern const wchar_t* IDS_DOWNLOAD;
+extern const wchar_t* IDS_DOWNLOAD_COUNTRY_FAILED;
 extern const wchar_t* IDS_KILOMETRES;
+extern const wchar_t* IDS_MB;
 extern const wchar_t* IDS_MEASUREMENT_UNITS;
 extern const wchar_t* IDS_MEASUREMENT_UNITS_SUMMARY;
 extern const wchar_t* IDS_MILES;
