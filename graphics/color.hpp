59bf4fa5290230eec214e8be63e14da60e2b4ce3
Alex Zolotarev <alex@mapswithme.com>
2014-07-07 23:03:36 +0200

    Color formatting fix

--- graphics/color.hpp
+++ graphics/color.hpp
@@ -6,44 +6,50 @@
 
 namespace graphics
 {
-  struct Color
-  {
-    uint8_t r;
-    uint8_t g;
-    uint8_t b;
-    uint8_t a;
 
-    Color(uint8_t r1, uint8_t g1, uint8_t b1, uint8_t a1);
-    Color();
-    Color(Color const & p);
-    Color const & operator=(Color const & p);
+struct Color
+{
+  uint8_t r;
+  uint8_t g;
+  uint8_t b;
+  uint8_t a;
 
-    static Color const fromARGB(uint32_t _c);
-    static Color const fromXRGB(uint32_t _c, uint8_t _a = 0);
+  /// alpha 255 means non-transparent and 0 means fully transparent
+  Color(uint8_t r1, uint8_t g1, uint8_t b1, uint8_t a1);
+  Color();
+  Color(Color const & p);
+  Color const & operator=(Color const & p);
 
-    Color const & operator /= (unsigned k);
+  static Color const fromARGB(uint32_t _c);
+  static Color const fromXRGB(uint32_t _c, uint8_t _a = 0);
 
-    static Color Black();
-    static Color White();
-  };
+  Color const & operator /= (unsigned k);
 
-  bool operator < (Color const & l, Color const & r);
-  bool operator > (Color const & l, Color const & r);
-  bool operator !=(Color const & l, Color const & r);
-  bool operator ==(Color const & l, Color const & r);
+  static Color Black();
+  static Color White();
+  static Color Red();
+  static Color Green();
+  static Color Blue();
+};
 
-  inline int redFromARGB(uint32_t c)   {return (c >> 16) & 0xFF;}
-  inline int greenFromARGB(uint32_t c) {return (c >> 8) & 0xFF; }
-  inline int blueFromARGB(uint32_t c)  {return (c & 0xFF);      }
-  inline int alphaFromARGB(uint32_t c) {return (c >> 24) & 0xFF;}
+bool operator < (Color const & l, Color const & r);
+bool operator > (Color const & l, Color const & r);
+bool operator !=(Color const & l, Color const & r);
+bool operator ==(Color const & l, Color const & r);
 
-  inline string DebugPrint(Color const & c)
-  {
-    ostringstream os;
-    os << "r: " << (int)c.r << " ";
-    os << "g: " << (int)c.g << " ";
-    os << "b: " << (int)c.b << " ";
-    os << "alpha: " << (int)c.a;
-    return os.str();
-  }
+inline int redFromARGB(uint32_t c)   {return (c >> 16) & 0xFF;}
+inline int greenFromARGB(uint32_t c) {return (c >> 8) & 0xFF; }
+inline int blueFromARGB(uint32_t c)  {return (c & 0xFF);      }
+inline int alphaFromARGB(uint32_t c) {return (c >> 24) & 0xFF;}
+
+inline string DebugPrint(Color const & c)
+{
+  ostringstream os;
+  os << "r: " << (int)c.r << " ";
+  os << "g: " << (int)c.g << " ";
+  os << "b: " << (int)c.b << " ";
+  os << "alpha: " << (int)c.a;
+  return os.str();
+}
+
 }
