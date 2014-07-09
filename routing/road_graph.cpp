90090a1ed4a39c7ce6b9b4f47acfbd42d2d4ba5f
Denis Koronchik <denis@mapswithme.com>
2014-07-09 18:27:35 +0200

    Factor out constructor to cpp.

--- routing/road_graph.cpp
+++ routing/road_graph.cpp
@@ -1,10 +1,19 @@
 #include "road_graph.hpp"
 
+#include "../base/assert.hpp"
+
 #include "../std/sstream.hpp"
 
 
 namespace routing
 {
+
+RoadPos::RoadPos(uint32_t featureId, bool bForward, size_t pointId)
+  : m_featureId((featureId << 1) + (bForward ? 1 : 0)), m_pointId(pointId)
+{
+  ASSERT_LESS(featureId, 1U << 31, ());
+  ASSERT_LESS(pointId, 0xFFFFFFFF, ());
+}
 
 string DebugPrint(RoadPos const & r)
 {
--- routing/road_graph.hpp
+++ routing/road_graph.hpp
@@ -2,7 +2,6 @@
 
 #include "../geometry/point2d.hpp"
 
-#include "../base/assert.hpp"
 #include "../base/string_utils.hpp"
 
 #include "../std/vector.hpp"
@@ -16,12 +15,7 @@ class RoadPos
 {
 public:
   RoadPos() : m_featureId(0), m_pointId(0) {}
-  RoadPos(uint32_t featureId, bool bForward, size_t pointId)
-    : m_featureId((featureId << 1) + (bForward ? 1 : 0)), m_pointId(pointId)
-  {
-    ASSERT_LESS(featureId, 1U << 31, ());
-    ASSERT_LESS(pointId, 0xFFFFFFFF, ());
-  }
+  RoadPos(uint32_t featureId, bool bForward, size_t pointId);
 
   uint32_t GetFeatureId() const { return m_featureId >> 1; }
   bool IsForward() const { return (m_featureId & 1) != 0; }
