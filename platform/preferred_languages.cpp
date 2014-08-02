108fb2820081c6d20909a7c6a03e7d88e5bc4866
Alex Zolotarev <alex@mapswithme.com>
2014-08-02 18:39:00 +0300

    We don’t need any specific filters for Chinese, it will be cut off to “zh” anyway

--- platform/preferred_languages.cpp
+++ platform/preferred_languages.cpp
@@ -45,14 +45,6 @@ static const MSLocale gLocales[] = {{0x1,"ar"},{0x2,"bg"},{0x3,"ca"},{0x4,"zh-Ha
 namespace languages
 {
 
-struct ChineseSimplifiedFilter
-{
-  bool operator() (string const & t) const
-  {
-    return ("zh-Hans" == t) || ("zh-rCN" == t);
-  }
-};
-
 class LangFilter
 {
   set<string> & m_known;
@@ -81,9 +73,6 @@ public:
 
 void FilterLanguages(vector<string> & langs)
 {
-  // we do not support simplified chinese by the momenty, so remove it from collection
-  langs.erase(remove_if(langs.begin(), langs.end(), ChineseSimplifiedFilter()), langs.end());
-
   // normalize languages: en-US -> en, ru_RU -> ru etc.
   for_each(langs.begin(), langs.end(), NormalizeFilter());
   {
--- platform/preferred_languages.hpp
+++ platform/preferred_languages.hpp
@@ -10,6 +10,9 @@ namespace languages
 /// @return system language preferences in the form "en|ru|es|zh"
 string PreferredLanguages();
 /// @return language code for current user in the form "en"
+/// Returned languages are normalized to our supported languages in the core, see multilang_utf8_string.cpp
+/// and should not be used with any sub-locales like zh-Hans/zh-Hant.
+/// Some langs like Danish (da) are not supported too in the core
 string CurrentLanguage();
 //@}
 
