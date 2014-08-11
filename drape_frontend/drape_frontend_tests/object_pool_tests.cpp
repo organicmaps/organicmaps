5fc909a4be8c62e5022c688925503abb1aff02c9
Roman Sorokin <sorok-roma@yandex.ru>
2014-08-11 16:24:50 +0300

    new tests for destructor

--- drape_frontend/drape_frontend_tests/object_pool_tests.cpp
+++ drape_frontend/drape_frontend_tests/object_pool_tests.cpp
@@ -2,9 +2,6 @@
 #include "../../testing/testing.hpp"
 
 #include "../../drape/object_pool.hpp"
-#include "../../drape_frontend/read_mwm_task.hpp"
-
-using namespace df;
 
 class vec2
 {
@@ -12,19 +9,18 @@ public:
   class vec2Factory
   {
   public:
-    vec2 * GetNew() const
-    {
-      return new vec2();
-    }
+    vec2 * GetNew() const { return new vec2(); }
   };
 
 public:
-  float x, y;
-  void Reset()
-  {
-    x = y = 0.0f;
-  }
+  float m_x, m_y;
+  static int m_counter;
+
+  vec2() : m_x(0.0f), m_y(0.0f) { m_counter++; }
+  ~vec2() { m_counter--; }
 };
+
+int vec2::m_counter;
 
 UNIT_TEST(ObjectPoolFilling)
 {
@@ -32,14 +28,49 @@ UNIT_TEST(ObjectPoolFilling)
   ObjectPool<vec2, vec2::vec2Factory> pool(1, factory);
   vec2 *pt1, *pt2, *pt3;
   pt1 = pool.Get();
-  pt1->x = 45;
   pool.Return(pt1);
-  ASSERT_EQUAL(pt1->x, 0, ());
   pt2 = pool.Get();
-  ASSERT_EQUAL(pt1, pt2, ());
+  TEST_EQUAL(pt1, pt2, ());
   pt3 = pool.Get();
 
   pool.Return(pt2);
   pool.Return(pt3);
 }
 
+UNIT_TEST(ObjectPoolClearing_1)
+{
+  vec2::m_counter = 0;
+  vec2::vec2Factory factory;
+  ObjectPool<vec2, vec2::vec2Factory> *pool = new ObjectPool<vec2, vec2::vec2Factory>(1, factory);
+  vec2 *pt1, *pt2, *pt3;
+  pt1 = pool->Get();
+  pool->Return(pt1);
+  pt2 = pool->Get();
+  pt3 = pool->Get();
+
+  pool->Return(pt2);
+  pool->Return(pt3);
+
+  delete pool;
+
+  TEST_EQUAL(vec2::m_counter, 0, ());
+}
+
+UNIT_TEST(ObjectPoolClearing_2)
+{
+  vec2::m_counter = 0;
+  vec2::vec2Factory factory;
+  ObjectPool<vec2, vec2::vec2Factory> *pool = new ObjectPool<vec2, vec2::vec2Factory>(100, factory);
+  vec2 *pt1, *pt2, *pt3;
+  pt1 = pool->Get();
+  pool->Return(pt1);
+  pt2 = pool->Get();
+  pt3 = pool->Get();
+
+  pool->Return(pt2);
+  pool->Return(pt3);
+
+  delete pool;
+
+  TEST_EQUAL(vec2::m_counter, 0, ());
+}
