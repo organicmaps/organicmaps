db5f685f01cbb94cf4533df3f5001d4aaad86959
Denis Koronchik <denis@mapswithme.com>
2014-09-17 13:02:32 +0300

    [succinct] Added missing files.

---
+++ 3party/succinct/succinct_config.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifndef SUCCINCT_USE_LIBCXX
+#    define SUCCINCT_USE_LIBCXX 0
+#endif
+
+#ifndef SUCCINCT_USE_INTRINSICS
+#    define SUCCINCT_USE_INTRINSICS 0
+#endif
+
+#ifndef SUCCINCT_USE_POPCNT
+#    define SUCCINCT_USE_POPCNT 0
+#endif
