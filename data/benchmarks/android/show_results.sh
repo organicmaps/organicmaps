32eae3487cb5308ab9aea4346bcdf7bafd32f215
ExMix <rahuba.youri@mapswithme.com>
2014-10-18 10:25:51 +0300

    executable for mac

--- data/benchmarks/android/show_results.sh
+++ data/benchmarks/android/show_results.sh
@@ -1,3 +1,5 @@
-adb pull /mnt/sdcard/MapsWithMe/bench/results.txt ../results.txt
+#!/bin/bash
+
+adb pull /mnt/sdcard/MapsWithMe/results.txt ../results.txt
 cd ..
 python tk_results_viewer.py config.txt results.txt
