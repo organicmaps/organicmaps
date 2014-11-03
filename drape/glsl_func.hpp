7d7a62f84c917496d728cc13f1648421452471b0
ExMix <rahuba.youri@mapswithme.com>
2014-11-03 14:21:50 +0300

    glm wrappers

---
+++ drape/glsl_func.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <glm_config.hpp>
+#include <glm/geometric.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+namespace glsl
+{
+
+using glm::dot;
+using glm::cross;
+using glm::normalize;
+using glm::length;
+using glm::distance;
+
+using glm::translate;
+using glm::rotate;
+using glm::scale;
+
+using glm::value_ptr;
+
+}
---
+++ drape/glsl_types.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <glm_config.hpp>
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+
+#include <glm/mat3x3.hpp>
+#include <glm/mat4x4.hpp>
+
+#include <glm/mat4x2.hpp>
+#include <glm/mat4x3.hpp>
+
+namespace glsl
+{
+
+using glm::vec2;
+using glm::vec3;
+using glm::vec4;
+
+using glm::dvec2;
+using glm::dvec3;
+using glm::dvec4;
+
+using glm::mat3;
+using glm::mat4;
+using glm::mat4x2;
+using glm::mat4x3;
+
+using glm::dmat3;
+using glm::dmat4;
+using glm::dmat4x2;
+using glm::dmat4x3;
+
+typedef vec4   Quad1;
+typedef mat4x2 Quad2;
+typedef mat4x3 Quad3;
+typedef mat4   Quad4;
+
+}
