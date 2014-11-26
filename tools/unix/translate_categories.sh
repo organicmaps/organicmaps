ca48b015a11d1ca37967a320bea146d02109c576
Alex Zolotarev <alex@mapswithme.com>
2014-11-25 15:50:10 -1000

    [unix][tools] Added two new languages to the script

--- tools/unix/translate_categories.sh
+++ tools/unix/translate_categories.sh
@@ -2,4 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-${DIR}/translate.awk {en=en+ru+uk+de+fr+it+es+ko+ja+cs+nl+zh-TW+pl+pt+hu+th+zh-CN+ar+da} "$1"
+${DIR}/translate.awk {en=en+ru+uk+de+fr+it+es+ko+ja+cs+nl+zh-TW+pl+pt+hu+th+zh-CN+ar+da+tr+sv} "$1"
