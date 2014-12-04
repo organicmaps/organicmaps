7e39514ddddf987cb4d60b7ab935282eb2774887
vng <viktor.govako@gmail.com>
2014-12-04 15:35:03 +0300

    [generator_test] Added smoke test for geometry index in mwm.

--- generator/generator_tests/check_mwms.cpp
+++ generator/generator_tests/check_mwms.cpp
@@ -2,7 +2,11 @@
 
 #include "../../map/feature_vec_model.hpp"
 
+#include "../../indexer/interval_index.hpp"
+
 #include "../../platform/platform.hpp"
+
+#include "../../base/logging.hpp"
 
 
 UNIT_TEST(CheckMWM_LoadAll)
@@ -26,4 +30,36 @@ UNIT_TEST(CheckMWM_LoadAll)
       TEST(false, ("Bad mwm file:", s));
     }
   }
+}
+
+UNIT_TEST(CheckMWM_GeomIndex)
+{
+  // Open mwm file from data path.
+  FilesContainerR cont(GetPlatform().GetReader("minsk-pass.mwm"));
+
+  // Initialize index reader section inside mwm.
+  typedef ModelReaderPtr ReaderT;
+  ReaderT reader = cont.GetReader(INDEX_FILE_TAG);
+  ReaderSource<ReaderT> source(reader);
+  VarSerialVectorReader<ReaderT> treesReader(source);
+
+  // Make interval index objects for each scale bucket.
+  vector<unique_ptr<IntervalIndex<ReaderT>>> scale2Index;
+  for (size_t i = 0; i < treesReader.Size(); ++i)
+    scale2Index.emplace_back(new IntervalIndex<ReaderT>(treesReader.SubReader(i)));
+
+  // Pass full coverage as input for test.
+  uint64_t beg = 0;
+  uint64_t end = static_cast<uint64_t>((1ULL << 63) - 1);
+
+  // Count objects for each scale bucket.
+  map<size_t, uint64_t> resCount;
+  for (size_t i = 0; i < scale2Index.size(); ++i)
+    scale2Index[i]->ForEach([i, &resCount](uint32_t)
+    {
+      ++resCount[i];
+    }, beg, end);
+
+  // Print results.
+  LOG(LINFO, (resCount));
 }
