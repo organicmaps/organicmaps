d8415cb119e00105fbeaacf8807f34518fac473b
ExMix <rahuba.youri@mapswithme.com>
2015-01-01 21:30:58 +0300

    review fixes

--- drape/oglcontext.hpp
+++ drape/oglcontext.hpp
@@ -11,7 +11,7 @@ public:
   virtual void makeCurrent() = 0;
   virtual void setDefaultFramebuffer() = 0;
   /// @ param w, h - pixel size of render target (logical size * visual scale)
-  virtual void resize(int w, int h) {}
+  virtual void resize(int /*w*/, int /*h*/) {}
 };
 
 } // namespace dp
