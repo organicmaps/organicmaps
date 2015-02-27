80231c1d2fc162abcf7cf366d50d743f25546990
Yuri Gorshenin <y@maps.me>
2015-02-27 13:38:37 +0300

    Fixes.

--- coding/coding_tests/timsort_test.cpp
+++ coding/coding_tests/timsort_test.cpp
@@ -16,13 +16,15 @@ int intCmp(void const * plhs, void const * prhs)
   return 1;
 }
 
-UNIT_TEST(TimSort_Empty) {
+UNIT_TEST(TimSort_Empty)
+{
   vector<int> buffer;
   timsort(&buffer[0], 0, sizeof(int), intCmp);
 }
 
-UNIT_TEST(Timsort_Simple) {
-  vector<int> buffer = { 5, 1, 0, 7, 9, 10, 1, 3, 4 };
+UNIT_TEST(Timsort_Simple)
+{
+  vector<int> buffer = {5, 1, 0, 7, 9, 10, 1, 3, 4};
 
   vector<int> sorted_buffer(buffer);
   sort(sorted_buffer.begin(), sorted_buffer.end());
