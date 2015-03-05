8e9a47249c21e12e42b026193fef26b99922130c
Alex Zolotarev <alex@maps.me>
2015-03-05 21:27:55 +0300

    [ios] Log basic user flow change between view controllers.

--- iphone/Maps/TableViewController.m
+++ iphone/Maps/TableViewController.m
@@ -7,12 +7,25 @@
 //
 
 #import "TableViewController.h"
+#import "../../3party/Alohalytics/src/alohalytics_objc.h"
 
 @implementation TableViewController
 
 - (BOOL)prefersStatusBarHidden
 {
   return NO;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+  [Alohalytics logEvent:@"$viewWillAppear" withValue:NSStringFromClass([self class])];
+  [super viewWillAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+  [Alohalytics logEvent:@"$viewWillDisappear" withValue:NSStringFromClass([self class])];
+  [super viewWillDisappear:animated];
 }
 
 @end
--- iphone/Maps/ViewController.m
+++ iphone/Maps/ViewController.m
@@ -7,12 +7,25 @@
 //
 
 #import "ViewController.h"
+#import "../../3party/Alohalytics/src/alohalytics_objc.h"
 
 @implementation ViewController
 
 - (BOOL)prefersStatusBarHidden
 {
   return NO;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+  [Alohalytics logEvent:@"$viewWillAppear" withValue:NSStringFromClass([self class])];
+  [super viewWillAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+  [Alohalytics logEvent:@"$viewWillDisappear" withValue:NSStringFromClass([self class])];
+  [super viewWillDisappear:animated];
 }
 
 @end
