770e03274c0bd636831eb5f5429024c6eb2bfc5f
vng <viktor.govako@gmail.com>
2015-03-13 14:42:11 +0300

    [tool] Fixed my mwm generator tool.

--- tools/unix/make_index_of_vng.sh
+++ tools/unix/make_index_of_vng.sh
@@ -16,15 +16,15 @@ then
 fi
 
 # Common part.
-$PV ../../../omim-maps/$2.osm.bz2 | bzip2 -d | $GENERATOR_TOOL --preprocess_xml=true --use_light_nodes=true --intermediate_data_path=$TMPDIR
+$PV ../../../omim-maps/$2.osm.bz2 | bzip2 -d | $GENERATOR_TOOL --preprocess=true --intermediate_data_path=$TMPDIR
 
 # 1. Generate coastlines.
-#$PV ../../../omim-maps/$2.osm.bz2 | bzip2 -d | $GENERATOR_TOOL --make_coasts=true --use_light_nodes=true --intermediate_data_path=$TMPDIR
-#$PV ../../../omim-maps/$2.osm.bz2 | bzip2 -d | $GENERATOR_TOOL --use_light_nodes=true --generate_world=true --split_by_polygons=true --generate_features=true --emit_coasts=true --intermediate_data_path=$TMPDIR
+#$PV ../../../omim-maps/$2.osm.bz2 | bzip2 -d | $GENERATOR_TOOL --make_coasts=true --intermediate_data_path=$TMPDIR
+#$PV ../../../omim-maps/$2.osm.bz2 | bzip2 -d | $GENERATOR_TOOL --generate_world=true --split_by_polygons=true --generate_features=true --emit_coasts=true --intermediate_data_path=$TMPDIR
 
 #$GENERATOR_TOOL --generate_geometry=true --generate_index=true --generate_search_index=true --output=$2
 #$GENERATOR_TOOL --generate_geometry=true --generate_index=true --generate_search_index=true --output=World
 #$GENERATOR_TOOL --generate_geometry=true --generate_index=true --output=WorldCoasts
 
 # 2. Generate country from --output.
-$PV ../../../omim-maps/$2.osm.bz2 | bzip2 -d | $GENERATOR_TOOL --use_light_nodes=true --generate_features=true --generate_geometry=true --generate_index=true --generate_search_index=true --intermediate_data_path=$TMPDIR --output=$2 --address_file_name=$2.addr
+$PV ../../../omim-maps/$2.osm.bz2 | bzip2 -d | $GENERATOR_TOOL  --generate_features=true --generate_geometry=true --generate_index=true --generate_search_index=true --intermediate_data_path=$TMPDIR --output=$2 --address_file_name=$2.addr
