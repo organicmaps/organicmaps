49f839f0b93e5c1be66e12da9c39a28a7255f2f4
Ilya Zverev <zverik@textual.ru>
2015-03-13 10:26:42 +0300

    Remove export_poly_path step from split_planet

--- tools/unix/split_planet_by_polygons.sh
+++ tools/unix/split_planet_by_polygons.sh
@@ -3,23 +3,18 @@
 set -e -u -x
 
 if [[ `hostname` == "Scout" ]]; then
-  POLY_FILES_PATH=~/toolchain/v2/data/poly
+  POLY_FILES_PATH=~/toolchain/v2/data/borders
   PLANET_FILE=~/toolchain/v2/data/planet-latest.o5m
   OUT_DIR=~/toolchain/v2/data/planet-split
-  GENERATOR_TOOL=/media/ssd/osm/omim-build-release/out/release/generator_tool
   OSMCONVERT_TOOL=~/toolchain/v2/git/osmctools/osmconvert
 else
-  POLY_FILES_PATH=~/poly
+  POLY_FILES_PATH=~/omim/data/borders
   PLANET_FILE=~/planet/planet-latest.o5m
   OUT_DIR=~/planet-split
-  GENERATOR_TOOL=~/omim-build-release/out/release/generator_tool
   OSMCONVERT_TOOL=~/osmctools/osmconvert
 fi
 
-mkdir $POLY_FILES_PATH || true
 mkdir $OUT_DIR || true
-
-$GENERATOR_TOOL -export_poly_path $POLY_FILES_PATH
 
 EXT=.poly 
 NUM_INSTANCES=8
