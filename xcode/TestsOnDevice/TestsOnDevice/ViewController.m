1355bc07bfade4e60f1aa49f8d9077d996733b51
Sergey Yershov <yershov@corp.mail.ru>
2015-05-19 15:09:57 +0300

    Build system for Xcode

---
+++ xcode/TestsOnDevice/TestsOnDevice/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestsOnDevice
+//
+//  Created by Sergey Yershov on 23.03.15.
+//  Copyright (c) 2015 Sergey Yershov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ xcode/TestsOnDevice/TestsOnDevice/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestsOnDevice
+//
+//  Created by Sergey Yershov on 23.03.15.
+//  Copyright (c) 2015 Sergey Yershov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
