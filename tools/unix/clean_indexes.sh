b694c963e97f7404a446f4262803ac3a9712c188
Lev Dragunov <l.dragunov@corp.mail.ru>
2015-06-02 17:27:49 +0300

    clean_indexes.sh script fix

--- tools/unix/clean_indexes.sh
+++ tools/unix/clean_indexes.sh
@@ -2,13 +2,13 @@
 
 set -u -x
 BASE_PATH=$(pwd)
-DATA_PATH=$BASE_PATH/../../../data
+DATA_PATH=$BASE_PATH/../../data
 
 COUNTRY_LIST=${COUNTRY_LIST-$(ls -1 $DATA_PATH/*.mwm)}
 
 if [ "$COUNTRY_LIST" ]
 then
   echo "$COUNTRY_LIST" | while read file ; do
-    rm -rf ${file%.mwm}/
+    rm -rf "${file%.mwm}/"
   done
 fi
