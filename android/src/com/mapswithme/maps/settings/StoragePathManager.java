f442492e80154dd9e5e3589c2b5280153dfa5855
Dmitry Yunitsky <yunik@mapswithme.com>
2015-06-22 16:47:54 +0300

    Fixed possible crash in file copying.

--- android/src/com/mapswithme/maps/settings/StoragePathManager.java
+++ android/src/com/mapswithme/maps/settings/StoragePathManager.java
@@ -531,8 +531,10 @@ public class StoragePathManager
       outputChannel.transferFrom(inputChannel, 0, inputChannel.size());
     } finally
     {
-      inputChannel.close();
-      outputChannel.close();
+      if (inputChannel != null)
+        inputChannel.close();
+      if (outputChannel != null)
+        outputChannel.close();
     }
   }
 
