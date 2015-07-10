3cd9cd8c9c7ad894f86645e4daf27efae369bfd4
Dmitry Yunitsky <yunik@mapswithme.com>
2015-07-10 13:14:45 +0300

    [android] Added base Recycler fragment.

---
+++ android/src/com/mapswithme/maps/base/BaseMwmRecyclerFragment.java
@@ -0,0 +1,86 @@
+package com.mapswithme.maps.base;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.NavUtils;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mapswithme.maps.R;
+import com.mapswithme.maps.activity.CustomNavigateUpListener;
+import com.mapswithme.util.UiUtils;
+
+public abstract class BaseMwmRecyclerFragment extends Fragment
+{
+  private Toolbar mToolbar;
+  protected RecyclerView mRecycler;
+
+  @Nullable
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+  {
+    return inflater.inflate(R.layout.fragment_recycler, container, false);
+  }
+
+  @Override
+  public void onViewCreated(View view, Bundle savedInstanceState)
+  {
+    super.onViewCreated(view, savedInstanceState);
+
+    mToolbar = (Toolbar) view.findViewById(R.id.toolbar);
+    if (mToolbar != null)
+    {
+      UiUtils.showHomeUpButton(mToolbar);
+      mToolbar.setNavigationOnClickListener(new View.OnClickListener()
+      {
+        @Override
+        public void onClick(View v)
+        {
+          navigateUpToParent();
+        }
+      });
+    }
+
+    mRecycler = (RecyclerView) view.findViewById(R.id.recycler);
+    if (mRecycler == null)
+      throw new IllegalStateException("RecyclerView not found in layout");
+  }
+
+  public Toolbar getToolbar()
+  {
+    return mToolbar;
+  }
+
+  public RecyclerView getRecyclerView()
+  {
+    return mRecycler;
+  }
+
+  public void onResume()
+  {
+    super.onResume();
+    org.alohalytics.Statistics.logEvent("$onResume", this.getClass().getSimpleName()
+        + ":" + UiUtils.deviceOrientationAsString(getActivity()));
+  }
+
+  @Override
+  public void onPause()
+  {
+    super.onPause();
+    org.alohalytics.Statistics.logEvent("$onPause", this.getClass().getSimpleName());
+  }
+
+  public void navigateUpToParent()
+  {
+    final Activity activity = getActivity();
+    if (activity instanceof CustomNavigateUpListener)
+      ((CustomNavigateUpListener) activity).customOnNavigateUp();
+    else
+      NavUtils.navigateUpFromSameTask(activity);
+  }
+}
