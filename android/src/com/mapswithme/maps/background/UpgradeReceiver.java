6081643ac7cfd5fd50f4a41add0d34d6460b5a3f
Dmitry Yunitsky <yunik@mapswithme.com>
2015-07-30 18:11:24 +0300

    [android] Catch 'app upgraded' intent. (cherry picked from commit 7b8097f)

---
+++ android/src/com/mapswithme/maps/background/UpgradeReceiver.java
@@ -0,0 +1,14 @@
+package com.mapswithme.maps.background;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class UpgradeReceiver extends BroadcastReceiver
+{
+  @Override
+  public void onReceive(Context context, Intent intent)
+  {
+    // TODO our package's updated, do smth
+  }
+}
---
+++ android/src/com/mapswithme/maps/background/UpgradeReceiverCompat.java
@@ -0,0 +1,17 @@
+package com.mapswithme.maps.background;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class UpgradeReceiverCompat extends BroadcastReceiver
+{
+  @Override
+  public void onReceive(Context context, Intent intent)
+  {
+    if (context.getPackageName().equals(intent.getData().getSchemeSpecificPart()))
+    {
+      // TODO our package's updated, do smth
+    }
+  }
+}
