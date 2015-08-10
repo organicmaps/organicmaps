e63c4075222c1e0bf23f17a322502795a375ef18
Vladimir Byko-Ianko <v.bykoianko@corp.mail.ru>
2015-08-10 09:51:02 +0300

    Adding a directory and files for TTS in xcode.

---
+++ iphone/Maps/Classes/CustomViews/NavigationDashboard/Sound/MWMTextToSpeech.cpp
@@ -0,0 +1,9 @@
+//
+//  MWMTextToSpeech.cpp
+//  Maps
+//
+//  Created by Vladimir Byko-Ianko on 10.08.15.
+//  Copyright (c) 2015 MapsWithMe. All rights reserved.
+//
+
+#include "MWMTextToSpeech.h"
---
+++ iphone/Maps/Classes/CustomViews/NavigationDashboard/Sound/MWMTextToSpeech.h
@@ -0,0 +1,9 @@
+//
+//  MWMTextToSpeech.h
+//  Maps
+//
+//  Created by Vladimir Byko-Ianko on 10.08.15.
+//  Copyright (c) 2015 MapsWithMe. All rights reserved.
+//
+
+
