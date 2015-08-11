1b27253169d6a1fa61a61f3b638a0bc9c452440f
Constantin Shalnev <c.shalnev@corp.mail.ru>
2015-08-11 13:22:09 +0300

    Removed unused const NONE

--- tools/kothic/mapcss/Eval.py
+++ tools/kothic/mapcss/Eval.py
@@ -15,8 +15,6 @@
 #   You should have received a copy of the GNU General Public License
 #   along with kothic.  If not, see <http://www.gnu.org/licenses/>.
 
-NONE = ""
-
 
 class Eval():
     def __init__(self, s='eval()'):
