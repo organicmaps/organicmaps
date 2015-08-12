c29638f3052979df018d0d24afe9513a308fe22c
ExMix <rahuba.youri@mapswithme.com>
2015-08-12 21:36:56 +0300

    [drape] restore debug TileNet drawing

--- drape_frontend/engine_context.cpp
+++ drape_frontend/engine_context.cpp
@@ -1,16 +1,7 @@
 #include "drape_frontend/engine_context.hpp"
 
-//#define DRAW_TILE_NET
-
 #include "drape_frontend/message_subclasses.hpp"
 #include "drape/texture_manager.hpp"
-
-#ifdef DRAW_TILE_NET
-#include "drape_frontend/line_shape.hpp"
-#include "drape_frontend/text_shape.hpp"
-
-#include "base/string_utils.hpp"
-#endif
 
 namespace df
 {
@@ -40,41 +31,6 @@ void EngineContext::Flush(list<drape_ptr<MapShape>> && shapes)
 
 void EngineContext::EndReadTile()
 {
-#ifdef DRAW_TILE_NET
-  m2::RectD r = m_tileKey.GetGlobalRect();
-  vector<m2::PointD> path;
-  path.push_back(r.LeftBottom());
-  path.push_back(r.LeftTop());
-  path.push_back(r.RightTop());
-  path.push_back(r.RightBottom());
-  path.push_back(r.LeftBottom());
-
-  m2::SharedSpline spline(path);
-  df::LineViewParams p;
-  p.m_baseGtoPScale = 1.0;
-  p.m_cap = dp::ButtCap;
-  p.m_color = dp::Color::Red();
-  p.m_depth = 20000;
-  p.m_width = 5;
-  p.m_join = dp::RoundJoin;
-
-  InsertShape(make_unique_dp<LineShape>(spline, p, m_texMng));
-
-  df::TextViewParams tp;
-  tp.m_anchor = dp::Center;
-  tp.m_depth = 0;
-  tp.m_primaryText = strings::to_string(m_tileKey.m_x) + " " +
-                     strings::to_string(m_tileKey.m_y) + " " +
-                     strings::to_string(m_tileKey.m_zoomLevel);
-
-  tp.m_primaryTextFont = dp::FontDecl(dp::Color::Red(), 30);
-
-  list<drape_ptr<MapShape>> shapes;
-  shapes.push_back(make_unique_dp<TextShape>(r.Center(), tp));
-
-  Flush(move(shapes));
-#endif
-
   PostMessage(make_unique_dp<TileReadEndMessage>(m_tileKey));
 }
 
--- drape_frontend/rule_drawer.cpp
+++ drape_frontend/rule_drawer.cpp
@@ -10,6 +10,15 @@
 #include "base/assert.hpp"
 #include "std/bind.hpp"
 
+//#define DRAW_TILE_NET
+
+#ifdef DRAW_TILE_NET
+#include "drape_frontend/line_shape.hpp"
+#include "drape_frontend/text_shape.hpp"
+
+#include "base/string_utils.hpp"
+#endif
+
 namespace df
 {
 
@@ -86,6 +95,39 @@ void RuleDrawer::operator()(FeatureType const & f)
     s.ForEachRule(bind(&ApplyPointFeature::ProcessRule, &apply, _1));
     apply.Finish();
   }
+
+#ifdef DRAW_TILE_NET
+  TileKey key = m_context->GetTileKey();
+  m2::RectD r = key.GetGlobalRect();
+  vector<m2::PointD> path;
+  path.push_back(r.LeftBottom());
+  path.push_back(r.LeftTop());
+  path.push_back(r.RightTop());
+  path.push_back(r.RightBottom());
+  path.push_back(r.LeftBottom());
+
+  m2::SharedSpline spline(path);
+  df::LineViewParams p;
+  p.m_baseGtoPScale = 1.0;
+  p.m_cap = dp::ButtCap;
+  p.m_color = dp::Color::Red();
+  p.m_depth = 20000;
+  p.m_width = 5;
+  p.m_join = dp::RoundJoin;
+
+  insertShape(make_unique_dp<LineShape>(spline, p));
+
+  df::TextViewParams tp;
+  tp.m_anchor = dp::Center;
+  tp.m_depth = 0;
+  tp.m_primaryText = strings::to_string(key.m_x) + " " +
+                     strings::to_string(key.m_y) + " " +
+                     strings::to_string(key.m_zoomLevel);
+
+  tp.m_primaryTextFont = dp::FontDecl(dp::Color::Red(), 30);
+
+  insertShape(make_unique_dp<TextShape>(r.Center(), tp));
+#endif
 
   for (auto & shape : m_mapShapes)
     shape->Prepare(m_context->GetTextureManager());
