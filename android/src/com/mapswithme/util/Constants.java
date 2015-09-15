ad07b0005d288f778600d0038a007895ca5645bf
Alex Zolotarev <alex@maps.me>
2015-09-15 16:51:36 -0700

    [android] Removed unused constants.

--- android/src/com/mapswithme/util/Constants.java
+++ android/src/com/mapswithme/util/Constants.java
@@ -3,7 +3,6 @@ package com.mapswithme.util;
 public final class Constants
 {
   public static final String STORAGE_PATH = "/Android/data/%s/%s/";
-  public static final String PACKAGE_STORAGES_PATH = "/Android/data/";
   public static final String OBB_PATH = "/Android/obb/%s/";
 
   public static final int KB = 1024;
@@ -17,18 +16,14 @@ public final class Constants
     public static final String MAIL_BODY = "&body=";
     public static final String HTTP_GE0_PREFIX = "http://ge0.me/";
 
-    public static final String PLAY_MARKET_APP_PREFIX = "market://details?id=";
     public static final String PLAY_MARKET_HTTPS_APP_PREFIX = "https://play.google.com/store/apps/details?id=";
-    public static final String GEOLOCATION_SERVER_MAPSME = "http://geolocation.server/";
 
     public static final String FB_MAPSME_COMMUNITY_HTTP = "http://www.facebook.com/MapsWithMe";
     // Profile id is taken from http://graph.facebook.com/MapsWithMe
     public static final String FB_MAPSME_COMMUNITY_NATIVE = "fb://profile/111923085594432";
     public static final String TWITTER_MAPSME_HTTP = "https://twitter.com/MAPS_ME";
-    public static final String TWITTER_MAPSME_NATIVE = "twitter://user?user_id=MAPS_ME";
 
     public static final String MAIL_MAPSME_INFO = "android@maps.me";
-    public static final String MAIL_MAPSME_BUGS = "android@maps.me";
     public static final String MAIL_MAPSME_SUBSCRIBE = "subscribe@maps.me";
     public static final String MAIL_MAPSME_RATING = "rating@maps.me";
 
