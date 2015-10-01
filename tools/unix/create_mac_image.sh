2081e75023f62a4a3175f7738c8d9eea990496d0
Ilya Zverev <zverik@textual.ru>
2015-10-01 14:49:29 +0300

    [tools] Actually, this doesn't work either

--- tools/unix/create_mac_image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -u -x
-TARGET=${1:-release}
-(
-  cd ../../../omim-build-$TARGET/out/$TARGET
-  umount /Volumes/MapsWithMe
-  rm MapsWithMe.dmg
-  macdeployqt MapsWithMe.app -dmg
-)
