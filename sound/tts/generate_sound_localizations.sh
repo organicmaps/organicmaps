704dcd0c9dccac25d4317d48ce462ceda120570a
Vladimir Byko-Ianko <v.bykoianko@corp.mail.ru>
2015-10-14 17:59:46 +0300

    Generating languages.hpp with TTS language supported by TTS while creating sounds.txt

--- sound/tts/generate_sound_localizations.sh
+++ sound/tts/generate_sound_localizations.sh
@@ -17,7 +17,7 @@ mkdir $SOUND_TMP_DIR
 python $MY_PATH/sound_csv_to_sound_txt.py $MY_PATH/sound.csv $MY_PATH/sound.txt $MY_PATH/languages.txt
 
 mkdir $SOUND_TMP_DIR/json
-python $MY_PATH/languages_dir.py $MY_PATH/languages.txt $SOUND_TMP_DIR/json/
+python $MY_PATH/languages.py $MY_PATH/languages.txt $MY_PATH/languages.hpp $SOUND_TMP_DIR/json/
 $MY_PATH/../../tools/twine/twine --format jquery generate-all-string-files $MY_PATH/sound.txt $SOUND_TMP_DIR/json
 
 readonly SOUND_OUTPUT_DIR=$MY_PATH/../../data/sound-strings/
