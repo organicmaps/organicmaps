4bc843b632142cc5ea52492cf0829fe010316bfc
Vladimir Byko-Ianko <v.bykoianko@corp.mail.ru>
2015-10-15 16:30:31 +0300

    Moving TTS sound scripts from sound/tts to tools/sound.

--- tools/sound/generate_sound_localizations.sh
+++ tools/sound/generate_sound_localizations.sh
@@ -9,16 +9,16 @@ MY_PATH="`dirname \"$0\"`"              # relative
 MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
 
 readonly SOUND_TMP_DIR=$MY_PATH/../../../sound-tmp
+readonly SOUND_DATA_DIR=$MY_PATH/../../sound/tts
 
-rm -f $MY_PATH/sound_strings.zip
 rm -rf $SOUND_TMP_DIR
 mkdir $SOUND_TMP_DIR
 
-python $MY_PATH/sound_csv_to_sound_txt.py $MY_PATH/sound.csv $MY_PATH/sound.txt $MY_PATH/languages.txt
+python $MY_PATH/sound_csv_to_sound_txt.py $SOUND_DATA_DIR/sound.csv $SOUND_DATA_DIR/sound.txt $SOUND_DATA_DIR/languages.txt
 
 mkdir $SOUND_TMP_DIR/json
-python $MY_PATH/languages.py $MY_PATH/languages.txt $MY_PATH/languages.hpp $SOUND_TMP_DIR/json/
-$MY_PATH/../../tools/twine/twine --format jquery generate-all-string-files $MY_PATH/sound.txt $SOUND_TMP_DIR/json
+python $MY_PATH/languages.py $SOUND_DATA_DIR/languages.txt $SOUND_DATA_DIR/languages.hpp $SOUND_TMP_DIR/json/
+$MY_PATH/../twine/twine --format jquery generate-all-string-files $SOUND_DATA_DIR/sound.txt $SOUND_TMP_DIR/json
 
 readonly SOUND_OUTPUT_DIR=$MY_PATH/../../data/sound-strings/
 if [ ! -d $SOUND_OUTPUT_DIR ]; then
