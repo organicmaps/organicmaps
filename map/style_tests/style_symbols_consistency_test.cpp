198dc7e284bdbb0a168df1bf3c4651ba4fb71c2d
Constantin Shalnev <c.shalnev@corp.mail.ru>
2015-10-15 18:36:03 +0300

    Fixed notes

--- map/style_tests/style_symbols_consistency_test.cpp
+++ map/style_tests/style_symbols_consistency_test.cpp
@@ -7,14 +7,14 @@
 
 #include "graphics/defines.hpp"
 
-#include "coding/reader.hpp"
-#include "coding/parse_xml.hpp"
-
 #include "base/logging.hpp"
 
-#include "std/array.hpp"
+#include "coding/parse_xml.hpp"
+#include "coding/reader.hpp"
+
+#include "std/algorithm.hpp"
+#include "std/set.hpp"
 #include "std/string.hpp"
-#include "std/unordered_set.hpp"
 
 namespace
 {
@@ -22,7 +22,7 @@ namespace
 class SdfParsingDispatcher
 {
 public:
-  SdfParsingDispatcher(unordered_set<string> & symbols)
+  SdfParsingDispatcher(set<string> & symbols)
       : m_symbols(symbols)
   {}
 
@@ -36,12 +36,12 @@ public:
   }
 
 private:
-  unordered_set<string> & m_symbols;
+  set<string> & m_symbols;
 };
 
-unordered_set<string> GetSymbolsSetFromDrawingRule()
+set<string> GetSymbolsSetFromDrawingRule()
 {
-  unordered_set<string> symbols;
+  set<string> symbols;
   drule::rules().ForEachRule([&symbols](int, int, int, drule::BaseRule const * rule)
   {
     SymbolRuleProto const * const symbol = rule->GetSymbol();
@@ -51,23 +51,14 @@ unordered_set<string> GetSymbolsSetFromDrawingRule()
   return symbols;
 }
 
-unordered_set<string> GetSymbolsSetFromResourcesFile(string const & density)
+set<string> GetSymbolsSetFromResourcesFile(string const & density)
 {
-  unordered_set<string> symbols;
+  set<string> symbols;
   SdfParsingDispatcher dispatcher(symbols);
   ReaderPtr<Reader> reader = GetStyleReader().GetResourceReader("symbols.sdf", density);
   ReaderSource<ReaderPtr<Reader> > source(reader);
   ParseXML(source, dispatcher);
   return symbols;
-}
-
-// returns s1 - s2
-unordered_set<string> Subtract(unordered_set<string> const & s1, unordered_set<string> const & s2)
-{
-  unordered_set<string> res(s1);
-  for (auto const & s : s2)
-    res.erase(s);
-  return res;
 }
 
 }  // namespace
@@ -85,22 +76,23 @@ UNIT_TEST(Test_SymbolsConsistency)
     GetStyleReader().SetCurrentStyle(mapStyle);
     classificator::Load();
 
-    unordered_set<string> const drawingRuleSymbols = GetSymbolsSetFromDrawingRule();
+    set<string> const drawingRuleSymbols = GetSymbolsSetFromDrawingRule();
 
     for (size_t d = 0; d < graphics::EDensityCount; ++d)
     {
       string const density = graphics::convert(static_cast<graphics::EDensity>(d));
 
-      unordered_set<string> const resourceStyles = GetSymbolsSetFromResourcesFile(density);
+      set<string> const resourceStyles = GetSymbolsSetFromResourcesFile(density);
 
-      unordered_set<string> const s = Subtract(drawingRuleSymbols, resourceStyles);
-
-      vector<string> const missed(s.begin(), s.end());
+      vector<string> missed;
+      set_difference(drawingRuleSymbols.begin(), drawingRuleSymbols.end(),
+                     resourceStyles.begin(), resourceStyles.end(),
+                     back_inserter(missed));
 
       if (!missed.empty())
       {
         // We are interested in all set of bugs, therefore we do not stop test here but
-        // continue it just keep in res that test failed.
+        // continue it just keeping in res that test failed.
         LOG(LINFO, ("Symbols mismatch: style", mapStyle, ", density", density, ", missed", missed));
         res = false;
       }
