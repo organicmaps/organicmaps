8d8e24905d2feffc90fc99d17fcb7d4507be5620
Alex Zolotarev <alex@maps.me>
2015-10-16 23:17:29 -0700

    Boost compilation fixes. These patches are better than those we used previously.

--- 3party/boost/boost/core/demangle.hpp
+++ 3party/boost/boost/core/demangle.hpp
@@ -31,6 +31,14 @@
 # include <cstddef>
 #endif
 
+// Android compilation error workaround: gabi++ does not define __cxa_demangle()
+// 3party/boost/boost/core/demangle.hpp:75:17: error: no member named '__cxa_demangle' in namespace '__cxxabiv1'
+//    return abi::__cxa_demangle( name, NULL, &size, &status );
+//           ~~~~~^
+#ifdef __GABIXX_CXXABI_H__
+# undef BOOST_CORE_HAS_CXXABI_H
+#endif
+
 namespace boost
 {
 
@@ -116,6 +124,9 @@ inline std::string demangle( char const * name )
 
 } // namespace boost
 
-#undef BOOST_CORE_HAS_CXXABI_H
+// Second part of work-around for Android, see above.
+#ifdef BOOST_CORE_HAS_CXXABI_H
+# undef BOOST_CORE_HAS_CXXABI_H
+#endif
 
 #endif // #ifndef BOOST_CORE_DEMANGLE_HPP_INCLUDED
