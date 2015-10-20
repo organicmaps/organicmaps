mapsme/omim

[android] refactor: Settings rewritten using native PreferenceFragment`s. refactor: Config reworked.

android/src/com/mapswithme/maps/settings/MiscPrefsFragment.java
@@ -1,0 +1,37 @@
+package com.mapswithme.maps.settings;
+
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.mapswithme.maps.MwmApplication;
+import com.mapswithme.maps.R;
+import com.mapswithme.util.Config;
+import com.mapswithme.util.statistics.Statistics;
+
+public class MiscPrefsFragment extends PreferenceFragment
+{
+  @Override
+  public void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    addPreferencesFromResource(R.xml.prefs_misc);
+
+    Preference pref = findPreference(getString(R.string.pref_send_statistics));
+    ((CheckBoxPreference)pref).setChecked(Config.isStatisticsEnabled());
+    pref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener()
+    {
+      @Override
+      public boolean onPreferenceChange(Preference preference, Object newValue)
+      {
+        Statistics.INSTANCE.setStatEnabled((Boolean) newValue);
+        return true;
+      }
+    });
+
+    if (GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MwmApplication.get()) != ConnectionResult.SUCCESS)
+      getPreferenceScreen().removePreference(findPreference(getString(R.string.pref_play_services)));
+  }
+}

android/src/com/mapswithme/util/statistics/AlohaHelper.java
@@ -19,17 +19,20 @@ public class AlohaHelper
     org.alohalytics.Statistics.logEvent(ALOHA_EXCEPTION, new String[] {e.getClass().getSimpleName(), e.getMessage()});
   }
 
-  public static final String SETTINGS_CONTACT_US = "contactUs";
-  public static final String SETTINGS_MAIL_SUBSCRIBE = "subscribeToNews";
-  public static final String SETTINGS_REPORT_BUG = "reportABug";
-  public static final String SETTINGS_RATE = "rate";
-  public static final String SETTINGS_FB = "likeOnFb";
-  public static final String SETTINGS_TWITTER = "followOnTwitter";
-  public static final String SETTINGS_HELP = "help";
-  public static final String SETTINGS_ABOUT = "about";
-  public static final String SETTINGS_COPYRIGHT = "copyright";
-  public static final String SETTINGS_COMMUNITY = "community";
-  public static final String SETTINGS_CHANGE_UNITS = "settingsMiles";
+  public static class Settings
+  {
+    public static final String CONTACT_US = "contactUs";
+    public static final String MAIL_SUBSCRIBE = "subscribeToNews";
+    public static final String REPORT_BUG = "reportABug";
+    public static final String RATE = "rate";
+    public static final String FACEBOOK = "likeOnFb";
+    public static final String TWITTER = "followOnTwitter";
+    public static final String HELP = "help";
+    public static final String ABOUT = "about";
+    public static final String COPYRIGHT = "copyright";
+    public static final String CHANGE_UNITS = "settingsMiles";
+  }
+
   // for aloha stats
   public static final String ALOHA_CLICK = "$onClick";
   public static final String ALOHA_LONG_CLICK = "$onLongClick";

organicmaps/organicmaps

android/src/com/mapswithme/maps/settings/MiscPrefsFragment.java
@@ -1,0 +1,37 @@
+package com.mapswithme.maps.settings;
+
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.SwitchPreference;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.mapswithme.maps.MwmApplication;
+import com.mapswithme.maps.R;
+import com.mapswithme.util.Config;
+import com.mapswithme.util.statistics.Statistics;
+
+public class MiscPrefsFragment extends PreferenceFragment
+{
+  @Override
+  public void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    addPreferencesFromResource(R.xml.prefs_misc);
+
+    Preference pref = findPreference(getString(R.string.pref_send_statistics));
+    ((SwitchPreference)pref).setChecked(Config.isStatisticsEnabled());
+    pref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener()
+    {
+      @Override
+      public boolean onPreferenceChange(Preference preference, Object newValue)
+      {
+        Statistics.INSTANCE.setStatEnabled((Boolean) newValue);
+        return true;
+      }
+    });
+
+    if (GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MwmApplication.get()) != ConnectionResult.SUCCESS)
+      getPreferenceScreen().removePreference(findPreference(getString(R.string.pref_play_services)));
+  }
+}

android/src/com/mapswithme/util/statistics/AlohaHelper.java
@@ -19,17 +19,22 @@ public class AlohaHelper
     org.alohalytics.Statistics.logEvent(ALOHA_EXCEPTION, new String[] {e.getClass().getSimpleName(), e.getMessage()});
   }
 
-  public static final String SETTINGS_CONTACT_US = "contactUs";
-  public static final String SETTINGS_MAIL_SUBSCRIBE = "subscribeToNews";
-  public static final String SETTINGS_REPORT_BUG = "reportABug";
-  public static final String SETTINGS_RATE = "rate";
-  public static final String SETTINGS_FB = "likeOnFb";
-  public static final String SETTINGS_TWITTER = "followOnTwitter";
-  public static final String SETTINGS_HELP = "help";
-  public static final String SETTINGS_ABOUT = "about";
-  public static final String SETTINGS_COPYRIGHT = "copyright";
-  public static final String SETTINGS_COMMUNITY = "community";
-  public static final String SETTINGS_CHANGE_UNITS = "settingsMiles";
+  public static class Settings
+  {
+    public static final String WEB_SITE = "webSite";
+    public static final String WEB_BLOG = "webBlog";
+    public static final String FEEDBACK_GENERAL = "generalFeedback";
+    public static final String MAIL_SUBSCRIBE = "subscribeToNews";
+    public static final String REPORT_BUG = "reportABug";
+    public static final String RATE = "rate";
+    public static final String FACEBOOK = "likeOnFb";
+    public static final String TWITTER = "followOnTwitter";
+    public static final String HELP = "help";
+    public static final String ABOUT = "about";
+    public static final String COPYRIGHT = "copyright";
+    public static final String CHANGE_UNITS = "settingsMiles";
+  }
+
   // for aloha stats
   public static final String ALOHA_CLICK = "$onClick";
   public static final String ALOHA_LONG_CLICK = "$onLongClick";