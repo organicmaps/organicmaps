4ba64f36cd93aa68846cbd7f6dfa289001ee0304
Ilya Grechuhin <i.grechuhin@gmail.com>
2015-10-22 15:13:42 +0300

    [ios] Fixed bookmark category editor.

--- iphone/Maps/Bookmarks/AddSetVC.mm
+++ iphone/Maps/Bookmarks/AddSetVC.mm
@@ -1,10 +1,19 @@
 
 #import "AddSetVC.h"
 #import "UIViewController+Navigation.h"
+#import "AddSetTableViewCell.h"
 
 #include "Framework.h"
 
 #define TEXT_FIELD_TAG 666
+
+static NSString * const kAddSetCellTableViewCell = @"AddSetTableViewCell";
+
+@interface AddSetVC () <AddSetTableViewCellProtocol>
+
+@property (nonatomic) AddSetTableViewCell * cell;
+
+@end
 
 @implementation AddSetVC
 
@@ -20,47 +29,26 @@
   self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemSave target:self action:@selector(onSaveClicked)];
   [(UIViewController *)self showBackButton];
   self.title = L(@"add_new_set");
-}
-
-- (void)viewWillAppear:(BOOL)animated
-{
-  [super viewWillAppear:animated];
-  self.navigationController.navigationBar.hidden = NO;
-}
-
-- (void)onCancelClicked
-{
-  [self.navigationController popViewControllerAnimated:YES];
+  [self.tableView registerNib:[UINib nibWithNibName:kAddSetCellTableViewCell bundle:nil]
+       forCellReuseIdentifier:kAddSetCellTableViewCell];
 }
 
 - (void)onSaveClicked
 {
-  UITextField * textField = (UITextField *)[[self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]] viewWithTag:TEXT_FIELD_TAG];
-  NSString * text = textField.text;
-  if (text.length)
-  {
-    [self.delegate addSetVC:self didAddSetWithIndex:static_cast<int>(GetFramework().AddCategory([text UTF8String]))];
-    [self.navigationController popViewControllerAnimated:YES];
-  }
+  [self onDone:self.cell.textField.text];
 }
 
-- (void)viewDidAppear:(BOOL)animated
+- (void)onDone:(NSString *)text
 {
-  // Set focus to editor
-  UITextField * textField = (UITextField *)[[self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]] viewWithTag:TEXT_FIELD_TAG];
-  [textField becomeFirstResponder];
-
-  [super viewDidAppear:animated];
+  if (text.length == 0)
+    return;
+  [self.delegate addSetVC:self didAddSetWithIndex:static_cast<int>(GetFramework().AddCategory([text UTF8String]))];
+  [self.navigationController popViewControllerAnimated:YES];
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
   return YES;
-}
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-  return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
@@ -70,42 +58,14 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-  UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:@"EditSetNameCell"];
-  if (!cell)
-  {
-    cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"EditSetNameCell"];
-    cell.textLabel.text = @"Temporary Name";
-    [cell layoutIfNeeded];
-    CGRect rect = cell.textLabel.frame;
-    rect.size.width = cell.contentView.bounds.size.width - cell.textLabel.frame.origin.x;
-    rect.origin.x = cell.textLabel.frame.origin.x;
-    UITextField * f = [[UITextField alloc] initWithFrame:rect];
-    f.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    f.returnKeyType = UIReturnKeyDone;
-    f.clearButtonMode = UITextFieldViewModeWhileEditing;
-    f.autocorrectionType = UITextAutocorrectionTypeNo;
-    f.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
-    f.delegate = self;
-    f.textColor = cell.textLabel.textColor;
-    f.placeholder = L(@"bookmark_set_name");
-    f.autocapitalizationType = UITextAutocapitalizationTypeWords;
-    f.font = [cell.textLabel.font fontWithSize:[cell.textLabel.font pointSize]];
-    f.tag = TEXT_FIELD_TAG;
-    [cell.contentView addSubview:f];
-    cell.selectionStyle = UITableViewCellSelectionStyleNone;
-    cell.textLabel.text =  @"";
-  }
-  return cell;
+  self.cell = (AddSetTableViewCell *)[tableView dequeueReusableCellWithIdentifier:kAddSetCellTableViewCell];
+  self.cell.delegate = self;
+  return self.cell;
 }
 
-- (BOOL)textFieldShouldReturn:(UITextField *)textField
+- (void)tableView:(UITableView *)tableView willDisplayCell:(AddSetTableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
 {
-  if ([textField.text length] == 0)
-    return YES;
-  
-  [textField resignFirstResponder];
-  [self onSaveClicked];
-  return NO;
+  [cell.textField becomeFirstResponder];
 }
 
 @end
