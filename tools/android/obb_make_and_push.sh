861af8c90e493829280a43a587c95d2f86615e3e
Dmitry Yunitsky <yunik@mapswithme.com>
2015-11-11 15:14:41 +0300

    [android] Removed obsolete bash scripts for obb creation.

--- tools/android/obb_make_and_push.sh
+++
@@ -1,8 +0,0 @@
-./obb_make.sh
-
-adb shell "mkdir /mnt/sdcard/Android/obb/com.mapswithme.maps.pro"
-adb push fonts.obb /mnt/sdcard/Android/obb/com.mapswithme.maps.pro/fonts.obb
-adb push world.obb /mnt/sdcard/Android/obb/com.mapswithme.maps.pro/world.obb
-
-rm fonts.obb
-rm world.obb
