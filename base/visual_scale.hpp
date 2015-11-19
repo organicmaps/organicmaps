92185dc598ab68418c05bc6564b90f738fc74596
Vladimir Byko-Ianko <v.bykoianko@corp.mail.ru>
2015-11-19 16:23:02 +0300

    Corrections after colleagues comments.

--- base/visual_scale.hpp
+++ base/visual_scale.hpp
@@ -2,10 +2,8 @@
 
 inline double VisualScale(double exactDensityDPI)
 {
-  double const mdpiDensityDPI = 160.;
+  double constexpr kMdpiDensityDPI = 160.;
   // For some old devices (for example iPad 2) the density could be less than 160 DPI.
   // Returns one in that case to keep readable text on the map.
-  if (exactDensityDPI <= mdpiDensityDPI)
-    return 1.;
-  return exactDensityDPI / mdpiDensityDPI;
+  return max(1., exactDensityDPI / kMdpiDensityDPI);
 }
