912c9cc686139d2c936be2c830dba331dc5ae0b0
Vladimir Byko-Ianko <v.bykoianko@corp.mail.ru>
2015-11-26 17:39:25 +0300

    Remove visual scale correction for tablets.

--- graphics/defines.cpp
+++ graphics/defines.cpp
@@ -82,10 +82,6 @@ namespace graphics
   double visualScaleExact(int exactDensityDPI)
   {
     double const mdpiDensityDPI = 160.;
-    double const tabletFactor = 1.28;
-    // In case of tablets and iPads increased DPI is used to make visual scale bigger.
-    if (GetPlatform().IsTablet())
-      exactDensityDPI *= tabletFactor;
 
     // For some old devices (for example iPad 2) the density could be less than 160 DPI.
     // Returns one in that case to keep readable text on the map.
