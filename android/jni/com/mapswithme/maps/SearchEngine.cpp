mapsme/omim

Rebase fixes after migration to github

android/jni/com/mapswithme/maps/SearchEngine.cpp
@@ -124,12 +124,7 @@ void OnResults(Results const & results, long long timestamp, bool interactive, b
   }
 
   if (interactive)
-  {
-    android::Platform::RunOnGuiThreadImpl([results]()
-    {
-      g_framework->NativeFramework()->UpdateSearchResults(results);
-    });
-  }
+    g_framework->NativeFramework()->UpdateSearchResults(results);
 
   jobjectArray const & jResults = BuildJavaResults(results, hasPosition, lat, lon);
   env->CallVoidMethod(g_javaListener, g_updateResultsId, jResults, static_cast<jlong>(timestamp));
@@ -198,19 +193,13 @@ extern "C"
     lock_guard<mutex> guard(g_resultsMutex);
     g_framework->DontLoadState();
     Result const & result = g_results.GetResult(index);
-    android::Platform::RunOnGuiThreadImpl([&result]()
-    {
-      g_framework->NativeFramework()->ShowSearchResult(result);
-    });
+    g_framework->NativeFramework()->ShowSearchResult(result);
   }
 
   JNIEXPORT void JNICALL
   Java_com_mapswithme_maps_search_SearchEngine_nativeShowAllResults(JNIEnv * env, jclass clazz)
   {
     g_framework->DontLoadState();
-    android::Platform::RunOnGuiThreadImpl([]()
-    {
-      g_framework->NativeFramework()->ShowAllSearchResults();
-    });
+    g_framework->NativeFramework()->ShowAllSearchResults();
   }
 } // extern "C"

organicmaps/organicmaps

android/jni/com/mapswithme/maps/SearchEngine.cpp
@@ -204,10 +204,7 @@ extern "C"
     g_framework->DontLoadState();
 
     Result const & result = g_results.GetResult(index);
-    android::Platform::RunOnGuiThreadImpl([result]()
-    {
-      g_framework->NativeFramework()->ShowSearchResult(result);
-    });
+    g_framework->NativeFramework()->ShowSearchResult(result);
   }
 
   JNIEXPORT void JNICALL
@@ -215,11 +212,7 @@ extern "C"
   {
     lock_guard<mutex> guard(g_resultsMutex);
     g_framework->DontLoadState();
-
-    android::Platform::RunOnGuiThreadImpl([results=g_results]()
-    {
-      g_framework->NativeFramework()->ShowAllSearchResults(results);
-    });
+    g_framework->NativeFramework()->ShowAllSearchResults(g_results);
   }
 
   JNIEXPORT void JNICALL