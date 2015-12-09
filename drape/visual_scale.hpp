5bcec020a3049c19a9a812ae0e9e5232cc58230d
Vladimir Byko-Ianko <v.bykoianko@corp.mail.ru>
2015-12-09 17:54:13 +0300

    Make fonts bigger for tablets.

--- drape/visual_scale.hpp
+++ drape/visual_scale.hpp
@@ -5,6 +5,11 @@ namespace dp
 inline double VisualScale(double exactDensityDPI) noexcept
 {
   double constexpr kMdpiDensityDPI = 160.;
+  double const tabletFactor = 1.2;
+  // In case of tablets and iPads increased DPI is used to make visual scale bigger.
+  if (GetPlatform().IsTablet())
+    exactDensityDPI *= tabletFactor;
+
   // For some old devices (for example iPad 2) the density could be less than 160 DPI.
   // Returns one in that case to keep readable text on the map.
   return max(1., exactDensityDPI / kMdpiDensityDPI);
