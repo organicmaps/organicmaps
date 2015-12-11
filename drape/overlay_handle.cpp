f9e202db4592ea42eda44ce96a6f86f8fb4377c6
Lev Dragunov <l.dragunov@corp.mail.ru>
2015-12-11 23:28:04 +0300

    Drape overaly depth normalization fix.

--- drape/overlay_handle.cpp
+++ drape/overlay_handle.cpp
@@ -248,7 +248,7 @@ uint64_t CalculateOverlayPriority(int minZoomLevel, uint8_t rank, float depth)
 
   float const kMinDepth = -100000.0f;
   float const kMaxDepth = 100000.0f;
-  float const d = my::clamp(depth, kMinDepth, kMaxDepth) + kMaxDepth;
+  float const d = my::clamp(depth, kMinDepth, kMaxDepth) - kMinDepth;
   uint32_t const priority = static_cast<uint32_t>(d);
 
   return (static_cast<uint64_t>(minZoom) << 56) |
