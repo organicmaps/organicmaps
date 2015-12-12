88ebb1d09a1f8a0efc93732f30060f0c5afb5b81
Илья Гречухин <i.grechuhin@gmail.com>
2015-12-12 21:07:29 +0300

    [ios] Updated colors grid.

--- iphone/Maps/UIColor+MapsMeColor.h
+++ iphone/Maps/UIColor+MapsMeColor.h
@@ -11,6 +11,7 @@
 + (UIColor *)orange;
 + (UIColor *)linkBlue;
 + (UIColor *)linkBlueDark;
++ (UIColor *)linkBlueDisabled;
 + (UIColor *)blackPrimaryText;
 + (UIColor *)blackSecondaryText;
 + (UIColor *)blackStatusBarBackground;
