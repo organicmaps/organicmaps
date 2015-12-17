228b0c70c3165c01966a6dcb5b32377320949c76
Yuri Gorshenin <y@maps.me>
2015-12-17 16:40:32 +0300

    [search] Enable SearchQueryV2 by default.

--- search/search_query_factory.hpp
+++ search/search_query_factory.hpp
@@ -1,11 +1,7 @@
 #pragma once
 
-#include "search/search_query.hpp"
 #include "search/suggest.hpp"
-
-#if defined(USE_SEARCH_QUERY_V2)
 #include "search/v2/search_query_v2.hpp"
-#endif  // defined(USE_SEARCH_QUERY_V2)
 
 #include "std/unique_ptr.hpp"
 
@@ -25,11 +21,7 @@ public:
                                              vector<Suggest> const & suggests,
                                              storage::CountryInfoGetter const & infoGetter)
   {
-#if defined(USE_SEARCH_QUERY_V2)
     return make_unique<v2::SearchQueryV2>(index, categories, suggests, infoGetter);
-#else
-    return make_unique<Query>(index, categories, suggests, infoGetter);
-#endif  // defined(USE_SEARCH_QUERY_V2)
   }
 };
 }  // namespace search
