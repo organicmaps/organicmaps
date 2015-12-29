mapsme/omim

Select POI in 3d.

map/framework.cpp
@@ -1427,7 +1427,7 @@ m2::PointD Framework::GetVisiblePOI(FeatureID id, search::AddressInfo & info, fe
 
   GetAddressInfo(ft, center, info);
 
-  return GtoP(center);
+  return m_currentMovelView.isPerspective() ? GtoP3d(center) : GtoP(center);
 }
 
 namespace
@@ -1593,7 +1593,7 @@ void Framework::ResetLastTapEvent()
 
 UserMark const * Framework::OnTapEventImpl(m2::PointD pxPoint, bool isLong, bool isMyPosition, FeatureID feature)
 {
-  pxPoint = m_currentMovelView.P3dToP(pxPoint);
+  m2::PointD const pxPoint2d = m_currentMovelView.P3dToP(pxPoint);
 
   if (isMyPosition)
   {
@@ -1609,13 +1609,13 @@ UserMark const * Framework::OnTapEventImpl(m2::PointD pxPoint, bool isLong, bool
 
   m2::AnyRectD rect;
   uint32_t const touchRadius = vp.GetTouchRectRadius();
-  m_currentMovelView.GetTouchRect(pxPoint, touchRadius, rect);
+  m_currentMovelView.GetTouchRect(pxPoint2d, touchRadius, rect);
 
   m2::AnyRectD bmSearchRect;
   double const bmAddition = BM_TOUCH_PIXEL_INCREASE * vp.GetVisualScale();
   double const pxWidth  =  touchRadius;
   double const pxHeight = touchRadius + bmAddition;
-  m_currentMovelView.GetTouchRect(pxPoint + m2::PointD(0, bmAddition),
+  m_currentMovelView.GetTouchRect(pxPoint2d + m2::PointD(0, bmAddition),
                                   pxWidth, pxHeight, bmSearchRect);
   UserMark const * mark = m_bmManager.FindNearestUserMark(
         [&rect, &bmSearchRect](UserMarkType type) -> m2::AnyRectD const &
@@ -1638,7 +1638,7 @@ UserMark const * Framework::OnTapEventImpl(m2::PointD pxPoint, bool isLong, bool
   }
   else if (isLong)
   {
-    GetAddressInfoForPixelPoint(pxPoint, info);
+    GetAddressInfoForPixelPoint(pxPoint2d, info);
     pxPivot = pxPoint;
     needMark = true;
   }
@@ -1646,7 +1646,7 @@ UserMark const * Framework::OnTapEventImpl(m2::PointD pxPoint, bool isLong, bool
   if (needMark)
   {
     PoiMarkPoint * poiMark = UserMarkContainer::UserMarkForPoi();
-    poiMark->SetPtOrg(m_currentMovelView.PtoG(pxPivot));
+    poiMark->SetPtOrg(m_currentMovelView.PtoG(m_currentMovelView.P3dToP(pxPivot)));
     poiMark->SetInfo(info);
     poiMark->SetMetadata(move(metadata));
     return poiMark;

map/framework.hpp
@@ -374,6 +374,7 @@ public:
   //@{
   inline m2::PointD PtoG(m2::PointD const & p) const { return m_currentMovelView.PtoG(p); }
   inline m2::PointD GtoP(m2::PointD const & p) const { return m_currentMovelView.GtoP(p); }
+  inline m2::PointD GtoP3d(m2::PointD const & p) const { return m_currentMovelView.PtoP3d(m_currentMovelView.GtoP(p)); }
 
   void SaveState();
   void LoadState();

organicmaps/organicmaps

Conflicts:
	map/framework.cpp
	map/framework.hpp

map/framework.cpp
@@ -1455,7 +1455,7 @@ m2::PointD Framework::GetVisiblePOI(FeatureID const & id, search::AddressInfo & 
 
   GetAddressInfo(ft, center, info);
 
-  return GtoP(center);
+  return m_currentModelView.isPerspective() ? GtoP3d(center) : GtoP(center);
 }
 
 namespace
@@ -1639,7 +1639,7 @@ void Framework::InvalidateRendering()
 
 UserMark const * Framework::OnTapEventImpl(m2::PointD pxPoint, bool isLong, bool isMyPosition, FeatureID const & feature)
 {
-  pxPoint = m_currentMovelView.P3dToP(pxPoint);
+  m2::PointD const pxPoint2d = m_currentModelView.P3dToP(pxPoint);
 
   if (isMyPosition)
   {
@@ -1655,13 +1655,13 @@ UserMark const * Framework::OnTapEventImpl(m2::PointD pxPoint, bool isLong, bool
 
   m2::AnyRectD rect;
   uint32_t const touchRadius = vp.GetTouchRectRadius();
-  m_currentModelView.GetTouchRect(pxPoint, touchRadius, rect);
+  m_currentModelView.GetTouchRect(pxPoint2d, touchRadius, rect);
 
   m2::AnyRectD bmSearchRect;
   double const bmAddition = BM_TOUCH_PIXEL_INCREASE * vp.GetVisualScale();
   double const pxWidth  =  touchRadius;
   double const pxHeight = touchRadius + bmAddition;
-  m_currentModelView.GetTouchRect(pxPoint + m2::PointD(0, bmAddition),
+  m_currentModelView.GetTouchRect(pxPoint2d + m2::PointD(0, bmAddition),
                                   pxWidth, pxHeight, bmSearchRect);
   UserMark const * mark = m_bmManager.FindNearestUserMark(
         [&rect, &bmSearchRect](UserMarkType type) -> m2::AnyRectD const &
@@ -1684,7 +1684,7 @@ UserMark const * Framework::OnTapEventImpl(m2::PointD pxPoint, bool isLong, bool
   }
   else if (isLong)
   {
-    GetAddressInfoForPixelPoint(pxPoint, info);
+    GetAddressInfoForPixelPoint(pxPoint2d, info);
     pxPivot = pxPoint;
     needMark = true;
   }
@@ -1692,7 +1692,7 @@ UserMark const * Framework::OnTapEventImpl(m2::PointD pxPoint, bool isLong, bool
   if (needMark)
   {
     PoiMarkPoint * poiMark = UserMarkContainer::UserMarkForPoi();
-    poiMark->SetPtOrg(m_currentModelView.PtoG(pxPivot));
+    poiMark->SetPtOrg(m_currentModelView.PtoG(m_currentModelView.P3dToP(pxPivot)));
     poiMark->SetInfo(info);
     poiMark->SetMetadata(move(metadata));
     return poiMark;

map/framework.hpp
@@ -377,6 +377,7 @@ public:
   //@{
   inline m2::PointD PtoG(m2::PointD const & p) const { return m_currentModelView.PtoG(p); }
   inline m2::PointD GtoP(m2::PointD const & p) const { return m_currentModelView.GtoP(p); }
+  inline m2::PointD GtoP3d(m2::PointD const & p) const { return m_currentModelView.PtoP3d(m_currentModelView.GtoP(p)); }
 
   void SaveState();
   void LoadState();