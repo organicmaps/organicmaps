587c58188307fffe823d444ee0b9859f41b1a957
Lev Dragunov <l.dragunov@corp.mail.ru>
2016-01-05 15:06:53 +0300

    Town points filtering.

--- generator/towns_dumper.cpp
+++ generator/towns_dumper.cpp
@@ -2,16 +2,66 @@
 
 #include "coding/file_writer.hpp"
 
+#include "geometry/tree4d.hpp"
+
 #include "base/logging.hpp"
 
 #include "std/string.hpp"
 #include "std/sstream.hpp"
+#include "std/queue.hpp"
+
+namespace
+{
+uint64_t constexpr kTownsEqualityMeters = 150000;
+}  // namespace
 
 TownsDumper::TownsDumper() {}
+void TownsDumper::FilterTowns()
+{
+  LOG(LINFO, ("Preprocessing started. Have", m_records.size(), "towns."));
+  m4::Tree<Town> resultTree;
+  priority_queue<Town> towns;
+  for (auto const & town : m_records)
+  {
+    if (town.capital)
+      resultTree.Add(town);
+    else
+      towns.push(town);
+  }
+
+  LOG(LINFO, ("Tree of capitals has size", resultTree.GetSize(), "towns has size:", towns.size()));
+  m_records.clear();
+
+  while (!towns.empty())
+  {
+    auto const & top = towns.top();
+    bool isUniq = true;
+    resultTree.ForEachInRect(
+        MercatorBounds::RectByCenterXYAndSizeInMeters(MercatorBounds::FromLatLon(top.point),
+                                                      kTownsEqualityMeters),
+        [&top, &isUniq](Town const & candidate)
+        {
+          if (MercatorBounds::DistanceOnEarth(MercatorBounds::FromLatLon(top.point),
+                                              MercatorBounds::FromLatLon(candidate.point)) <
+              kTownsEqualityMeters)
+            isUniq = false;
+        });
+    if (isUniq)
+      resultTree.Add(top);
+    towns.pop();
+  }
+
+  resultTree.ForEach([this](Town const & town)
+                     {
+                       m_records.push_back(town);
+                     });
+  LOG(LINFO, ("Preprocessing finished. Have", m_records.size(), "towns."));
+}
+
 void TownsDumper::Dump(string filePath)
 {
+  FilterTowns();
   ASSERT(!filePath.empty(), ());
-  LOG(LINFO, ("Have", m_records.size(), "Towns"));
   ostringstream stream;
   stream.precision(9);
   for (auto const & record : m_records)
--- generator/towns_dumper.hpp
+++ generator/towns_dumper.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "geometry/latlon.hpp"
+#include "geometry/mercator.hpp"
+#include "geometry/rect2d.hpp"
 
 #include "std/string.hpp"
 #include "std/vector.hpp"
@@ -15,27 +17,59 @@ public:
   {
     if (em.type != TElement::EntityType::Node)
       return;
+    uint64_t population = 1;
+    bool town = false;
+    bool capital = false;
     for (auto const & tag : em.Tags())
     {
       string key(tag.key), value(tag.value);
-      if (key == "capital" || (key == "place" && (value == "city" || value == "town")))
+      if (key == "population")
       {
-       m_records.emplace_back(em.lat, em.lon, em.id);
-       return;
+        try
+        {
+          population = stoul(value);
+        }
+        catch (std::invalid_argument const &)
+        {
+          continue;
+        }
+      }
+      else if (key == "capital")
+      {
+        capital = true;
+      }
+      else if (key == "place" && (value == "city" || value == "town"))
+      {
+        town = true;
       }
     }
+    if (town || capital)
+      m_records.emplace_back(em.lat, em.lon, em.id, capital, population);
   }
 
-void Dump(string filePath);
+  void Dump(string filePath);
 
 private:
+  void FilterTowns();
 
-struct Town{
-  ms::LatLon point;
-  uint64_t id;
+  struct Town
+  {
+    ms::LatLon point;
+    uint64_t id;
+    bool capital;
+    uint64_t population;
 
-  Town(double lat, double lon, uint64_t id) : point(lat, lon), id(id) {}
-};
+    Town(double lat, double lon, uint64_t id, bool isCapital, uint64_t population)
+      : point(lat, lon), id(id), capital(isCapital), population(population)
+    {
+    }
 
-vector<Town> m_records;
+    bool operator<(Town const & rhs) const { return population < rhs.population; }
+    m2::RectD GetLimitRect() const
+    {
+      return m2::RectD(MercatorBounds::FromLatLon(point), MercatorBounds::FromLatLon(point));
+    }
+  };
+
+  vector<Town> m_records;
 };
