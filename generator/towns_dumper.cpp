771d9af0f25a96c10f3a271815de5616f58f82a7
Lev Dragunov <l.dragunov@corp.mail.ru>
2016-01-06 11:20:19 +0300

    towns_dumper enhancement.

--- generator/towns_dumper.cpp
+++ generator/towns_dumper.cpp
@@ -2,13 +2,14 @@
 
 #include "coding/file_writer.hpp"
 
+#include "geometry/distance_on_sphere.hpp"
 #include "geometry/tree4d.hpp"
 
 #include "base/logging.hpp"
 
-#include "std/string.hpp"
 #include "std/sstream.hpp"
-#include "std/queue.hpp"
+#include "std/string.hpp"
+#include "std/vector.hpp"
 
 namespace
 {
@@ -20,35 +21,35 @@ void TownsDumper::FilterTowns()
 {
   LOG(LINFO, ("Preprocessing started. Have", m_records.size(), "towns."));
   m4::Tree<Town> resultTree;
-  priority_queue<Town> towns;
+  vector<Town> towns;
+  towns.reserve(m_records.size());
   for (auto const & town : m_records)
   {
     if (town.capital)
       resultTree.Add(town);
     else
-      towns.push(town);
+      towns.push_back(town);
   }
+  sort(towns.begin(), towns.end());
 
   LOG(LINFO, ("Tree of capitals has size", resultTree.GetSize(), "towns has size:", towns.size()));
   m_records.clear();
 
   while (!towns.empty())
   {
-    auto const & top = towns.top();
+    auto const & top = towns.back();
     bool isUniq = true;
     resultTree.ForEachInRect(
         MercatorBounds::RectByCenterXYAndSizeInMeters(MercatorBounds::FromLatLon(top.point),
                                                       kTownsEqualityMeters),
         [&top, &isUniq](Town const & candidate)
         {
-          if (MercatorBounds::DistanceOnEarth(MercatorBounds::FromLatLon(top.point),
-                                              MercatorBounds::FromLatLon(candidate.point)) <
-              kTownsEqualityMeters)
+          if (ms::DistanceOnEarth(top.point, candidate.point) < kTownsEqualityMeters)
             isUniq = false;
         });
     if (isUniq)
       resultTree.Add(top);
-    towns.pop();
+    towns.pop_back();
   }
 
   resultTree.ForEach([this](Town const & town)
