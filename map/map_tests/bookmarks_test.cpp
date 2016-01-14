7f58054224cca5e2aa4d29cfae1834d9087d614d
Alex Zolotarev <alex@maps.me>
2016-01-14 23:40:33 +0300

    Fixed bookmarks test - we always use native (default) street language name.

--- map/map_tests/bookmarks_test.cpp
+++ map/map_tests/bookmarks_test.cpp
@@ -413,30 +413,9 @@ UNIT_TEST(Bookmarks_AddressInfo)
   fm.RegisterMap(platform::LocalCountryFile::MakeForTesting("minsk-pass"));
   fm.OnSize(800, 600);
 
-  // assume that developers have English or Russian system language :)
-  string const lang = languages::GetCurrentNorm();
-  LOG(LINFO, ("Current language =", lang));
-
-  // default name (street in russian, category in english).
-  size_t index = 0;
-  if (lang == "ru")
-    index = 1;
-  if (lang == "en")
-    index = 2;
-
-  POIInfo poi1[] = {
-    { "Планета Pizza", "улица Карла Маркса", "10", "Cafe" },
-    { "Планета Pizza", "улица Карла Маркса", "10", "Кафе" },
-    { "Планета Pizza", "vulica Karla Marksa", "10", "Cafe" }
-  };
-  CheckPlace(fm, 53.8964918, 27.555559, poi1[index]);
-
-  POIInfo poi2[] = {
-    { "Нц Шашек И Шахмат", "улица Карла Маркса", "10", "Hotel" },
-    { "Нц Шашек И Шахмат", "улица Карла Маркса", "10", "Гостиница" },
-    { "Нц Шашек И Шахмат", "vulica Karla Marksa", "10", "Hotel" }
-  };
-  CheckPlace(fm, 53.8964365, 27.5554007, poi2[index]);
+  // Our code always uses "default" street name for addresses.
+  CheckPlace(fm, 53.8964918, 27.555559, { "Планета Pizza", "улица Карла Маркса", "10", "cafe" });
+  CheckPlace(fm, 53.8964365, 27.5554007, { "Нц Шашек И Шахмат", "улица Карла Маркса", "10", "hotel" });
 }
 
 UNIT_TEST(Bookmarks_IllegalFileName)
