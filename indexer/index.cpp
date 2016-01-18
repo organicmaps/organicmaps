65fb91232602e20b67bbc78b14662bb8803eb73f
Alex Zolotarev <alex@maps.me>
2016-01-18 19:20:30 +0300

    Avoid copy-paste.

--- indexer/index.cpp
+++ indexer/index.cpp
@@ -123,15 +123,11 @@ void Index::FeaturesLoaderGuard::GetFeatureByIndex(uint32_t index, FeatureType &
   ASSERT_NOT_EQUAL(osm::Editor::FeatureStatus::Deleted, m_editor.GetFeatureStatus(id, index),
                    ("Deleted feature was cached. Please review your code."));
   if (!m_editor.Instance().GetEditedFeature(id, index, ft))
-  {
-    m_vector.GetByIndex(index, ft);
-    ft.SetID(FeatureID(id, index));
-  }
+    GetOriginalFeatureByIndex(index, ft);
 }
 
 void Index::FeaturesLoaderGuard::GetOriginalFeatureByIndex(uint32_t index, FeatureType & ft) const
 {
-  MwmId const & id = m_handle.GetId();
   m_vector.GetByIndex(index, ft);
-  ft.SetID(FeatureID(id, index));
+  ft.SetID(FeatureID(m_handle.GetId(), index));
 }
