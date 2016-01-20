ba28af0463571e77f69b209f195ed368b051a155
Ilya Zverev <zverik@textual.ru>
2016-01-20 17:51:19 +0300

    [build] Delete android assets scripts

--- tools/android/update_assets.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-SCRIPTS_PATH="$(dirname "$0")"
-OMIM_PATH="${OMIM_PATH:-$SCRIPTS_PATH/../..}"
-"$SCRIPTS_PATH/update_assets_for_version.sh" "$OMIM_PATH/data" "$OMIM_PATH/android/assets"
-"$SCRIPTS_PATH/add_assets_mwm-ttf.sh" "$OMIM_PATH/data" "$OMIM_PATH/android/flavors/mwm-ttf-assets"
--- tools/unix/generate_styles.sh
+++ tools/unix/generate_styles.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 set -e -u
-
 SCRIPTS_PATH="$(dirname "$0")"
 "$SCRIPTS_PATH/generate_symbols.sh"
 "$SCRIPTS_PATH/generate_drules.sh"
-
-ANDROID_PATH="$SCRIPTS_PATH/../android"
-"$ANDROID_PATH/update_assets.sh"
-"$ANDROID_PATH/update_assets_yota.sh"
