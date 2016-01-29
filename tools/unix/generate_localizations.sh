6d911588f105dd867ee982c820939662778a1a4c
Alex Zolotarev <alex@maps.me>
2016-01-29 17:57:47 +0300

    [strings] TODO for duplicates check.

--- tools/unix/generate_localizations.sh
+++ tools/unix/generate_localizations.sh
@@ -3,6 +3,7 @@
 set -e -u -x
 
 # TODO: Add "--untagged --tags android" when tags are properly set.
+# TODO: Add validate-strings-file call to check for duplicates (and avoid Android build errors) when tags are properly set.
 ./tools/twine/twine --format android generate-all-string-files ./strings.txt ./android/res/
 ./tools/twine/twine --format apple generate-all-string-files ./strings.txt ./iphone/Maps/
 ./tools/twine/twine --format apple --file-name InfoPlist.strings generate-all-string-files ./iphone/plist.txt ./iphone/Maps/
