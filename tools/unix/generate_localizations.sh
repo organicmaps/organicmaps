5888e92c7701e6b245e5bcea36ed79d738ea857f
Alex Zolotarev <alex@maps.me>
2016-01-29 17:57:30 +0300

    [strings] Commented strings generation for Tizen platform.

--- tools/unix/generate_localizations.sh
+++ tools/unix/generate_localizations.sh
@@ -6,5 +6,5 @@ set -e -u -x
 ./tools/twine/twine --format android generate-all-string-files ./strings.txt ./android/res/
 ./tools/twine/twine --format apple generate-all-string-files ./strings.txt ./iphone/Maps/
 ./tools/twine/twine --format apple --file-name InfoPlist.strings generate-all-string-files ./iphone/plist.txt ./iphone/Maps/
-./tools/twine/twine --format tizen generate-all-string-files ./strings.txt ./tizen/MapsWithMe/res/ --tags tizen
+#./tools/twine/twine --format tizen generate-all-string-files ./strings.txt ./tizen/MapsWithMe/res/ --tags tizen
 
