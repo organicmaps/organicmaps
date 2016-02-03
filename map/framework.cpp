463e4837c16bfdcf4fa44bc520aef4bcfcfb4cf1
Sergey Yershov <yershov@corp.mail.ru>
2016-02-03 19:00:25 +0300

    [new downloader] Fix fast migrate

--- map/framework.cpp
+++ map/framework.cpp
@@ -539,6 +539,7 @@ void Framework::RegisterAllMaps()
     Settings::Get("DisableFastMigrate", disableFastMigrate);
     if (!disableFastMigrate && !m_storage.HaveDownloadedCountries())
     {
+      Storage().PrefetchMigrateData();
       Migrate();
       return;
     }
