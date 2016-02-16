4e499bb353c8d2cda6ea2bb12bbf769b9d2424f9
Vladimir Byko-Ianko <v.bykoianko@corp.mail.ru>
2016-02-16 15:23:55 +0300

    [new downloader] Unit tests on SimpleTree.

--- storage/storage_tests/simple_tree_test.cpp
+++ storage/storage_tests/simple_tree_test.cpp
@@ -5,7 +5,6 @@
 
 namespace
 {
-
 template <class TNode>
 struct Calculator
 {
@@ -16,8 +15,7 @@ struct Calculator
     ++count;
   }
 };
-
-}
+} // namespace
 
 UNIT_TEST(SimpleTree_Smoke)
 {
@@ -33,16 +31,21 @@ UNIT_TEST(SimpleTree_Smoke)
   tree.AddAtDepth(1, 10);  // 1 is parent
   tree.AddAtDepth(1, 30);  // 1 is parent
 
-  tree.Sort();
-  // test sorting
-  TEST_EQUAL(tree.Child(0).Value(), 1, ());
-  TEST_EQUAL(tree.Child(1).Value(), 2, ());
-  TEST_EQUAL(tree.Child(2).Value(), 3, ());
-  TEST_EQUAL(tree.Child(3).Value(), 4, ());
-  TEST_EQUAL(tree.Child(4).Value(), 5, ());
-  TEST_EQUAL(tree.Child(0).Child(0).Value(), 10, ());
-  TEST_EQUAL(tree.Child(0).Child(1).Value(), 20, ());
-  TEST_EQUAL(tree.Child(0).Child(2).Value(), 30, ());
+  // children test
+  TEST_EQUAL(tree.Child(0).Value(), 4, ());
+  TEST_EQUAL(tree.Child(1).Value(), 3, ());
+  TEST_EQUAL(tree.Child(2).Value(), 5, ());
+  TEST_EQUAL(tree.Child(3).Value(), 2, ());
+  TEST_EQUAL(tree.Child(4).Value(), 1, ());
+  TEST_EQUAL(tree.Child(4).Child(0).Value(), 20, ());
+  TEST_EQUAL(tree.Child(4).Child(1).Value(), 10, ());
+  TEST_EQUAL(tree.Child(4).Child(2).Value(), 30, ());
+
+  // parent test
+  TEST(!tree.HasParent(), ());
+  TEST(!tree.Child(0).Parent().HasParent(), ());
+  TEST_EQUAL(tree.Child(4).Child(0).Parent().Value(), 1, ());
+  TEST_EQUAL(tree.Child(4).Child(2).Parent().Value(), 1, ());
 
   Calculator<TreeT> c1;
   tree.ForEachChild(c1);
@@ -52,8 +55,12 @@ UNIT_TEST(SimpleTree_Smoke)
   tree.ForEachDescendant(c2);
   TEST_EQUAL(c2.count, 8, ());
 
-  tree.Clear();
   Calculator<TreeT> c3;
-  tree.ForEachDescendant(c3);
-  TEST_EQUAL(c3.count, 0, ("Tree should be empty"));
+  tree.Child(4).Child(0).ForEachParent(c3);
+  TEST_EQUAL(c3.count, 2, ());
+
+  tree.Clear();
+  Calculator<TreeT> c4;
+  tree.ForEachDescendant(c4);
+  TEST_EQUAL(c4.count, 0, ("Tree should be empty"));
 }
