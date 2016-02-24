0f1c6a83e490e1f5a80d084628777958064b77ab
Constantin Shalnev <c.shalnev@corp.mail.ru>
2016-02-24 17:32:11 +0300

    Use THREAD_CHECKER under Android

--- base/thread_checker.hpp
+++ base/thread_checker.hpp
@@ -23,9 +23,7 @@ private:
   DISALLOW_COPY_AND_MOVE(ThreadChecker);
 };
 
-// ThreadChecker checker is not valid on Android.
-// UI thread (NV thread) can change it's handle value during app lifecycle.
-#if defined(DEBUG) && !defined(OMIM_OS_ANDROID)
+#if defined(DEBUG)
   #define DECLARE_THREAD_CHECKER(threadCheckerName) ThreadChecker threadCheckerName
   #define ASSERT_THREAD_CHECKER(threadCheckerName, msg) ASSERT(threadCheckerName.CalledOnOriginalThread(), msg)
 #else
