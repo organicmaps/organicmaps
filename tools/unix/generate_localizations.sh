65414de6dff9bdfefe212ff4e565fc5a55541227
Alex Zolotarev <alex@maps.me>
2016-02-29 16:21:26 +0300

    Add command to generate cuisines localizations into a common script.

--- tools/unix/generate_localizations.sh
+++ tools/unix/generate_localizations.sh
@@ -7,5 +7,6 @@ set -e -u -x
 ./tools/twine/twine --format android generate-all-string-files ./strings.txt ./android/res/
 ./tools/twine/twine --format apple generate-all-string-files ./strings.txt ./iphone/Maps/
 ./tools/twine/twine --format apple --file-name InfoPlist.strings generate-all-string-files ./iphone/plist.txt ./iphone/Maps/
+./tools/twine/twine --format jquery generate-all-string-files ./data/cuisines.txt ./data/cuisine-strings/
 #./tools/twine/twine --format tizen generate-all-string-files ./strings.txt ./tizen/MapsWithMe/res/ --tags tizen
 
