a7d12eda035dd136b30f48eedef9bc4380c84013
VladiMihaylenko <vxmihaylenko@gmail.com>
2016-03-03 15:25:47 +0300

    [ios] Added MWMAddPlaceNavigationBar class.

---
+++ iphone/Maps/Classes/Components/MWMAddPlaceNavigationBar.h
@@ -0,0 +1,5 @@
+@interface MWMAddPlaceNavigationBar : SolidTouchView
+
++ (void)showInSuperview:(nonnull UIView *)superview doneBlock:(nonnull TMWMVoidBlock)done cancelBlock:(nonnull TMWMVoidBlock)cancel;
+
+@end
