8419537d1881a13b8d276e5ae858954c548f4b79
VladiMihaylenko <vxmihaylenko@gmail.com>
2016-03-03 15:34:08 +0300

    [ios] Removed excess method from MWMBottomMenuCollectionViewCell.h.

--- iphone/Maps/Classes/CustomViews/MapViewControls/BottomMenu/MWMBottomMenuCollectionViewCell.h
+++ iphone/Maps/Classes/CustomViews/MapViewControls/BottomMenu/MWMBottomMenuCollectionViewCell.h
@@ -2,10 +2,6 @@
 
 @property (weak, nonatomic) IBOutlet UIImageView * icon;
 
-- (void)configureWithImage:(UIImage *)image
-                    label:(NSString *)label
-               badgeCount:(NSUInteger)badgeCount;
-
 - (void)configureWithImageName:(NSString *)imageName
                          label:(NSString *)label
                     badgeCount:(NSUInteger)badgeCount;
