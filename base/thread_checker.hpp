0faa81ef5716d6611f4f199e4c222638c75d614b
Alex Zolotarev <alex@maps.me>
2016-03-09 13:01:52 +0300

    Convenient macro for thread checker to use it once in-place.

--- base/thread_checker.hpp
+++ base/thread_checker.hpp
@@ -26,7 +26,12 @@ private:
 #if defined(DEBUG)
   #define DECLARE_THREAD_CHECKER(threadCheckerName) ThreadChecker threadCheckerName
   #define ASSERT_THREAD_CHECKER(threadCheckerName, msg) ASSERT(threadCheckerName.CalledOnOriginalThread(), msg)
+  #define DECLARE_AND_ASSERT_THREAD_CHECKER(msg) { \
+    static const ThreadChecker threadChecker; \
+    ASSERT(threadChecker.CalledOnOriginalThread(), (msg)); \
+  }
 #else
   #define DECLARE_THREAD_CHECKER(threadCheckerName)
   #define ASSERT_THREAD_CHECKER(threadCheckerName, msg)
+  #define DECLARE_AND_ASSERT_THREAD_CHECKER(msg)
 #endif
