63d9cdd071724a6c745c4e3085e5765461b6fd51
Alex Zolotarev <alex@maps.me>
2016-03-18 13:54:43 +0300

    Fixed some cuisines duplication.

--- generator/osm2meta.cpp
+++ generator/osm2meta.cpp
@@ -193,8 +193,13 @@ string MetadataTagProcessorImpl::ValidateAndFormat_cuisine(string v) const
     strings::Trim(normalized, " ");
     CollapseMultipleConsecutiveCharsIntoOne(' ', normalized);
     replace(normalized.begin(), normalized.end(), ' ', '_');
+    // Avoid duplication for some cuisines.
     if (normalized == "bbq" || normalized == "barbeque")
       normalized = "barbecue";
+    if (normalized == "doughnut")
+      normalized = "donut";
+    if (normalized == "steak")
+      normalized = "steak_house";
     collector(normalized);
     ++iter;
   }
