f6521b5a967726d821b226ed5ef93ac305462612
Maxim Pimenov <m@maps.me>
2016-03-22 17:32:08 +0300

    [search] Accept lines and areas as village localities.

--- search/v2/geocoder.cpp
+++ search/v2/geocoder.cpp
@@ -748,7 +748,7 @@ void Geocoder::FillLocalitiesTable()
         ++numCities;
         City city(l, SearchModel::SEARCH_TYPE_CITY);
         city.m_rect = MercatorBounds::RectByCenterXYAndSizeInMeters(
-            feature::GetCenter(ft), ftypes::GetRadiusByPopulation(ft.GetPopulation()));
+            ft.GetCenter(), ftypes::GetRadiusByPopulation(ft.GetPopulation()));
 
 #if defined(DEBUG)
         ft.GetName(StringUtf8Multilang::kDefaultCode, city.m_defaultName);
@@ -822,15 +822,13 @@ void Geocoder::FillVillageLocalities()
 
     if (m_model.GetSearchType(ft) != SearchModel::SEARCH_TYPE_VILLAGE)
       continue;
-    if (ft.GetFeatureType() != feature::GEOM_POINT)
-      continue;
-    if (numVillages >= kMaxNumVillages)
-      continue;
 
+    // We accept lines and areas as village features.
+    auto const center = feature::GetCenter(ft);
     ++numVillages;
     City village(l, SearchModel::SEARCH_TYPE_VILLAGE);
     village.m_rect = MercatorBounds::RectByCenterXYAndSizeInMeters(
-        feature::GetCenter(ft), ftypes::GetRadiusByPopulation(ft.GetPopulation()));
+        center, ftypes::GetRadiusByPopulation(ft.GetPopulation()));
 
 #if defined(DEBUG)
     ft.GetName(StringUtf8Multilang::kDefaultCode, village.m_defaultName);
@@ -838,6 +836,8 @@ void Geocoder::FillVillageLocalities()
 #endif
 
     m_cities[{l.m_startToken, l.m_endToken}].push_back(village);
+    if (numVillages >= kMaxNumVillages)
+      break;
   }
 }
 
