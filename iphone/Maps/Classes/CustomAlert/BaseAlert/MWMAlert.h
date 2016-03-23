mapsme/omim

[old-map-downloader][ios] Added mwm migration alert.

iphone/Maps/Classes/CustomAlert/BaseAlert/MWMAlert.h
@@ -13,7 +13,8 @@ typedef void (^RightButtonAction)();
 + (MWMAlert *)alert:(routing::IRouter::ResultCode)type;
 + (MWMAlert *)downloaderAlertWithAbsentCountries:(vector<storage::TIndex> const &)countries
                                           routes:(vector<storage::TIndex> const &)routes
-                                            code:(routing::IRouter::ResultCode)code;
+                                            code:(routing::IRouter::ResultCode)code
+                                           block:(TMWMVoidBlock)block;
 + (MWMAlert *)rateAlert;
 + (MWMAlert *)facebookAlert;
 + (MWMAlert *)locationAlert;
@@ -24,6 +25,7 @@ typedef void (^RightButtonAction)();
 + (MWMAlert *)locationServiceNotSupportedAlert;
 + (MWMAlert *)pedestrianToastShareAlert:(BOOL)isFirstLaunch;
 + (MWMAlert *)point2PointAlertWithOkBlock:(RightButtonAction)block needToRebuild:(BOOL)needToRebuild;
++ (MWMAlert *)needMigrationAlertWithOkBlock:(RightButtonAction)block;
 - (void)close;
 
 - (void)setNeedsCloseAlertAfterEnterBackground;

iphone/Maps/Classes/CustomAlert/BaseAlert/MWMAlert.mm
@@ -31,6 +31,11 @@
   return [MWMDefaultAlert point2PointAlertWithOkBlock:block needToRebuild:needToRebuild];
 }
 
++ (MWMAlert *)needMigrationAlertWithOkBlock:(RightButtonAction)block
+{
+  return [MWMDefaultAlert needMigrationAlertWithOkBlock:block];
+}
+
 + (MWMAlert *)routingDisclaimerAlertWithInitialOrientation:(UIInterfaceOrientation)orientation
 {
   return [MWMRoutingDisclaimerAlert alertWithInitialOrientation:orientation];
@@ -59,8 +64,9 @@
 + (MWMAlert *)downloaderAlertWithAbsentCountries:(vector<storage::TIndex> const &)countries
                                           routes:(vector<storage::TIndex> const &)routes
                                             code:(routing::IRouter::ResultCode)code
+                                           block:(TMWMVoidBlock)block
 {
-  return [MWMDownloadTransitMapAlert downloaderAlertWithMaps:countries routes:routes code:code];
+  return [MWMDownloadTransitMapAlert downloaderAlertWithMaps:countries routes:routes code:code block:block];
 }
 
 + (MWMAlert *)alert:(routing::IRouter::ResultCode)type

iphone/Maps/Classes/CustomAlert/DefaultAlert/MWMDefaultAlert.h
@@ -14,5 +14,6 @@
 + (instancetype)noConnectionAlert;
 + (instancetype)locationServiceNotSupportedAlert;
 + (instancetype)point2PointAlertWithOkBlock:(RightButtonAction)block needToRebuild:(BOOL)needToRebuild;
++ (instancetype)needMigrationAlertWithOkBlock:(RightButtonAction)block;
 
 @end

iphone/Maps/Classes/CustomAlert/DefaultAlert/MWMDefaultAlert.mm
@@ -124,6 +124,15 @@ static NSString * const kDefaultAlertNibName = @"MWMDefaultAlert";
   }
 }
 
++ (instancetype)needMigrationAlertWithOkBlock:(RightButtonAction)block
+{
+  return [self defaultAlertWithTitle:@"need_migrate_title"
+                             message:@"need_migrate_message"
+                    rightButtonTitle:@"migrate"
+                     leftButtonTitle:@"not_now"
+                   rightButtonAction:block];
+}
+
 + (instancetype)defaultAlertWithTitle:(nonnull NSString *)title message:(nullable NSString *)message rightButtonTitle:(nonnull NSString *)rightButtonTitle leftButtonTitle:(nullable NSString *)leftButtonTitle rightButtonAction:(nullable RightButtonAction)action
 {
   [[Statistics instance] logEvent:kStatisticsEvent withParameters:@{kStatAction : kStatOpen}];

organicmaps/organicmaps

iphone/Maps/Classes/CustomAlert/BaseAlert/MWMAlert.h
@@ -9,7 +9,8 @@
 + (MWMAlert *)alert:(routing::IRouter::ResultCode)type;
 + (MWMAlert *)downloaderAlertWithAbsentCountries:(vector<storage::TIndex> const &)countries
                                           routes:(vector<storage::TIndex> const &)routes
-                                            code:(routing::IRouter::ResultCode)code;
+                                            code:(routing::IRouter::ResultCode)code
+                                           block:(TMWMVoidBlock)block;
 + (MWMAlert *)rateAlert;
 + (MWMAlert *)facebookAlert;
 + (MWMAlert *)locationAlert;
@@ -20,6 +21,7 @@
 + (MWMAlert *)locationServiceNotSupportedAlert;
 + (MWMAlert *)pedestrianToastShareAlert:(BOOL)isFirstLaunch;
 + (MWMAlert *)point2PointAlertWithOkBlock:(TMWMVoidBlock)block needToRebuild:(BOOL)needToRebuild;
++ (MWMAlert *)needMigrationAlertWithOkBlock:(TMWMVoidBlock)block;
 - (void)close;
 
 - (void)setNeedsCloseAlertAfterEnterBackground;

iphone/Maps/Classes/CustomAlert/BaseAlert/MWMAlert.mm
@@ -31,6 +31,11 @@
   return [MWMDefaultAlert point2PointAlertWithOkBlock:block needToRebuild:needToRebuild];
 }
 
++ (MWMAlert *)needMigrationAlertWithOkBlock:(TMWMVoidBlock)block
+{
+  return [MWMDefaultAlert needMigrationAlertWithOkBlock:block];
+}
+
 + (MWMAlert *)routingDisclaimerAlertWithInitialOrientation:(UIInterfaceOrientation)orientation
 {
   return [MWMRoutingDisclaimerAlert alertWithInitialOrientation:orientation];
@@ -59,8 +64,9 @@
 + (MWMAlert *)downloaderAlertWithAbsentCountries:(vector<storage::TIndex> const &)countries
                                           routes:(vector<storage::TIndex> const &)routes
                                             code:(routing::IRouter::ResultCode)code
+                                           block:(TMWMVoidBlock)block
 {
-  return [MWMDownloadTransitMapAlert downloaderAlertWithMaps:countries routes:routes code:code];
+  return [MWMDownloadTransitMapAlert downloaderAlertWithMaps:countries routes:routes code:code block:block];
 }
 
 + (MWMAlert *)alert:(routing::IRouter::ResultCode)type

iphone/Maps/Classes/CustomAlert/DefaultAlert/MWMDefaultAlert.h
@@ -14,5 +14,6 @@
 + (instancetype)noConnectionAlert;
 + (instancetype)locationServiceNotSupportedAlert;
 + (instancetype)point2PointAlertWithOkBlock:(TMWMVoidBlock)block needToRebuild:(BOOL)needToRebuild;
++ (instancetype)needMigrationAlertWithOkBlock:(TMWMVoidBlock)block;
 
 @end

iphone/Maps/Classes/CustomAlert/DefaultAlert/MWMDefaultAlert.mm
@@ -124,6 +124,15 @@ static NSString * const kDefaultAlertNibName = @"MWMDefaultAlert";
   }
 }
 
++ (instancetype)needMigrationAlertWithOkBlock:(TMWMVoidBlock)block
+{
+  return [self defaultAlertWithTitle:@"need_migrate_title"
+                             message:@"need_migrate_message"
+                    rightButtonTitle:@"migrate"
+                     leftButtonTitle:@"not_now"
+                   rightButtonAction:block];
+}
+
 + (instancetype)defaultAlertWithTitle:(nonnull NSString *)title message:(nullable NSString *)message rightButtonTitle:(nonnull NSString *)rightButtonTitle leftButtonTitle:(nullable NSString *)leftButtonTitle rightButtonAction:(nullable TMWMVoidBlock)action
 {
   [[Statistics instance] logEvent:kStatisticsEvent withParameters:@{kStatAction : kStatOpen}];