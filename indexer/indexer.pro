biodranik/omim

Editable feature designed to connect UI and editor’s core.

indexer/indexer.pro
@@ -21,6 +21,7 @@ SOURCES += \
     drawing_rules.cpp \
     drules_selector.cpp \
     drules_selector_parser.cpp \
+    editable_feature.cpp \
     edits_migration.cpp \
     feature.cpp \
     feature_algo.cpp \
@@ -67,6 +68,7 @@ HEADERS += \
     drules_include.hpp \
     drules_selector.hpp \
     drules_selector_parser.hpp \
+    editable_feature.hpp \
     edits_migration.hpp \
     feature.hpp \
     feature_algo.hpp \

organicmaps/organicmaps

indexer/indexer.pro
@@ -22,6 +22,7 @@ SOURCES += \
     drawing_rules.cpp \
     drules_selector.cpp \
     drules_selector_parser.cpp \
+    editable_map_object.cpp \
     edits_migration.cpp \
     feature.cpp \
     feature_algo.cpp \
@@ -70,6 +71,7 @@ HEADERS += \
     drules_include.hpp \
     drules_selector.hpp \
     drules_selector_parser.hpp \
+    editable_map_object.hpp \
     edits_migration.hpp \
     feature.hpp \
     feature_algo.hpp \