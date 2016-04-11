66550bd3aebad990caaeb729d147f38e0d191de4
Lev Dragunov <l.dragunov@corp.mail.ru>
2016-04-11 15:59:07 +0300

    OSRM to features mapping integration test.

--- routing/routing_integration_tests/cross_section_tests.cpp
+++ routing/routing_integration_tests/cross_section_tests.cpp
@@ -1,6 +1,8 @@
 #include "testing/testing.hpp"
 
 #include "routing/cross_routing_context.hpp"
+#include "routing/osrm2feature_map.hpp"
+#include "routing/routing_mapping.hpp"
 
 #include "platform/local_country_file.hpp"
 #include "platform/local_country_file_utils.hpp"
@@ -8,6 +10,7 @@
 
 #include "geometry/point2d.hpp"
 
+#include "base/buffer_vector.hpp"
 #include "base/logging.hpp"
 
 #include "std/limits.hpp"
@@ -69,5 +72,55 @@ UNIT_TEST(CheckCrossSections)
   TEST_EQUAL(outgoingErrors, 0, ("Some countries have zero point exits."));
   LOG(LINFO, ("Found", localFiles.size(), "countries.",
               noRouting, "countries are without routing file."));
+}
+
+UNIT_TEST(CheckOsrmToFeatureMapping)
+{
+  vector<platform::LocalCountryFile> localFiles;
+  platform::FindAllLocalMapsAndCleanup(numeric_limits<int64_t>::max() /* latestVersion */,
+                                       localFiles);
+
+  size_t errors = 0;
+  size_t checked = 0;
+  for (auto & file : localFiles)
+  {
+    LOG(LINFO, ("Checking nodes for country:", file.GetCountryName()));
+
+    file.SyncWithDisk();
+    if (file.GetFiles() != MapOptions::MapWithCarRouting)
+    {
+      LOG(LINFO, ("Warning! Routing file not found for:", file.GetCountryName()));
+      continue;
+    }
+
+    FilesMappingContainer container(file.GetPath(MapOptions::CarRouting));
+    if (!container.IsExist(ROUTING_FTSEG_FILE_TAG))
+    {
+      LOG(LINFO, ("Warning! Mwm file has not routing ftseg section:", file.GetCountryName()));
+      continue;
+    }
+    ++checked;
+
+    routing::TDataFacade dataFacade;
+    dataFacade.Load(container);
+
+    OsrmFtSegMapping segMapping;
+    segMapping.Load(container, file);
+    segMapping.Map(container);
+
+    for (size_t i = 0; i < dataFacade.GetNumberOfNodes(); ++i)
+    {
+      buffer_vector<OsrmMappingTypes::FtSeg, 8> buffer;
+      segMapping.ForEachFtSeg(i, MakeBackInsertFunctor(buffer));
+      if (buffer.empty())
+      {
+        LOG(LINFO, ("Error found in ", file.GetCountryName()));
+        errors++;
+        break;
+      }
+    }
+  }
+  LOG(LINFO, ("Found", localFiles.size(), "countries. In", checked, "maps with routing have", errors, "with errors."));
+  TEST_EQUAL(errors, 0, ("Some countries have osrm and features mismatch."));
 }
 }  // namespace
