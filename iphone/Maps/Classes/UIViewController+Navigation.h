a009b7d416aec92f5a6322a3a3b2616f3ab762c9
Ilya Grechuhin <i.grechuhin@gmail.com>
2016-04-18 15:28:13 +0300

    [ios] Refactored navigation bar configuration.

--- iphone/Maps/Classes/UIViewController+Navigation.h
+++ iphone/Maps/Classes/UIViewController+Navigation.h
@@ -3,6 +3,10 @@
 - (void)showBackButton;
 - (void)backTap;
 
+- (UIBarButtonItem *)navBarButtonWithImage:(UIImage *)image highlightedImage:(UIImage *)highlightedImage action:(SEL)action;
+- (NSArray<UIBarButtonItem *> *)alignedNavBarButtonItems:(NSArray<UIBarButtonItem *> *)items;
+
+
 @property (nonatomic, readonly) UIStoryboard * mainStoryboard;
 
 @end
