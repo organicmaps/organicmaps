daba2c2673f195c9feffbb64a017796a863174cf
Sergey Yershov <yershov@corp.mail.ru>
2016-05-16 14:11:58 +0300

    [downloader] Consider failed mwm as available for downloading

--- storage/storage.cpp
+++ storage/storage.cpp
@@ -1198,7 +1198,7 @@ void Storage::GetChildrenInGroups(TCountryId const & parent,
     for (auto const & disputed : disputedTerritoriesAndStatus)
       allDisputedTerritories.push_back(disputed.first);
 
-    if (childStatus.status == NodeStatus::NotDownloaded)
+    if (childStatus.status == NodeStatus::NotDownloaded || childStatus.status == NodeStatus::Error)
     {
       availChildren.push_back(childValue);
       for (auto const & disputed : disputedTerritoriesAndStatus)
