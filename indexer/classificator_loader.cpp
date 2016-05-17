18b5ef89650ef458390f0c333b99d8123877c508
Ilya Zverev <zverik@textual.ru>
2016-05-17 12:55:33 +0300

    [classificator] Restore old classificator loading algorythm

--- indexer/classificator_loader.cpp
+++ indexer/classificator_loader.cpp
@@ -45,16 +45,24 @@ namespace classificator
     LOG(LDEBUG, ("Reading of classificator started"));
 
     Platform & p = GetPlatform();
-    StyleReader & reader = GetStyleReader();
 
-    for (auto style : { MapStyleLight, MapStyleDark, MapStyleClear })
+    MapStyle const originMapStyle = GetStyleReader().GetCurrentStyle();
+
+    for (size_t i = 0; i < MapStyleCount; ++i)
     {
-      reader.SetCurrentStyle(style);
+      MapStyle const mapStyle = static_cast<MapStyle>(i);
+      // Read the merged style only if it was requested.
+      if (mapStyle != MapStyleMerged || originMapStyle == MapStyleMerged)
+      {
+        GetStyleReader().SetCurrentStyle(mapStyle);
+        ReadCommon(p.GetReader("classificator.txt"),
+                   p.GetReader("types.txt"));
 
-      ReadCommon(p.GetReader("classificator.txt"), p.GetReader("types.txt"));
-
-      drule::LoadRules();
+        drule::LoadRules();
+      }
     }
+
+    GetStyleReader().SetCurrentStyle(originMapStyle);
 
     LOG(LDEBUG, ("Reading of classificator finished"));
   }
