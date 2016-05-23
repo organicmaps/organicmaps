783e35005d9aa727007fe20eefa830d8fa03cd7c
Daria Volvenkova <d.volvenkova@corp.mail.ru>
2016-05-23 19:35:55 +0300

    Fixed the arrow position recovering after a routing deactivation.

--- drape_frontend/animation/animation.cpp
+++ drape_frontend/animation/animation.cpp
@@ -5,8 +5,19 @@ namespace df
 
 bool Animation::CouldBeBlendedWith(Animation const & animation) const
 {
-  return (GetType() != animation.GetType()) &&
-      m_couldBeBlended && animation.m_couldBeBlended;
+  bool hasSameObject = false;
+  TAnimObjects const & objects = animation.GetObjects();
+  for (auto const & object : objects)
+  {
+    if (HasObject(object))
+    {
+      hasSameObject = true;
+      break;
+    }
+  }
+
+  return !hasSameObject || ((GetType() != animation.GetType()) &&
+      m_couldBeBlended && animation.m_couldBeBlended);
 }
 
 } // namespace df
