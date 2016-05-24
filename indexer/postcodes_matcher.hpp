ebf0f1d3e776bab62e21c322a7ed30c6fac5c485
Sergey Magidovich <mgsergio@mapswithme.com>
2016-05-24 11:24:05 +0300

    Fix comment.

--- indexer/postcodes_matcher.hpp
+++ indexer/postcodes_matcher.hpp
@@ -9,8 +9,8 @@ namespace search
 {
 bool LooksLikePostcode(StringSliceBase const & slice, bool handleAsPrefix);
 /// Splits s into tokens and call LooksLikePostcode(TokenSlice) on the result.
-/// If checkPrefix is true returns true if some postcode starts with s.
-/// If checkPrefix is false returns true if s equals to some postcode.
+/// If handleAsPrefix is true returns true if some postcode starts with s.
+/// If handleAsPrefix is false returns true if s equals to some postcode.
 bool LooksLikePostcode(string const & s, bool handleAsPrefix);
 
 size_t GetMaxNumTokensInPostcode();
