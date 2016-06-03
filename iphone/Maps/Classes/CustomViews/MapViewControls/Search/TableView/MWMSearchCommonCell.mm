970d617f4bbf2f93f5f59d9dfa5e0db1002d435b
Ilya Grechuhin <i.grechuhin@gmail.com>
2016-06-03 14:53:18 +0300

    [ios] Fixed search layout for ios 7.

--- iphone/Maps/Classes/CustomViews/MapViewControls/Search/TableView/MWMSearchCommonCell.mm
+++ iphone/Maps/Classes/CustomViews/MapViewControls/Search/TableView/MWMSearchCommonCell.mm
@@ -72,6 +72,19 @@
     [self layoutIfNeeded];
 }
 
+- (void)layoutSubviews
+{
+  [super layoutSubviews];
+  if (isIOS7)
+  {
+    self.typeLabel.preferredMaxLayoutWidth = floor(self.typeLabel.width);
+    self.infoLabel.preferredMaxLayoutWidth = floor(self.infoLabel.width);
+    self.locationLabel.preferredMaxLayoutWidth = floor(self.locationLabel.width);
+    self.distanceLabel.preferredMaxLayoutWidth = floor(self.distanceLabel.width);
+    [super layoutSubviews];
+  }
+}
+
 - (void)setInfoText:(NSString *)infoText
 {
   self.infoView.hidden = NO;
