ab37ccd357eb84d017bb315b8cf384b3d8f57f91
Ilya Grechuhin <i.grechuhin@gmail.com>
2016-06-08 09:54:20 +0300

    [ios] Added Pushwoosh Framework.

--- iphone/Maps/Classes/MapsAppDelegate.mm
+++ iphone/Maps/Classes/MapsAppDelegate.mm
@@ -19,6 +19,7 @@
 #import "UIFont+MapsMeFonts.h"
 #import <CoreTelephony/CTTelephonyNetworkInfo.h>
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <Pushwoosh/PushNotificationManager.h>
 
 #import "3party/Alohalytics/src/alohalytics_objc.h"
 
@@ -162,20 +163,42 @@ using namespace osm_auth_ios;
 
 - (void)initPushNotificationsWithLaunchOptions:(NSDictionary *)launchOptions
 {
-  // Do not initialize Parse for open-source version due to an error:
-  // Terminating app due to uncaught exception 'NSInternalInconsistencyException', reason: ''applicationId' should not be nil.'
-  //if (!string(PARSE_APPLICATION_ID).empty())
+  // Do not initialize Pushwoosh for open-source version.
+  if (string(PUSHWOOSH_APPLICATION_ID).empty())
+    return;
+  [PushNotificationManager initializeWithAppCode:@(PUSHWOOSH_APPLICATION_ID) appName:[[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleDisplayName"]];
+  PushNotificationManager * pushManager = [PushNotificationManager pushManager];
+
+  // handling push on app start
+  [pushManager handlePushReceived:launchOptions];
+
+  // make sure we count app open in Pushwoosh stats
+  [pushManager sendAppOpen];
+
+  // register for push notifications!
+  [pushManager registerForPushNotifications];
 }
 
+// system push notification registration success callback, delegate to pushManager
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
 {
-  //[Alohalytics logEvent:kPushDeviceTokenLogEvent withValue:currentInstallation.deviceToken];
+  PushNotificationManager * pushManager = [PushNotificationManager pushManager];
+  [pushManager handlePushRegistration:deviceToken];
+  [Alohalytics logEvent:kPushDeviceTokenLogEvent withValue:pushManager.getHWID];
 }
 
+// system push notification registration error callback, delegate to pushManager
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+  [[PushNotificationManager pushManager] handlePushRegistrationFailure:error];
+}
+
+// system push notifications callback, delegate to pushManager
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
 {
   [Statistics logEvent:kStatEventName(kStatApplication, kStatPushReceived) withParameters:userInfo];
-  [self handleURLPush:userInfo];
+  if (![self handleURLPush:userInfo])
+    [[PushNotificationManager pushManager] handlePushReceived:userInfo];
   completionHandler(UIBackgroundFetchResultNoData);
 }
 
@@ -387,6 +410,7 @@ using namespace osm_auth_ios;
   [self.mapViewController onEnterForeground];
   self.isDaemonMode = NO;
 
+  [self initPushNotificationsWithLaunchOptions:launchOptions];
   [self commonInit];
 
   LocalNotificationManager * notificationManager = [LocalNotificationManager sharedManager];
