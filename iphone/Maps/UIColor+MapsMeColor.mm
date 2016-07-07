c217a7c142e315b2eeffe0da041afb5eb2c0555b
Ilya Grechuhin <i.grechuhin@gmail.com>
2016-07-07 09:58:40 +0300

    [ios] Added colors for red button.

--- iphone/Maps/UIColor+MapsMeColor.mm
+++ iphone/Maps/UIColor+MapsMeColor.mm
@@ -41,6 +41,8 @@ NSDictionary<NSString *, UIColor *> * night =
   @"linkBlue" : [UIColor colorWithRed:scaled(255.) green:scaled(230.) blue:scaled(140.) alpha:alpha100],
   @"linkBlueHighlighted" : [UIColor colorWithRed:scaled(255.) green:scaled(230.) blue:scaled(140.) alpha:alpha30],
   @"linkBlueDark" : [UIColor colorWithRed:scaled(200.) green:scaled(180.) blue:scaled(110.) alpha:alpha100],
+  @"buttonRed" : [UIColor colorWithRed:scaled(244.) green:scaled(67.) blue:scaled(67.) alpha:alpha100],
+  @"buttonRedHighlighted" : [UIColor colorWithRed:scaled(183.) green:scaled(28.) blue:scaled(28.) alpha:alpha100],
   @"blackPrimaryText" : [UIColor colorWithWhite:1. alpha:alpha90],
   @"blackSecondaryText" : [UIColor colorWithWhite:1. alpha:alpha70],
   @"blackHintText" : [UIColor colorWithWhite:1. alpha:alpha30],
@@ -72,6 +74,8 @@ NSDictionary<NSString *, UIColor *> * day =
   @"linkBlue" : [UIColor colorWithRed:scaled(30.) green:scaled(150.) blue:scaled(240.) alpha:alpha100],
   @"linkBlueHighlighted" : [UIColor colorWithRed:scaled(30.) green:scaled(150.) blue:scaled(240.) alpha:alpha30],
   @"linkBlueDark" : [UIColor colorWithRed:scaled(25.) green:scaled(135.) blue:scaled(215.) alpha:alpha100],
+  @"buttonRed" : [UIColor colorWithRed:scaled(244.) green:scaled(67.) blue:scaled(67.) alpha:alpha100],
+  @"buttonRedHighlighted" : [UIColor colorWithRed:scaled(183.) green:scaled(28.) blue:scaled(28.) alpha:alpha100],
   @"blackPrimaryText" : [UIColor colorWithWhite:0. alpha:alpha87],
   @"blackSecondaryText" : [UIColor colorWithWhite:0. alpha:alpha54],
   @"blackHintText" : [UIColor colorWithWhite:0. alpha:alpha26],
@@ -185,6 +189,16 @@ UIColor * color(SEL cmd)
 }
 
 + (UIColor *)linkBlueDark
+{
+  return color(_cmd);
+}
+
++ (UIColor *)buttonRed
+{
+  return color(_cmd);
+}
+
++ (UIColor *)buttonRedHighlighted
 {
   return color(_cmd);
 }
