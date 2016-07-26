679870edf1dd93d30778c1cdbf3ed920ff31f065
Ilya Grechuhin <i.grechuhin@gmail.com>
2016-07-26 13:05:03 +0300

    [ios] Added map status check to PP action buttons state.

--- iphone/Maps/Classes/MWMPlacePageButtonCell.mm
+++ iphone/Maps/Classes/MWMPlacePageButtonCell.mm
@@ -1,12 +1,14 @@
 #import "MWMPlacePageButtonCell.h"
 #import "MWMPlacePageViewManager.h"
 #import "UIColor+MapsMeColor.h"
+#import "MWMFrameworkListener.h"
 
-@interface MWMPlacePageButtonCell ()
+@interface MWMPlacePageButtonCell ()<MWMFrameworkStorageObserver>
 
 @property (weak, nonatomic) MWMPlacePageViewManager * manager;
 @property (weak, nonatomic) IBOutlet UIButton * titleButton;
 @property (nonatomic) MWMPlacePageCellType type;
+@property (nonatomic) storage::TCountryId countryId;
 
 @end
 
@@ -14,26 +16,10 @@
 
 - (void)config:(MWMPlacePageViewManager *)manager forType:(MWMPlacePageCellType)type
 {
+  self.countryId = GetFramework().CountryInfoGetter().GetRegionCountryId(manager.entity.mercator);
   self.manager = manager;
-  switch (type)
-  {
-  case MWMPlacePageCellTypeAddBusinessButton:
-    [self.titleButton setTitle:L(@"placepage_add_business_button") forState:UIControlStateNormal];
-    break;
-  case MWMPlacePageCellTypeEditButton:
-    [self.titleButton setTitle:L(@"edit_place") forState:UIControlStateNormal];
-    break;
-  case MWMPlacePageCellTypeAddPlaceButton:
-    [self.titleButton setTitle:L(@"placepage_add_place_button") forState:UIControlStateNormal];
-    break;
-  case MWMPlacePageCellTypeBookingMore:
-    [self.titleButton setTitle:L(@"details") forState:UIControlStateNormal];
-    break;
-  default:
-    NSAssert(false, @"Invalid place page cell type!");
-    break;
-  }
   self.type = type;
+  [self refreshButtonEnabledState];
 }
 
 - (IBAction)buttonTap
@@ -55,6 +41,51 @@
   default:
     NSAssert(false, @"Incorrect cell type!");
     break;
+  }
+}
+
+- (void)refreshButtonEnabledState
+{
+  NodeAttrs nodeAttrs;
+  GetFramework().Storage().GetNodeAttrs(self.countryId, nodeAttrs);
+  auto const status = nodeAttrs.m_status;
+  self.titleButton.enabled = status == NodeStatus::OnDisk || status == NodeStatus::OnDiskOutOfDate;
+}
+
+#pragma mark - MWMFrameworkStorageObserver
+
+- (void)processCountryEvent:(TCountryId const &)countryId
+{
+  if (self.countryId != countryId)
+    return;
+  [self refreshButtonEnabledState];
+}
+
+#pragma mark - Properties
+
+- (void)setType:(MWMPlacePageCellType)type
+{
+  _type = type;
+  switch (type)
+  {
+    case MWMPlacePageCellTypeAddBusinessButton:
+      [self.titleButton setTitle:L(@"placepage_add_business_button") forState:UIControlStateNormal];
+      [MWMFrameworkListener addObserver:self];
+      break;
+    case MWMPlacePageCellTypeEditButton:
+      [self.titleButton setTitle:L(@"edit_place") forState:UIControlStateNormal];
+      [MWMFrameworkListener addObserver:self];
+      break;
+    case MWMPlacePageCellTypeAddPlaceButton:
+      [self.titleButton setTitle:L(@"placepage_add_place_button") forState:UIControlStateNormal];
+      [MWMFrameworkListener addObserver:self];
+      break;
+    case MWMPlacePageCellTypeBookingMore:
+      [self.titleButton setTitle:L(@"details") forState:UIControlStateNormal];
+      break;
+    default:
+      NSAssert(false, @"Invalid place page cell type!");
+      break;
   }
 }
 
