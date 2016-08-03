9b91217fe119e0b06190271f5c2a8f99113f0d02
Ilya Grechuhin <i.grechuhin@gmail.com>
2016-08-03 10:29:59 +0300

    [ios] Fixed back button orientation for Arabic language.

---
+++ iphone/Maps/UIButton+Orientation.mm
@@ -0,0 +1,12 @@
+#import "Common.h"
+#import "UIButton+Orientation.h"
+
+@implementation UIButton (Orientation)
+
+- (void)matchInterfaceOrientation
+{
+  if (isInterfaceRightToLeft())
+    self.imageView.transform = CGAffineTransformMakeScale(-1, 1);
+}
+
+@end
