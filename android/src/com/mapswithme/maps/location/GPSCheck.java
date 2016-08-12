fbd83374812d1a15b6882e2ae7569d5b7035ac4d
Vladimir Byko-Ianko <v.bykoianko@corp.mail.ru>
2016-08-12 15:21:13 +0300

    Review fixes.

--- android/src/com/mapswithme/maps/location/GPSCheck.java
+++ android/src/com/mapswithme/maps/location/GPSCheck.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.location.LocationManager;
 import android.util.Log;
 
+import com.mapswithme.maps.MwmApplication;
+
 public class GPSCheck extends BroadcastReceiver
 {
   @Override
@@ -13,7 +15,9 @@ public class GPSCheck extends BroadcastReceiver
 
     LocationManager locationManager = (LocationManager) context.getSystemService(context.LOCATION_SERVICE);
 
-    if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
+    if ((locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
+        && MwmApplication.get().isFrameworkInitialized() && MwmApplication.backgroundTracker().isForeground())
     {
       LocationHelper.INSTANCE.addLocationListener();
     }
