fa9b6c3805e070ae78633d35db3cf0c30bd80ea6
Добрый Ээх <bukharaev@gmail.com>
2016-10-12 12:09:53 +0300

    pull request #4478 review fixes

--- platform/socket.hpp
+++ platform/socket.hpp
@@ -1,19 +1,24 @@
 #pragma once
 
-#include "std/mutex.hpp"
 #include "std/string.hpp"
 #include "std/target_os.hpp"
-#include "std/vector.hpp"
 
 namespace platform
 {
 class Socket
 {
 public:
-  virtual ~Socket() {}
+  virtual ~Socket() = default;
+
+  // Open/Close contract:
+  // 1. You can call Open+Close pair multiple times for the same Socket instance.
+  // 2. There are should be Close call after each Open call.
+  // 3. Open+Open: second Open does nothing and returns false.
+  // 4. Close+Close: second Close does nothing,
   virtual bool Open(string const & host, uint16_t port) = 0;
   virtual void Close() = 0;
 
+  // Read is blocking, it waits for the 'count' data size.
   virtual bool Read(uint8_t * data, uint32_t count) = 0;
   virtual bool Write(uint8_t const * data, uint32_t count) = 0;
 
@@ -31,5 +36,4 @@ class PlatformSocket final : public Socket
   bool Write(uint8_t const * data, uint32_t count) override;
   void SetTimeout(uint32_t milliseconds) override;
 };
-
 }  // namespace platform
