752d95fa3d43da66a17cab32f4295a03c8cc84a6
Sergey Yershov <syershov@maps.me>
2016-11-16 17:44:39 +0300

    Delete old unused ios projects

--- iphone/QuartzBenchmark/Draw2D.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-@interface Draw2D : UIView {
-  int m_frameCount;
-  double m_totalTime;
-}
-
-@end
--- iphone/QuartzBenchmark/QuartzBenchmark/QuartzBenchmarkViewController.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface QuartzBenchmarkViewController : UIViewController {
-    
-}
-
-@end
