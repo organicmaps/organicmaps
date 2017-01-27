cd36eed793b64b2e76943d18bf20682feafd1a75
Ilya Grechuhin <i.grechuhin@gmail.com>
2017-01-27 13:58:13 +0300

    [cleanup] [ios] Naming cleanup.

--- iphone/Maps/Categories/UIView+Id.swift
+++
@@ -1,3 +0,0 @@
-extension UIView {
-  static func stringId() -> String { return toString(self) }
-}
--- iphone/Maps/UI/Storyboard/Storyboard.swift
+++ iphone/Maps/UI/Storyboard/Storyboard.swift
@@ -3,12 +3,12 @@ import UIKit
 @objc(MWMStoryboard)
 enum Storyboard: Int {
 
-  case Authorization
-  case LaunchScreen
-  case Main
-  case SearchFilters
-  case Settings
-  case Welcome
+  case authorization
+  case launchScreen
+  case main
+  case searchFilters
+  case settings
+  case welcome
 }
 
 
@@ -17,12 +17,12 @@ extension UIStoryboard {
   static func instance(_ id: Storyboard) -> UIStoryboard {
     let name: String
     switch id {
-    case .Authorization: name = "Authorization"
-    case .LaunchScreen: name = "LaunchScreen"
-    case .Main: name = "Main"
-    case .SearchFilters: name = "SearchFilters"
-    case .Settings: name = "Settings"
-    case .Welcome: name = "Welcome"
+    case .authorization: name = "Authorization"
+    case .launchScreen: name = "LaunchScreen"
+    case .main: name = "Main"
+    case .searchFilters: name = "SearchFilters"
+    case .settings: name = "Settings"
+    case .welcome: name = "Welcome"
     }
     return UIStoryboard(name: name, bundle: nil)
   }
