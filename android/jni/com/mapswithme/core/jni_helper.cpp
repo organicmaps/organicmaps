mapsme/omim

[android] Bound core debug logs to 'Enable logging' setting

android/jni/com/mapswithme/core/jni_helper.cpp
@@ -24,6 +24,7 @@ jclass g_platformSocketClazz;
 jclass g_utilsClazz;
 jclass g_bannerClazz;
 jclass g_arrayListClazz;
+jclass g_loggerFactory;
 
 extern "C"
 {
@@ -44,6 +45,7 @@ JNI_OnLoad(JavaVM * jvm, void *)
   g_platformSocketClazz = jni::GetGlobalClassRef(env, "com/mapswithme/maps/location/PlatformSocket");
   g_utilsClazz = jni::GetGlobalClassRef(env, "com/mapswithme/util/Utils");
   g_bannerClazz = jni::GetGlobalClassRef(env, "com/mapswithme/maps/bookmarks/data/Banner");
+  g_loggerFactory = jni::GetGlobalClassRef(env, "com/mapswithme/util/log/LoggerFactory");
 
   return JNI_VERSION_1_6;
 }
@@ -62,6 +64,7 @@ JNI_OnUnload(JavaVM *, void *)
   env->DeleteGlobalRef(g_platformSocketClazz);
   env->DeleteGlobalRef(g_utilsClazz);
   env->DeleteGlobalRef(g_bannerClazz);
+  env->DeleteGlobalRef(g_loggerFactory);
 }
 } // extern "C"
 

android/jni/com/mapswithme/core/jni_helper.hpp
@@ -18,6 +18,7 @@ extern jclass g_httpHeaderClazz;
 extern jclass g_platformSocketClazz;
 extern jclass g_utilsClazz;
 extern jclass g_bannerClazz;
+extern jclass g_loggerFactory;
 
 namespace jni
 {

android/jni/com/mapswithme/core/logging.cpp
@@ -1,13 +1,9 @@
+#include "jni_helper.hpp"
 #include "logging.hpp"
-
 #include "base/exception.hpp"
 #include "base/logging.hpp"
-
 #include "coding/file_writer.hpp"
-
-#include "platform/file_logging.hpp"
 #include "platform/platform.hpp"
-
 #include "../util/crashlytics.h"
 
 #include <android/log.h>
@@ -28,17 +24,21 @@ void AndroidMessage(LogLevel level, SrcPoint const & src, string const & s)
 
   switch (level)
   {
-  case LINFO: pr = ANDROID_LOG_INFO; break;
-  case LDEBUG: pr = ANDROID_LOG_DEBUG; break;
-  case LWARNING: pr = ANDROID_LOG_WARN; break;
-  case LERROR: pr = ANDROID_LOG_ERROR; break;
-  case LCRITICAL: pr = ANDROID_LOG_FATAL; break;
+    case LINFO: pr = ANDROID_LOG_INFO; break;
+    case LDEBUG: pr = ANDROID_LOG_DEBUG; break;
+    case LWARNING: pr = ANDROID_LOG_WARN; break;
+    case LERROR: pr = ANDROID_LOG_ERROR; break;
+    case LCRITICAL: pr = ANDROID_LOG_ERROR; break;
   }
 
+  JNIEnv * env = jni::GetEnv();
+  static jmethodID const logCoreMsgMethod = jni::GetStaticMethodID(env, g_loggerFactory,
+     "logCoreMessage", "(ILjava/lang/String;)V");
+
   string const out = DebugPrint(src) + " " + s;
+  env->CallStaticVoidMethod(g_loggerFactory, logCoreMsgMethod, pr, jni::ToJavaString(env, out));
   if (g_crashlytics)
     g_crashlytics->log(g_crashlytics, out.c_str());
-  __android_log_write(pr, "MapsWithMe_JNI", out.c_str());
 }
 
 void AndroidLogMessage(LogLevel level, SrcPoint const & src, string const & s)
@@ -49,12 +49,7 @@ void AndroidLogMessage(LogLevel level, SrcPoint const & src, string const & s)
 
 void AndroidAssertMessage(SrcPoint const & src, string const & s)
 {
-#ifdef MWM_LOG_TO_FILE
-  LogMessageFile(LCRITICAL, src, s);
-#else
   AndroidMessage(LCRITICAL, src, s);
-#endif
-
 #ifdef DEBUG
   assert(false);
 #else
@@ -64,11 +59,7 @@ void AndroidAssertMessage(SrcPoint const & src, string const & s)
 
 void InitSystemLog()
 {
-#ifdef MWM_LOG_TO_FILE
-  SetLogMessageFn(&LogMessageFile);
-#else
   SetLogMessageFn(&AndroidLogMessage);
-#endif
 }
 
 void InitAssertLog()
@@ -76,6 +67,13 @@ void InitAssertLog()
   SetAssertFunction(&AndroidAssertMessage);
 }
 
+void ToggleDebugLogs(bool enabled)
+{
+  if (enabled)
+    g_LogLevel = LDEBUG;
+  else
+    g_LogLevel = LINFO;
+}
 }
 
 extern "C" {

organicmaps/organicmaps

android/jni/com/mapswithme/core/jni_helper.cpp
@@ -24,6 +24,7 @@ jclass g_platformSocketClazz;
 jclass g_utilsClazz;
 jclass g_bannerClazz;
 jclass g_arrayListClazz;
+jclass g_loggerFactoryClazz;
 
 extern "C"
 {
@@ -44,6 +45,7 @@ JNI_OnLoad(JavaVM * jvm, void *)
   g_platformSocketClazz = jni::GetGlobalClassRef(env, "com/mapswithme/maps/location/PlatformSocket");
   g_utilsClazz = jni::GetGlobalClassRef(env, "com/mapswithme/util/Utils");
   g_bannerClazz = jni::GetGlobalClassRef(env, "com/mapswithme/maps/bookmarks/data/Banner");
+  g_loggerFactoryClazz = jni::GetGlobalClassRef(env, "com/mapswithme/util/log/LoggerFactory");
 
   return JNI_VERSION_1_6;
 }
@@ -62,6 +64,7 @@ JNI_OnUnload(JavaVM *, void *)
   env->DeleteGlobalRef(g_platformSocketClazz);
   env->DeleteGlobalRef(g_utilsClazz);
   env->DeleteGlobalRef(g_bannerClazz);
+  env->DeleteGlobalRef(g_loggerFactoryClazz);
 }
 } // extern "C"
 

android/jni/com/mapswithme/core/jni_helper.hpp
@@ -18,6 +18,7 @@ extern jclass g_httpHeaderClazz;
 extern jclass g_platformSocketClazz;
 extern jclass g_utilsClazz;
 extern jclass g_bannerClazz;
+extern jclass g_loggerFactoryClazz;
 
 namespace jni
 {

android/jni/com/mapswithme/core/logging.cpp
@@ -1,13 +1,9 @@
+#include "jni_helper.hpp"
 #include "logging.hpp"
-
 #include "base/exception.hpp"
 #include "base/logging.hpp"
-
 #include "coding/file_writer.hpp"
-
-#include "platform/file_logging.hpp"
 #include "platform/platform.hpp"
-
 #include "../util/crashlytics.h"
 
 #include <android/log.h>
@@ -28,17 +24,21 @@ void AndroidMessage(LogLevel level, SrcPoint const & src, string const & s)
 
   switch (level)
   {
-  case LINFO: pr = ANDROID_LOG_INFO; break;
-  case LDEBUG: pr = ANDROID_LOG_DEBUG; break;
-  case LWARNING: pr = ANDROID_LOG_WARN; break;
-  case LERROR: pr = ANDROID_LOG_ERROR; break;
-  case LCRITICAL: pr = ANDROID_LOG_FATAL; break;
+    case LINFO: pr = ANDROID_LOG_INFO; break;
+    case LDEBUG: pr = ANDROID_LOG_DEBUG; break;
+    case LWARNING: pr = ANDROID_LOG_WARN; break;
+    case LERROR: pr = ANDROID_LOG_ERROR; break;
+    case LCRITICAL: pr = ANDROID_LOG_ERROR; break;
   }
 
+  JNIEnv * env = jni::GetEnv();
+  static jmethodID const logCoreMsgMethod = jni::GetStaticMethodID(env, g_loggerFactoryClazz,
+     "logCoreMessage", "(ILjava/lang/String;)V");
+
   string const out = DebugPrint(src) + " " + s;
+  env->CallStaticVoidMethod(g_loggerFactoryClazz, logCoreMsgMethod, pr, jni::ToJavaString(env, out));
   if (g_crashlytics)
     g_crashlytics->log(g_crashlytics, out.c_str());
-  __android_log_write(pr, "MapsWithMe_JNI", out.c_str());
 }
 
 void AndroidLogMessage(LogLevel level, SrcPoint const & src, string const & s)
@@ -49,12 +49,7 @@ void AndroidLogMessage(LogLevel level, SrcPoint const & src, string const & s)
 
 void AndroidAssertMessage(SrcPoint const & src, string const & s)
 {
-#ifdef MWM_LOG_TO_FILE
-  LogMessageFile(LCRITICAL, src, s);
-#else
   AndroidMessage(LCRITICAL, src, s);
-#endif
-
 #ifdef DEBUG
   assert(false);
 #else
@@ -64,11 +59,7 @@ void AndroidAssertMessage(SrcPoint const & src, string const & s)
 
 void InitSystemLog()
 {
-#ifdef MWM_LOG_TO_FILE
-  SetLogMessageFn(&LogMessageFile);
-#else
   SetLogMessageFn(&AndroidLogMessage);
-#endif
 }
 
 void InitAssertLog()
@@ -76,6 +67,13 @@ void InitAssertLog()
   SetAssertFunction(&AndroidAssertMessage);
 }
 
+void ToggleDebugLogs(bool enabled)
+{
+  if (enabled)
+    g_LogLevel = LDEBUG;
+  else
+    g_LogLevel = LINFO;
+}
 }
 
 extern "C" {