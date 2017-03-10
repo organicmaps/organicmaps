185fbecda79c620d64301b86204f08649f1eeed1
Arsentiy Milchakov <milcars@mapswithme.com>
2017-03-10 15:36:09 +0300

    Http client fix encoding for osrm

--- platform/http_client.cpp
+++ platform/http_client.cpp
@@ -10,7 +10,11 @@ namespace platform
 {
 HttpClient::HttpClient(string const & url) : m_urlRequested(url)
 {
-  m_headers.emplace("Accept-Encoding", "deflate");
+// Http client for linux supports only "deflate" encoding, but osrm server cannot
+// correctly process "Accept-Encoding: deflate" header, so do not encode data on linux.
+#if !defined(OMIM_OS_LINUX)
+  m_headers.emplace("Accept-Encoding", "gzip, deflate");
+#endif
 }
 
 HttpClient & HttpClient::SetUrlRequested(string const & url)
