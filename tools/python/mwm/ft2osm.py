67b1b0bced5fb30e9897296c627956ebdf80b711
Sergey Magidovich <mgsergio@mapswithme.com>
2017-04-07 14:55:10 +0300

    Add a script to generate mappings in csv.

--- tools/python/mwm/ft2osm.py
+++ tools/python/mwm/ft2osm.py
@@ -8,14 +8,17 @@ if len(sys.argv) < 3:
     sys.exit(1)
 
 with open(sys.argv[1], 'rb') as f:
-    ft2osm = mwm.read_osm2ft(f, True)
+    ft2osm = mwm.read_osm2ft(f, ft2osm=True)
 
 code = 0
+type_abbr = {'n': 'node', 'w': 'way', 'r': 'relation'}
 for ftid in sys.argv[2:]:
     ftid = int(ftid)
     if ftid in ft2osm:
-        type_abbr = {'n': 'node', 'w': 'way', 'r': 'relation'}
-        print('https://www.openstreetmap.org/{}/{}'.format(type_abbr[ft2osm[ftid][0]], ft2osm[ftid][1]))
+        print('https://www.openstreetmap.org/{}/{}'.format(
+            type_abbr[ft2osm[ftid][0]],
+            ft2osm[ftid][1]
+        ))
     else:
         print('Could not find osm id for feature {}'.format(ftid))
         code = 2
