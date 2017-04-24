771ba35be1a92b83c69180056cb1643b288721a1
Ilya Grechuhin <i.grechuhin@gmail.com>
2017-04-24 16:09:20 +0300

    [MAPSME-4331] [ios] Reduced min banner time on screen.
    30 -> 5

--- iphone/Maps/Core/Ads/Mopub/MopubBanner.swift
+++ iphone/Maps/Core/Ads/Mopub/MopubBanner.swift
@@ -1,7 +1,7 @@
 final class MopubBanner: NSObject, Banner {
   private enum Limits {
     static let minTimeOnScreen: TimeInterval = 3
-    static let minTimeSinceLastRequest: TimeInterval = 30
+    static let minTimeSinceLastRequest: TimeInterval = 5
   }
 
   fileprivate var success: Banner.Success!
