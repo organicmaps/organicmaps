25741e8844f2bc4568e3b488e2a3730581653e7c
Roman Romanov <rromanov@65gb.net>
2017-05-17 16:30:45 +0400

    [android] added mapswithme scheme

--- android/src/com/mapswithme/maps/DownloadResourcesActivity.java
+++ android/src/com/mapswithme/maps/DownloadResourcesActivity.java
@@ -635,7 +635,7 @@ public class DownloadResourcesActivity extends BaseMwmFragmentActivity
       if (TextUtils.isEmpty(scheme) || TextUtils.isEmpty(host))
         return false;
 
-      return scheme.equals("mapsme") && "lead".equals(host);
+      return (scheme.equals("mapsme") || scheme.equals("mapswithme")) && "lead".equals(host);
     }
 
     @Override
