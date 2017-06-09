3dc17dfbb84b24332f25a2ad4dff661b03fe08e5
Ilya Zverev <zverik@textual.ru>
2017-06-09 20:49:37 +0300

    [python] Update strings converters

--- tools/python/categories_merge_language.py
+++ tools/python/categories_merge_language.py
@@ -2,45 +2,44 @@
 import sys
 import os
 
+path = os.path.join(os.path.dirname(sys.argv[0]), '..', '..', 'data', 'categories.txt')
 if len(sys.argv) < 2:
     print('Merges some language in categories.txt with English')
     print('Usage: {} <lang> [path_to_categories.txt]'.format(sys.argv[0]))
+    print('Default path to categories: {}'.format(path))
+    if not os.path.exists(path):
+        print('Warning: default path to categories.txt will fail')
     sys.exit(1)
 
 lang = sys.argv[1]
 if len(sys.argv) > 2:
     path = sys.argv[2]
-else:
-    path = os.path.join(os.path.dirname(sys.argv[0]), '..', '..', 'data', 'categories.txt')
 
 with open(path, 'r') as f:
-    first = True
     langs = []
     trans = None
 
     def flush_langs():
-        for l in langs:
+        for lang in langs:
             if trans and l[0] == 'en':
-                parts = l[1].split('|')
+                parts = lang[1].split('|')
                 parts[0] = '{} - {}'.format(parts[0], trans)
-                l[1] = '|'.join(parts)
-            print(':'.join(l))
+                lang[1] = '|'.join(parts)
+            print(':'.join(lang))
 
-    for line in f:
-        if len(line.strip()) == 0 or line[0] == '#':
+    for line in map(str.strip, f):
+        if len(line) == 0 or line[0] == '#':
             if langs:
                 flush_langs()
                 langs = []
                 trans = None
-            print(line.strip())
-            first = True
-        elif first:
-            print(line.strip())
-            first = False
+            print(line)
+        elif not langs:
+            print(line)
         else:
             if ':' not in line:
                 raise Exception('Line {} is not a translation line'.format(line))
-            l = line.strip().split(':')
+            l = line.split(':')
             langs.append(l)
             if l[0] == lang:
                 trans = l[1].split('|')[0]
