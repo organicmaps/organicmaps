4a4899e5eac2cade6dc9c348fc520d687914eca7
Ilya Zverev <zverik@textual.ru>
2017-06-18 21:15:42 +0300

    [tts] Remove languages.hpp generation and move sound.txt to omim/data

--- tools/unix/generate_localizations.sh
+++ tools/unix/generate_localizations.sh
@@ -11,10 +11,5 @@ $TWINE generate-all-localization-files --format apple "$OMIM_PATH/strings.txt" "
 $TWINE generate-all-localization-files --format apple --file-name InfoPlist.strings "$OMIM_PATH/iphone/plist.txt" "$OMIM_PATH/iphone/Maps/LocalizedStrings/"
 $TWINE generate-all-localization-files --format jquery "$OMIM_PATH/data/cuisines.txt" "$OMIM_PATH/data/cuisine-strings/"
 $TWINE generate-all-localization-files --format jquery "$OMIM_PATH/data/countries_names.txt" "$OMIM_PATH/data/countries-strings/"
+$TWINE generate-all-localization-files --format jquery "$OMIM_PATH/data/sound.txt" "$OMIM_PATH/data/sound-strings/"
 #$TWINE generate-all-localization-files --include translated --format tizen "$OMIM_PATH/strings.txt" "$OMIM_PATH/tizen/MapsWithMe/res/" --tags tizen
-
-# Generate sound tts files.
-SOUND_DATA_DIR="$OMIM_PATH/sound/tts"
-LANGUAGES="$OMIM_PATH/tools/python/tts_languages.py"
-python "$LANGUAGES" "$SOUND_DATA_DIR/sound.txt" "$SOUND_DATA_DIR/languages.hpp"
-$TWINE generate-all-localization-files --format jquery "$SOUND_DATA_DIR/sound.txt" "$OMIM_PATH/data/sound-strings/"
