kcwu/omim

The MAPS.ME.Designer tool for DRAPE 2.0

qt/update_dialog.cpp
@@ -54,6 +54,21 @@ enum
 #define COLOR_INQUEUE         Qt::gray
 #define COLOR_OUTOFDATE       Qt::magenta
 
+namespace
+{
+MapOptions GetMapOptionsAvailableForDownload(Storage & st, TIndex const & countryIndex)
+{
+  platform::CountryFile const & countryFile = st.GetCountryFile(countryIndex);
+  bool const hasCarRouting = (0 != countryFile.GetRemoteSize(MapOptions::CarRouting));
+
+  MapOptions options = MapOptions::Map;
+  if (hasCarRouting)
+    options = SetOptions(options, MapOptions::CarRouting);
+
+  return options;
+}
+}  // namespace
+
 namespace qt
 {
   /// adds custom sorting for "Size" column
@@ -150,7 +165,7 @@ namespace qt
         QAbstractButton * res = ask.clickedButton();
 
         if (res == btns[0])
-          m_framework.GetActiveMaps()->DownloadMap(countryIndex, MapOptions::MapWithCarRouting);
+          m_framework.GetActiveMaps()->DownloadMap(countryIndex, GetMapOptionsAvailableForDownload(st, countryIndex));
 
         if (res == btns[1])
           m_framework.GetActiveMaps()->DeleteMap(countryIndex, MapOptions::MapWithCarRouting);
@@ -173,7 +188,7 @@ namespace qt
 
     case TStatus::ENotDownloaded:
     case TStatus::EDownloadFailed:
-      m_framework.GetActiveMaps()->DownloadMap(countryIndex, MapOptions::MapWithCarRouting);
+      m_framework.GetActiveMaps()->DownloadMap(countryIndex, GetMapOptionsAvailableForDownload(st, countryIndex));
       break;
 
     case TStatus::EInQueue:

organicmaps/organicmaps

qt/update_dialog.cpp
@@ -57,6 +57,21 @@ bool DeleteNotUploadedEditsConfirmation()
 }
 }  // namespace
 
+namespace
+{
+MapOptions GetMapOptionsAvailableForDownload(Storage & st, TCountryId const & countryIndex)
+{
+  platform::CountryFile const & countryFile = st.GetCountryFile(countryIndex);
+  bool const hasCarRouting = (0 != countryFile.GetRemoteSize(MapOptions::CarRouting));
+
+  MapOptions options = MapOptions::Map;
+  if (hasCarRouting)
+    options = SetOptions(options, MapOptions::CarRouting);
+
+  return options;
+}
+}  // namespace
+
 namespace qt
 {
   /// adds custom sorting for "Size" column