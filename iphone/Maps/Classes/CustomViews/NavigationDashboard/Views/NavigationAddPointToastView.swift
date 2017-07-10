8e6a84af5bca60e56486e942f784965d18708b55
Ilya Grechuhin <i.grechuhin@gmail.com>
2017-07-10 18:31:11 +0300

    [MAPSME-4930] [ios] Updated (#2) navigation toast view.

--- iphone/Maps/Classes/CustomViews/NavigationDashboard/Views/NavigationAddPointToastView.swift
+++ iphone/Maps/Classes/CustomViews/NavigationDashboard/Views/NavigationAddPointToastView.swift
@@ -4,36 +4,30 @@ final class NavigationAddPointToastView: UIView {
     didSet {
       actionButton.setTitleColor(UIColor.linkBlue(), for: .normal)
       actionButton.tintColor = UIColor.linkBlue()
-      actionButton.titleLabel?.font = UIFont.regular17()
+      actionButton.titleLabel?.font = UIFont.regular16()
     }
   }
   @IBOutlet private var locationButton: UIButton! {
     didSet {
-      locationButton.setTitle(L("button_use"), for: .normal)
-      locationButton.setTitleColor(UIColor.linkBlue(), for: .normal)
       locationButton.tintColor = UIColor.linkBlue()
-      locationButton.titleLabel?.font = UIFont.regular17()
-
-      let flipTransform = CGAffineTransform(scaleX: -1, y: 1)
-      locationButton.transform = flipTransform
-      locationButton.titleLabel?.transform = flipTransform
-      locationButton.imageView?.transform = flipTransform
     }
   }
-  @IBOutlet private var locationButtonHidden: NSLayoutConstraint!
+  @IBOutlet private var separator: UIView! {
+    didSet {
+      separator.backgroundColor = UIColor.blackDividers()
+    }
+  }
 
   func config(text: String, withLocationButton: Bool) {
     actionButton.setTitle(text, for: .normal)
     backgroundColor = UIColor.white()
 
-    setNeedsLayout()
     if withLocationButton {
-      locationButtonHidden.priority = UILayoutPriorityDefaultHigh
       locationButton.isHidden = false
+      separator.isHidden = false
     } else {
-      locationButtonHidden.priority = UILayoutPriorityDefaultLow
       locationButton.isHidden = true
+      separator.isHidden = true
     }
-    UIView.animate(withDuration: kDefaultAnimationDuration) { self.layoutIfNeeded() }
   }
 }
