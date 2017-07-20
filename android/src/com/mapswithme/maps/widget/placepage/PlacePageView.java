dd2a9e3124e675dad38c4c9235d4eb7219ac1308
Roman Romanov <rromanov@65gb.net>
2017-07-20 11:58:04 +0400

    [android] viator view initialization fix

--- android/src/com/mapswithme/maps/widget/placepage/PlacePageView.java
+++ android/src/com/mapswithme/maps/widget/placepage/PlacePageView.java
@@ -446,6 +446,7 @@ public class PlacePageView extends RelativeLayout
     initHotelGalleryView();
     initHotelNearbyView();
     initHotelRatingView();
+    initViatorView();
 
     initUgcView();
 
