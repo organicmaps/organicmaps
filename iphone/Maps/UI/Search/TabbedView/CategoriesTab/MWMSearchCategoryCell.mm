7f92f1d568c3a544791b34166aa8fa08fa91b531
Ilya Grechuhin <i.grechuhin@gmail.com>
2017-07-24 17:19:32 +0300

    [MAPSME-4971] [ios] Updated cian title in search categories.

--- iphone/Maps/UI/Search/TabbedView/CategoriesTab/MWMSearchCategoryCell.mm
+++ iphone/Maps/UI/Search/TabbedView/CategoriesTab/MWMSearchCategoryCell.mm
@@ -27,16 +27,17 @@ extern NSString * const kCianCategory;
 - (void)setCategory:(NSString *)category
 {
   UILabel * label = self.label;
-  label.text = L(category);
   label.textColor = [UIColor blackPrimaryText];
   self.icon.mwm_name = [NSString stringWithFormat:@"ic_%@", category];
   if ([category isEqualToString:kCianCategory])
   {
+    label.text = L(@"real_estate");
     self.adIcon.hidden = NO;
     self.adIcon.mwm_name = @"logo_cian";
   }
   else
   {
+    label.text = L(category);
     self.adIcon.hidden = YES;
   }
 }
