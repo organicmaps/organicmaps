8168ea1e604d9a75b314a8b739cf5a774a261a6e
Ilya Grechuhin <i.grechuhin@gmail.com>
2017-09-29 12:47:42 +0300

    [secure-storage] [ios] Added secure storage implementation.

--- platform/secure_storage_ios.mm
+++ platform/secure_storage_ios.mm
@@ -1,20 +1,31 @@
 #include "platform/secure_storage.hpp"
 
+#import <Foundation/Foundation.h>
+
 namespace platform
 {
+
+NSString * StorageKey(std::string const & key)
+{
+  return [NSString stringWithFormat:@"Maps.me::PlatrormKey::%@", @(key.c_str())];
+}
+
 void SecureStorage::Save(std::string const & key, std::string const & value)
 {
-  // TODO: implement @igrechuhin
+  [NSUserDefaults.standardUserDefaults setObject:@(value.c_str()) forKey:StorageKey(key)];
 }
 
 bool SecureStorage::Load(std::string const & key, std::string & value)
 {
-  // TODO: implement @igrechuhin
-  return false;
+  NSString * val = [NSUserDefaults.standardUserDefaults objectForKey:StorageKey(key)];
+  if (!val)
+    return false;
+  value = val.UTF8String;
+  return true;
 }
 
 void SecureStorage::Remove(std::string const & key)
 {
-  // TODO: implement @igrechuhin
+  [NSUserDefaults.standardUserDefaults removeObjectForKey:StorageKey(key)];
 }
 }  // namespace platform
