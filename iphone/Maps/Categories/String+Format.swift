f01432d41c7a463e9440c4a5ae4313f5b1355eb3
Ilya Grechuhin <i.grechuhin@gmail.com>
2017-11-10 10:11:21 +0300

    [cherry] [release-76-crash-fix] [ios] Fixed crash with wrong format specifier.

--- iphone/Maps/Categories/String+Format.swift
+++ iphone/Maps/Categories/String+Format.swift
@@ -1,6 +1,6 @@
 extension String {
   init(coreFormat: String, arguments: [CVarArg]) {
     let format = coreFormat.replacingOccurrences(of: "%s", with: "%@")
-    self.init(format: format, arguments: arguments)
+    self.init(format: format, arguments: arguments.map { "\($0)" })
   }
 }
