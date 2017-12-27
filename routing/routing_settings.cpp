19b006d9c86f230bdae564eab1d940ceff5d0f49
Vladimir Byko-Ianko <v.bykoianko@corp.mail.ru>
2017-12-27 10:10:56 +0300

    Widening passage for transit routing.

--- routing/routing_settings.cpp
+++ routing/routing_settings.cpp
@@ -7,9 +7,12 @@ RoutingSettings GetRoutingSettings(VehicleType vehicleType)
   switch (vehicleType)
   {
   case VehicleType::Pedestrian:
-  case VehicleType::Transit:
     return {true /* m_matchRoute */,         false /* m_soundDirection */,
             20. /* m_matchingThresholdM */,  true /* m_keepPedestrianInfo */,
+            false /* m_showTurnAfterNext */, false /* m_speedCameraWarning*/};
+  case VehicleType::Transit:
+    return {true /* m_matchRoute */,         false /* m_soundDirection */,
+            40. /* m_matchingThresholdM */,  true /* m_keepPedestrianInfo */,
             false /* m_showTurnAfterNext */, false /* m_speedCameraWarning*/};
   case VehicleType::Bicycle:
     return {true /* m_matchRoute */,         true /* m_soundDirection */,
