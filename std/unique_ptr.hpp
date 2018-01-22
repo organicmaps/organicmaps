38de57a77a28a1484dc9b3e2b762a5dfe33f2890
greshilov <slovaricheg@gmail.com>
2018-01-22 20:43:26 +0300

    [cmake] Adding c++14 support - Fix XCode build - Remove old compile opt from skin_generator project

--- std/unique_ptr.hpp
+++ std/unique_ptr.hpp
@@ -6,14 +6,7 @@
 
 #include <memory>
 using std::unique_ptr;
-
-/// @todo(y): replace this hand-written helper function by
-/// std::make_unique when it will be available in C++14
-template <typename T, typename... Args>
-unique_ptr<T> make_unique(Args &&... args)
-{
-  return unique_ptr<T>(new T(std::forward<Args>(args)...));
-}
+using std::make_unique;
 
 #ifdef DEBUG_NEW
 #define new DEBUG_NEW
