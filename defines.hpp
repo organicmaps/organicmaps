tatiana-kondakova/omim

Add LocalityIndexBuilder

defines.hpp
@@ -45,6 +45,9 @@
 #define TRANSIT_FILE_TAG "transit"
 #define UGC_FILE_TAG "ugc"
 
+#define LOCALITY_DATA_FILE_TAG "loc_data"
+#define LOCALITY_INDEX_FILE_TAG "loc_idx"
+
 #define READY_FILE_EXTENSION ".ready"
 #define RESUME_FILE_EXTENSION ".resume"
 #define DOWNLOADING_FILE_EXTENSION ".downloading"
@@ -56,6 +59,10 @@
 #define GEOM_INDEX_TMP_EXT ".geomidx.tmp"
 #define CELL2FEATURE_SORTED_EXT ".c2f.sorted"
 #define CELL2FEATURE_TMP_EXT ".c2f.tmp"
+
+#define LOCALITY_INDEX_TMP_EXT ".locidx.tmp"
+#define CELL2LOCALITY_SORTED_EXT ".c2l.sorted"
+#define CELL2LOCALITY_TMP_EXT ".c2l.tmp"
 
 #define COUNTRIES_FILE "countries.txt"
 #define COUNTRIES_META_FILE "countries_meta.txt"

indexer/geometry_serialization.hpp
@@ -153,9 +153,19 @@ namespace serial
   }
 
   inline void const * LoadInnerTriangles(void const * pBeg, size_t count,
-                                         CodingParams const & params, OutPointsT & points)
+                                         CodingParams const & params, OutPointsT & triangles)
   {
-    return LoadInner(&geo_coding::DecodeTriangleStrip, pBeg, count, params, points);
+    triangles.clear();
+    OutPointsT points;
+    void const * res = LoadInner(&geo_coding::DecodeTriangleStrip, pBeg, count, params, points);
+
+    for (uint8_t i = 2; i < count; ++i)
+    {
+      triangles.push_back(points[i - 2]);
+      triangles.push_back(points[i - 1]);
+      triangles.push_back(points[i]);
+    }
+    return res;
   }
 
   class TrianglesChainSaver

indexer/locality_index_builder.hpp
@@ -1,0 +1,59 @@
+#pragma once
+#include "indexer/cell_id.hpp"
+#include "indexer/cell_value_pair.hpp"
+#include "indexer/feature_covering.hpp"
+#include "indexer/interval_index_builder.hpp"
+#include "indexer/locality_object.hpp"
+#include "indexer/scales.hpp"
+
+#include "coding/dd_vector.hpp"
+#include "coding/file_sort.hpp"
+#include "coding/writer.hpp"
+
+#include "base/logging.hpp"
+#include "base/macros.hpp"
+#include "base/scope_guard.hpp"
+
+#include "defines.hpp"
+
+#include <string>
+#include <vector>
+
+namespace covering
+{
+template <class TObjectsVector, class TWriter>
+void BuildLocalityIndex(TObjectsVector const & objects, TWriter & writer,
+                        string const & tmpFilePrefix)
+{
+  string const cellsToValueFile = tmpFilePrefix + CELL2LOCALITY_SORTED_EXT + ".all";
+  MY_SCOPE_GUARD(cellsToValueFileGuard, bind(&FileWriter::DeleteFileX, cellsToValueFile));
+  {
+    FileWriter cellsToValueWriter(cellsToValueFile);
+
+    WriterFunctor<FileWriter> out(cellsToValueWriter);
+    FileSorter<CellValuePair<uint64_t>, WriterFunctor<FileWriter>> sorter(
+        1024 * 1024 /* bufferBytes */, tmpFilePrefix + CELL2LOCALITY_TMP_EXT, out);
+    objects.ForEach([&sorter](indexer::LocalityObject const & o) {
+      // @todo(t.yan): adjust cellPenaltyArea for whole world locality index.
+      std::vector<int64_t> const cells =
+          covering::CoverLocality(o, GetCodingDepth(scales::GetUpperScale()), 250 /* cellPenaltyArea */);
+      for (auto const & cell : cells)
+        sorter.Add(CellValuePair<uint64_t>(cell, o.GetStoredId()));
+    });
+    sorter.SortAndFinish();
+  }
+
+  FileReader reader(cellsToValueFile);
+  DDVector<CellValuePair<uint64_t>, FileReader, uint64_t> cellsToValue(reader);
+
+  {
+    BuildIntervalIndex(cellsToValue.begin(), cellsToValue.end(), writer,
+                       RectId::DEPTH_LEVELS * 2 + 1);
+  }
+}
+}  // namespace covering
+
+namespace indexer
+{
+bool BuildLocalityIndexFromDataFile(std::string const & datFile, std::string const & tmpFile);
+}  // namespace indexer

organicmaps/organicmaps

defines.hpp
@@ -45,6 +45,9 @@
 #define TRANSIT_FILE_TAG "transit"
 #define UGC_FILE_TAG "ugc"
 
+#define LOCALITY_DATA_FILE_TAG "locdata"
+#define LOCALITY_INDEX_FILE_TAG "locidx"
+
 #define READY_FILE_EXTENSION ".ready"
 #define RESUME_FILE_EXTENSION ".resume"
 #define DOWNLOADING_FILE_EXTENSION ".downloading"
@@ -56,6 +59,10 @@
 #define GEOM_INDEX_TMP_EXT ".geomidx.tmp"
 #define CELL2FEATURE_SORTED_EXT ".c2f.sorted"
 #define CELL2FEATURE_TMP_EXT ".c2f.tmp"
+
+#define LOCALITY_INDEX_TMP_EXT ".locidx.tmp"
+#define CELL2LOCALITY_SORTED_EXT ".c2l.sorted"
+#define CELL2LOCALITY_TMP_EXT ".c2l.tmp"
 
 #define COUNTRIES_FILE "countries.txt"
 #define COUNTRIES_META_FILE "countries_meta.txt"

indexer/geometry_serialization.hpp
@@ -153,9 +153,21 @@ namespace serial
   }
 
   inline void const * LoadInnerTriangles(void const * pBeg, size_t count,
-                                         CodingParams const & params, OutPointsT & points)
+                                         CodingParams const & params, OutPointsT & triangles)
   {
-    return LoadInner(&geo_coding::DecodeTriangleStrip, pBeg, count, params, points);
+    CHECK_GREATER_OR_EQUAL(count, 2, ());
+    triangles.clear();
+    OutPointsT points;
+    void const * res = LoadInner(&geo_coding::DecodeTriangleStrip, pBeg, count, params, points);
+
+    triangles.reserve((count - 2) * 3);
+    for (size_t i = 2; i < count; ++i)
+    {
+      triangles.push_back(points[i - 2]);
+      triangles.push_back(points[i - 1]);
+      triangles.push_back(points[i]);
+    }
+    return res;
   }
 
   class TrianglesChainSaver

indexer/locality_index_builder.hpp
@@ -1,0 +1,59 @@
+#pragma once
+#include "indexer/cell_id.hpp"
+#include "indexer/cell_value_pair.hpp"
+#include "indexer/feature_covering.hpp"
+#include "indexer/interval_index_builder.hpp"
+#include "indexer/locality_object.hpp"
+#include "indexer/scales.hpp"
+
+#include "coding/dd_vector.hpp"
+#include "coding/file_sort.hpp"
+#include "coding/writer.hpp"
+
+#include "base/logging.hpp"
+#include "base/macros.hpp"
+#include "base/scope_guard.hpp"
+
+#include "defines.hpp"
+
+#include <string>
+#include <vector>
+
+namespace covering
+{
+template <class TObjectsVector, class TWriter>
+void BuildLocalityIndex(TObjectsVector const & objects, TWriter & writer,
+                        string const & tmpFilePrefix)
+{
+  string const cellsToValueFile = tmpFilePrefix + CELL2LOCALITY_SORTED_EXT + ".all";
+  MY_SCOPE_GUARD(cellsToValueFileGuard, bind(&FileWriter::DeleteFileX, cellsToValueFile));
+  {
+    FileWriter cellsToValueWriter(cellsToValueFile);
+
+    WriterFunctor<FileWriter> out(cellsToValueWriter);
+    FileSorter<CellValuePair<uint64_t>, WriterFunctor<FileWriter>> sorter(
+        1024 * 1024 /* bufferBytes */, tmpFilePrefix + CELL2LOCALITY_TMP_EXT, out);
+    objects.ForEach([&sorter](indexer::LocalityObject const & o) {
+      // @todo(t.yan): adjust cellPenaltyArea for whole world locality index.
+      std::vector<int64_t> const cells =
+          covering::CoverLocality(o, GetCodingDepth(scales::GetUpperScale()), 250 /* cellPenaltyArea */);
+      for (auto const & cell : cells)
+        sorter.Add(CellValuePair<uint64_t>(cell, o.GetStoredId()));
+    });
+    sorter.SortAndFinish();
+  }
+
+  FileReader reader(cellsToValueFile);
+  DDVector<CellValuePair<uint64_t>, FileReader, uint64_t> cellsToValue(reader);
+
+  {
+    BuildIntervalIndex(cellsToValue.begin(), cellsToValue.end(), writer,
+                       RectId::DEPTH_LEVELS * 2 + 1);
+  }
+}
+}  // namespace covering
+
+namespace indexer
+{
+bool BuildLocalityIndexFromDataFile(std::string const & dataFile, std::string const & tmpFile);
+}  // namespace indexer