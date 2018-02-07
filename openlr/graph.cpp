mgsergio/omim

[OpenLR] Assemble all things together. Cache edges within one run.

openlr/graph.cpp
@@ -6,32 +6,58 @@ using namespace routing;
 
 namespace openlr
 {
+namespace
+{
+using EdgeGetterFn = void (IRoadGraph::*)(Junction const &, RoadGraphBase::TEdgeVector &) const;
+
+void GetRegularEdges(Junction const & junction, IRoadGraph const & graph,
+                     EdgeGetterFn const edgeGetterFn,
+                     map<openlr::Graph::Junction, Graph::EdgeVector> & cache,
+                     Graph::EdgeVector & edges)
+{
+  auto const it = cache.find(junction);
+  if (it == end(cache))
+  {
+    auto & es = cache[junction];
+    (graph.*edgeGetterFn)(junction, es);
+    edges.insert(end(edges), begin(es), end(es));
+  }
+  else
+  {
+    auto const & es = it->second;
+    edges.insert(end(edges), begin(es), end(es));
+  }
+}
+}  // namespace
+
 Graph::Graph(Index const & index, shared_ptr<CarModelFactory> carModelFactory)
   : m_graph(index, IRoadGraph::Mode::ObeyOnewayTag, carModelFactory)
 {
 }
 
-void Graph::GetOutgoingEdges(Junction const & junction, EdgeVector & edges) const
+void Graph::GetOutgoingEdges(Junction const & junction, EdgeVector & edges)
 {
-  m_graph.GetOutgoingEdges(junction, edges);
+  GetRegularOutgoingEdges(junction, edges);
+  m_graph.GetFakeOutgoingEdges(junction, edges);
 }
 
-void Graph::GetIngoingEdges(Junction const & junction, EdgeVector & edges) const
+void Graph::GetIngoingEdges(Junction const & junction, EdgeVector & edges)
 {
-  m_graph.GetIngoingEdges(junction, edges);
+  GetRegularIngoingEdges(junction, edges);
+  m_graph.GetFakeIngoingEdges(junction, edges);
 }
 
-void Graph::GetRegularOutgoingEdges(Junction const & junction, EdgeVector & edges) const
+void Graph::GetRegularOutgoingEdges(Junction const & junction, EdgeVector & edges)
 {
-  m_graph.GetRegularOutgoingEdges(junction, edges);
+  GetRegularEdges(junction, m_graph, &IRoadGraph::GetRegularOutgoingEdges, m_outgoingCache, edges);
 }
 
-void Graph::GetRegularIngoingEdges(Junction const & junction, EdgeVector & edges) const
+void Graph::GetRegularIngoingEdges(Junction const & junction, EdgeVector & edges)
 {
-  m_graph.GetRegularIngoingEdges(junction, edges);
+  GetRegularEdges(junction, m_graph, &IRoadGraph::GetRegularIngoingEdges, m_ingoingCache, edges);
 }
 
-void Graph::FindClosestEdges(m2::PointD const & point, uint32_t count,
+void Graph::FindClosestEdges(m2::PointD const & point, uint32_t const count,
                              vector<pair<Edge, Junction>> & vicinities) const
 {
   m_graph.FindClosestEdges(point, count, vicinities);

organicmaps/organicmaps

openlr/graph.cpp
@@ -6,32 +6,58 @@ using namespace routing;
 
 namespace openlr
 {
+namespace
+{
+using EdgeGetter = void (IRoadGraph::*)(Junction const &, RoadGraphBase::TEdgeVector &) const;
+
+void GetRegularEdges(Junction const & junction, IRoadGraph const & graph,
+                     EdgeGetter const edgeGetter,
+                     map<openlr::Graph::Junction, Graph::EdgeVector> & cache,
+                     Graph::EdgeVector & edges)
+{
+  auto const it = cache.find(junction);
+  if (it == end(cache))
+  {
+    auto & es = cache[junction];
+    (graph.*edgeGetter)(junction, es);
+    edges.insert(end(edges), begin(es), end(es));
+  }
+  else
+  {
+    auto const & es = it->second;
+    edges.insert(end(edges), begin(es), end(es));
+  }
+}
+}  // namespace
+
 Graph::Graph(Index const & index, shared_ptr<CarModelFactory> carModelFactory)
   : m_graph(index, IRoadGraph::Mode::ObeyOnewayTag, carModelFactory)
 {
 }
 
-void Graph::GetOutgoingEdges(Junction const & junction, EdgeVector & edges) const
+void Graph::GetOutgoingEdges(Junction const & junction, EdgeVector & edges)
 {
-  m_graph.GetOutgoingEdges(junction, edges);
+  GetRegularOutgoingEdges(junction, edges);
+  m_graph.GetFakeOutgoingEdges(junction, edges);
 }
 
-void Graph::GetIngoingEdges(Junction const & junction, EdgeVector & edges) const
+void Graph::GetIngoingEdges(Junction const & junction, EdgeVector & edges)
 {
-  m_graph.GetIngoingEdges(junction, edges);
+  GetRegularIngoingEdges(junction, edges);
+  m_graph.GetFakeIngoingEdges(junction, edges);
 }
 
-void Graph::GetRegularOutgoingEdges(Junction const & junction, EdgeVector & edges) const
+void Graph::GetRegularOutgoingEdges(Junction const & junction, EdgeVector & edges)
 {
-  m_graph.GetRegularOutgoingEdges(junction, edges);
+  GetRegularEdges(junction, m_graph, &IRoadGraph::GetRegularOutgoingEdges, m_outgoingCache, edges);
 }
 
-void Graph::GetRegularIngoingEdges(Junction const & junction, EdgeVector & edges) const
+void Graph::GetRegularIngoingEdges(Junction const & junction, EdgeVector & edges)
 {
-  m_graph.GetRegularIngoingEdges(junction, edges);
+  GetRegularEdges(junction, m_graph, &IRoadGraph::GetRegularIngoingEdges, m_ingoingCache, edges);
 }
 
-void Graph::FindClosestEdges(m2::PointD const & point, uint32_t count,
+void Graph::FindClosestEdges(m2::PointD const & point, uint32_t const count,
                              vector<pair<Edge, Junction>> & vicinities) const
 {
   m_graph.FindClosestEdges(point, count, vicinities);