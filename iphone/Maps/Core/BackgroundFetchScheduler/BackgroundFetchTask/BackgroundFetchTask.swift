f45f16e2e4a643c6f4ca5a92db9231d1bf37b925
Ilya Grechuhin <i.grechuhin@gmail.com>
2018-03-06 22:51:57 +0300

    [cherry] [FABRIC-12673] [ios] Crash fix.

--- iphone/Maps/Core/BackgroundFetchScheduler/BackgroundFetchTask/BackgroundFetchTask.swift
+++ iphone/Maps/Core/BackgroundFetchScheduler/BackgroundFetchTask/BackgroundFetchTask.swift
@@ -8,18 +8,24 @@
   var completionHandler: BackgroundFetchScheduler.FetchResultHandler!
 
   func start() {
-    backgroundTaskIdentifier = UIApplication.shared.beginBackgroundTask(expirationHandler: {
-      self.finish(.failed)
-    })
-    if backgroundTaskIdentifier != UIBackgroundTaskInvalid {
-      queue.async(execute: block)
+    DispatchQueue.main.async {
+      self.backgroundTaskIdentifier = UIApplication.shared.beginBackgroundTask(expirationHandler: {
+        self.finish(.failed)
+      })
+      if self.backgroundTaskIdentifier != UIBackgroundTaskInvalid {
+        self.queue.async(execute: self.block)
+      }
     }
   }
 
   func finish(_ result: UIBackgroundFetchResult) {
     guard backgroundTaskIdentifier != UIBackgroundTaskInvalid else { return }
-    UIApplication.shared.endBackgroundTask(backgroundTaskIdentifier)
-    backgroundTaskIdentifier = UIBackgroundTaskInvalid
-    completionHandler(result)
+    DispatchQueue.main.async {
+      UIApplication.shared.endBackgroundTask(self.backgroundTaskIdentifier)
+      self.backgroundTaskIdentifier = UIBackgroundTaskInvalid
+      self.queue.async {
+        self.completionHandler(result)
+      }
+    }
   }
 }
