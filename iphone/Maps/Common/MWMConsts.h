ee06a502da58304ee07c179048eade7a3a283cde
Aleksey Belousov <aleksey.belousov@corp.mail.ru>
2018-05-07 15:58:20 +0300

    Changed global animation duration to be in sync with table view animations

--- iphone/Maps/Common/MWMConsts.h
+++ iphone/Maps/Common/MWMConsts.h
@@ -1,6 +1,6 @@
 static NSString * const kMapsmeErrorDomain = @"com.mapsme.error";
 
-static NSTimeInterval const kDefaultAnimationDuration = .2;
+static NSTimeInterval const kDefaultAnimationDuration = .3;
 
 static uint64_t const KB = 1024;
 static uint64_t const MB = 1024 * 1024;
