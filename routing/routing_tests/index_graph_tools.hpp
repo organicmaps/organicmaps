vmihaylenko/omim

[routing] First step to separation between weight speed and eta.

routing/routing_tests/index_graph_tools.hpp
@@ -132,6 +132,7 @@ public:
   ~WeightedEdgeEstimator() override = default;
 
   double CalcSegmentWeight(Segment const & segment, RoadGeometry const & /* road */) const override;
+  double CalcSegmentTransitTime(Segment const & segment, RoadGeometry const & road) const override { return 0.0; }
   double GetUTurnPenalty() const override;
   bool LeapIsAllowed(NumMwmId /* mwmId */) const override;
 

routing/single_vehicle_world_graph.cpp
@@ -102,6 +102,11 @@ RouteWeight SingleVehicleWorldGraph::CalcOffroadWeight(m2::PointD const & from,
   return RouteWeight(m_estimator->CalcOffroadWeight(from, to));
 }
 
+double SingleVehicleWorldGraph::CalcSegmentTransitTime(Segment const & segment)
+{
+  return m_estimator->CalcSegmentTransitTime(segment, GetRoadGeometry(segment.GetMwmId(), segment.GetFeatureId()));
+}
+
 bool SingleVehicleWorldGraph::LeapIsAllowed(NumMwmId mwmId) const
 {
   return m_estimator->LeapIsAllowed(mwmId);

routing/transit_world_graph.hpp
@@ -56,6 +56,7 @@ public:
   RouteWeight CalcSegmentWeight(Segment const & segment) override;
   RouteWeight CalcLeapWeight(m2::PointD const & from, m2::PointD const & to) const override;
   RouteWeight CalcOffroadWeight(m2::PointD const & from, m2::PointD const & to) const override;
+  double CalcSegmentTransitTime(Segment const & segment) override;
   bool LeapIsAllowed(NumMwmId mwmId) const override;
   std::vector<Segment> const & GetTransitions(NumMwmId numMwmId, bool isEnter) override;
   std::unique_ptr<TransitInfo> GetTransitInfo(Segment const & segment) override;

routing/world_graph.hpp
@@ -64,6 +64,7 @@ public:
   virtual RouteWeight CalcSegmentWeight(Segment const & segment) = 0;
   virtual RouteWeight CalcLeapWeight(m2::PointD const & from, m2::PointD const & to) const = 0;
   virtual RouteWeight CalcOffroadWeight(m2::PointD const & from, m2::PointD const & to) const = 0;
+  virtual double CalcSegmentTransitTime(Segment const & segment) = 0;
   virtual bool LeapIsAllowed(NumMwmId mwmId) const = 0;
 
   /// \returns transitions for mwm with id |numMwmId|.

organicmaps/organicmaps

routing/routing_tests/index_graph_tools.hpp
@@ -132,6 +132,7 @@ public:
   ~WeightedEdgeEstimator() override = default;
 
   double CalcSegmentWeight(Segment const & segment, RoadGeometry const & /* road */) const override;
+  double CalcSegmentETA(Segment const & segment, RoadGeometry const & road) const override { return 0.0; }
   double GetUTurnPenalty() const override;
   bool LeapIsAllowed(NumMwmId /* mwmId */) const override;
 

routing/single_vehicle_world_graph.cpp
@@ -102,6 +102,11 @@ RouteWeight SingleVehicleWorldGraph::CalcOffroadWeight(m2::PointD const & from,
   return RouteWeight(m_estimator->CalcOffroadWeight(from, to));
 }
 
+double SingleVehicleWorldGraph::CalcSegmentETA(Segment const & segment)
+{
+  return m_estimator->CalcSegmentETA(segment, GetRoadGeometry(segment.GetMwmId(), segment.GetFeatureId()));
+}
+
 bool SingleVehicleWorldGraph::LeapIsAllowed(NumMwmId mwmId) const
 {
   return m_estimator->LeapIsAllowed(mwmId);

routing/transit_world_graph.hpp
@@ -56,6 +56,7 @@ public:
   RouteWeight CalcSegmentWeight(Segment const & segment) override;
   RouteWeight CalcLeapWeight(m2::PointD const & from, m2::PointD const & to) const override;
   RouteWeight CalcOffroadWeight(m2::PointD const & from, m2::PointD const & to) const override;
+  double CalcSegmentETA(Segment const & segment) override;
   bool LeapIsAllowed(NumMwmId mwmId) const override;
   std::vector<Segment> const & GetTransitions(NumMwmId numMwmId, bool isEnter) override;
   std::unique_ptr<TransitInfo> GetTransitInfo(Segment const & segment) override;

routing/world_graph.hpp
@@ -64,6 +64,7 @@ public:
   virtual RouteWeight CalcSegmentWeight(Segment const & segment) = 0;
   virtual RouteWeight CalcLeapWeight(m2::PointD const & from, m2::PointD const & to) const = 0;
   virtual RouteWeight CalcOffroadWeight(m2::PointD const & from, m2::PointD const & to) const = 0;
+  virtual double CalcSegmentETA(Segment const & segment) = 0;
   virtual bool LeapIsAllowed(NumMwmId mwmId) const = 0;
 
   /// \returns transitions for mwm with id |numMwmId|.