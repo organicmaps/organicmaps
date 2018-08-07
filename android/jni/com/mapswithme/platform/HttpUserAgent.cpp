84ecf864c0bffee26469066c7d943515b6ea3551
Alexander Zatsepin <az@mapswithme.com>
2018-08-07 14:15:27 +0300

    [android] Implemented getting the app version from native code

--- android/jni/com/mapswithme/platform/HttpUserAgent.cpp
+++ android/jni/com/mapswithme/platform/HttpUserAgent.cpp
@@ -1,10 +1,17 @@
+#include "com/mapswithme/core/jni_helper.hpp"
 #include "platform/http_user_agent.hpp"
 
 namespace platform
 {
 std::string HttpUserAgent::ExtractAppVersion() const
 {
-  //TODO(@alexzatsepin): implement app version extraction.
-  return {};
+    JNIEnv * env = jni::GetEnv();
+    static auto const buildConfigClass = jni::GetGlobalClassRef(env,
+                                                                "com/mapswithme/maps/BuildConfig");
+    auto const versionNameField = jni::GetStaticFieldID(env, buildConfigClass, "VERSION_NAME",
+                                                        "Ljava/lang/String;");
+    jni::TScopedLocalRef versionNameResult(env, env->GetStaticObjectField(buildConfigClass,
+                                                                          versionNameField));
+    return jni::ToNativeString(env, static_cast<jstring>(versionNameResult.get()));
 }
 }  // platform
