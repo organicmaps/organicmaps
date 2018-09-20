03fd6120f4f21cc0970b2d9e254c5b156536a499
Vladimir Byko-Ianko <v.bykoianko@corp.mail.ru>
2018-09-20 11:23:54 +0300

    unused-private-field clang warning fix in includes.

--- indexer/features_offsets_table.hpp
+++ indexer/features_offsets_table.hpp
@@ -10,8 +10,17 @@
 #include <string>
 #include <vector>
 
+#if defined(__clang__)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-private-field"
+#endif
+
 #include "3party/succinct/elias_fano.hpp"
 #include "3party/succinct/mapper.hpp"
+
+#if defined(__clang__)
+#pragma clang diagnostic pop
+#endif
 
 namespace platform
 {
