110485449ebeb7b2d6598514b81fb6b1a38d08ad
Dmitry Donskoy <donskdmitry@mail.ru>
2018-09-27 12:47:57 +0300

    [android] Cleanup imports

--- android/src/com/mapswithme/maps/base/BaseActivityDelegate.java
+++ android/src/com/mapswithme/maps/base/BaseActivityDelegate.java
@@ -5,14 +5,12 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 import android.util.Log;
-
 import com.mapswithme.util.Config;
 import com.mapswithme.util.CrashlyticsUtils;
 import com.mapswithme.util.UiUtils;
 import com.mapswithme.util.ViewServer;
 import com.mapswithme.util.concurrency.UiThread;
 import com.mapswithme.util.statistics.Statistics;
-import com.my.tracker.MyTracker;
 
 public class BaseActivityDelegate
 {
