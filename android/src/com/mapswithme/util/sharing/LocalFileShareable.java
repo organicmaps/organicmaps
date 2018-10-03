a7aa8e2f2d37d1bd5725a5797c50a9d29a64c0c9
Александр Зацепин <az@mapswithme.com>
2018-10-03 14:27:03 +0300

    [android] Fixed sharing boomarks from external storage

--- android/src/com/mapswithme/util/sharing/LocalFileShareable.java
+++ android/src/com/mapswithme/util/sharing/LocalFileShareable.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.annotation.Nullable;
+import android.support.v4.content.FileProvider;
+
+import com.mapswithme.maps.BuildConfig;
 
 import java.io.File;
 
@@ -12,7 +15,7 @@ public class LocalFileShareable extends BaseShareable
   private final String mFileName;
   private final String mMimeType;
 
-  public LocalFileShareable(Activity context, String fileName, String mimeType)
+  LocalFileShareable(Activity context, String fileName, String mimeType)
   {
     super(context);
     mFileName = fileName;
@@ -23,7 +26,9 @@ public class LocalFileShareable extends BaseShareable
   protected void modifyIntent(Intent intent, @Nullable SharingTarget target)
   {
     super.modifyIntent(intent, target);
-    intent.putExtra(android.content.Intent.EXTRA_STREAM, Uri.fromFile(new File(mFileName)));
+    Uri fileUri = FileProvider.getUriForFile(getActivity(), BuildConfig.FILE_PROVIDER_AUTHORITY,
+                                             new File(mFileName));
+    intent.putExtra(android.content.Intent.EXTRA_STREAM, fileUri);
   }
 
   @Override
