72f96e45d18c9dc3bce7514b917a07772a233a75
Dmitry Donskoy <donskdmitry@mail.ru>
2018-11-14 15:12:04 +0300

    [android] Moved divider to level up, add using new access rules strings

--- android/src/com/mapswithme/maps/bookmarks/data/BookmarkCategory.java
+++ android/src/com/mapswithme/maps/bookmarks/data/BookmarkCategory.java
@@ -399,22 +399,8 @@ public class BookmarkCategory implements Parcelable
     ACCESS_RULES_LOCAL(R.string.not_shared, R.drawable.ic_public_inline),
     ACCESS_RULES_PUBLIC(R.string.public_access, R.drawable.ic_public_inline),
     ACCESS_RULES_DIRECT_LINK(R.string.limited_access, R.drawable.ic_link_inline),
-    ACCESS_RULES_P2P(UiUtils.NO_ID, R.drawable.ic_public_inline)
-        {
-          @Override
-          public int getNameResId()
-          {
-            throw new IllegalStateException("Unsupported here");
-          }
-        },
-    ACCESS_RULES_PAID(UiUtils.NO_ID, R.drawable.ic_public_inline)
-        {
-          @Override
-          public int getNameResId()
-          {
-            throw new IllegalStateException("Unsupported here");
-          }
-        };
+    ACCESS_RULES_P2P(R.string.access_rules_p_to_p, R.drawable.ic_public_inline),
+    ACCESS_RULES_PAID(R.string.access_rules_paid, R.drawable.ic_public_inline);
 
     private final int mResId;
     private final int mDrawableResId;
