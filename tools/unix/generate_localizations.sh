8cd6b87215ab268bfecfd924e634f80a22c5a243
tatiana-yan <tatiana.kondakova@gmail.com>
2018-11-19 15:33:11 +0300

    [strings] Strings for brands.

--- tools/unix/generate_localizations.sh
+++ tools/unix/generate_localizations.sh
@@ -6,7 +6,7 @@ TWINE="$OMIM_PATH/tools/twine/twine"
 STRINGS_PATH="$OMIM_PATH/data/strings"
 
 MERGED_FILE="$(mktemp)"
-cat "$STRINGS_PATH/strings.txt" "$STRINGS_PATH/partners_strings.txt" "$STRINGS_PATH/types_strings.txt" > "$MERGED_FILE"
+cat "$STRINGS_PATH"/{strings,partners_strings,types_strings,brands_strings}.txt> "$MERGED_FILE"
 
 # TODO: Add "--untagged --tags android" when tags are properly set.
 # TODO: Add validate-strings-file call to check for duplicates (and avoid Android build errors) when tags are properly set.
