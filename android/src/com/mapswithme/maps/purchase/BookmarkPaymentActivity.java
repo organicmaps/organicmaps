b0aa3a280c64fbfddf007f74eb980cfb19edeb43
Александр Зацепин <az@mapswithme.com>
2018-12-18 15:38:38 +0300

    [android] Added clear_top to payment activity to avoid activity instance duplication in stack when tap multiple times on check_price

--- android/src/com/mapswithme/maps/purchase/BookmarkPaymentActivity.java
+++ android/src/com/mapswithme/maps/purchase/BookmarkPaymentActivity.java
@@ -3,6 +3,7 @@ package com.mapswithme.maps.purchase;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 
 import com.mapswithme.maps.base.BaseMwmFragmentActivity;
@@ -14,10 +15,18 @@ public class BookmarkPaymentActivity extends BaseMwmFragmentActivity
                                     int requestCode)
   {
     Intent intent = new Intent(fragment.getActivity(), BookmarkPaymentActivity.class);
+    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
     Bundle args = new Bundle();
     args.putParcelable(BookmarkPaymentFragment.ARG_PAYMENT_DATA, paymentData);
     intent.putExtras(args);
     fragment.startActivityForResult(intent, requestCode);
+  }
+
+  @Override
+  protected void safeOnCreate(@Nullable Bundle savedInstanceState)
+  {
+    super.safeOnCreate(savedInstanceState);
+    overridePendingTransition(0, 0);
   }
 
   @Override
