4957e1566dbcef35851487b866628ef84e2ae027
Александр Зацепин <az@mapswithme.com>
2018-12-21 20:11:14 +0300

    [search] Added search results count check before emitment

--- search/ranker.cpp
+++ search/ranker.cpp
@@ -553,7 +553,7 @@ void Ranker::UpdateResults(bool lastUpdate)
       // For categorial requests, it is usual that several batches arrive
       // in the first call to UpdateResults(). Emit them as soon as they are available
       // to improve responsiveness.
-      if (count % m_params.m_batchSize == 0)
+      if (count != 0 && count % m_params.m_batchSize == 0)
         m_emitter.Emit();
     }
 
