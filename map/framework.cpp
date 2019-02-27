da29e201f9ad52e69a30d1a7b4a11424a7573672
Arsentiy Milchakov <milcars@mapswithme.com>
2019-02-27 10:22:55 +0300

    [power manager] loading and subscribing on startup are rearranged

--- map/framework.cpp
+++ map/framework.cpp
@@ -563,8 +563,8 @@ Framework::Framework(FrameworkParams const & params)
   eye::Eye::Instance().TrimExpired();
   eye::Eye::Instance().Subscribe(&m_notificationManager);
 
-  GetPowerManager().Load();
   GetPowerManager().Subscribe(this);
+  GetPowerManager().Load();
 }
 
 Framework::~Framework()
