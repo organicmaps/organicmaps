e411c6a2a6ae7abf2d4a1bd69a77d1a9f9bef0dd
Dmitry Donskoy <donskdmitry@mail.ru>
2019-04-02 22:51:34 +0300

    [android] Fixed legacy review notes

--- android/jni/com/mapswithme/maps/routing/RoutingOptions.cpp
+++ android/jni/com/mapswithme/maps/routing/RoutingOptions.cpp
@@ -5,7 +5,7 @@
 
 routing::RoutingOptions::Road makeValue(jint option)
 {
-  uint8_t const road = static_cast<const uint8_t>(1u << static_cast<int>(option));
+  uint8_t const road = static_cast<uint8_t>(1u << static_cast<int>(option));
   CHECK_LESS(road, static_cast<uint8_t>(routing::RoutingOptions::Road::Max), ());
   return static_cast<routing::RoutingOptions::Road>(road);
 }
@@ -16,30 +16,30 @@ extern "C"
 JNIEXPORT jboolean JNICALL
 Java_com_mapswithme_maps_routing_RoutingOptions_nativeHasOption(JNIEnv * env, jclass clazz, jint option)
 {
-    CHECK(g_framework, ("Framework isn't created yet!"));
-    routing::RoutingOptions routingOptions = routing::RoutingOptions::LoadCarOptionsFromSettings();
-    routing::RoutingOptions::Road road = makeValue(option);
-    return static_cast<jboolean>(routingOptions.Has(road));
+  CHECK(g_framework, ("Framework isn't created yet!"));
+  routing::RoutingOptions routingOptions = routing::RoutingOptions::LoadCarOptionsFromSettings();
+  routing::RoutingOptions::Road road = makeValue(option);
+  return static_cast<jboolean>(routingOptions.Has(road));
 }
 
 JNIEXPORT void JNICALL
 Java_com_mapswithme_maps_routing_RoutingOptions_nativeAddOption(JNIEnv * env, jclass clazz, jint option)
 {
-    CHECK(g_framework, ("Framework isn't created yet!"));
-    routing::RoutingOptions routingOptions = routing::RoutingOptions::LoadCarOptionsFromSettings();
-    routing::RoutingOptions::Road road = makeValue(option);
-    routingOptions.Add(road);
-    routing::RoutingOptions::SaveCarOptionsToSettings(routingOptions);
+  CHECK(g_framework, ("Framework isn't created yet!"));
+  routing::RoutingOptions routingOptions = routing::RoutingOptions::LoadCarOptionsFromSettings();
+  routing::RoutingOptions::Road road = makeValue(option);
+  routingOptions.Add(road);
+  routing::RoutingOptions::SaveCarOptionsToSettings(routingOptions);
 }
 
 
 JNIEXPORT void JNICALL
 Java_com_mapswithme_maps_routing_RoutingOptions_nativeRemoveOption(JNIEnv * env, jclass clazz, jint option)
 {
-    CHECK(g_framework, ("Framework isn't created yet!"));
-    routing::RoutingOptions routingOptions = routing::RoutingOptions::LoadCarOptionsFromSettings();
-    routing::RoutingOptions::Road road = makeValue(option);
-    routingOptions.Remove(road);
-    routing::RoutingOptions::SaveCarOptionsToSettings(routingOptions);
+  CHECK(g_framework, ("Framework isn't created yet!"));
+  routing::RoutingOptions routingOptions = routing::RoutingOptions::LoadCarOptionsFromSettings();
+  routing::RoutingOptions::Road road = makeValue(option);
+  routingOptions.Remove(road);
+  routing::RoutingOptions::SaveCarOptionsToSettings(routingOptions);
 }
 }
