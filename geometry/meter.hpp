0f5ed60f10c3d3ebdb511dc8a91336d961e5285d
Anatoly Serdtcev <serdtcev@maps.me>
2019-06-14 20:54:47 +0300

    [geometry:meter] Fix compile warnings

--- geometry/meter.hpp
+++ geometry/meter.hpp
@@ -6,14 +6,14 @@ using Meter = double;
 
 inline namespace literals
 {
-inline constexpr auto operator"" _m(long double value) noexcept -> Meter
+inline constexpr Meter operator"" _m(long double value) noexcept
 {
-  return {static_cast<double>(value)};
+  return static_cast<double>(value);
 }
 
-inline constexpr auto operator"" _km(long double value) noexcept -> Meter
+inline constexpr Meter operator"" _km(long double value) noexcept
 {
-  return {static_cast<double>(value * 1000.0)};
+  return static_cast<double>(value * 1000.0);
 }
 }  // namespace literals
 }  // namespace m2
