mapsme/omim

[search] Add IsStreetSynonymWithMisprints method.

indexer/search_string_utils.cpp
@@ -2,12 +2,16 @@
 #include "indexer/string_set.hpp"
 
 #include "base/assert.hpp"
+#include "base/dfa_helpers.hpp"
 #include "base/macros.hpp"
 #include "base/mem_trie.hpp"
 
 #include "3party/utfcpp/source/utf8/unchecked.h"
 
 #include <algorithm>
+#include <memory>
+#include <queue>
+#include <vector>
 
 using namespace std;
 using namespace strings;
@@ -16,6 +20,17 @@ namespace search
 {
 namespace
 {
+vector<strings::UniString> const kAllowedMisprints = {
+    strings::MakeUniString("ckq"),
+    strings::MakeUniString("eyjiu"),
+    strings::MakeUniString("gh"),
+    strings::MakeUniString("pf"),
+    strings::MakeUniString("vw"),
+    strings::MakeUniString("ао"),
+    strings::MakeUniString("еиэ"),
+    strings::MakeUniString("шщ"),
+};
+
 // Replaces '#' followed by an end-of-string or a digit with space.
 void RemoveNumeroSigns(UniString & s)
 {
@@ -41,6 +56,26 @@ void RemoveNumeroSigns(UniString & s)
   }
 }
 }  // namespace
+
+size_t GetMaxErrorsForToken(strings::UniString const & token)
+{
+  bool const digitsOnly = all_of(token.begin(), token.end(), ::isdigit);
+  if (digitsOnly)
+    return 0;
+  if (token.size() < 4)
+    return 0;
+  if (token.size() < 8)
+    return 1;
+  return 2;
+}
+
+strings::LevenshteinDFA BuildLevenshteinDFA(strings::UniString const & s)
+{
+  // In search we use LevenshteinDFAs for fuzzy matching. But due to
+  // performance reasons, we limit prefix misprints to fixed set of substitutions defined in
+  // kAllowedMisprints and skipped letters.
+  return strings::LevenshteinDFA(s, 1 /* prefixSize */, kAllowedMisprints, GetMaxErrorsForToken(s));
+}
 
 UniString NormalizeAndSimplifyString(string const & s)
 {
@@ -205,6 +240,8 @@ public:
     bool m_empty;
   };
 
+  using Trie = base::MemTrie<UniString, BooleanSum, base::VectorMoves>;
+
   StreetsSynonymsHolder()
   {
     char const * affics[] =
@@ -283,8 +320,43 @@ public:
   bool MatchPrefix(UniString const & s) const { return m_strings.HasPrefix(s); }
   bool FullMatch(UniString const & s) const { return m_strings.HasKey(s); }
 
+  template <typename DFA>
+  bool MatchWithMisprints(DFA const & dfa) const
+  {
+    using TrieIt = Trie::Iterator;
+    using DFAIt = strings::LevenshteinDFA::Iterator;
+    using State = pair<TrieIt, DFAIt>;
+
+    auto const trieRoot = m_strings.GetRootIterator();
+
+    queue<State> q;
+    q.emplace(trieRoot, dfa.Begin());
+
+    while (!q.empty())
+    {
+      auto const p = q.front();
+      q.pop();
+
+      auto const & trieIt = p.first;
+      auto const & dfaIt = p.second;
+
+      if (dfaIt.Accepts())
+        return true;
+
+      trieIt.ForEachMove([&q, &dfaIt](UniChar const & c, TrieIt const & curDfaIt) {
+        auto curIt = dfaIt;
+        curIt.Move(c);
+        strings::DFAMove(curIt, curDfaIt.GetLabel());
+        if (!curIt.Rejects())
+          q.emplace(curDfaIt, curIt);
+      });
+    }
+
+    return false;
+  }
+
 private:
-  base::MemTrie<UniString, BooleanSum, base::VectorMoves> m_strings;
+  Trie m_strings;
 };
 
 StreetsSynonymsHolder g_streets;
@@ -340,6 +412,12 @@ bool IsStreetSynonym(UniString const & s)
 bool IsStreetSynonymPrefix(UniString const & s)
 {
   return g_streets.MatchPrefix(s);
+}
+
+bool IsStreetSynonymWithMisprints(UniString const & s)
+{
+  auto const dfa = BuildLevenshteinDFA(s);
+  return g_streets.MatchWithMisprints(dfa);
 }
 
 bool ContainsNormalized(string const & str, string const & substr)

organicmaps/organicmaps

indexer/search_string_utils.cpp
@@ -2,12 +2,16 @@
 #include "indexer/string_set.hpp"
 
 #include "base/assert.hpp"
+#include "base/dfa_helpers.hpp"
 #include "base/macros.hpp"
 #include "base/mem_trie.hpp"
 
 #include "3party/utfcpp/source/utf8/unchecked.h"
 
 #include <algorithm>
+#include <memory>
+#include <queue>
+#include <vector>
 
 using namespace std;
 using namespace strings;
@@ -16,6 +20,17 @@ namespace search
 {
 namespace
 {
+vector<strings::UniString> const kAllowedMisprints = {
+    strings::MakeUniString("ckq"),
+    strings::MakeUniString("eyjiu"),
+    strings::MakeUniString("gh"),
+    strings::MakeUniString("pf"),
+    strings::MakeUniString("vw"),
+    strings::MakeUniString("ао"),
+    strings::MakeUniString("еиэ"),
+    strings::MakeUniString("шщ"),
+};
+
 // Replaces '#' followed by an end-of-string or a digit with space.
 void RemoveNumeroSigns(UniString & s)
 {
@@ -41,6 +56,26 @@ void RemoveNumeroSigns(UniString & s)
   }
 }
 }  // namespace
+
+size_t GetMaxErrorsForToken(strings::UniString const & token)
+{
+  bool const digitsOnly = all_of(token.begin(), token.end(), ::isdigit);
+  if (digitsOnly)
+    return 0;
+  if (token.size() < 4)
+    return 0;
+  if (token.size() < 8)
+    return 1;
+  return 2;
+}
+
+strings::LevenshteinDFA BuildLevenshteinDFA(strings::UniString const & s)
+{
+  // In search we use LevenshteinDFAs for fuzzy matching. But due to
+  // performance reasons, we limit prefix misprints to fixed set of substitutions defined in
+  // kAllowedMisprints and skipped letters.
+  return strings::LevenshteinDFA(s, 1 /* prefixSize */, kAllowedMisprints, GetMaxErrorsForToken(s));
+}
 
 UniString NormalizeAndSimplifyString(string const & s)
 {
@@ -205,6 +240,8 @@ public:
     bool m_empty;
   };
 
+  using Trie = base::MemTrie<UniString, BooleanSum, base::VectorMoves>;
+
   StreetsSynonymsHolder()
   {
     char const * affics[] =
@@ -283,8 +320,42 @@ public:
   bool MatchPrefix(UniString const & s) const { return m_strings.HasPrefix(s); }
   bool FullMatch(UniString const & s) const { return m_strings.HasKey(s); }
 
+  template <typename DFA>
+  bool MatchWithMisprints(DFA const & dfa) const
+  {
+    using TrieIt = Trie::Iterator;
+    using State = pair<TrieIt, typename DFA::Iterator>;
+
+    auto const trieRoot = m_strings.GetRootIterator();
+
+    queue<State> q;
+    q.emplace(trieRoot, dfa.Begin());
+
+    while (!q.empty())
+    {
+      auto const p = q.front();
+      q.pop();
+
+      auto const & currTrieIt = p.first;
+      auto const & currDfaIt = p.second;
+
+      if (currDfaIt.Accepts())
+        return true;
+
+      currTrieIt.ForEachMove([&q, &currDfaIt](UniChar const & c, TrieIt const & nextTrieIt) {
+        auto nextDfaIt = currDfaIt;
+        nextDfaIt.Move(c);
+        strings::DFAMove(nextDfaIt, nextTrieIt.GetLabel());
+        if (!nextDfaIt.Rejects())
+          q.emplace(nextTrieIt, nextDfaIt);
+      });
+    }
+
+    return false;
+  }
+
 private:
-  base::MemTrie<UniString, BooleanSum, base::VectorMoves> m_strings;
+  Trie m_strings;
 };
 
 StreetsSynonymsHolder g_streets;
@@ -340,6 +411,18 @@ bool IsStreetSynonym(UniString const & s)
 bool IsStreetSynonymPrefix(UniString const & s)
 {
   return g_streets.MatchPrefix(s);
+}
+
+bool IsStreetSynonymWithMisprints(UniString const & s)
+{
+  auto const dfa = BuildLevenshteinDFA(s);
+  return g_streets.MatchWithMisprints(dfa);
+}
+
+bool IsStreetSynonymPrefixWithMisprints(UniString const & s)
+{
+  auto const dfa = strings::PrefixDFAModifier<strings::LevenshteinDFA>(BuildLevenshteinDFA(s));
+  return g_streets.MatchWithMisprints(dfa);
 }
 
 bool ContainsNormalized(string const & str, string const & substr)