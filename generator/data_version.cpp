54db4869b04f00e6af5d2c086e6d9cff68c877d3
LaGrunge <l.fedorov@corp.mail.ru>
2019-08-20 16:44:00 +0300

    [generator] rename field

--- generator/data_version.cpp
+++ generator/data_version.cpp
@@ -33,7 +33,7 @@ DataVersion::DataVersion(std::string const & planetFilePath)
   ToJSONObject(*m_json, "generator_build_time", omim::build_version::git::kTimestamp);
   ToJSONObject(*m_json, "generator_git_hash", omim::build_version::git::kHash);
   ToJSONObject(*m_json, "planet_md5", ReadWholeFile(planetFilePath + ".md5"));
-  ToJSONObject(*m_json, "planet_timestamp", ReadWholeFile(planetFilePath + ".timestamp"));
+  ToJSONObject(*m_json, "planet_last_changeset_timestamp", ReadWholeFile(planetFilePath + ".timestamp"));
   ToJSONObject(*m_json, "planet_file_timestamp", planetTimestamp);
 }
 
