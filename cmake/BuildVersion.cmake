08784a19b1ed153db05b9e259ce26b86ce187056
LaGrunge <l.fedorov@corp.mail.ru>
2019-08-20 17:40:16 +0300

    version more custom information

--- cmake/BuildVersion.cmake
+++ cmake/BuildVersion.cmake
@@ -3,7 +3,7 @@ function(get_last_git_commit_hash result_name)
   if (GIT_FOUND)
     execute_process(COMMAND
       "${GIT_EXECUTABLE}" describe --match="" --always --abbrev=40 --dirty
-      WORKING_DIRECTORY "${OMIM_ROOT}"
+      WORKING_DIRECTORY "${MAPSME_CURRENT_PROJECT_ROOT}"
       OUTPUT_VARIABLE GIT_HASH
       ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
   endif()
@@ -21,7 +21,7 @@ function(get_last_git_commit_timestamp result_name)
   if (GIT_FOUND)
     execute_process(COMMAND
       "${GIT_EXECUTABLE}" show -s --format=%ct HEAD
-      WORKING_DIRECTORY "${OMIM_ROOT}"
+      WORKING_DIRECTORY "${MAPSME_CURRENT_PROJECT_ROOT}"
       OUTPUT_VARIABLE GIT_TIMESTAMP
       ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
   endif()
@@ -34,11 +34,30 @@ function(get_last_git_commit_timestamp result_name)
   set(${result_name} ${GIT_TIMESTAMP} PARENT_SCOPE)
 endfunction()
 
+function(get_git_tag_name result_name)
+  find_package(Git)
+  if (GIT_FOUND)
+    execute_process(COMMAND
+      "${GIT_EXECUTABLE}" git tag --points-at HEAD
+      WORKING_DIRECTORY "${MAPSME_CURRENT_PROJECT_ROOT}"
+      OUTPUT_VARIABLE GIT_TAG
+      ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+  endif()
+
+  if (NOT GIT_TAG)
+    message(WARNING "Failed to get last commit from git.")
+    set(GIT_TAG "no_tag_set")
+  endif()
+
+  set(${result_name} ${GIT_TAG} PARENT_SCOPE)
+endfunction()
+
 function(configure_build_version_hpp)
   message("Configure build version ...")
   get_last_git_commit_hash(GIT_HASH)
   get_last_git_commit_timestamp(GIT_TIMESTAMP)
-  configure_file("${OMIM_ROOT}/build_version.hpp.in" "${OMIM_ROOT}/build_version.hpp" @ONLY)
+  get_git_tag_name(GIT_TAG)
+  configure_file("${PATH_WITH_BUILD_VERSION_HPP}/build_version.hpp.in" "${MAPSME_CURRENT_PROJECT_ROOT}/build_version.hpp" @ONLY)
 endfunction()
 
 
