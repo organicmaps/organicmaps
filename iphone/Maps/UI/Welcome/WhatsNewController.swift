41b3c4d4ea0e2bd7212d892935152c5e544cad84
Aleksey Belouosv <aleksey@maps.me>
2019-09-20 16:01:43 +0300

    [iOS] whats new 9.4
    https://jira.mail.ru/browse/MAPSME-11901

--- iphone/Maps/UI/Welcome/WhatsNewController.swift
+++ iphone/Maps/UI/Welcome/WhatsNewController.swift
@@ -9,9 +9,9 @@ final class WhatsNewController: WelcomeViewController {
   
   static var welcomeConfigs: [WelcomeConfig] {
   return [
-    WhatsNewConfig(image: UIImage(named: "img_wnew_bookmarks_sort")!,
-                   title: "whatsnew_bookmarks_search_sort_title",
-                   text: "whatsnew_bookmarks_search_sort_message",
+    WhatsNewConfig(image: UIImage(named: "img_whats_new_mapboost")!,
+                   title: "whatsnew_map_updates_title",
+                   text: "whatsnew_map_updates_message",
                    buttonTitle: "done")
     ]
   }
