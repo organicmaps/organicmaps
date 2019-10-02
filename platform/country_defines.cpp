940ff51457299db4187c12497cca21c50d665b6b
Arsentiy Milchakov <milcars@mapswithme.com>
2019-10-02 15:22:04 +0300

    [storage] got rid unused mapoptions

--- platform/country_defines.cpp
+++ platform/country_defines.cpp
@@ -2,41 +2,12 @@
 
 #include "base/assert.hpp"
 
-bool HasOptions(MapOptions mask, MapOptions options)
-{
-  return (static_cast<uint8_t>(mask) & static_cast<uint8_t>(options)) ==
-         static_cast<uint8_t>(options);
-}
-
-MapOptions SetOptions(MapOptions mask, MapOptions options)
-{
-  return static_cast<MapOptions>(static_cast<uint8_t>(mask) | static_cast<uint8_t>(options));
-}
-
-MapOptions UnsetOptions(MapOptions mask, MapOptions options)
-{
-  return static_cast<MapOptions>(static_cast<uint8_t>(mask) & ~static_cast<uint8_t>(options));
-}
-
-MapOptions LeastSignificantOption(MapOptions mask)
-{
-  return static_cast<MapOptions>(static_cast<uint8_t>(mask) & -static_cast<uint8_t>(mask));
-}
-
 std::string DebugPrint(MapOptions options)
 {
   switch (options)
   {
-    case MapOptions::Nothing:
-      return "Nothing";
-    case MapOptions::Map:
-      return "MapOnly";
-    case MapOptions::CarRouting:
-      return "CarRouting";
-    case MapOptions::MapWithCarRouting:
-      return "MapWithCarRouting";
-    case MapOptions::Diff:
-      return "Diff";
+  case MapOptions::Map: return "MapOnly";
+  case MapOptions::Diff: return "Diff";
   }
   UNREACHABLE();
 }
--- platform/country_defines.hpp
+++ platform/country_defines.hpp
@@ -8,25 +8,14 @@
 // value when count of elements is changed.
 enum class MapOptions : uint8_t
 {
-  Nothing = 0x0,
-  Map = 0x1,
-  CarRouting = 0x2,
-  MapWithCarRouting = 0x3,
-  Diff = 0x4
+  Map,
+  Diff
 };
 
-uint8_t constexpr kMapOptionsCount = 5;
+uint8_t constexpr kMapOptionsCount = 2;
 
 using MwmCounter = uint32_t;
 using MwmSize = uint64_t;
 using LocalAndRemoteSize = std::pair<MwmSize, MwmSize>;
-
-bool HasOptions(MapOptions mask, MapOptions options);
-
-MapOptions SetOptions(MapOptions mask, MapOptions options);
-
-MapOptions UnsetOptions(MapOptions mask, MapOptions options);
-
-MapOptions LeastSignificantOption(MapOptions mask);
 
 std::string DebugPrint(MapOptions options);
