bac380878b413d8f4abb46bce5799e948f8ef615
Александр Зацепин <az@mapswithme.com>
2019-10-18 18:47:44 +0300

    [android] Fixed build

--- android/src/com/mapswithme/maps/base/Observable.java
+++ android/src/com/mapswithme/maps/base/Observable.java
@@ -1,6 +1,6 @@
 package com.mapswithme.maps.base;
 
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 
 import java.util.ArrayList;
 import java.util.List;
--- android/src/com/mapswithme/util/GeoUtils.java
+++ android/src/com/mapswithme/util/GeoUtils.java
@@ -1,6 +1,6 @@
 package com.mapswithme.util;
 
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 
 import com.mapswithme.maps.bookmarks.data.ParcelablePointD;
 
