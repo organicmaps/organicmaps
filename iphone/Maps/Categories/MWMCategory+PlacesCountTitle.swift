d17d75c8d6f28d099e0a5b5f3d9da58dda532856
Alexander Boriskov <a.boriskov@corp.mail.ru>
2019-11-05 18:18:28 +0300

    [iOS] Fix Invalid character error

--- iphone/Maps/Categories/MWMCategory+PlacesCountTitle.swift
+++ iphone/Maps/Categories/MWMCategory+PlacesCountTitle.swift
@@ -5,7 +5,7 @@ extension MWMCategory {
     if (bookmarksCount > 0 && trackCount > 0) ||
       (bookmarksCount == 0 && trackCount == 0) {
        return String(format: L("objects"), bookmarksCount + trackCount)
-    } else if (bookmarksCount > 0) {
+    } else if (bookmarksCount > 0) {
       return String(format: L("bookmarks_places"), bookmarksCount)
     } else {
        return String(format: L("tracks"), trackCount)
