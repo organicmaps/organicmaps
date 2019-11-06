dc5ffbe0694fcc60c23c124cbd8761eb2f1f8fff
Alexander Boriskov <a.boriskov@corp.mail.ru>
2019-11-06 15:44:11 +0300

    [onboarding] Fix catalog urls

--- map/onboarding.cpp
+++ map/onboarding.cpp
@@ -14,10 +14,11 @@ namespace
 {
 auto constexpr kTipsCount = static_cast<uint8_t>(onboarding::Tip::Type::Count);
 
-std::string const kBaseUrl = "/guides_page?utm_source=maps.me&utm_medium=onboarding_button";
+std::string const kBaseUrl = "/en/v2/mobilefront/";
+std::string const kUTMParams = "&utm_source=maps.me&utm_medium=onboarding_button";
 std::array<std::string, kTipsCount> const kTipsLinks = {
-  kBaseUrl + "&url=%00&utm_campaign=catalog_discovery",
-  kBaseUrl + "&url=search%2F%3Ftag%3D181&utm_campaign=sample_discovery",
+  kBaseUrl + "?utm_campaign=catalog_discovery" + kUTMParams,
+  kBaseUrl + "search/?tag=181&utm_campaign=sample_discovery" + kUTMParams,
   ""};
 }  // namespace
 
@@ -28,7 +29,7 @@ Tip GetTip()
   auto const tipIndex = std::time(nullptr) % kTipsCount;
   auto const url = kTipsLinks[tipIndex];
 
-  return {static_cast<Tip::Type>(tipIndex), url.empty() ? url : base::url::Join(DLINK_URL, url)};
+  return {static_cast<Tip::Type>(tipIndex), url.empty() ? url : base::url::Join(BOOKMARKS_CATALOG_FRONT_URL, url)};
 }
 
 bool CanShowTipButton() { return GetPlatform().IsConnected(); }
