a27b63788c86b149c56a665d1cdd29656e65bc07
Dmitry Donskoy <donskdmitry@mail.ru>
2019-11-08 14:42:32 +0300

    Changed set of possible welcome screens, modified some values of it

--- android/src/com/mapswithme/maps/news/WelcomeScreenBindingType.java
+++ android/src/com/mapswithme/maps/news/WelcomeScreenBindingType.java
@@ -7,44 +7,68 @@ import com.mapswithme.maps.R;
 
 public enum WelcomeScreenBindingType
 {
-  DOWNLOAD_GUIDE(R.string.visible, R.string.visible, R.string.view_campaign_button,
+  READY_TO_USE_GUIDE(R.string.visible,
+                     null,
+                     R.string.view_campaign_button,
+                     R.string.visible,
+                     R.drawable.img_download_guide),
+  DOWNLOAD_GUIDE(R.string.visible,
+                 null,
+                 R.string.view_campaign_button,
                  R.string.visible,
                  R.drawable.img_download_guide),
-  CHECK_OUT_SIGHTS(R.string.visible, R.string.visible, R.string.view_campaign_button,
+  CHECK_OUT_SIGHTS(R.string.visible,
+                   R.string.visible,
+                   R.string.view_campaign_button,
                    R.string.visible,
                    R.drawable.img_check_sights_out),
-  SUBSCRIBE_TO_CATALOG(R.string.visible, R.string.visible, R.string.view_campaign_button,
+  SUBSCRIBE_TO_CATALOG(R.string.visible,
+                       R.string.visible,
+                       R.string.view_campaign_button,
                        R.string.visible,
                        R.drawable.img_discover_guides),
-  DISCOVER_GUIDES(R.string.visible, R.string.visible, R.string.view_campaign_button,
+  DISCOVER_GUIDES(R.string.visible,
+                  R.string.visible,
+                  R.string.view_campaign_button,
                   R.string.visible,
                   R.drawable.img_discover_guides),
-  SHARE_EMOTIONS(R.string.visible, R.string.visible, R.string.view_campaign_button,
+  SHARE_EMOTIONS(R.string.visible,
+                 null,
+                 R.string.view_campaign_button,
                  R.string.visible,
                  R.drawable.img_share_emptions),
-  EXPERIENCE(R.string.visible, R.string.visible, R.string.view_campaign_button, R.string.visible,
+  EXPERIENCE(R.string.visible,
+             null,
+             R.string.view_campaign_button,
+             R.string.visible,
              R.drawable.img_experience),
-  DREAM_AND_PLAN(R.string.visible, null, R.string.view_campaign_button, null,
+  DREAM_AND_PLAN(R.string.visible,
+                 null,
+                 R.string.view_campaign_button,
+                 R.string.visible,
                  R.drawable.img_dream_and_plan),
-  PERMISSION_EXPLANATION(R.string.visible, R.string.visible, R.string.view_campaign_button, null,
+  PERMISSION_EXPLANATION(R.string.visible,
+                         R.string.visible,
+                         R.string.view_campaign_button,
+                         R.string.visible,
                          R.drawable.img_permission_explanation);
 
   @StringRes
-  private final int mFirstButton;
+  private final int mAcceptButton;
   @Nullable
-  private final Integer mSecondButton;
+  private final Integer mDeclineButton;
   @StringRes
   private final int mTitle;
-  @Nullable
-  private final Integer mSubtitle;
+  @StringRes
+  private final int mSubtitle;
   @DrawableRes
   private final int mImage;
 
-  WelcomeScreenBindingType(@StringRes int firstButton, @Nullable Integer secondButton,
-                           @StringRes int title, @Nullable Integer subtitle, @DrawableRes int image)
+  WelcomeScreenBindingType(@StringRes int acceptButton, @Nullable Integer declineButton,
+                           @StringRes int title, @StringRes int subtitle, @DrawableRes int image)
   {
-    mFirstButton = firstButton;
-    mSecondButton = secondButton;
+    mAcceptButton = acceptButton;
+    mDeclineButton = declineButton;
     mTitle = title;
     mSubtitle = subtitle;
     mImage = image;
@@ -53,13 +77,13 @@ public enum WelcomeScreenBindingType
   @StringRes
   public int getAcceptButton()
   {
-    return mFirstButton;
+    return mAcceptButton;
   }
 
   @Nullable
   public Integer getDeclineButton()
   {
-    return mSecondButton;
+    return mDeclineButton;
   }
 
   @StringRes
@@ -68,12 +92,13 @@ public enum WelcomeScreenBindingType
     return mTitle;
   }
 
-  @Nullable
-  public Integer getSubtitle()
+  @StringRes
+  public int getSubtitle()
   {
     return mSubtitle;
   }
 
+  @DrawableRes
   public int getImage()
   {
     return mImage;
