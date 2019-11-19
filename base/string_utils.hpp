6e3b8dfea7f503a2854d7f735a57ebc981f7dc2c
Arsentiy Milchakov <milcars@mapswithme.com>
2019-11-19 10:41:07 +0300

    [base] redefinition fix

--- base/string_utils.hpp
+++ base/string_utils.hpp
@@ -430,7 +430,7 @@ std::string to_string(T t)
 }
 
 template <typename T>
-struct ToString { std::string operator()(T const & v) { return to_string(v); } };
+struct ToStringConverter { std::string operator()(T const & v) { return to_string(v); } };
 
 WARN_UNUSED_RESULT inline bool to_any(std::string const & s, int & i) { return to_int(s, i); }
 WARN_UNUSED_RESULT inline bool to_any(std::string const & s, unsigned int & i)
@@ -594,7 +594,7 @@ std::string JoinAny(Container const & container,
                     Delimiter const & delimiter = ',',
                     std::function<
                         std::string (typename Container::value_type const & v)> const & converter =
-                          ToString<typename Container::value_type>())
+                          ToStringConverter<typename Container::value_type>())
 {
   return JoinAny(std::cbegin(container), std::cend(container), delimiter, converter);
 }
