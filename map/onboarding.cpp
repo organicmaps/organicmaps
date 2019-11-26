cf0a75065a0dcdb851325318786ab8634d3275c7
Arsentiy Milchakov <milcars@mapswithme.com>
2019-11-26 15:15:14 +0300

    [onboarding] correct way to inject utm

--- map/onboarding.cpp
+++ map/onboarding.cpp
@@ -1,5 +1,7 @@
 #include "map/onboarding.hpp"
 
+#include "partners_api/utm.hpp"
+
 #include "platform/platform.hpp"
 
 #include "base/url_helpers.hpp"
@@ -15,10 +17,9 @@ namespace
 auto constexpr kTipsCount = static_cast<uint8_t>(onboarding::Tip::Type::Count);
 
 std::string const kBaseUrl = "/en/v2/mobilefront/";
-std::string const kUTMParams = "&utm_source=maps.me&utm_medium=onboarding_button";
 std::array<std::string, kTipsCount> const kTipsLinks = {
-  kBaseUrl + "?utm_campaign=catalog_discovery" + kUTMParams,
-  kBaseUrl + "search/?tag=181&utm_campaign=sample_discovery" + kUTMParams,
+  InjectUTM(kBaseUrl, UTM::DiscoverCatalogOnboarding),
+  InjectUTM(kBaseUrl + "search/?tag=181", UTM::FreeSamplesOnboading),
   ""};
 }  // namespace
 
@@ -29,7 +30,8 @@ Tip GetTip()
   auto const tipIndex = std::time(nullptr) % kTipsCount;
   auto const url = kTipsLinks[tipIndex];
 
-  return {static_cast<Tip::Type>(tipIndex), url.empty() ? url : base::url::Join(BOOKMARKS_CATALOG_FRONT_URL, url)};
+  return {static_cast<Tip::Type>(tipIndex),
+          url.empty() ? url : base::url::Join(BOOKMARKS_CATALOG_FRONT_URL, url)};
 }
 
 bool CanShowTipButton() { return GetPlatform().IsConnected(); }
