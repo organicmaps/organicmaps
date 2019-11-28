7579b29d08f26e4cf6bc8169f1779ecd90635c8d
Alexander Boriskov <a.boriskov@corp.mail.ru>
2019-11-28 12:30:56 +0300

    [iOS] Fixed localizations in onboarding/subscriptions

--- iphone/Maps/UI/Welcome/TermsOfUse/TermsOfUsePresenter.swift
+++ iphone/Maps/UI/Welcome/TermsOfUse/TermsOfUsePresenter.swift
@@ -19,8 +19,8 @@ class TermsOfUsePresenter {
 extension TermsOfUsePresenter: ITermsOfUsePresenter {
   func configure() {
     view?.setTitleImage(UIImage(named: "img_onboarding_travelbuddy"))
-    view?.setTitle(L("new_onboarding_step1_header"))
-    view?.setText(L("new_onboarding_step1_header_2"))
+    view?.setTitle("\(L("new_onboarding_step1_header"))\n\(L("new_onboarding_step1_header_2"))")
+    view?.setText(L("sign_message_gdpr"))
     view?.setPrivacyPolicyTitle(String(coreFormat: L("sign_agree_pp_gdpr"), arguments: [privacyPolicyLink]))
     view?.setTermsOfUseTitle(String(coreFormat: L("sign_agree_tof_gdpr"), arguments: [termsOfUseLink]))
   }
