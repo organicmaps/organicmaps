da83929bd84bddad58cbc9b49de28ff565721ce8
Александр Зацепин <az@mapswithme.com>
2019-12-02 14:35:32 +0300

    [core] Added lang to the onboarding tip url

--- map/onboarding.cpp
+++ map/onboarding.cpp
@@ -3,6 +3,7 @@
 #include "partners_api/utm.hpp"
 
 #include "platform/platform.hpp"
+#include "platform/preferred_languages.hpp"
 
 #include "base/url_helpers.hpp"
 
@@ -16,7 +17,7 @@ namespace
 {
 auto constexpr kTipsCount = static_cast<uint8_t>(onboarding::Tip::Type::Count);
 
-std::string const kBaseUrl = "/en/v2/mobilefront/";
+std::string const kBaseUrl = "/v2/mobilefront/";
 std::array<std::string, kTipsCount> const kTipsLinks = {
   InjectUTM(kBaseUrl, UTM::DiscoverCatalogOnboarding),
   InjectUTM(kBaseUrl + "search/?tag=181", UTM::FreeSamplesOnboading),
@@ -28,10 +29,10 @@ namespace onboarding
 Tip GetTip()
 {
   auto const tipIndex = std::time(nullptr) % kTipsCount;
-  auto const url = kTipsLinks[tipIndex];
-
+  auto const link = kTipsLinks[tipIndex];
+  auto const catalogUrl = base::url::Join(BOOKMARKS_CATALOG_FRONT_URL, languages::GetCurrentNorm());
   return {static_cast<Tip::Type>(tipIndex),
-          url.empty() ? url : base::url::Join(BOOKMARKS_CATALOG_FRONT_URL, url)};
+          link.empty() ? link : base::url::Join(catalogUrl, link)};
 }
 
 bool CanShowTipButton() { return GetPlatform().IsConnected(); }
