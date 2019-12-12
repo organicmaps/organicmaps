2842b6147f0b03576e5cef0fd085cbe39e5e954e
Aleksey Belouosv <aleksey@maps.me>
2019-12-12 16:50:00 +0300

    [iOS] fix mobile internet permission

--- iphone/Maps/Core/NetworkPolicy/MWMNetworkPolicy+UI.m
+++ iphone/Maps/Core/NetworkPolicy/MWMNetworkPolicy+UI.m
@@ -12,12 +12,15 @@
   [alertController presentMobileInternetAlertWithBlock:^(MWMMobileInternetAlertResult result) {
     switch (result) {
       case MWMMobileInternetAlertResultAlways:
+        self.permission = MWMNetworkPolicyPermissionAlways;
         completion(YES);
         break;
       case MWMMobileInternetAlertResultToday:
+        self.permission = MWMNetworkPolicyPermissionToday;
         completion(YES);
         break;
       case MWMMobileInternetAlertResultNotToday:
+        self.permission = MWMNetworkPolicyPermissionNotToday;
         completion(NO);
         break;
     }
