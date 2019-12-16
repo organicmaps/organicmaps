a82efed41564a5900637d7e362e05defa52c7af6
tatiana-yan <tatiana.kondakova@gmail.com>
2019-12-16 17:03:09 +0300

    [search][search_quality] Add tool which creates search samples from aloha.

---
+++ search/search_quality/aloha_to_samples_tool/aloha_to_samples_tool.cpp
@@ -0,0 +1,261 @@
+#include "search/search_quality/sample.hpp"
+
+#include "indexer/categories_holder.hpp"
+#include "indexer/classificator_loader.hpp"
+#include "indexer/search_string_utils.hpp"
+
+#include "platform/platform.hpp"
+
+#include "geometry/mercator.hpp"
+#include "geometry/point2d.hpp"
+#include "geometry/rect2d.hpp"
+
+#include "base/logging.hpp"
+#include "base/string_utils.hpp"
+
+#include <iostream>
+#include <map>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#include <boost/optional.hpp>
+
+#include "3party/gflags/src/gflags/gflags.h"
+
+#define ALOHALYTICS_SERVER
+#include "3party/Alohalytics/src/event_base.h"
+
+using namespace search;
+using namespace std;
+using namespace strings;
+
+DEFINE_string(data_path, "", "Path to data directory (resources dir).");
+DEFINE_bool(categorial_only, false, "Save only categorial requests.");
+
+struct EmitInfo
+{
+  m2::PointD m_pos;
+  m2::RectD m_viewport;
+  vector<string> m_results;
+  string m_query;
+  string m_locale;
+  bool m_isCategorial;
+};
+
+struct ResultInfo
+{
+  size_t m_pos;
+  string m_result;
+};
+
+bool IsCategorialRequest(string query, uint8_t locale)
+{
+  if (query.empty() || query.back() != ' ')
+    return false;
+
+  Trim(query);
+  auto const normalizedQuery = NormalizeAndSimplifyString(query);
+
+  auto const & catHolder = GetDefaultCategories();
+
+  bool isCategorialRequest = false;
+  catHolder.ForEachName([&](auto const & categorySynonym) {
+    if (isCategorialRequest)
+      return;
+
+    if (categorySynonym.m_locale != locale &&
+        categorySynonym.m_locale != CategoriesHolder::kEnglishCode)
+    {
+      return;
+    }
+
+    auto const normalizedCat = NormalizeAndSimplifyString(categorySynonym.m_name);
+    if (normalizedCat == normalizedQuery)
+      isCategorialRequest = true;
+  });
+  return isCategorialRequest;
+}
+
+boost::optional<EmitInfo> ParseEmitResultsAndCoords(map<string, string> const & kpePairs)
+{
+  EmitInfo info;
+  try
+  {
+    if (!to_double(kpePairs.at("posX"), info.m_pos.x))
+      return {};
+    if (!to_double(kpePairs.at("posY"), info.m_pos.y))
+      return {};
+
+    double minX, minY, maxX, maxY;
+    bool gotViewport = true;
+    gotViewport = gotViewport && to_double(kpePairs.at("viewportMinX"), minX);
+    gotViewport = gotViewport && to_double(kpePairs.at("viewportMinY"), minY);
+    gotViewport = gotViewport && to_double(kpePairs.at("viewportMaxX"), maxX);
+    gotViewport = gotViewport && to_double(kpePairs.at("viewportMaxY"), maxY);
+    if (!gotViewport)
+      return {};
+    info.m_viewport = m2::RectD(minX, minY, maxX, maxY);
+
+    info.m_locale = kpePairs.at("locale");
+    info.m_query = kpePairs.at("query");
+
+    auto const locale = CategoriesHolder::MapLocaleToInteger(info.m_locale);
+    info.m_isCategorial = IsCategorialRequest(info.m_query, locale);
+
+    info.m_results = Tokenize(kpePairs.at("results"), "\t");
+    // Skip results number.
+    if (!info.m_results.empty())
+      info.m_results.erase(info.m_results.begin());
+  }
+  catch (out_of_range)
+  {
+    return {};
+  }
+
+  return info;
+}
+
+boost::optional<ResultInfo> ParseShowResult(map<string, string> const & kpePairs)
+{
+  ResultInfo info;
+  try
+  {
+    if (!to_size_t(kpePairs.at("pos"), info.m_pos))
+      return {};
+    info.m_result = kpePairs.at("result");
+  }
+  catch (out_of_range)
+  {
+    return {};
+  }
+
+  return info;
+}
+
+boost::optional<Sample::Result> ParseResultWithCoords(string const & str)
+{
+  auto const tokens = Tokenize(str, "|");
+  // No coords.
+  if (tokens.size() < 5)
+    return {};
+
+  // Suggest.
+  if (tokens[2] == "1")
+    return {};
+
+  Sample::Result res;
+  res.m_name = MakeUniString(tokens[0]);
+  res.m_types = {tokens[1]};
+
+  double lat;
+  if (!to_double(tokens[3], lat))
+    return {};
+
+  double lon;
+  if (!to_double(tokens[4], lon))
+    return {};
+
+  res.m_pos = mercator::FromLatLon(lat, lon);
+
+  return res;
+}
+
+boost::optional<Sample> MakeSample(EmitInfo const & info, size_t relevantPos)
+{
+  Sample sample;
+  sample.m_query = MakeUniString(info.m_query);
+  sample.m_locale = info.m_locale;
+  sample.m_pos = info.m_pos;
+  sample.m_viewport = info.m_viewport;
+  sample.m_results.reserve(info.m_results.size());
+  for (size_t i = 0; i < info.m_results.size(); ++i)
+  {
+    auto res = ParseResultWithCoords(info.m_results[i]);
+    if (!res)
+      return {};
+
+    res->m_relevance = i == relevantPos ? Sample::Result::Relevance::Relevant
+                                        : Sample::Result::Relevance::Irrelevant;
+    sample.m_results.push_back(*res);
+  }
+  return sample;
+}
+
+int main(int argc, char * argv[])
+{
+  google::SetUsageMessage("This tool converts events from Alohalytics to search samples.");
+  google::ParseCommandLineFlags(&argc, &argv, true);
+
+  if (!FLAGS_data_path.empty())
+  {
+    Platform & platform = GetPlatform();
+    platform.SetResourceDir(FLAGS_data_path);
+    platform.SetWritableDirForTests(FLAGS_data_path);
+  }
+
+  classificator::Load();
+
+  cereal::BinaryInputArchive ar(std::cin);
+  unique_ptr<AlohalyticsBaseEvent> ptr;
+  bool newUser = true;
+  boost::optional<EmitInfo> info;
+
+  while (true)
+  {
+    try
+    {
+      ar(ptr);
+    }
+    catch (const cereal::Exception & ex)
+    {
+      if (string("Failed to read 4 bytes from input stream! Read 0") != ex.what())
+      {
+        // The exception above is a normal one, Cereal lacks to detect the end of the stream.
+        cerr << ex.what() << endl;
+        return -1;
+      }
+      return 0;
+    }
+
+    auto const * event = dynamic_cast<AlohalyticsIdServerEvent const *>(ptr.get());
+    if (event)
+    {
+      newUser = true;
+      continue;
+    }
+
+    auto const * kpe = dynamic_cast<AlohalyticsKeyPairsEvent const *>(ptr.get());
+    if (!kpe)
+      continue;
+
+    if (kpe->key == "searchEmitResultsAndCoords")
+    {
+      info = ParseEmitResultsAndCoords(kpe->pairs);
+      newUser = false;
+    }
+    else if (kpe->key == "searchShowResult" && !newUser)
+    {
+      auto const result = ParseShowResult(kpe->pairs);
+      if (!info || !result)
+        continue;
+
+      if (FLAGS_categorial_only && !info->m_isCategorial)
+        continue;
+
+      auto const resultMatches = info->m_results.size() > result->m_pos &&
+                                 info->m_results[result->m_pos] == result->m_result;
+      if (!resultMatches)
+        continue;
+
+      if (auto const sample = MakeSample(*info, result->m_pos))
+      {
+        string json;
+        Sample::SerializeToJSONLines({*sample}, json);
+        cout << json;
+      }
+    }
+  }
+  return 0;
+}
