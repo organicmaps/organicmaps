47bc0b926e53dc0f10030090a3e1db0df2b5710d
Maxim Pimenov <m@maps.me>
2020-01-30 13:03:25 +0300

    [ge0] Removed an old TODO.
    The original author writes at [0]:
    
    Была идея сделать API, чтобы другие приложения на iOS могли вызывать
    mapsme и слегка кастомизировать под себя, а по выбору пина или кнопке
    назад, mapsme вызывала переданный callback url. Это как вариант
    недо-SDK для оффлайн карт, которые можно встроить в другие приложения,
    но при этом чтобы карты скачивались из mapsme.
    
    [0] https://github.com/mapsme/omim/pull/12514#discussion_r372612938

--- ge0/url_generator.hpp
+++ ge0/url_generator.hpp
@@ -25,37 +25,4 @@ int LatToInt(double lat, int maxValue);
 double LonIn180180(double lon);
 int LonToInt(double lon, int maxValue);
 void LatLonToString(double lat, double lon, char * s, size_t nBytes);
-
-/* @TODO(melnichek): Finish URL Scheme API implementation and uncomment.
-typedef struct
-{
-  double lat;
-  double lon;
-} Coordinate;
-
-typedef struct
-{
-  double lat;
-  double lon;
-  char const * name;
-  char const * id;
-} Pin;
-
-typedef struct
-{
-  char const * toolbarTitle;
-  char const * backButtonTitle;
-  char const * backButtonUrl;
-  char const * backPinUrl;
-
-  Pin const * pins;
-  int pinsSize;
-
-  Coordinate viewportCenter;
-  double viewportZoomLevel;
-} ShowMapRequest;
-
-// Generate a url to shows a map view.
-char * GenShowMapUrl(ShowMapRequest const * request);
-*/
 }  // namespace ge0
