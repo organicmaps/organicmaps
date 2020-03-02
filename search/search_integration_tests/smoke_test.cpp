029c638a10a961f66a89a812cdd54be45adbd12f
tatiana-yan <tatiana.kondakova@gmail.com>
2020-03-02 13:42:15 +0300

    [search_integration_tests] Remove landuse-residential from SmokeTest_CategoriesTest exceptions.

--- search/search_integration_tests/smoke_test.cpp
+++ search/search_integration_tests/smoke_test.cpp
@@ -155,7 +155,6 @@ UNIT_CLASS_TEST(SmokeTest, CategoriesTest)
                                                        {"waterway", "riverbank"},
                                                        {"waterway", "stream"},
                                                        {"landuse", "basin"},
-                                                       {"landuse", "residential"},
                                                        {"place", "county"},
                                                        {"place", "islet"},
                                                        {"power", "pole"},
