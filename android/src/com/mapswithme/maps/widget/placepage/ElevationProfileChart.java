1c0283a8e18bd6d4aad5366a9990c0d2a6776539
Dmitry Donskoy <donskdmitry@mail.ru>
2020-03-04 16:39:46 +0300

    [android] Added more intuitive behaviour for chart - scroll, close , zoom operations

---
+++ android/src/com/mapswithme/maps/widget/placepage/ElevationProfileChart.java
@@ -0,0 +1,37 @@
+package com.mapswithme.maps.widget.placepage;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+import com.github.mikephil.charting.charts.LineChart;
+
+public class ElevationProfileChart extends LineChart
+{
+  public ElevationProfileChart(Context context)
+  {
+    super(context);
+  }
+
+  public ElevationProfileChart(Context context, AttributeSet attrs)
+  {
+    super(context, attrs);
+  }
+
+  public ElevationProfileChart(Context context, AttributeSet attrs, int defStyle)
+  {
+    super(context, attrs, defStyle);
+  }
+
+  @Override
+  public boolean onInterceptTouchEvent(MotionEvent ev)
+  {
+    getParent().requestDisallowInterceptTouchEvent(true);
+    return hasZoom() || super.onInterceptTouchEvent(ev);
+  }
+
+  private boolean hasZoom()
+  {
+    return getScaleX() < 1  || getScaleY() < 1;
+  }
+}
