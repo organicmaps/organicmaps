41f8ce42090ce1330adbccbcbe2c28811c6ea30b
Dmitry Donskoy <donskdmitry@mail.ru>
2020-04-07 12:16:25 +0300

    [android] Fixed order views subs screen

--- android/src/com/mapswithme/maps/purchase/BookmarksAllSubscriptionPage.java
+++ android/src/com/mapswithme/maps/purchase/BookmarksAllSubscriptionPage.java
@@ -7,10 +7,10 @@ public enum BookmarksAllSubscriptionPage
 {
   FIRST(R.string.all_pass_subscription_message_title,
         R.string.all_pass_subscription_message_subtitle),
-  SECOND(R.string.all_pass_subscription_message_title_2,
-         R.string.all_pass_subscription_message_subtitle_2),
-  THIRD(R.string.all_pass_subscription_message_title_3,
-        R.string.all_pass_subscription_message_subtitle_3);
+  SECOND(R.string.all_pass_subscription_message_title_3,
+         R.string.all_pass_subscription_message_subtitle_3),
+  THIRD(R.string.all_pass_subscription_message_title_2,
+        R.string.all_pass_subscription_message_subtitle_2);
 
   @StringRes
   private final int mTitleId;
