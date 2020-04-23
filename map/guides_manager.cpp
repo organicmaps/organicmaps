19e4a1e46cd5934dca1cc4994be0ae97fd9fcef6
Arsentiy Milchakov <milcars@mapswithme.com>
2020-04-23 15:55:58 +0300

    [map] guides on map dummies

--- map/guides_manager.cpp
+++ map/guides_manager.cpp
@@ -41,14 +41,174 @@ void GuidesManager::ChangeState(GuidesState newState)
 
 GuidesManager::GuidesGallery GuidesManager::GetGallery() const
 {
-  // TODO: Implement.
-  return {};
+  // Dummy gallery for debug only.
+  GuidesGallery gallery;
+  {
+    GuidesGallery::Item item;
+    item.m_guideId = "048f4c49-ee80-463f-8513-e57ade2303ee";
+    item.m_url = "https://routes.maps.me/en/v3/mobilefront/route/048f4c49-ee80-463f-8513-e57ade2303ee";
+    item.m_imageUrl = "https://storage.maps.me/bookmarks_catalogue/"
+                      "002dc2ae-7b5c-4d3c-88bc-7c7ba109d0e8.jpg?t=1584470956.009026";
+    item.m_title = "Moscow by The Village";
+    item.m_subTitle = "awesome city guide";
+    item.m_type = GuidesGallery::Item::Type::City;
+    item.m_downloaded = false;
+    item.m_cityParams.m_bookmarksCount = 32;
+    item.m_cityParams.m_trackIsAvailable = false;
+
+    gallery.m_items.push_back(item);
+  }
+
+  {
+    GuidesGallery::Item item;
+    item.m_guideId = "e2d448eb-7fa4-4fab-93e7-ef0fea91cfff";
+    item.m_url = "https://routes.maps.me/en/v3/mobilefront/route/e2d448eb-7fa4-4fab-93e7-ef0fea91cfff";
+    item.m_imageUrl = "https://storage.maps.me/bookmarks_catalogue/"
+                      "002dc2ae-7b5c-4d3c-88bc-7c7ba109d0e8.jpg?t=1584470956.009026";
+    item.m_title = "Riga City Tour";
+    item.m_subTitle = "awesome city guide";
+    item.m_type = GuidesGallery::Item::Type::City;
+    item.m_downloaded = true;
+    item.m_cityParams.m_bookmarksCount = 31;
+    item.m_cityParams.m_trackIsAvailable = true;
+
+    gallery.m_items.push_back(item);
+  }
+
+  {
+    GuidesGallery::Item item;
+    item.m_guideId = "d26a6662-20a3-432c-a357-c9cb3cce6d57";
+    item.m_url = "https://routes.maps.me/en/v3/mobilefront/route/d26a6662-20a3-432c-a357-c9cb3cce6d57";
+    item.m_imageUrl = "https://img.oastatic.com/img2/1966324/834x417s/t.jpg";
+    item.m_title = "Klassik trifft Romantik";
+    item.m_subTitle = "Hiking / Trekking";
+    item.m_type = GuidesGallery::Item::Type::Outdoor;
+    item.m_downloaded = false;
+    item.m_outdoorsParams.m_ascent = 400;
+    item.m_outdoorsParams.m_distance = 24100;
+    item.m_outdoorsParams.m_duration = 749246;
+
+    gallery.m_items.push_back(item);
+  }
+
+  {
+    GuidesGallery::Item item;
+    item.m_guideId = "048f4c49-ee80-463f-8513-e57ade2303ee";
+    item.m_url = "https://routes.maps.me/en/v3/mobilefront/route/048f4c49-ee80-463f-8513-e57ade2303ee";
+    item.m_imageUrl = "https://storage.maps.me/bookmarks_catalogue/"
+                      "002dc2ae-7b5c-4d3c-88bc-7c7ba109d0e8.jpg?t=1584470956.009026";
+    item.m_title = "Moscow by The Village";
+    item.m_subTitle = "awesome city guide";
+    item.m_type = GuidesGallery::Item::Type::City;
+    item.m_downloaded = false;
+    item.m_cityParams.m_bookmarksCount = 32;
+    item.m_cityParams.m_trackIsAvailable = false;
+
+    gallery.m_items.push_back(item);
+  }
+
+  {
+    GuidesGallery::Item item;
+    item.m_guideId = "e2d448eb-7fa4-4fab-93e7-ef0fea91cfff";
+    item.m_url = "https://routes.maps.me/en/v3/mobilefront/route/e2d448eb-7fa4-4fab-93e7-ef0fea91cfff";
+    item.m_imageUrl = "https://storage.maps.me/bookmarks_catalogue/"
+                      "002dc2ae-7b5c-4d3c-88bc-7c7ba109d0e8.jpg?t=1584470956.009026";
+    item.m_title = "Riga City Tour";
+    item.m_subTitle = "awesome city guide";
+    item.m_type = GuidesGallery::Item::Type::City;
+    item.m_downloaded = false;
+    item.m_cityParams.m_bookmarksCount = 31;
+    item.m_cityParams.m_trackIsAvailable = true;
+
+    gallery.m_items.push_back(item);
+  }
+
+  {
+    GuidesGallery::Item item;
+    item.m_guideId = "d26a6662-20a3-432c-a357-c9cb3cce6d57";
+    item.m_url = "https://routes.maps.me/en/v3/mobilefront/route/d26a6662-20a3-432c-a357-c9cb3cce6d57";
+    item.m_imageUrl = "https://img.oastatic.com/img2/1966324/834x417s/t.jpg";
+    item.m_title = "Klassik trifft Romantik";
+    item.m_subTitle = "Hiking / Trekking";
+    item.m_type = GuidesGallery::Item::Type::Outdoor;
+    item.m_downloaded = true;
+    item.m_outdoorsParams.m_ascent = 400;
+    item.m_outdoorsParams.m_distance = 24100;
+    item.m_outdoorsParams.m_duration = 749246;
+
+    gallery.m_items.push_back(item);
+  }
+  {
+    GuidesGallery::Item item;
+    item.m_guideId = "048f4c49-ee80-463f-8513-e57ade2303ee";
+    item.m_url = "https://routes.maps.me/en/v3/mobilefront/route/048f4c49-ee80-463f-8513-e57ade2303ee";
+    item.m_imageUrl = "https://storage.maps.me/bookmarks_catalogue/"
+                      "002dc2ae-7b5c-4d3c-88bc-7c7ba109d0e8.jpg?t=1584470956.009026";
+    item.m_title = "Moscow by The Village";
+    item.m_subTitle = "awesome city guide";
+    item.m_type = GuidesGallery::Item::Type::City;
+    item.m_downloaded = false;
+    item.m_cityParams.m_bookmarksCount = 32;
+    item.m_cityParams.m_trackIsAvailable = false;
+
+    gallery.m_items.push_back(item);
+  }
+
+  {
+    GuidesGallery::Item item;
+    item.m_guideId = "e2d448eb-7fa4-4fab-93e7-ef0fea91cfff";
+    item.m_url = "https://routes.maps.me/en/v3/mobilefront/route/e2d448eb-7fa4-4fab-93e7-ef0fea91cfff";
+    item.m_imageUrl = "https://storage.maps.me/bookmarks_catalogue/"
+                      "002dc2ae-7b5c-4d3c-88bc-7c7ba109d0e8.jpg?t=1584470956.009026";
+    item.m_title = "Riga City Tour";
+    item.m_subTitle = "awesome city guide";
+    item.m_type = GuidesGallery::Item::Type::City;
+    item.m_downloaded = false;
+    item.m_cityParams.m_bookmarksCount = 31;
+    item.m_cityParams.m_trackIsAvailable = true;
+
+    gallery.m_items.push_back(item);
+  }
+
+  {
+    GuidesGallery::Item item;
+    item.m_guideId = "d26a6662-20a3-432c-a357-c9cb3cce6d57";
+    item.m_url = "https://routes.maps.me/en/v3/mobilefront/route/d26a6662-20a3-432c-a357-c9cb3cce6d57";
+    item.m_imageUrl = "https://img.oastatic.com/img2/1966324/834x417s/t.jpg";
+    item.m_title = "Klassik trifft Romantik";
+    item.m_subTitle = "Hiking / Trekking";
+    item.m_type = GuidesGallery::Item::Type::Outdoor;
+    item.m_downloaded = false;
+    item.m_outdoorsParams.m_ascent = 400;
+    item.m_outdoorsParams.m_distance = 24100;
+    item.m_outdoorsParams.m_duration = 749246;
+
+    gallery.m_items.push_back(item);
+  }
+
+  {
+    GuidesGallery::Item item;
+    item.m_guideId = "d26a6662-20a3-432c-a357-c9cb3cce6d57";
+    item.m_url = "https://routes.maps.me/en/v3/mobilefront/route/d26a6662-20a3-432c-a357-c9cb3cce6d57";
+    item.m_imageUrl = "https://img.oastatic.com/img2/1966324/834x417s/t.jpg";
+    item.m_title = "Klassik trifft Romantik";
+    item.m_subTitle = "Hiking / Trekking";
+    item.m_type = GuidesGallery::Item::Type::Outdoor;
+    item.m_downloaded = false;
+    item.m_outdoorsParams.m_ascent = 400;
+    item.m_outdoorsParams.m_distance = 24100;
+    item.m_outdoorsParams.m_duration = 749246;
+
+    gallery.m_items.push_back(item);
+  }
+
+  return gallery;
 }
 
 std::string GuidesManager::GetActiveGuide() const
 {
-  // TODO: Implement.
-  return "";
+  // Dummy active guide for debug only.
+  return "048f4c49-ee80-463f-8513-e57ade2303ee";
 }
 
 void GuidesManager::SetActiveGuide(std::string const & guideId)
