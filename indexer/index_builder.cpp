8e923cf3000470b691cc64f2268a825b53eccaac
tatiana-yan <tatiana.kondakova@gmail.com>
2020-04-27 14:25:49 +0300

    [indexer] Clang-format.

--- indexer/index_builder.cpp
+++ indexer/index_builder.cpp
@@ -8,32 +8,32 @@
 
 namespace indexer
 {
-  bool BuildIndexFromDataFile(std::string const & dataFile, std::string const & tmpFile)
+bool BuildIndexFromDataFile(std::string const & dataFile, std::string const & tmpFile)
+{
+  try
   {
-    try
+    std::string const idxFileName(tmpFile + GEOM_INDEX_TMP_EXT);
     {
-      std::string const idxFileName(tmpFile + GEOM_INDEX_TMP_EXT);
-      {
-        FeaturesVectorTest features(dataFile);
-        FileWriter writer(idxFileName);
+      FeaturesVectorTest features(dataFile);
+      FileWriter writer(idxFileName);
 
-        BuildIndex(features.GetHeader(), features.GetVector(), writer, tmpFile);
-      }
-
-      FilesContainerW(dataFile, FileWriter::OP_WRITE_EXISTING).Write(idxFileName, INDEX_FILE_TAG);
-      FileWriter::DeleteFileX(idxFileName);
-    }
-    catch (Reader::Exception const & e)
-    {
-      LOG(LERROR, ("Error while reading file: ", e.Msg()));
-      return false;
-    }
-    catch (Writer::Exception const & e)
-    {
-      LOG(LERROR, ("Error writing index file: ", e.Msg()));
-      return false;
+      BuildIndex(features.GetHeader(), features.GetVector(), writer, tmpFile);
     }
 
-    return true;
+    FilesContainerW(dataFile, FileWriter::OP_WRITE_EXISTING).Write(idxFileName, INDEX_FILE_TAG);
+    FileWriter::DeleteFileX(idxFileName);
   }
+  catch (Reader::Exception const & e)
+  {
+    LOG(LERROR, ("Error while reading file: ", e.Msg()));
+    return false;
+  }
+  catch (Writer::Exception const & e)
+  {
+    LOG(LERROR, ("Error writing index file: ", e.Msg()));
+    return false;
+  }
+
+  return true;
+}
 }  // namespace indexer
