ece7bba9a682ec53260e1d89b104a5f33291adfe
Arsentiy Milchakov <milcars@mapswithme.com>
2020-05-14 16:59:39 +0300

    [guides on map] calculation of bookmark catalog link is fixed

--- map/guides_manager.cpp
+++ map/guides_manager.cpp
@@ -188,8 +188,8 @@ GuidesManager::GuidesGallery GuidesManager::GetGallery() const
     GuidesGallery::Item item;
     item.m_guideId = info.m_id;
 
-    auto url = url::Join(BOOKMARKS_CATALOG_FRONT_URL, languages::GetCurrentNorm(), "v3/mobilefront",
-                         info.m_id);
+    auto url = url::Join(BOOKMARKS_CATALOG_FRONT_URL, languages::GetCurrentNorm(),
+                         "v3/mobilefront/route", info.m_id);
     InjectUTM(url, UTM::GuidesOnMapGallery);
     InjectUTMTerm(url, std::to_string(m_shownGuides.size()));
 
