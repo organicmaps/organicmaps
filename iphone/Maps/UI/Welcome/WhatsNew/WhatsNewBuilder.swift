072e50feb190f676794583a8c595974b3b0c087a
Alexander Boriskov <a.boriskov@corp.mail.ru>
2020-06-01 17:53:23 +0300

    [iOS] What's new 10.1
    https://jira.mail.ru/browse/MAPSME-13926

--- iphone/Maps/UI/Welcome/WhatsNew/WhatsNewBuilder.swift
+++ iphone/Maps/UI/Welcome/WhatsNew/WhatsNewBuilder.swift
@@ -1,20 +1,9 @@
 class WhatsNewBuilder {
-  static var catalogUrl = URL(string: "https://routes.maps.me/v3/mobilefront/?utm_source=maps.me&utm_medium=whatsnew&utm_campaign=100_minor&utm_content=download_guides_button")
   static var configs:[WhatsNewPresenter.WhatsNewConfig] {
     return [
-      WhatsNewPresenter.WhatsNewConfig(image: UIImage(named: "img_onboarding_elevprofile"),
-                                       title: "whatsnew_elevation_profile_title",
-                                       text: "whatsnew_elevation_profile_message",
-                                       buttonNextTitle: "new_onboarding_button"),
-      WhatsNewPresenter.WhatsNewConfig(image: UIImage(named: "img_onboarding_newcatalog"),
-                                       title: "whatsnew_catalog_new_title",
-                                       text: "whatsnew_catalog_new_message",
-                                       buttonNextTitle: "download_guides_button",
-                                       isCloseButtonHidden: false,
-                                       action: { MapViewController.shared()?.openCatalogAbsoluteUrl(catalogUrl, animated: true, utm: .none) }),
-      WhatsNewPresenter.WhatsNewConfig(image: UIImage(named: "img_onboarding_newcatalog"),
-                                       title: "whatsnew_catalog_new_title",
-                                       text: "whatsnew_catalog_new_message",
+      WhatsNewPresenter.WhatsNewConfig(image: UIImage(named: "img_wnew_newguides"),
+                                       title: "whatsnew_guides_on_map_title",
+                                       text: "whatsnew_guides_on_map_message",
                                        buttonNextTitle: "done")
     ]
   }
