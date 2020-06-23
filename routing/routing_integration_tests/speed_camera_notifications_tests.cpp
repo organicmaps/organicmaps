d896c1efb0b5edec6f71de4605c2d744c5308836
Vladimir Byko-Ianko <v.bykoianko@corp.mail.ru>
2020-06-23 18:44:14 +0300

    [routing] Fixing routing integration tests for map 200607.

--- routing/routing_integration_tests/speed_camera_notifications_tests.cpp
+++ routing/routing_integration_tests/speed_camera_notifications_tests.cpp
@@ -355,34 +355,34 @@ UNIT_TEST(SpeedCameraNotification_AutoAlwaysMode_8)
   for (auto const mode : modes)
   {
     RoutingSession routingSession;
-    InitRoutingSession({55.678536, 37.531112} /* from */,
-                       {55.671112, 37.520202} /* to   */,
+    InitRoutingSession({55.67547, 37.52662} /* from */,
+                       {55.67052, 37.51893}   /* to   */,
                        routingSession,
                        mode);
 
     {
       double const speedKmPH = 180.0;
-      ChangePosition({55.67840, 37.53090}, speedKmPH, routingSession);
+      ChangePosition({55.67533, 37.5264}, speedKmPH, routingSession);
       TEST(!CheckVoiceNotification(routingSession), ());
       TEST(!CheckBeepSignal(routingSession), ());
     }
     {
       double const speedKmPH = 180.0;
-      ChangePosition({55.67810, 37.53050}, speedKmPH, routingSession);
+      ChangePosition({55.67515, 37.52577}, speedKmPH, routingSession);
       TEST_EQUAL(CheckZone(routingSession, speedKmPH), SpeedCameraManager::Interval::VoiceNotificationZone, ());
       TEST(CheckVoiceNotification(routingSession), ());
       TEST(!CheckBeepSignal(routingSession), ());
     }
     {
       double const speedKmPH = 180.0;
-      ChangePosition({55.67810, 37.53050}, speedKmPH, routingSession);
+      ChangePosition({55.67515, 37.52577}, speedKmPH, routingSession);
       TEST_EQUAL(CheckZone(routingSession, speedKmPH), SpeedCameraManager::Interval::VoiceNotificationZone, ());
       TEST(!CheckVoiceNotification(routingSession), ());
       TEST(!CheckBeepSignal(routingSession), ());
     }
     {
       double const speedKmPH = 180.0;
-      ChangePosition({55.67790, 37.53020}, speedKmPH, routingSession);
+      ChangePosition({55.67505, 37.52542}, speedKmPH, routingSession);
       TEST_EQUAL(CheckZone(routingSession, speedKmPH), SpeedCameraManager::Interval::BeepSignalZone, ());
       TEST(!CheckVoiceNotification(routingSession), ());
       TEST(CheckBeepSignal(routingSession), ());
