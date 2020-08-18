fe19a7035bf45277e09f7a180f413184bf91203c
Aleksey Belousov <aleksey@maps.me>
2020-08-18 14:33:10 +0300

    [iOS] add missing font to styles

--- iphone/Maps/Core/Theme/FontStyleSheet.swift
+++ iphone/Maps/Core/Theme/FontStyleSheet.swift
@@ -105,6 +105,9 @@ class FontStyleSheet: IStyleSheet {
     theme.add(styleName: "bold17") { (s) -> (Void) in
       s.font = fonts.bold17
     }
+    theme.add(styleName: "bold18") { (s) -> (Void) in
+      s.font = fonts.bold18
+    }
     theme.add(styleName: "bold20") { (s) -> (Void) in
       s.font = fonts.bold20
     }
