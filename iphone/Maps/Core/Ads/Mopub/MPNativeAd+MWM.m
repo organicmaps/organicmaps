03c67cc9c17636380de4c49f654ae8e0847cbee6
Alexander Boriskov <a.boriskov@corp.mail.ru>
2020-08-19 16:01:43 +0300

    [iOS] Removed unnecessary check from ad banner
    https://jira.mail.ru/browse/MAPSME-14420

--- iphone/Maps/Core/Ads/Mopub/MPNativeAd+MWM.m
+++ iphone/Maps/Core/Ads/Mopub/MPNativeAd+MWM.m
@@ -1,11 +1,10 @@
+#import <FBAudienceNetwork/FBNativeAd.h>
 #import "MPNativeAd+MWM.h"
 #import "SwiftBridge.h"
-#import <FBAudienceNetwork/FBNativeAd.h>
 
 @interface MPNativeAd ()
 
-@property(nonatomic) MPNativeView * associatedView;
-@property(nonatomic) BOOL hasAttachedToView;
+@property(nonatomic) MPNativeView *associatedView;
 @property(nonatomic, readonly) id<MPNativeAdAdapter> adAdapter;
 
 - (void)willAttachToView:(UIView *)view withAdContentViews:(NSArray *)adContentViews;
@@ -16,46 +15,35 @@
 
 @implementation MPNativeAd (MWM)
 
-- (void)setAdView:(UIView *)view iconView:(UIImageView *)iconImageView actionButtons:(NSArray<UIButton *> *)buttons
-{
+- (void)setAdView:(UIView *)view iconView:(UIImageView *)iconImageView actionButtons:(NSArray<UIButton *> *)buttons {
   self.associatedView = (MPNativeView *)view;
   ((AdBannerView *)view).mpNativeAd = self;
-  if (!self.hasAttachedToView) {
-    id<MPNativeAdAdapter> adapter = self.adAdapter;
-    if ([adapter isKindOfClass:[FacebookNativeAdAdapter class]])
-    {
-     FacebookNativeAdAdapter *fbAdapter = (FacebookNativeAdAdapter *)adapter;
-     FBNativeBannerAd *fbNativeAd = (FBNativeBannerAd *)(fbAdapter.fbNativeAdBase);
-     if (fbNativeAd) {
-       [fbNativeAd registerViewForInteraction:self.associatedView
-                                iconImageView:iconImageView
-                               viewController:[MapViewController sharedController]
-                               clickableViews:buttons];
-     }
+  id<MPNativeAdAdapter> adapter = self.adAdapter;
+  if ([adapter isKindOfClass:[FacebookNativeAdAdapter class]]) {
+    FacebookNativeAdAdapter *fbAdapter = (FacebookNativeAdAdapter *)adapter;
+    FBNativeBannerAd *fbNativeAd = (FBNativeBannerAd *)(fbAdapter.fbNativeAdBase);
+    if (fbNativeAd) {
+      [fbNativeAd unregisterView];
+      [fbNativeAd registerViewForInteraction:self.associatedView
+                               iconImageView:iconImageView
+                              viewController:[MapViewController sharedController]
+                              clickableViews:buttons];
     }
-    else
-    {
-      [self willAttachToView:self.associatedView withAdContentViews:self.associatedView.subviews];
-      for (UIButton * button in buttons)
-      {
-        [button addTarget:self
-                      action:@selector(adViewTapped)
-            forControlEvents:UIControlEventTouchUpInside];
-      }
+  } else {
+    [self willAttachToView:self.associatedView withAdContentViews:self.associatedView.subviews];
+    for (UIButton *button in buttons) {
+      [button removeTarget:nil action:NULL forControlEvents:UIControlEventAllEvents];
+      [button addTarget:self action:@selector(adViewTapped) forControlEvents:UIControlEventTouchUpInside];
     }
-    self.hasAttachedToView = YES;
   }
 }
 
-- (void)unregister
-{
+- (void)unregister {
   id<MPNativeAdAdapter> adapter = self.adAdapter;
   self.delegate = nil;
   [self nativeViewWillMoveToSuperview:nil];
   self.associatedView = nil;
-  self.hasAttachedToView = NO;
-  if ([adapter isKindOfClass:[FacebookNativeAdAdapter class]])
-  {
+  if ([adapter isKindOfClass:[FacebookNativeAdAdapter class]]) {
     FacebookNativeAdAdapter *fbAdapter = (FacebookNativeAdAdapter *)adapter;
     [fbAdapter.fbNativeAdBase unregisterView];
   }
