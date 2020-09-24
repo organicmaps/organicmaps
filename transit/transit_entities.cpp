e413ad6323956f5eb5a9be71141d94b454779ce9
Olga Khlopkova <o.khlopkova@corp.mail.ru>
2020-09-24 10:05:35 +0300

    [transit] Replace inline static kSubwayLayerTypes with extern + cpp.

---
+++ transit/transit_entities.cpp
@@ -0,0 +1,7 @@
+#include "transit/transit_entities.hpp"
+
+namespace transit
+{
+std::unordered_set<std::string> const kSubwayLayerTypes{"subway", "train", "light_rail",
+                                                        "monorail"};
+}  // namespace transit
