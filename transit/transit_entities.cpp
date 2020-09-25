e9707c1f4b2adb34b5e17cb8a148d1fd84b3072d
Olga Khlopkova <o.khlopkova@corp.mail.ru>
2020-09-25 11:27:26 +0300

    [transit] Inline kSubwayLayerTypes.

--- transit/transit_entities.cpp
+++
@@ -1,7 +0,0 @@
-#include "transit/transit_entities.hpp"
-
-namespace transit
-{
-std::unordered_set<std::string> const kSubwayLayerTypes{"subway", "train", "light_rail",
-                                                        "monorail"};
-}  // namespace transit
