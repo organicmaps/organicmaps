8cf76b35a5c17c5ffc84a71a071f770a9ecc4e9d
Alexander Boriskov <a.boriskov@corp.mail.ru>
2020-10-06 12:26:28 +0300

    [iOS] Fixed crash in carplay

--- iphone/Maps/Classes/CarPlay/CarPlayService.swift
+++ iphone/Maps/Classes/CarPlay/CarPlayService.swift
@@ -367,8 +367,10 @@ extension CarPlayService: CPMapTemplateDelegate {
     }
     
     MapTemplateBuilder.configureNavigationUI(mapTemplate: rootMapTemplate)
-    interfaceController.popToRootTemplate(animated: false)
-    
+
+    if interfaceController.templates.count > 1 {
+      interfaceController.popToRootTemplate(animated: false)
+    }
     router.startNavigationSession(forTrip: trip, template: rootMapTemplate)
     router.startRoute()
     if let estimates = createEstimates(routeInfo: info) {
@@ -617,7 +619,10 @@ extension CarPlayService {
     let mapTemplate = MapTemplateBuilder.buildTripPreviewTemplate(forTrips: trips)
     if let interfaceController = interfaceController {
       mapTemplate.mapDelegate = self
-      interfaceController.popToRootTemplate(animated: false)
+
+      if interfaceController.templates.count > 1 {
+        interfaceController.popToRootTemplate(animated: false)
+      }
       interfaceController.pushTemplate(mapTemplate, animated: false)
     }
   }
