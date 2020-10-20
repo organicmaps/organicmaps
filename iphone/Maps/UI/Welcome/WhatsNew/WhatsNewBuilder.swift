e81269d6bd64415b79fe252dc7b5cdf0cec28d29
Alexander Boriskov <a.boriskov@corp.mail.ru>
2020-10-20 14:19:22 +0300

    [iOS] What's new 10.3 major
    https://jira.mail.ru/browse/MAPSME-15081

--- iphone/Maps/UI/Welcome/WhatsNew/WhatsNewBuilder.swift
+++ iphone/Maps/UI/Welcome/WhatsNew/WhatsNewBuilder.swift
@@ -1,22 +1,21 @@
 class WhatsNewBuilder {
   static var configs:[WhatsNewPresenter.WhatsNewConfig] {
     return [
-      WhatsNewPresenter.WhatsNewConfig(image: UIImage(named: "img_whatsnew_trial.png"),
-                                       title: "whatsnew_trial_title",
-                                       text: "whatsnew_trial_message",
+      WhatsNewPresenter.WhatsNewConfig(image: UIImage(named: "img_whatsnew_lp"),
+                                       title: "whatsnew_lp_title",
+                                       text: "whatsnew_lp_message",
                                        buttonNextTitle: "whatsnew_trial_cta",
                                        isCloseButtonHidden: false,
                                        action: {
-                                        let subscribeViewController = SubscriptionViewBuilder.build(type: .allPass,
-                                                                                                    parentViewController: MapViewController.shared(),
-                                                                                                    source: kStatWhatsNew,
-                                                                                                    successDialog: .goToCatalog,
-                                                                                                    completion: nil)
+                                        let subscribeViewController = SubscriptionViewBuilder.buildLonelyPlanet(parentViewController: MapViewController.shared(),
+                                                                                                                source: kStatWhatsNew,
+                                                                                                                successDialog: .goToCatalog,
+                                                                                                                completion: nil)
                                         MapViewController.shared().present(subscribeViewController, animated: true)
       }),
-      WhatsNewPresenter.WhatsNewConfig(image: UIImage(named: "img_whatsnew_trial.png"),
-                                       title: "whatsnew_trial_title",
-                                       text: "whatsnew_trial_message",
+      WhatsNewPresenter.WhatsNewConfig(image: UIImage(named: "img_whatsnew_lp"),
+                                       title: "whatsnew_lp_title",
+                                       text: "whatsnew_lp_message",
                                        buttonNextTitle: "done")
     ]
   }
