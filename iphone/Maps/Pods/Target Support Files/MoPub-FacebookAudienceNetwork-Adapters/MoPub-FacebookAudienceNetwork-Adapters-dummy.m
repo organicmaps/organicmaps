029a19914e96840f8c95405ed912f2354b281415
vng <viktor.govako@gmail.com>
2020-12-20 13:26:55 +0300

    [iOS] Removed PODs libraries from repo. Signed-off-by: Viktar Havaka viktor.govako@gmail.com

--- iphone/Maps/Pods/Target Support Files/MoPub-FacebookAudienceNetwork-Adapters/MoPub-FacebookAudienceNetwork-Adapters-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MoPub_FacebookAudienceNetwork_Adapters : NSObject
-@end
-@implementation PodsDummy_MoPub_FacebookAudienceNetwork_Adapters
-@end
--- iphone/Maps/Pods/Target Support Files/mopub-ios-sdk/mopub-ios-sdk-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_mopub_ios_sdk : NSObject
-@end
-@implementation PodsDummy_mopub_ios_sdk
-@end
