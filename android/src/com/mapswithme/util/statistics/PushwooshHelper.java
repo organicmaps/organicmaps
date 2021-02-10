ee8f8844af13c3c4ce36be11aad716af6c97a719
Altynaika <altynaika.toktomuratovaa@gmail.com>
2021-02-10 18:20:34 +0600

    [android] Removed pushwoosh dependency and logic #118
    Signed-off-by: Altynaika <altynaika.toktomuratovaa@gmail.com>

--- android/src/com/mapswithme/util/statistics/PushwooshHelper.java
+++ android/src/com/mapswithme/util/statistics/PushwooshHelper.java
@@ -5,12 +5,7 @@ import androidx.annotation.Nullable;
 
 import com.mapswithme.util.log.Logger;
 import com.mapswithme.util.log.LoggerFactory;
-import com.pushwoosh.Pushwoosh;
-import com.pushwoosh.exception.PushwooshException;
-import com.pushwoosh.function.Result;
-import com.pushwoosh.tags.TagsBundle;
 
-import java.util.Arrays;
 
 public final class PushwooshHelper
 {
@@ -22,32 +17,9 @@ public final class PushwooshHelper
     if (params == null)
       return;
 
-    TagsBundle.Builder builder = new TagsBundle.Builder();
-    boolean isSingleParam = params.length == 1;
-    TagsBundle tagsBundle = isSingleParam ? builder.putString(tag, params[0]).build()
-                                          : builder.putList(tag, Arrays.asList(params)).build();
-    Pushwoosh.getInstance().sendTags(tagsBundle, this::onPostExecute);
+
   }
 
-  private void onPostExecute(@NonNull Result<Void, PushwooshException> result)
-  {
-    if (result.isSuccess())
-      onSuccess(result);
-    else
-      onError(result);
-  }
-
-  private void onError(@NonNull Result<Void, PushwooshException> result)
-  {
-    PushwooshException exception = result.getException();
-    String msg = exception == null ? null : exception.getLocalizedMessage();
-    LOGGER.e("Pushwoosh", msg != null ? msg : "onSentTagsError");
-  }
-
-  private void onSuccess(@NonNull Result<Void, PushwooshException> result)
-  {
-    /* Do nothing by default */
-  }
 
   public static native void nativeProcessFirstLaunch();
   public static native void nativeSendEditorAddObjectTag();
