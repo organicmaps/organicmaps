ddd86ca46166ee4b5d18c8a687b29d690fde8765
Alexander Borsuk <me@alex.bio>
2021-03-14 13:37:31 +0100

    [ios] Leftovers cleanup
    Signed-off-by: Alexander Borsuk <me@alex.bio>

--- iphone/Maps/3party/MyTarget/MyTargetSDK.framework/Headers/MTRGNavigationType.h
+++
@@ -1,9 +0,0 @@
-//
-//  MTRGNavigationType.h
-//  myTargetSDK 4.6.15
-//
-//  Created by Anton Bulankin on 30.05.16.
-//  Copyright © 2016 Mail.ru. All rights reserved.
-//
-
-typedef enum { MTRGNavigationTypeWeb, MTRGNavigationTypeStore } MTRGNavigationType;
