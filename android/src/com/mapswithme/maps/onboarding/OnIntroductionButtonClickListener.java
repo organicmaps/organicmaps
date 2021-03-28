720708499cf4b71cbf97b33b399d5af8a7d2c45b
Roman Tsisyk <roman@tsisyk.com>
2021-03-28 11:03:39 +0300

    [android]: Remove news and traces of onboarding
    Signed-off-by: Roman Tsisyk <roman@tsisyk.com>

--- android/src/com/mapswithme/maps/onboarding/OnIntroductionButtonClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.mapswithme.maps.onboarding;
-
-import android.app.Activity;
-import androidx.annotation.NonNull;
-
-public interface OnIntroductionButtonClickListener
-{
-  void onIntroductionButtonClick(@NonNull Activity activity, @NonNull String deeplink);
-}
