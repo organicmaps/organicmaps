30b0086e6789cea1bc6f67b94d84202e033a7af6
Alexander Borsuk <me@alex.bio>
2021-04-29 18:11:04 +0200

    [ios] Removed unhelpful and unnecessary toast (string was already removed)
    Signed-off-by: Alexander Borsuk <me@alex.bio>

--- iphone/Maps/UI/BottomMenu/Menu/BottomMenuViewController.swift
+++ iphone/Maps/UI/BottomMenu/Menu/BottomMenuViewController.swift
@@ -33,9 +33,6 @@ class BottomMenuViewController: MWMViewController {
     tableView.delegate = presenter
     tableView.registerNib(cell: BottomMenuItemCell.self)
     tableView.registerNib(cell: BottomMenuLayersCell.self)
-    performOnce({
-      Toast.toast(withText: L("routes_layer_in_menu_toast")).show(withAlignment: .top)
-    }, "routes_layer_in_menu_toast")
   }
   
   override func viewDidLayoutSubviews() {
