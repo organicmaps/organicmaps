1c1b1883f4b4cee15d02a7c28416f0064c298170
Roman Tsisyk <roman@tsisyk.com>
2021-05-16 10:56:41 +0300

    [android] Remove overengineering from BookmarkCategoryAdapter
    Signed-off-by: Roman Tsisyk <roman@tsisyk.com>

--- android/src/com/mapswithme/maps/bookmarks/CategoryListCallback.java
+++ android/src/com/mapswithme/maps/bookmarks/CategoryListCallback.java
@@ -1,12 +1,6 @@
 package com.mapswithme.maps.bookmarks;
 
-import androidx.annotation.NonNull;
-
-import com.mapswithme.maps.bookmarks.data.BookmarkCategory;
-
 interface CategoryListCallback
 {
-  void onFooterClick();
-
-  void onMoreOperationClick(@NonNull BookmarkCategory item);
+  void onAddButtonClick();
 }
