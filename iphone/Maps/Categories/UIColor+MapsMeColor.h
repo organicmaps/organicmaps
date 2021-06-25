a290f9145d5a696a6832ff105be897bdd267ba90
Alexander Borsuk <me@alex.bio>
2021-06-25 10:25:20 +0200

    [ios] Removed annoying deprecation warnings
    TODO: investigate the reason behind them and fix properly.
    Signed-off-by: Alexander Borsuk <me@alex.bio>

--- iphone/Maps/Categories/UIColor+MapsMeColor.h
+++ iphone/Maps/Categories/UIColor+MapsMeColor.h
@@ -4,38 +4,38 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface UIColor (MapsMeColor)
 
-+ (UIColor *)blackPrimaryText __attribute__ ((deprecated));
-+ (UIColor *)blackSecondaryText __attribute__ ((deprecated));
-+ (UIColor *)blackHintText __attribute__ ((deprecated));
-+ (UIColor *)red __attribute__ ((deprecated));
-+ (UIColor *)white __attribute__ ((deprecated));
-+ (UIColor *)primary __attribute__ ((deprecated));
-+ (UIColor *)pressBackground __attribute__ ((deprecated));
-+ (UIColor *)linkBlue __attribute__ ((deprecated));
-+ (UIColor *)linkBlueHighlighted __attribute__ ((deprecated));
-+ (UIColor *)buttonRed __attribute__ ((deprecated));
-+ (UIColor *)blackDividers __attribute__ ((deprecated));
-+ (UIColor *)whitePrimaryText __attribute__ ((deprecated));
-+ (UIColor *)whitePrimaryTextHighlighted __attribute__ ((deprecated));
-+ (UIColor *)whiteHintText __attribute__ ((deprecated));
-+ (UIColor *)buttonDisabledBlueText __attribute__ ((deprecated));
-+ (UIColor *)blackOpaque __attribute__ ((deprecated));
-+ (UIColor *)bookingBackground __attribute__ ((deprecated));
-+ (UIColor *)opentableBackground __attribute__ ((deprecated));
-+ (UIColor *)transparentGreen __attribute__ ((deprecated));
-+ (UIColor *)speedLimitRed __attribute__ ((deprecated));
-+ (UIColor *)speedLimitGeen __attribute__ ((deprecated));
-+ (UIColor *)speedLimitWhite __attribute__ ((deprecated));
-+ (UIColor *)speedLimitLightGray __attribute__ ((deprecated));
-+ (UIColor *)speedLimitDarkGray __attribute__ ((deprecated));
++ (UIColor *)blackPrimaryText;
++ (UIColor *)blackSecondaryText;
++ (UIColor *)blackHintText;
++ (UIColor *)red;
++ (UIColor *)white;
++ (UIColor *)primary;
++ (UIColor *)pressBackground;
++ (UIColor *)linkBlue;
++ (UIColor *)linkBlueHighlighted;
++ (UIColor *)buttonRed;
++ (UIColor *)blackDividers;
++ (UIColor *)whitePrimaryText;
++ (UIColor *)whitePrimaryTextHighlighted;
++ (UIColor *)whiteHintText;
++ (UIColor *)buttonDisabledBlueText;
++ (UIColor *)blackOpaque;
++ (UIColor *)bookingBackground;
++ (UIColor *)opentableBackground;
++ (UIColor *)transparentGreen;
++ (UIColor *)speedLimitRed;
++ (UIColor *)speedLimitGeen;
++ (UIColor *)speedLimitWhite;
++ (UIColor *)speedLimitLightGray;
++ (UIColor *)speedLimitDarkGray;
 
-+ (UIColor *)colorWithName:(NSString *)colorName __attribute__ ((deprecated));
++ (UIColor *)colorWithName:(NSString *)colorName;
 + (UIColor *)colorFromHexString:(NSString *)hexString;
 
 + (void)setNightMode:(BOOL)mode;
 + (BOOL)isNightMode;
 
-- (UIColor *)opposite __attribute__ ((deprecated));
+- (UIColor *)opposite;
 
 @end
 
