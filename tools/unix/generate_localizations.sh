95199a4347a799586600100628faccbac2255a34
Alexander Borsuk <me@alex.bio>
2021-07-24 23:41:02 +0200

    [strings] Include only translated strings in apple localizations
    Signed-off-by: Alexander Borsuk <me@alex.bio>

--- tools/unix/generate_localizations.sh
+++ tools/unix/generate_localizations.sh
@@ -51,9 +51,9 @@ cat "$STRINGS_PATH"/{strings,types_strings}.txt> "$MERGED_FILE"
 # TODO: Add "--untagged --tags android" when tags are properly set.
 # TODO: Add validate-strings-file call to check for duplicates (and avoid Android build errors) when tags are properly set.
 $TWINE generate-all-localization-files --include translated --format android "$MERGED_FILE" "$OMIM_PATH/android/res/"
-$TWINE generate-all-localization-files --format apple "$MERGED_FILE" "$OMIM_PATH/iphone/Maps/LocalizedStrings/"
-$TWINE generate-all-localization-files --format apple-plural "$MERGED_FILE" "$OMIM_PATH/iphone/Maps/LocalizedStrings/"
-$TWINE generate-all-localization-files --format apple --file-name InfoPlist.strings "$OMIM_PATH/iphone/plist.txt" "$OMIM_PATH/iphone/Maps/LocalizedStrings/"
+$TWINE generate-all-localization-files --include translated --format apple "$MERGED_FILE" "$OMIM_PATH/iphone/Maps/LocalizedStrings/"
+$TWINE generate-all-localization-files --include translated --format apple-plural "$MERGED_FILE" "$OMIM_PATH/iphone/Maps/LocalizedStrings/"
+$TWINE generate-all-localization-files --include translated --format apple --file-name InfoPlist.strings "$OMIM_PATH/iphone/plist.txt" "$OMIM_PATH/iphone/Maps/LocalizedStrings/"
 $TWINE generate-all-localization-files --format jquery "$OMIM_PATH/data/countries_names.txt" "$OMIM_PATH/data/countries-strings/"
 $TWINE generate-all-localization-files --format jquery "$OMIM_PATH/data/sound.txt" "$OMIM_PATH/data/sound-strings/"
 
