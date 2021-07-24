959dc0834a4eb576968678b440d91d90d93fa148
Dzmitry Yarmolenka <86627724+Endem10n@users.noreply.github.com>
2021-07-25 00:32:27 +0200

    [android] Silence media volume while announcing navigation directions (#901)
    * [android] Silenced media volume while announcing navigation directions.
    
    Signed-off-by: Dzmitry Yarmolenka <dzmitry.yarmolenka.1986@gmail.com>
    
    * [android] Changed AudioFocusManager according to project code style.
    
    Signed-off-by: Dzmitry Yarmolenka <dzmitry.yarmolenka.1986@gmail.com>
    
    * [android] Reduced delay after requesting audio focus to 50 milliseconds.
    
    Signed-off-by: Dzmitry Yarmolenka <dzmitry.yarmolenka.1986@gmail.com>
    
    * [android] removed small delay during requesting audio focus for cases, when background music is not playing.
    
    Signed-off-by: Dzmitry Yarmolenka <dzmitry.yarmolenka.1986@gmail.com>

---
+++ android/src/com/mapswithme/maps/sound/AudioFocusManager.java
@@ -0,0 +1,51 @@
+package com.mapswithme.maps.sound;
+
+import android.content.Context;
+import android.media.AudioFocusRequest;
+import android.media.AudioManager;
+import android.os.Build;
+
+import androidx.annotation.Nullable;
+
+import static android.media.AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_EXCLUSIVE;
+
+public class AudioFocusManager
+{
+  @Nullable
+  private AudioManager audioManager = null;
+
+  public AudioFocusManager(@Nullable Context context)
+  {
+    if (context != null)
+      audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+  }
+
+  public boolean requestAudioFocus()
+  {
+    boolean isMusicActive = false;
+
+    if (audioManager != null)
+      isMusicActive = audioManager.isMusicActive();
+
+    if (audioManager != null)
+    {
+      if (Build.VERSION.SDK_INT >= 26)
+        audioManager.requestAudioFocus(new AudioFocusRequest.Builder(AUDIOFOCUS_GAIN_TRANSIENT_EXCLUSIVE).build());
+      else
+        audioManager.requestAudioFocus(focusChange -> {}, AudioManager.STREAM_VOICE_CALL, AUDIOFOCUS_GAIN_TRANSIENT_EXCLUSIVE);
+    }
+
+    return isMusicActive;
+  }
+
+  public void releaseAudioFocus()
+  {
+    if (audioManager != null)
+    {
+      if (Build.VERSION.SDK_INT >= 26 )
+        audioManager.abandonAudioFocusRequest(new AudioFocusRequest.Builder(AUDIOFOCUS_GAIN_TRANSIENT_EXCLUSIVE).build());
+      else
+        audioManager.abandonAudioFocus(focusChange -> {});
+    }
+  }
+}
