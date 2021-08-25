daeaaf59ee472361db8ffa020dadad5a37c9afbf
Alexander Borsuk <me@alex.bio>
2021-08-25 08:29:51 +0200

    Default metaserver for the open source mode
    Signed-off-by: Alexander Borsuk <me@alex.bio>

--- private_default.h
+++ private_default.h
@@ -3,7 +3,7 @@
 #define OSM_CONSUMER_KEY "zz16eUusTr4TcVhoKQrgOJaRzZAeI2ktbVx6e3yn"
 #define OSM_CONSUMER_SECRET "7g4XrlhFkiOG1I3Zpqh8ajo94P9cNNXaU2YCMgL7"
 #define MWM_GEOLOCATION_SERVER ""
-#define METASERVER_URL ""
+#define METASERVER_URL "https://meta.omaps.app/maps"
 #define DIFF_LIST_URL ""
 #define DEFAULT_URLS_JSON "[ \"https://cdn.organicmaps.app/\" ]"
 #define TRAFFIC_DATA_BASE_URL ""
