a87200bb1313651520827b1ca1c2f0f57facc3a1
S. Kozyr <s.trump@gmail.com>
2021-09-28 10:36:47 +0300

    Updated open-location-code library to v1.0.4
    Signed-off-by: S. Kozyr <s.trump@gmail.com>

--- 3party/open-location-code/openlocationcode.h
+++ 3party/open-location-code/openlocationcode.h
@@ -90,6 +90,9 @@ extern const size_t kMaximumDigitCount;
 extern const char kPaddingCharacter;
 // The alphabet of the codes.
 extern const char kAlphabet[];
+// Lookup table of the alphabet positions of characters 'C' through 'X',
+// inclusive. A value of -1 means the character isn't part of the alphabet.
+extern const int kPositionLUT['X' - 'C' + 1];
 // The number base used for the encoding.
 extern const size_t kEncodingBase;
 // How many characters use the pair algorithm.
@@ -98,9 +101,6 @@ extern const size_t kPairCodeLength;
 extern const size_t kGridColumns;
 // Number of rows in the grid refinement method.
 extern const size_t kGridRows;
-// Defines the minimum pair resolution (in degrees) to remove when shortening a
-// code.
-extern const double kMinShortenDegrees;
 // Gives the exponent used for the first pair.
 extern const size_t kInitialExponent;
 // Size of the initial grid in degrees. This is the size of the area represented
