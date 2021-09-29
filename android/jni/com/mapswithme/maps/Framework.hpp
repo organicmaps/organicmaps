58b2f7da1f5a2a5ab060f930941d70b77d7f7c28
S. Kozyr <s.trump@gmail.com>
2021-09-29 12:39:49 +0300

    Added CoordinatesFormat enum to C++ lib to reflect Java enum. Changed measurement_utils::FormatOsmLink function to use bits::BitwiseMerge and remove call to ceil().
    Signed-off-by: S. Kozyr <s.trump@gmail.com>

--- android/jni/com/mapswithme/maps/Framework.hpp
+++ android/jni/com/mapswithme/maps/Framework.hpp
@@ -39,6 +39,14 @@ struct EverywhereSearchParams;
 
 namespace android
 {
+  enum CoordinatesFormat // See Java enum com.mapswithme.maps.widget.placepage.CoordinatesFormat for all possible values.
+  {
+    LatLonDMS = 0,     // Latitude, Longitude in degrees minutes seconds format, comma separated
+    LatLonDecimal = 1, // Latitude, Longitude in decimal format, comma separated
+    OLCFull = 2,       // Open location code, full format
+    OSMLink = 3        // Link to the OSM. E.g. https://osm.org/go/xcXjyqQlq-?m=
+  };
+
   class Framework : private power_management::PowerManager::Subscriber
   {
   private:
