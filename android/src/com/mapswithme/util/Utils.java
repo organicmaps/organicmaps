b212e79193d6977f6bc1f6285feec4279758c50d
Alexander Borsuk <me@alex.bio>
2021-10-21 10:55:07 +0300

    Revert "[android] ability for debugging webview"
    This reverts commit 44eb20bd6fc5d76aea1fc07fdf561cc926d126a1.
    
    It crashes the app on some watches because of missing
    WebView.setWebContentsDebuggingEnabled implementation.
    
    Also, we don't use and don't need this feature in our debug/beta builds.
    
    See https://github.com/mapsme/omim/pull/14064 for more details.
    
    Signed-off-by: Alexander Borsuk <me@alex.bio>

--- android/src/com/mapswithme/util/Utils.java
+++ android/src/com/mapswithme/util/Utils.java
@@ -601,17 +601,12 @@ public class Utils
     catch (RuntimeException e)
     {
       LOGGER.e(TAG, "Failed to get string with id '" + key + "'", e);
-      if (isDebugOrBeta())
+      if (BuildConfig.BUILD_TYPE.equals("debug") || BuildConfig.BUILD_TYPE.equals("beta"))
       {
         Toast.makeText(context, "Add string id for '" + key + "'!", Toast.LENGTH_LONG).show();
       }
     }
     return INVALID_ID;
-  }
-
-  public static boolean isDebugOrBeta()
-  {
-    return BuildConfig.BUILD_TYPE.equals("debug") || BuildConfig.BUILD_TYPE.equals("beta");
   }
 
   /**
