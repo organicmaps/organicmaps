d50e7a75b4ccc18ed51a55ee75092d0b4bc25841
Kazing <41507326+Kazing@users.noreply.github.com>
2021-11-14 10:48:00 +0100

    [android] modified PlacePageView to fix hotel subtitles color (#1547)
    * modified PlacePageView setspan to fix hotel subtitles color
    
    Signed-off-by: Thibaud Besnard <thibaudk@posteo.net>
    
    * corrected my fix after test on debug apk
    
    Signed-off-by: Thibaud Besnard <thibaudk@posteo.net>
    
    * typo
    
    Signed-off-by: Thibaud Besnard <thibaudk@posteo.net>
    
    * typo again
    
    Signed-off-by: Thibaud Besnard <thibaudk@posteo.net>

--- android/src/com/mapswithme/maps/widget/placepage/PlacePageView.java
+++ android/src/com/mapswithme/maps/widget/placepage/PlacePageView.java
@@ -826,10 +826,11 @@ public class PlacePageView extends NestedScrollViewClickFixed
     {
       SpannableStringBuilder sb = new SpannableStringBuilder(text);
       int start = text.indexOf("★");
+      int end = text.lastIndexOf("★") + 1;
       if (start > -1)
       {
         sb.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.base_yellow)),
-                   start, sb.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+                   start, end, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
 
       }
       mTvSubtitle.setText(sb);
