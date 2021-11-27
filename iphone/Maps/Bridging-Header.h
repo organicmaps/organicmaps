c4739bd492322e5942f8f87b7d91c1c0c81d7ba7
Alexander Borsuk <me@alex.bio>
2021-11-28 00:46:07 +0100

    [ios] Fixed compilation
    Signed-off-by: Alexander Borsuk <me@alex.bio>

--- iphone/Maps/Bridging-Header.h
+++ iphone/Maps/Bridging-Header.h
@@ -2,8 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-@import Chart;
-
 #import "AuthenticationServices/AuthenticationServices.h"
 #import "CarPlay/CarPlay.h"
 #import "UIKit/UIKit.h"
