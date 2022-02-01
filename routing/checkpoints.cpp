8ec18b92355c71be756b3f4eb66c5d958a434504
Viktor Govako <viktor.govako@gmail.com>
2022-02-01 13:46:34 +0300

    [routing] Better DebugPrint for checkpoints.
    Signed-off-by: Viktor Govako <viktor.govako@gmail.com>

--- routing/checkpoints.cpp
+++ routing/checkpoints.cpp
@@ -56,13 +56,9 @@ double Checkpoints::GetSummaryLengthBetweenPointsMeters() const
 std::string DebugPrint(Checkpoints const & checkpoints)
 {
   std::ostringstream out;
-  out << std::fixed << std::setprecision(6);
   out << "Checkpoints(";
-  for (auto const & point : checkpoints.GetPoints())
-  {
-    auto const latlon = mercator::ToLatLon(point);
-    out << latlon.m_lat << ", " << latlon.m_lon << "; ";
-  }
+  for (auto const & pt : checkpoints.GetPoints())
+    out << DebugPrint(mercator::ToLatLon(pt)) << "; ";
   out << "passed: " << checkpoints.GetPassedIdx() << ")";
   return out.str();
 }
