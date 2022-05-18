a8e573c3589cdfdba811ff2f198fdf9af0ca2c89
Viktor Govako <viktor.govako@gmail.com>
2022-05-18 21:55:45 +0300

    [classifier] Prettify FeatureType visibility processing code.
    Signed-off-by: Viktor Govako <viktor.govako@gmail.com>

--- search/categories_cache.cpp
+++ search/categories_cache.cpp
@@ -43,12 +43,12 @@ CBV CategoriesCache::Load(MwmContext const & context) const
   SearchTrieRequest<strings::UniStringDFA> request;
 
   // m_categories usually has truncated types; add them together with their subtrees.
-  m_categories.ForEach([&request, &c](uint32_t const type) {
-    c.ForEachInSubtree(
-        [&](uint32_t descendantType) {
-          request.m_categories.emplace_back(FeatureTypeToString(c.GetIndexForType(descendantType)));
-        },
-        type);
+  m_categories.ForEach([&request, &c](uint32_t const type)
+  {
+    c.ForEachInSubtree([&](uint32_t descendantType)
+    {
+      request.m_categories.emplace_back(FeatureTypeToString(c.GetIndexForType(descendantType)));
+    }, type);
   });
 
   Retrieval retrieval(context, m_cancellable);
