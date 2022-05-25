7424c71b57fd97cc48e8c9597ef4be9c9b95cb44
Priyank Shankar <shankarpriyank312002@gmail.com>
2022-05-25 22:36:54 +0530

    App finally showing up on DHU
    Signed-off-by: Priyank Shankar <shankarpriyank312002@gmail.com>

---
+++ android/flavors/gms-enabled/com/mapswithme/maps/HelloWorldScreen.java
@@ -0,0 +1,25 @@
+package com.mapswithme.maps;
+
+import androidx.annotation.NonNull;
+import androidx.car.app.CarContext;
+import androidx.car.app.Screen;
+import androidx.car.app.model.Action;
+import androidx.car.app.model.Pane;
+import androidx.car.app.model.PaneTemplate;
+import androidx.car.app.model.Row;
+import androidx.car.app.model.Template;
+
+public class HelloWorldScreen extends Screen {
+    public HelloWorldScreen(@NonNull CarContext carContext) {
+        super(carContext);
+    }
+
+    @NonNull
+    @Override
+    public Template onGetTemplate() {
+        Row row = new Row.Builder().setTitle("Hello AA Organic Maps!").build();
+        return new PaneTemplate.Builder(new Pane.Builder().addRow(row).build())
+                .setHeaderAction(Action.APP_ICON)
+                .build();
+    }
+}
