983d8ac4d9c821f7fde45212be7b2321c194b8c8
Priyank Shankar <shankarpriyank312002@gmail.com>
2022-06-23 21:25:40 +0530

    Added Log Statement
    Signed-off-by: Priyank Shankar <shankarpriyank312002@gmail.com>

--- android/src/com/mapswithme/maps/MwmApplication.java
+++ android/src/com/mapswithme/maps/MwmApplication.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import com.mapswithme.maps.background.AppBackgroundTracker;
