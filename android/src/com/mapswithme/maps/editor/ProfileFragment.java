b56f07c2459c9e0a2cc1369ac8341e4252853f8b
Arnaud Vergnet <arnaud.vergnet@mailo.com>
2022-07-03 17:18:52 +0200

    [android] refactor profile and add changes loading indicator
    Signed-off-by: Arnaud Vergnet <arnaud.vergnet@mailo.com>

--- android/src/com/mapswithme/maps/editor/ProfileFragment.java
+++ android/src/com/mapswithme/maps/editor/ProfileFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,30 +16,13 @@ import com.mapswithme.maps.R;
 import com.mapswithme.maps.base.BaseMwmToolbarFragment;
 import com.mapswithme.util.Constants;
 import com.mapswithme.util.UiUtils;
-import com.mapswithme.util.bottomsheet.MenuBottomSheetFragment;
-import com.mapswithme.util.bottomsheet.MenuBottomSheetItem;
 import com.mapswithme.util.concurrency.ThreadPool;
 import com.mapswithme.util.concurrency.UiThread;
 
-import java.util.ArrayList;
-
-public class ProfileFragment extends BaseMwmToolbarFragment implements View.OnClickListener
+public class ProfileFragment extends BaseMwmToolbarFragment
 {
   private TextView mEditsSent;
-
-  private void onLogoutActionSelected(final ProfileFragment fragment)
-  {
-    new AlertDialog.Builder(fragment.requireContext())
-        .setMessage(R.string.are_you_sure)
-        .setPositiveButton(android.R.string.ok, (dialog, which) ->
-        {
-          OsmOAuth.clearAuthorization(fragment.requireContext());
-          fragment.refreshViews();
-        })
-        .setNegativeButton(android.R.string.no, null)
-        .create()
-        .show();
-  }
+  private ProgressBar mEditsSentProgress;
 
   @Nullable
   @Override
@@ -58,14 +42,15 @@ public class ProfileFragment extends BaseMwmToolbarFragment implements View.OnCl
 
   private void initViews(View view)
   {
-    View more = getToolbarController().getToolbar().findViewById(R.id.more);
-    more.setOnClickListener(this);
+    View logoutButton = getToolbarController().getToolbar().findViewById(R.id.logout);
+    logoutButton.setOnClickListener((v) -> logout());
     View editsBlock = view.findViewById(R.id.block_edits);
     UiUtils.show(editsBlock);
     View sentBlock = editsBlock.findViewById(R.id.sent_edits);
     mEditsSent = sentBlock.findViewById(R.id.edits_count);
-    view.findViewById(R.id.about_osm).setOnClickListener(this);
-    view.findViewById(R.id.osm_history).setOnClickListener(this);
+    mEditsSentProgress = sentBlock.findViewById(R.id.edits_count_progress);
+    view.findViewById(R.id.about_osm).setOnClickListener((v) -> openOsmAboutUrl());
+    view.findViewById(R.id.osm_history).setOnClickListener((v) -> openOsmHistoryUrl());
   }
 
   private void refreshViews()
@@ -74,8 +59,17 @@ public class ProfileFragment extends BaseMwmToolbarFragment implements View.OnCl
     {
       // Update the number of uploaded changesets from OSM.
       ThreadPool.getWorker().execute(() -> {
+        if (mEditsSent.getText().equals(""))
+        {
+          UiUtils.hide(mEditsSent);
+          UiUtils.show(mEditsSentProgress);
+        }
         final int count = OsmOAuth.getOsmChangesetsCount(requireContext(), getParentFragmentManager());
-        UiThread.run(() -> mEditsSent.setText(String.valueOf(count)));
+        UiThread.run(() -> {
+          mEditsSent.setText(String.valueOf(count));
+          UiUtils.show(mEditsSent);
+          UiUtils.hide(mEditsSentProgress);
+        });
       });
     }
     else
@@ -87,27 +81,27 @@ public class ProfileFragment extends BaseMwmToolbarFragment implements View.OnCl
     }
   }
 
-  @Override
-  public void onClick(View v)
+  private void logout()
   {
-    switch (v.getId())
-    {
-      case R.id.more:
-        showBottomSheet();
-        break;
-      case R.id.about_osm:
-        startActivity(new Intent((Intent.ACTION_VIEW), Uri.parse(Constants.Url.OSM_ABOUT)));
-        break;
-      case R.id.osm_history:
-        startActivity(new Intent((Intent.ACTION_VIEW), Uri.parse(OsmOAuth.getHistoryUrl(requireContext()))));
-        break;
-    }
+    new AlertDialog.Builder(requireContext())
+        .setMessage(R.string.are_you_sure)
+        .setPositiveButton(android.R.string.ok, (dialog, which) ->
+        {
+          OsmOAuth.clearAuthorization(requireContext());
+          refreshViews();
+        })
+        .setNegativeButton(android.R.string.no, null)
+        .create()
+        .show();
   }
 
-  private void showBottomSheet()
+  private void openOsmAboutUrl()
   {
-    ArrayList<MenuBottomSheetItem> items = new ArrayList<>();
-    items.add(new MenuBottomSheetItem(R.string.logout, R.drawable.ic_logout, () -> onLogoutActionSelected(ProfileFragment.this)));
-    new MenuBottomSheetFragment(items).show(getParentFragmentManager(), "profileBottomSheet");
+    startActivity(new Intent((Intent.ACTION_VIEW), Uri.parse(Constants.Url.OSM_ABOUT)));
+  }
+
+  private void openOsmHistoryUrl()
+  {
+    startActivity(new Intent((Intent.ACTION_VIEW), Uri.parse(OsmOAuth.getHistoryUrl(requireContext()))));
   }
 }
