8c2f268c2b146583dd6ebaa45147d5f00826b6bc
Viktor Govako <viktor.govako@gmail.com>
2022-08-28 12:07:23 +0300

    Removed obsolete functions.
    Signed-off-by: Viktor Govako <viktor.govako@gmail.com>

--- indexer/shared_load_info.cpp
+++ indexer/shared_load_info.cpp
@@ -21,16 +21,6 @@ SharedLoadInfo::Reader SharedLoadInfo::GetMetadataReader() const
   return m_cont.GetReader(METADATA_FILE_TAG);
 }
 
-SharedLoadInfo::Reader SharedLoadInfo::GetMetadataIndexReader() const
-{
-  return m_cont.GetReader(METADATA_INDEX_FILE_TAG);
-}
-
-SharedLoadInfo::Reader SharedLoadInfo::GetAltitudeReader() const
-{
-  return m_cont.GetReader(ALTITUDES_FILE_TAG);
-}
-
 SharedLoadInfo::Reader SharedLoadInfo::GetGeometryReader(int ind) const
 {
   return m_cont.GetReader(GetTagForIndex(GEOMETRY_FILE_TAG, ind));
@@ -41,11 +31,4 @@ SharedLoadInfo::Reader SharedLoadInfo::GetTrianglesReader(int ind) const
   return m_cont.GetReader(GetTagForIndex(TRIANGLE_FILE_TAG, ind));
 }
 
-std::optional<SharedLoadInfo::Reader> SharedLoadInfo::GetPostcodesReader() const
-{
-  if (!m_cont.IsExist(POSTCODES_FILE_TAG))
-    return {};
-
-  return m_cont.GetReader(POSTCODES_FILE_TAG);
-}
 }  // namespace feature
