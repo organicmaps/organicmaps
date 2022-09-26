d3bd60f6c006c9833b9a37956216e5e89d370127
Lauren Prieto <lauren.prieto17@gmail.com>
2022-09-25 21:27:39 -0400

    changed time format to be locale dependent             Signed-off-by: Lauren Prieto <lauren.prieto17@gmail.com>
    Signed-off-by: Lauren Prieto <lauren.prieto@ufl.edu>

--- iphone/Maps/UI/PlacePage/Components/PlacePagePreviewViewController.swift
+++ iphone/Maps/UI/PlacePage/Components/PlacePagePreviewViewController.swift
@@ -136,8 +136,8 @@ final class PlacePagePreviewViewController: UIViewController {
     let now = time_t(Date().timeIntervalSince1970);
     
     let hourFormatter = DateFormatter();
-    hourFormatter.locale = Locale.current;
-    hourFormatter.timeStyle = .short;
+    hourFormatter.locale = Locale.current
+    hourFormatter.timeStyle = .short
     
     switch placePagePreviewData.schedule.state {
     case .allDay:
