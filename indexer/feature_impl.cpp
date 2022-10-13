bfd347440086e0c0501fbeacd31919f229a25b19
Viktor Govako <viktor.govako@gmail.com>
2022-10-13 14:43:36 +0300

    Removed obsolete functions.
    Signed-off-by: Viktor Govako <viktor.govako@gmail.com>

--- indexer/feature_impl.cpp
+++ indexer/feature_impl.cpp
@@ -1,66 +1,15 @@
 #include "indexer/feature_impl.hpp"
 
 #include "base/string_utils.hpp"
-#include "base/logging.hpp"
 #include "base/math.hpp"
-
-#include <algorithm>
-
-using namespace std;
 
 namespace feature
 {
-
-bool IsDigit(int c)
-{
-  return (int('0') <= c && c <= int('9'));
-}
-
-bool IsNumber(strings::UniString const & s)
-{
-  for (size_t i = 0; i < s.size(); ++i)
-  {
-    // android production ndk-r8d has bug. "еда" detected as a number.
-    if (!IsDigit(s[i]))
-      return false;
-  }
-  return true;
-}
-
-bool IsStreetNumber(strings::UniString const & s)
-{
-  if (s.size() < 2)
-    return false;
-
-  /// add different localities in future, if it's a problem.
-  for (auto const & streetEnding : {"st", "nd", "rd", "th"})
-  {
-    if (strings::EndsWith(strings::ToUtf8(s), streetEnding))
-      return true;
-  }
-  return false;
-}
-
-bool IsHouseNumberDeepCheck(strings::UniString const & s)
-{
-  size_t const count = s.size();
-  if (count == 0)
-    return false;
-  if (!IsDigit(s[0]))
-    return false;
-  if (IsStreetNumber(s))
-    return false;
-  return (count < 8);
-}
+using namespace std;
 
 bool IsHouseNumber(string const & s)
 {
-  return (!s.empty() && IsDigit(s[0]));
-}
-
-bool IsHouseNumber(strings::UniString const & s)
-{
-  return (!s.empty() && IsDigit(s[0]));
+  return !s.empty() && strings::IsASCIIDigit(s[0]);
 }
 
 uint8_t PopulationToRank(uint64_t p)
