d9e320eb8c65198311563efa8795c038687cbe90
Alexander Borsuk <me@alex.bio>
2022-11-16 18:24:55 +0100

    [tools] Convert more than one kmb to kml with one call
    Signed-off-by: Alexander Borsuk <me@alex.bio>

--- kml/kmb_to_kml/kmb_to_kml.cpp
+++ kml/kmb_to_kml/kmb_to_kml.cpp
@@ -12,44 +12,48 @@
 int main(int argc, char** argv)
 {
   if (argc < 2) {
-    std::cout << "Converts kmb file to kml\n";
-    std::cout << "Usage: " << argv[0] << " path_to_kmb_file\n";
+    std::cout << "Converts kmb file(s) to kml\n";
+    std::cout << "Usage: " << argv[0] << " path_to_kmb_file [path_to_another_kmb_file...]\n";
     return 1;
   }
   // TODO: Why bookmarks serialization requires classifier?
   classificator::Load();
 
-  std::string filePath = argv[1];
-  kml::FileData kmlData;
-  try
+  do
   {
-    FileReader reader(filePath);
-    kml::binary::DeserializerKml des(kmlData);
-    des.Deserialize(reader);
-  }
-  catch (kml::binary::DeserializerKml::DeserializeException const & ex)
-  {
-    std::cerr << "Error reading kmb file " << filePath << ": " << ex.what() << std::endl;
-    return 1;
-  }
+    std::string filePath = argv[argc - 1];
+    kml::FileData kmlData;
+    try
+    {
+      FileReader reader(filePath);
+      kml::binary::DeserializerKml des(kmlData);
+      des.Deserialize(reader);
+    }
+    catch (kml::binary::DeserializerKml::DeserializeException const & ex)
+    {
+      std::cerr << "Error reading kmb file " << filePath << ": " << ex.what() << std::endl;
+      return 1;
+    }
 
-  try
-  {
-    // Change extension to kml.
-    filePath[filePath.size() - 1] = 'l';
-    kml::SerializerKml ser(kmlData);
-    FileWriter kmlFile(filePath);
-    ser.Serialize(kmlFile);
-  }
-  catch (kml::SerializerKml::SerializeException const & ex)
-  {
-    std::cerr << "Error encoding to kml file " << filePath << ": " << ex.what() << std::endl;
-    return 1;
-  }
-  catch (FileWriter::Exception const & ex)
-  {
-    std::cerr << "Error writing to kml file " << filePath << ": " << ex.what() << std::endl;
-    return 1;
-  }
+    try
+    {
+      // Change extension to kml.
+      filePath[filePath.size() - 1] = 'l';
+      kml::SerializerKml ser(kmlData);
+      FileWriter kmlFile(filePath);
+      ser.Serialize(kmlFile);
+    }
+    catch (kml::SerializerKml::SerializeException const & ex)
+    {
+      std::cerr << "Error encoding to kml file " << filePath << ": " << ex.what() << std::endl;
+      return 1;
+    }
+    catch (FileWriter::Exception const & ex)
+    {
+      std::cerr << "Error writing to kml file " << filePath << ": " << ex.what() << std::endl;
+      return 1;
+    }
+    std::cout << "Saved converted file as " << filePath << std::endl;
+  } while (--argc > 1);
   return 0;
 }
