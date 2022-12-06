ddb4bf8cce68eecd3f05d077de6087eab25f6730
Siarhei Fedartsou <siarhei.fedartsou@gmail.com>
2022-12-06 10:17:13 +0100

    [ios] Settings and routing options table headers are visible now in the Dark Mode
    Signed-off-by: Siarhei Fedartsou <siarhei.fedartsou@gmail.com>

--- iphone/Maps/Classes/Components/MWMTableViewController.m
+++ iphone/Maps/Classes/Components/MWMTableViewController.m
@@ -19,6 +19,23 @@
   return NO;
 }
 
+- (void)viewWillAppear:(BOOL)animated
+{
+  [super viewWillAppear:animated];
+  
+  for (NSInteger i = 0; i < [self numberOfSectionsInTableView:self.tableView]; i++) {
+    [self updateHeaderView:[self.tableView headerViewForSection:i]];
+  }
+}
+
+// Fix table section header font color for all tables, including Setting and Route Options.
+- (void)updateHeaderView:(UIView*)headerView {
+  if ([headerView isKindOfClass: [UITableViewHeaderFooterView class]]) {
+    UITableViewHeaderFooterView* header = (UITableViewHeaderFooterView *)headerView;
+    header.textLabel.textColor = [UIColor blackSecondaryText];
+  }
+}
+
 - (void)viewDidLoad
 {
   [super viewDidLoad];
@@ -29,6 +46,10 @@
          forCellReuseIdentifier:[UITableViewCell className]];
   [self.tableView registerClass:[MWMTableViewSubtitleCell class]
          forCellReuseIdentifier:[MWMTableViewSubtitleCell className]];
+}
+
+- (void)tableView:(UITableView *)tableView willDisplayHeaderView:(UIView *)view forSection:(NSInteger)section {
+  [self updateHeaderView:view];
 }
 
 #pragma mark - Properties
