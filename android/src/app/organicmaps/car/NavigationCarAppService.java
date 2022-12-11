8fe646042249220e565cad1ecd9457f6efd2fe06
Andrew Shkrob <andrew.shkrob.social@yandex.by>
2022-12-11 16:58:37 +0100

    [android-auto] Remove car_hosts.xml and refactor createHostValidator function
    Signed-off-by: Andrew Shkrob <andrew.shkrob.social@yandex.by>

--- android/src/app/organicmaps/car/NavigationCarAppService.java
+++ android/src/app/organicmaps/car/NavigationCarAppService.java
@@ -1,13 +1,11 @@
 package app.organicmaps.car;
 
-import android.content.pm.ApplicationInfo;
-
 import androidx.annotation.NonNull;
 import androidx.car.app.CarAppService;
 import androidx.car.app.Session;
 import androidx.car.app.validation.HostValidator;
 
-import app.organicmaps.R;
+import app.organicmaps.BuildConfig;
 
 public final class NavigationCarAppService extends CarAppService
 {
@@ -15,16 +13,12 @@ public final class NavigationCarAppService extends CarAppService
   @Override
   public HostValidator createHostValidator()
   {
-    if ((getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0)
-    {
+    if (BuildConfig.DEBUG)
       return HostValidator.ALLOW_ALL_HOSTS_VALIDATOR;
-    }
-    else
-    {
-      return new HostValidator.Builder(getApplicationContext())
-          .addAllowedHosts(R.array.hosts_allowlist)
-          .build();
-    }
+
+    return new HostValidator.Builder(getApplicationContext())
+        .addAllowedHosts(androidx.car.app.R.array.hosts_allowlist_sample)
+        .build();
   }
 
   @NonNull
