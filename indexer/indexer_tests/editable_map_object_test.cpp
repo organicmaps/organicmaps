c439868c65132cec0e47e72e8358b952a31eab2f
Harry Bond <endim8@pm.me>
2022-12-20 01:25:15 +0000

    [indexer] allow names to contain /,\,°,[,]
    Signed-off-by: Harry Bond <endim8@pm.me>

--- indexer/indexer_tests/editable_map_object_test.cpp
+++ indexer/indexer_tests/editable_map_object_test.cpp
@@ -199,8 +199,8 @@ UNIT_TEST(EditableMapObject_ValidateEmail)
 UNIT_TEST(EditableMapObject_ValidateName)
 {
   vector<string> correctNames = {"abc", "абв", "ᆺᆯㅕ", "꫞ꪺꫀꪸ", "a b?c", "a!b.c", "a(b)c", "a,b.c",
-                                 "a$bc", "a%bc", "a#bc", "a№bc", "c&a"};
-  vector<string> incorrectNames = {"a^bc", "a~bc", "a§bc", "a>bc", "a<bc", "a{bc", "a[bc", "*",
+                                 "a$bc", "a%bc", "a#bc", "a№bc", "c&a", "a[bc"};
+  vector<string> incorrectNames = {"a^bc", "a~bc", "a§bc", "a>bc", "a<bc", "a{bc", "*",
                                    "a*bc", "a=bc", "a_bc", "a±bc", "a\nbc", "a\tbc", "a\rbc",
                                    "a\vbc", "a\fbc", "a|bc", "N√", "Hello World!\U0001F600",
                                    "Exit →", "∫0dx = C", "\U0001210A"};
