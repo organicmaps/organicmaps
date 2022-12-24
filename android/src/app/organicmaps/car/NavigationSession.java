a4e85a97b4f8b6371cce57378723b577cbadbc65
Andrew Shkrob <andrew.shkrob.social@yandex.by>
2022-12-24 18:20:45 +0100

    [android-auto] Add location button
    Signed-off-by: Andrew Shkrob <andrew.shkrob.social@yandex.by>

--- android/src/app/organicmaps/car/NavigationSession.java
+++ android/src/app/organicmaps/car/NavigationSession.java
@@ -1,21 +1,28 @@
 package app.organicmaps.car;
 
 import android.content.Intent;
+import android.location.Location;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.car.app.Screen;
+import androidx.car.app.ScreenManager;
 import androidx.car.app.Session;
 import androidx.lifecycle.DefaultLifecycleObserver;
 import androidx.lifecycle.LifecycleOwner;
 
+import app.organicmaps.Map;
 import app.organicmaps.MwmApplication;
 import app.organicmaps.car.screens.ErrorScreen;
+import app.organicmaps.car.screens.MapScreen;
 import app.organicmaps.car.screens.NavigationScreen;
+import app.organicmaps.location.LocationHelper;
+import app.organicmaps.location.LocationListener;
+import app.organicmaps.location.LocationState;
 
 import java.io.IOException;
 
-public final class NavigationSession extends Session implements DefaultLifecycleObserver
+public final class NavigationSession extends Session implements DefaultLifecycleObserver, LocationListener, LocationState.ModeChangeListener
 {
   private static final String TAG = NavigationSession.class.getSimpleName();
 
@@ -40,6 +47,22 @@ public final class NavigationSession extends Session implements DefaultLifecycle
   }
 
   @Override
+  public void onStart(@NonNull LifecycleOwner owner)
+  {
+    LocationHelper.INSTANCE.addListener(this);
+    LocationState.nativeSetListener(this);
+    LocationHelper.INSTANCE.onTransit(true);
+  }
+
+  @Override
+  public void onStop(@NonNull LifecycleOwner owner)
+  {
+    LocationHelper.INSTANCE.removeListener(this);
+    LocationState.nativeRemoveListener();
+    LocationHelper.INSTANCE.onTransit(false);
+  }
+
+  @Override
   public void onCreate(@NonNull LifecycleOwner owner)
   {
     Log.d(TAG, "onCreate()");
@@ -56,14 +79,32 @@ public final class NavigationSession extends Session implements DefaultLifecycle
   private void init()
   {
     mInitFailed = false;
-    MwmApplication app = MwmApplication.from(getCarContext());
     try
     {
-      app.init();
+      MwmApplication.from(getCarContext()).init();
     } catch (IOException e)
     {
       mInitFailed = true;
       Log.e(TAG, "Failed to initialize the app.");
     }
+  }
+
+  @Override
+  public void onMyPositionModeChanged(int newMode)
+  {
+    final Screen screen = getCarContext().getCarService(ScreenManager.class).getTop();
+    if (screen instanceof MapScreen)
+      screen.invalidate();
+  }
+
+  @Override
+  public void onLocationUpdated(@NonNull Location location)
+  {
+  }
+
+  @Override
+  public void onCompassUpdated(double north)
+  {
+    Map.onCompassUpdated(north, false);
   }
 }
