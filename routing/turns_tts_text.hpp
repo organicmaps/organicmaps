859ba7781029f144904069b7151cdc3c28ab39d5
Shura <shura0@yandex.ru>
2023-01-28 16:46:29 +0300

    Update turns_tts_text.hpp

--- routing/turns_tts_text.hpp
+++ routing/turns_tts_text.hpp
@@ -1,33 +1,9 @@
-#include "routing/turns_sound_settings.hpp"
-#include "routing/turns_tts_text.hpp"
+#pragma once
 
-#include "base/string_utils.hpp"
+#include "platform/get_text_by_id.hpp"
 
-#include <algorithm>
-#include <iterator>
+#include <memory>
 #include <string>
-#include <utility>
-
-namespace
-{
-using namespace routing::turns::sound;
-
-template <class TIter> std::string DistToTextId(TIter begin, TIter end, uint32_t dist)
-{
-  using TValue = typename std::iterator_traits<TIter>::value_type;
-
-  TIter distToSound = lower_bound(begin, end, dist, [](TValue const & p1, uint32_t p2)
-                      {
-                        return p1.first < p2;
-                      });
-  if (distToSound == end)
-  {
-    ASSERT(false, ("notification.m_distanceUnits is not correct."));
-    return std::string{};
-  }
-  return distToSound->second;
-}
-}  //  namespace
 
 namespace routing
 {
@@ -35,184 +11,49 @@ namespace turns
 {
 namespace sound
 {
-void GetTtsText::SetLocale(std::string const & locale)
+struct Notification;
+
+/// GetTtsText is responsible for generating text message for TTS in a specified locale
+/// by notification. To get this message use operator().
+/// If the message is not available for specified locale GetTtsText tries to find it in
+/// English locale.
+class GetTtsText
 {
-  m_getCurLang = platform::GetTextByIdFactory(platform::TextSource::TtsSound, locale);
-}
+public:
 
-void GetTtsText::ForTestingSetLocaleWithJson(std::string const & jsonBuffer, std::string const & locale)
-{
-  m_getCurLang = platform::ForTestingGetTextByIdFactory(jsonBuffer, locale);
-}
+  std::string GetTurnNotification(Notification const & notification) const;
 
-std::string GetTtsText::GetTurnNotification(Notification const & notification) const
-{
-  if (notification.m_distanceUnits == 0 && !notification.m_useThenInsteadOfDistance)
-    return GetTextById(GetDirectionTextId(notification));
+  std::string GetSpeedCameraNotification() const;
 
-  if (notification.IsPedestrianNotification())
-  {
-    if (notification.m_useThenInsteadOfDistance &&
-        notification.m_turnDirPedestrian == PedestrianDirection::None)
-      return std::string{};
-  }
+  /// \brief Sets a locale.
+  /// @param locale is a string representation of locale. For example "en", "ru", "zh-Hant" and so on.
+  /// \note See sound/tts/languages.txt for the full list of available locales.
+  void SetLocale(std::string const & locale);
 
-  if (notification.m_useThenInsteadOfDistance && notification.m_turnDir == CarDirection::None)
-    return std::string{};
+  /// @return current TTS locale. For example "en", "ru", "zh-Hant" and so on.
+  /// \note The method returns correct locale after SetLocale has been called.
+  /// If not, it returns an empty string.
+  std::string GetLocale() const;
 
-  std::string const dirStr = GetTextById(GetDirectionTextId(notification));
-  if (dirStr.empty())
-    return std::string{};
+  void ForTestingSetLocaleWithJson(std::string const & jsonBuffer, std::string const & locale);
 
-  std::string distStr;
-  if ( notification.m_useThenInsteadOfDistance )
-  {
-    if (!notification.m_distanceUnits)
-      distStr = GetTextById("then");
-    else
-      distStr = GetTextById("then") + " " + GetTextById(GetDistanceTextId(notification));
-  }
-  else
-    distStr = GetTextById(GetDistanceTextId(notification));
-  return distStr + " " + dirStr;
-}
+private:
+  std::string GetTextById(std::string const & textId) const;
 
-std::string GetTtsText::GetSpeedCameraNotification() const
-{
-  return GetTextById("unknown_camera");
-}
+  std::unique_ptr<platform::GetTextById> m_getCurLang;
+};
 
-std::string GetTtsText::GetLocale() const
-{
-  if (m_getCurLang == nullptr)
-  {
-    ASSERT(false, ());
-    return std::string{};
-  }
-  return m_getCurLang->GetLocale();
-}
-
-std::string GetTtsText::GetTextById(std::string const & textId) const
-{
-  ASSERT(!textId.empty(), ());
-
-  if (m_getCurLang == nullptr)
-  {
-    ASSERT(false, ());
-    return std::string{};
-  }
-  return (*m_getCurLang)(textId);
-}
-
-std::string GetDistanceTextId(Notification const & notification)
-{
-//  if (notification.m_useThenInsteadOfDistance)
-//    return "then";
-
-  switch (notification.m_lengthUnits)
-  {
-  case measurement_utils::Units::Metric:
-    return DistToTextId(GetAllSoundedDistMeters().cbegin(), GetAllSoundedDistMeters().cend(),
-                        notification.m_distanceUnits);
-  case measurement_utils::Units::Imperial:
-    return DistToTextId(GetAllSoundedDistFeet().cbegin(), GetAllSoundedDistFeet().cend(),
-                        notification.m_distanceUnits);
-  }
-  ASSERT(false, ());
-  return std::string{};
-}
-
-std::string GetRoundaboutTextId(Notification const & notification)
-{
-  if (notification.m_turnDir != CarDirection::LeaveRoundAbout)
-  {
-    ASSERT(false, ());
-    return std::string{};
-  }
-  if (!notification.m_useThenInsteadOfDistance)
-    return "leave_the_roundabout"; // Notification just before leaving a roundabout.
-
-  static const uint8_t kMaxSoundedExit = 11;
-  if (notification.m_exitNum == 0 || notification.m_exitNum > kMaxSoundedExit)
-    return "leave_the_roundabout";
-
-  return "take_the_" + strings::to_string(static_cast<int>(notification.m_exitNum)) + "_exit";
-}
-
-std::string GetYouArriveTextId(Notification const & notification)
-{
-  if (!notification.IsPedestrianNotification() &&
-      notification.m_turnDir != CarDirection::ReachedYourDestination)
-  {
-    ASSERT(false, ());
-    return std::string{};
-  }
-
-  if (notification.IsPedestrianNotification() &&
-      notification.m_turnDirPedestrian != PedestrianDirection::ReachedYourDestination)
-  {
-    ASSERT(false, ());
-    return std::string{};
-  }
-
-  if (notification.m_distanceUnits != 0 || notification.m_useThenInsteadOfDistance)
-    return "destination";
-  return "you_have_reached_the_destination";
-}
-
-std::string GetDirectionTextId(Notification const & notification)
-{
-  if (notification.IsPedestrianNotification())
-  {
-    switch (notification.m_turnDirPedestrian)
-    {
-    case PedestrianDirection::GoStraight: return "go_straight";
-    case PedestrianDirection::TurnRight: return "make_a_right_turn";
-    case PedestrianDirection::TurnLeft: return "make_a_left_turn";
-    case PedestrianDirection::ReachedYourDestination: return GetYouArriveTextId(notification);
-    case PedestrianDirection::None:
-    case PedestrianDirection::Count: ASSERT(false, (notification)); return std::string{};
-    }
-  }
-
-  switch (notification.m_turnDir)
-  {
-    case CarDirection::GoStraight:
-      return "go_straight";
-    case CarDirection::TurnRight:
-      return "make_a_right_turn";
-    case CarDirection::TurnSharpRight:
-      return "make_a_sharp_right_turn";
-    case CarDirection::TurnSlightRight:
-      return "make_a_slight_right_turn";
-    case CarDirection::TurnLeft:
-      return "make_a_left_turn";
-    case CarDirection::TurnSharpLeft:
-      return "make_a_sharp_left_turn";
-    case CarDirection::TurnSlightLeft:
-      return "make_a_slight_left_turn";
-    case CarDirection::UTurnLeft:
-    case CarDirection::UTurnRight:
-      return "make_a_u_turn";
-    case CarDirection::EnterRoundAbout:
-      return "enter_the_roundabout";
-    case CarDirection::LeaveRoundAbout:
-      return GetRoundaboutTextId(notification);
-    case CarDirection::ReachedYourDestination:
-      return GetYouArriveTextId(notification);
-    case CarDirection::ExitHighwayToLeft:
-    case CarDirection::ExitHighwayToRight:
-      return "exit";
-    case CarDirection::StayOnRoundAbout:
-    case CarDirection::StartAtEndOfStreet:
-    case CarDirection::None:
-    case CarDirection::Count:
-      ASSERT(false, ());
-      return std::string{};
-  }
-  ASSERT(false, ());
-  return std::string{};
-}
+/// Generates text message id about the distance of the notification. For example: In 300 meters.
+std::string GetDistanceTextId(Notification const & notification);
+/// Generates text message id for roundabouts.
+/// For example: leave_the_roundabout or take_the_3_exit
+std::string GetRoundaboutTextId(Notification const & notification);
+/// Generates text message id for the finish of the route.
+/// For example: destination or you_have_reached_the_destination
+std::string GetYouArriveTextId(Notification const & notification);
+/// Generates text message id about the direction of the notification. For example: Make a right
+/// turn.
+std::string GetDirectionTextId(Notification const & notification);
 }  // namespace sound
 }  // namespace turns
 }  // namespace routing
