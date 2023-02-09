2748875ba4701ffb5d6bc8bcc1d7dfa2f8cc08c4
Alexander Borsuk <me@alex.bio>
2023-02-09 23:14:12 +0100

    [tools] Script to reset all whats new to en-US version
    Signed-off-by: Alexander Borsuk <me@alex.bio>

---
+++ tools/unix/clone_en_release_notes.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+ANDROID_LISTINGS=android/src/fdroid/play/listings
+ANDROID_NOTES=$ANDROID_LISTINGS/en-US/release-notes.txt
+IOS_METADATA=iphone/metadata
+IOS_NOTES=$IOS_METADATA/en-US/release_notes.txt
+
+find $ANDROID_LISTINGS -name release-notes.txt -exec rsync -a $ANDROID_NOTES {} \;
+find $IOS_METADATA -name release_notes.txt -exec rsync -a $IOS_NOTES {} \;
