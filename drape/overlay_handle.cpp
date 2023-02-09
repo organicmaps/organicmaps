906ecce4760b62ff8cccdae6eed3b03b109823c5
Konstantin Pastbin <konstantin.pastbin@gmail.com>
2023-02-09 20:25:28 +0000

    [drape] Don't add 100000 to overlay priorities and don't cap them
    Overlay priorities are always positive and way below 100000 anyway.
    
    Needed-for: #4314
    
    Signed-off-by: Konstantin Pastbin <konstantin.pastbin@gmail.com>

--- drape/overlay_handle.cpp
+++ drape/overlay_handle.cpp
@@ -232,18 +232,16 @@ std::string SquareHandle::GetOverlayDebugInfo()
 /// @param[in] depth        Manual priority from styles (bigger is better)
 uint64_t CalculateOverlayPriority(int minZoomLevel, uint8_t rank, float depth)
 {
+  ASSERT(0 <= depth && depth <= 100000, (depth));
+
   // Even if minZoomLevel < 0 (-1 is not visible), we will get more consistent |minZoom| value (less is worse).
   ASSERT_GREATER_OR_EQUAL(minZoomLevel, 0, ());
   uint8_t const minZoom = 0xFF - static_cast<uint8_t>(minZoomLevel);
 
-  float constexpr kMinDepth = -100000.0f;
-  float constexpr kMaxDepth = 100000.0f;
-  float const d = base::Clamp(depth, kMinDepth, kMaxDepth) - kMinDepth;
-
   // Pack into uint64_t priority value (bigger is better).
   // [1 byte - zoom][4 bytes - priority][1 byte - rank][2 bytes - 0xFFFF].
   return (static_cast<uint64_t>(minZoom) << 56) |
-         (static_cast<uint64_t>(d) << 24) |
+         (static_cast<uint64_t>(depth) << 24) |
          (static_cast<uint64_t>(rank) << 16) |
          static_cast<uint64_t>(0xFFFF);
 }
