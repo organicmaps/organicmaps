2ab9c3b18a68e27ccb47b035e5011e9b58191c1d
Alexander Borsuk <me@alex.bio>
2023-02-18 23:42:08 +0100

    [ios] Removed unnecessary input validators
    Signed-off-by: Alexander Borsuk <me@alex.bio>

--- iphone/Maps/Categories/UITextField+RuntimeAttributes.m
+++ iphone/Maps/Categories/UITextField+RuntimeAttributes.m
@@ -1,6 +1,4 @@
-#import "MWMInputValidatorFactory.h"
 #import "UITextField+RuntimeAttributes.h"
-#import <objc/runtime.h>
 
 @implementation UITextField (RuntimeAttributes)
 
@@ -13,32 +11,6 @@
 {
   NSString * placeholder = self.placeholder;
   return L(placeholder);
-}
-
-- (void)setValidator:(MWMInputValidator *)validator
-{
-  objc_setAssociatedObject(self, @selector(validator), validator, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-}
-
-- (MWMInputValidator *)validator
-{
-  return objc_getAssociatedObject(self, @selector(validator));
-}
-
-- (void)setValidatorName:(NSString *)validatorName
-{
-  objc_setAssociatedObject(self, @selector(validatorName), validatorName, OBJC_ASSOCIATION_COPY_NONATOMIC);
-  self.validator = [MWMInputValidatorFactory validator:validatorName];
-}
-
-- (MWMInputValidator *)validatorName
-{
-  return objc_getAssociatedObject(self, @selector(validatorName));
-}
-
-- (BOOL)isValid
-{
-  return [self.validator validateInput:self];
 }
 
 @end
