f1c5337b5ea6faa8dbe96c9a15ca22f07ccd407d
Arjun Devasi <ardevasi97@gmail.com>
2023-02-25 03:15:42 +0530

    Removed deprecated JobIntentService
    this is my first attempt to contribute.
    
    Signed-off-by: arjundevasi <Ardevasi97@gmail.com>

--- android/src/app/organicmaps/MwmJobIntentService.java
+++ android/src/app/organicmaps/MwmJobIntentService.java
@@ -1,5 +1,6 @@
 package app.organicmaps;
 
+import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
@@ -8,32 +9,42 @@ import androidx.core.app.JobIntentService;
 import app.organicmaps.util.CrashlyticsUtils;
 import app.organicmaps.util.log.Logger;
 
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+import java.util.logging.Logger;
+
 @SuppressWarnings("deprecation") // https://github.com/organicmaps/organicmaps/issues/4531
-public abstract class MwmJobIntentService extends JobIntentService
-{
-  private static final String TAG = MwmJobIntentService.class.getSimpleName();
+public abstract class MwmJobIntentService extends Worker {
+    private static final String TAG = MwmJobIntentService.class.getSimpleName();
 
-  @NonNull
-  protected String getTag()
-  {
-    return getClass().getSimpleName();
-  }
-
-  protected abstract void onHandleWorkInitialized(@NonNull Intent intent);
-
-  @Override
-  protected void onHandleWork(@NonNull Intent intent)
-  {
-    MwmApplication app = MwmApplication.from(this);
-    String msg = "onHandleWork: " + intent;
-    Logger.i(TAG, msg);
-    CrashlyticsUtils.INSTANCE.log(Log.INFO, getTag(), msg);
-    if (!app.arePlatformAndCoreInitialized())
-    {
-      Logger.w(TAG, "Application is not initialized, ignoring " + intent);
-      return;
+    @NonNull
+    protected String getTag() {
+        return getClass().getSimpleName();
     }
 
-    onHandleWorkInitialized(intent);
-  }
+    protected abstract void onHandleWorkInitialized(@NonNull Intent intent);
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        Context applicationContext = getApplicationContext();
+
+        try {
+
+            MwmApplication app = MwmApplication.from(this);
+            String msg = "onHandleWork: " + intent;
+            Logger.i(TAG, msg);
+            CrashlyticsUtils.INSTANCE.log(Log.INFO, getTag(), msg);
+
+            onHandleWorkInitialized(intent);
+
+            return Result.success();
+        } catch (Throwable throwable) {
+            Logger.w(TAG, "Application is not initialized, ignoring " + intent);
+            return Result.failure();;
+        }
+
+    }
 }
+
