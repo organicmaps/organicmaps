6f56564dcfa57775bdb6b228b16bb93c1df95ffc
Aryan <sagavekararyan@gmail.com>
2023-03-04 16:41:28 +0530

    Fixed where the zoom buttons are not disabled from the settings

--- android/src/app/organicmaps/maplayer/MapButtonsController.java
+++ android/src/app/organicmaps/maplayer/MapButtonsController.java
@@ -268,10 +268,14 @@ public class MapButtonsController extends Fragment
 
   public void showMapButtons(boolean show)
   {
-    if (show)
+    if (show) {
       UiUtils.show(mFrame);
-    else
+      showButton(true, MapButtons.zoom);
+    }
+    else {
       UiUtils.hide(mFrame);
+      showButton(false, MapButtons.zoom);
+    }
     mOnBottomButtonsHeightChangedListener.OnBottomButtonsHeightChanged();
   }
 
