ccdc60dc1de6e8e317f5c434318c5919cb8a8b53
Viktor Govako <viktor.govako@gmail.com>
2023-04-05 11:54:15 -0300

    Fixed US shields, SR - is not US Highway.
    Signed-off-by: Viktor Govako <viktor.govako@gmail.com>

--- indexer/indexer_tests/road_shields_parser_tests.cpp
+++ indexer/indexer_tests/road_shields_parser_tests.cpp
@@ -8,27 +8,35 @@ UNIT_TEST(RoadShields_Smoke)
 
   auto shields = GetRoadShields("France", "D 116A");
   TEST_EQUAL(shields.size(), 1, ());
-  TEST_EQUAL(shields.begin()->m_type, RoadShieldType::Generic_Orange, ());
+  TEST_EQUAL(shields[0].m_type, RoadShieldType::Generic_Orange, ());
 
   shields = GetRoadShields("Belarus", "M1"); // latin letter M
   TEST_EQUAL(shields.size(), 1, ());
-  TEST_EQUAL(shields.begin()->m_type, RoadShieldType::Generic_Red, ());
+  TEST_EQUAL(shields[0].m_type, RoadShieldType::Generic_Red, ());
 
   shields = GetRoadShields("Belarus", "Е2");  // cyrillic letter Е
   TEST_EQUAL(shields.size(), 1, ());
-  TEST_EQUAL(shields.begin()->m_type, RoadShieldType::Generic_Green, ());
+  TEST_EQUAL(shields[0].m_type, RoadShieldType::Generic_Green, ());
 
   shields = GetRoadShields("Ukraine", "Р50");  // cyrillic letter Р
   TEST_EQUAL(shields.size(), 1, ());
-  TEST_EQUAL(shields.begin()->m_type, RoadShieldType::Generic_Blue, ());
+  TEST_EQUAL(shields[0].m_type, RoadShieldType::Generic_Blue, ());
 
   shields = GetRoadShields("Malaysia", "AH7");
   TEST_EQUAL(shields.size(), 1, ());
-  TEST_EQUAL(shields.begin()->m_type, RoadShieldType::Generic_Blue, ());
+  TEST_EQUAL(shields[0].m_type, RoadShieldType::Generic_Blue, ());
 
   shields = GetRoadShields("Germany", "A 3;A 7");
   TEST_EQUAL(shields.size(), 2, ());
-  auto it = shields.begin();
-  TEST_EQUAL(it->m_type, RoadShieldType::Generic_Blue, ());
-  TEST_EQUAL((++it)->m_type, RoadShieldType::Generic_Blue, ());
+  TEST_EQUAL(shields[0].m_type, RoadShieldType::Generic_Blue, ());
+  TEST_EQUAL(shields[1].m_type, RoadShieldType::Generic_Blue, ());
+
+  shields = GetRoadShields("US", "US:IN");
+  TEST_EQUAL(shields.size(), 1, ());
+  TEST_EQUAL(shields[0].m_type, RoadShieldType::Default, ());
+
+  shields = GetRoadShields("US", "SR 38;US:IN");
+  TEST_EQUAL(shields.size(), 2, ());
+  TEST_EQUAL(shields[0].m_type, RoadShieldType::Generic_White, ());
+  TEST_EQUAL(shields[1].m_type, RoadShieldType::Default, ());
 }
