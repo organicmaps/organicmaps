895771e617628ffe7db3f2e9d10c4f54216b5975
Alexander Borsuk <me@alex.bio>
2023-05-01 23:00:17 +0200

    Enabled TTS for es-MX, eu, nb, pt-BR and mr
    Signed-off-by: Alexander Borsuk <me@alex.bio>

--- platform/languages.hpp
+++ platform/languages.hpp
@@ -6,49 +6,50 @@
 
 // The list of languages which can be used by TTS.
 // It shall be included in Android (jni) and iOS parts to get the languages list.
+// TODO: Now it is used only on iOS.
+// Manual sync with android/res/values/strings-tts.xml is needed.
 
-namespace routing
+namespace routing::turns::sound
 {
-namespace turns
-{
-namespace sound
-{
-std::array<std::pair<std::string, std::string>, 33> const kLanguageList =
+std::array<std::pair<std::string, std::string>, 38> const kLanguageList =
 {{
   {"en", "English"},
-  {"ru", "Русский"},
-  {"be", "Беларуская"},
+  {"id", "Bahasa Indonesia"},
   {"ca", "Català"},
-  {"cs", "Čeština"},
   {"da", "Dansk"},
   {"de", "Deutsch"},
   {"es", "Español"},
+  {"es-MX", "Español (México)"},
+  {"eu", "Euskara"},
   {"fr", "Français"},
   {"hr", "Hrvatski"},
-  {"id", "Indonesia"},
   {"it", "Italiano"},
   {"sw", "Kiswahili"},
   {"hu", "Magyar"},
   {"nl", "Nederlands"},
+  {"nb", "Norsk Bokmål"},
   {"pl", "Polski"},
   {"pt", "Português"},
+  {"pt-BR", "Português (Brasil)"},
   {"ro", "Română"},
   {"sk", "Slovenčina"},
   {"fi", "Suomi"},
   {"sv", "Svenska"},
   {"vi", "Tiếng Việt"},
   {"tr", "Türkçe"},
+  {"cs", "Čeština"},
   {"el", "Ελληνικά"},
+  {"be", "Беларуская"},
+  {"ru", "Русский"},
   {"uk", "Українська"},
   {"ar", "العربية"},
   {"fa", "فارسی"},
+  {"mr", "मराठी"},
   {"hi", "हिंदी"},
+  {"th", "ไทย"},
+  {"zh-Hans", "中文简体"},
+  {"zh-Hant", "中文繁體"},
   {"ja", "日本語"},
   {"ko", "한국어"},
-  {"th", "ภาษาไทย"},
-  {"zh-Hant", "中文繁體"},
-  {"zh-Hans", "中文简体"},
 }};
-}  // namespace sound
-}  // namespace turns
-}  // namespace routing
+}  // namespace routing::turns::sound
