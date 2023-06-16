40745999ebc73f46fae1a7b7ffac644e46bafd09
rokuz <r.kuznetsow@gmail.com>
2023-06-16 15:49:12 +0100

    Fix script for generating Vulkan shaders
    Signed-off-by: rokuz <r.kuznetsow@gmail.com>

--- tools/unix/generate_vulkan_shaders.sh
+++ tools/unix/generate_vulkan_shaders.sh
@@ -4,13 +4,24 @@ set -e -u
 # Prevent python from generating compiled *.pyc files
 export PYTHONDONTWRITEBYTECODE=1
 
-DEBUG="${1:-empty}"
+NDK_PATH="${1:-empty}"
+DEBUG="${2:-empty}"
 
 MY_PATH="`dirname \"$0\"`"              # relative
 MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
 
-source "$MY_PATH/../autobuild/ndk_helper.sh"
-export NDK_ROOT=$(GetNdkRoot) || ( echo "Can't read NDK root path from android/local.properties"; exit 1 )
+if [ -z "$NDK_PATH" ]
+then
+  source "$MY_PATH/../autobuild/ndk_helper.sh"
+  export NDK_ROOT=$(GetNdkRoot)
+  if [ -z "$NDK_ROOT" ]
+  then
+    echo "Can't read NDK root path from android/local.properties"; exit 1 
+  fi
+else
+  export NDK_ROOT="$NDK_PATH"
+fi
+
 KERNEL_NAME="$( uname -s )"
 if [[ $KERNEL_NAME == 'Darwin' ]]
 then
@@ -25,4 +36,4 @@ fi
 OMIM_PATH="${OMIM_PATH:-$(cd "$(dirname "$0")/../.."; pwd)}"
 SHADERS_GENERATOR="$OMIM_PATH/shaders/vulkan_shaders_preprocessor.py"
 
-python "$SHADERS_GENERATOR" "$OMIM_PATH/shaders/GL" shader_index.txt programs.hpp program_params.hpp shaders_lib.glsl "$OMIM_PATH/data/vulkan_shaders" "$GLSLC_PATH" "$DEBUG"
+python3 "$SHADERS_GENERATOR" "$OMIM_PATH/shaders/GL" shader_index.txt programs.hpp program_params.hpp shaders_lib.glsl "$OMIM_PATH/data/vulkan_shaders" "$GLSLC_PATH" "$DEBUG"
