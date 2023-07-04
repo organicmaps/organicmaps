271a796c5f5be458ed598c06e0b9af41347bf7f0
Andrew Shkrob <andrew.shkrob.social@yandex.by>
2023-07-04 15:28:17 +0200

    [cmake] Add CTest
    Signed-off-by: Andrew Shkrob <andrew.shkrob.social@yandex.by>

--- cmake/OmimHelpers.cmake
+++ cmake/OmimHelpers.cmake
@@ -1,11 +1,4 @@
-# Flags for all
-set(OMIM_WARNING_FLAGS
-  $<$<NOT:$<CXX_COMPILER_ID:MSVC>>:-Wall>
-  $<$<NOT:$<CXX_COMPILER_ID:MSVC>>:-Wextra>
-  $<$<NOT:$<CXX_COMPILER_ID:MSVC>>:-Wpedantic>
-  $<$<NOT:$<CXX_COMPILER_ID:MSVC>>:-Wno-unused-parameter>  # We have a lot of functions with unused parameters
-)
-set(3PARTY_INCLUDE_DIRS "${OMIM_ROOT}/3party/boost")
+include(OmimConfig)
 
 # Function for setting target platform:
 function(omim_set_platform_var PLATFORM_VAR pattern)
@@ -86,47 +79,6 @@ function(omim_add_library library)
   endif()
   if (USE_PCH)
     add_precompiled_headers_to_target(${library} ${OMIM_PCH_TARGET_NAME})
-  endif()
-endfunction()
-
-function(omim_add_test_impl disable_platform_init executable)
-  if (NOT SKIP_TESTS)
-    omim_add_executable(${executable}
-      ${ARGN}
-      ${OMIM_ROOT}/testing/testingmain.cpp
-    )
-    target_compile_options(${executable} PRIVATE ${OMIM_WARNING_FLAGS})
-    target_include_directories(${executable} SYSTEM PRIVATE ${3PARTY_INCLUDE_DIRS})
-    if(disable_platform_init)
-      target_compile_definitions(${PROJECT_NAME} PRIVATE OMIM_UNIT_TEST_DISABLE_PLATFORM_INIT)
-    else()
-      target_link_libraries(${executable} platform)
-    endif()
-    # testingmain.cpp uses base::HighResTimer::ElapsedNano
-    target_link_libraries(${executable} base)
-  endif()
-endfunction()
-
-function(omim_add_test executable)
-  omim_add_test_impl(NO ${executable} ${ARGN})
-endfunction()
-
-function(omim_add_test_with_qt_event_loop executable)
-  omim_add_test_impl(NO ${executable} ${ARGN})
-  target_compile_definitions(${executable} PRIVATE OMIM_UNIT_TEST_WITH_QT_EVENT_LOOP)
-  target_link_libraries(${executable} Qt6::Widgets)
-endfunction()
-
-function(omim_add_test_no_platform_init executable)
-  omim_add_test_impl(YES ${executable} ${ARGN})
-endfunction()
-
-
-function(omim_add_test_subdirectory subdir)
-  if (NOT SKIP_TESTS)
-    add_subdirectory(${subdir})
-  else()
-    message("SKIP_TESTS: Skipping subdirectory ${subdir}")
   endif()
 endfunction()
 
