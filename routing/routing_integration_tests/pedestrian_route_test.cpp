ac74a3c7267d45ce72bca01ade5984614fc2763d
Viktor Govako <viktor.govako@gmail.com>
2023-07-22 13:19:19 -0300

    [routing] Increased "primary" speed for Turkey and Georgia.
    Signed-off-by: Viktor Govako <viktor.govako@gmail.com>

--- routing/routing_integration_tests/pedestrian_route_test.cpp
+++ routing/routing_integration_tests/pedestrian_route_test.cpp
@@ -11,6 +11,8 @@ namespace pedestrian_route_test
 {
 using namespace routing;
 using namespace routing::turns;
+using namespace integration;
+using mercator::FromLatLon;
 
 UNIT_TEST(GermanyBremenJunctionToCycleway)
 {
@@ -612,9 +614,6 @@ UNIT_TEST(Hungary_UseFootways)
 
 UNIT_TEST(France_Uphill_Downlhill)
 {
-  using namespace integration;
-  using mercator::FromLatLon;
-
   double timeDownhill, timeUphill;
   {
     TRouteResult const routeResult = CalculateRoute(GetVehicleComponents(VehicleType::Pedestrian),
@@ -667,9 +666,6 @@ UNIT_TEST(Italy_Rome_Altitude_Footway)
 
 UNIT_TEST(Romania_Mountains_ETA)
 {
-  using namespace integration;
-  using mercator::FromLatLon;
-
   TRouteResult const routeResult = CalculateRoute(GetVehicleComponents(VehicleType::Pedestrian),
       FromLatLon(45.5450, 25.2584), {0., 0.},
       FromLatLon(45.5223, 25.2806));
@@ -697,9 +693,6 @@ UNIT_TEST(Spain_N634_Piligrim_Road)
 // https://github.com/organicmaps/organicmaps/issues/5410
 UNIT_TEST(Australia_Mountains_Downlhill)
 {
-  using namespace integration;
-  using mercator::FromLatLon;
-
   TRouteResult const routeResult = CalculateRoute(GetVehicleComponents(VehicleType::Pedestrian),
                                                   FromLatLon(-33.7374217, 150.283098), {0., 0.},
                                                   FromLatLon(-33.7375399, 150.283358));
@@ -712,6 +705,32 @@ UNIT_TEST(Australia_Mountains_Downlhill)
   // Altitudes diff is (914 -> 798).
   double const eta = route.GetTotalTimeSec();
   TEST(8 * 60 < eta && eta < 11 * 60, (eta));
+}
+
+UNIT_TEST(Turkey_UsePrimary)
+{
+  CalculateRouteAndTestRouteLength(GetVehicleComponents(VehicleType::Pedestrian),
+      FromLatLon(38.7352697, 35.516104), {0., 0.},
+      FromLatLon(38.7398797, 35.5170627), 679.702);
+
+  CalculateRouteAndTestRouteLength(GetVehicleComponents(VehicleType::Pedestrian),
+      FromLatLon(38.7168708, 35.4903164), {0., 0.},
+      FromLatLon(38.7207386, 35.4811178), 1050.39);
+}
+
+UNIT_TEST(Georgia_UsePrimary)
+{
+  TRouteResult const routeResult = CalculateRoute(GetVehicleComponents(VehicleType::Pedestrian),
+                                                  FromLatLon(42.7175722, 42.0496444), {0., 0.},
+                                                  FromLatLon(43.0451, 42.3742778));
+
+  TEST_EQUAL(routeResult.second, RouterResultCode::NoError, ());
+  TEST(routeResult.first, ());
+  Route const & route = *routeResult.first;
+
+  TestRouteLength(route, 68595);
+  double const eta = route.GetTotalTimeSec();
+  TEST(22 * 3600 < eta && eta < 24 * 3600, (eta));
 }
 
 } // namespace pedestrian_route_test
