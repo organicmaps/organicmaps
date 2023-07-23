93e76feb863d6d414f356e9676bbcf5bffc03e4b
Artem Trukhanov <trukhanovartem@me.com>
2023-07-23 16:59:09 +0300

    [ios] Fixes wikipedia place description text in dark mode
    Updated codestyle and refactored the solution for converting to HEX color code.
    
    Fixes: #5364, #3042
    Signed-off-by: Artem Trukhanov <trukhanovartem@me.com>

--- iphone/Maps/Core/Theme/Extensions/UIColor+hexString.swift
+++ iphone/Maps/Core/Theme/Extensions/UIColor+hexString.swift
@@ -1,23 +1,41 @@
 extension UIColor {
-    var hexString: String {
-      let cgColorInRGB = cgColor.converted(to: CGColorSpace(name: CGColorSpace.sRGB)!, intent: .defaultIntent, options: nil)!
-              let colorRef = cgColorInRGB.components
-              let r = colorRef?[0] ?? 0
-              let g = colorRef?[1] ?? 0
-              let b = ((colorRef?.count ?? 0) > 2 ? colorRef?[2] : g) ?? 0
-              let a = cgColor.alpha
-
-              var color = String(
-                  format: "#%02lX%02lX%02lX",
-                  lroundf(Float(r * 255)),
-                  lroundf(Float(g * 255)),
-                  lroundf(Float(b * 255))
-              )
-
-              if a < 1 {
-                  color += String(format: "%02lX", lroundf(Float(a * 255)))
-              }
-
-              return color
+  private func convertToHEX(component: CGFloat) -> Int {
+    return lroundf(Float(component * 255));
+  }
+  
+  private var hexColorComponentTemplate: String {
+    get {
+      return "%02lX";
     }
+  }
+  
+  private var hexColorTemplate: String {
+    get {
+      return "#\(hexColorComponentTemplate)\(hexColorComponentTemplate)\(hexColorComponentTemplate)";
+    }
+  }
+  
+  var hexString: String {
+    get {
+      let cgColorInRGB = cgColor.converted(to: CGColorSpace(name: CGColorSpace.sRGB)!, intent: .defaultIntent, options: nil)!
+      let colorRef = cgColorInRGB.components
+      let r = colorRef?[0] ?? 0
+      let g = colorRef?[1] ?? 0
+      let b = ((colorRef?.count ?? 0) > 2 ? colorRef?[2] : g) ?? 0
+      let alpha = cgColor.alpha
+      
+      var color = String(
+        format: hexColorTemplate,
+        convertToHEX(component: r),
+        convertToHEX(component: g),
+        convertToHEX(component: b)
+      )
+
+      if alpha < 1 {
+        color += String(format: hexColorComponentTemplate, convertToHEX(component: alpha))
+      }
+
+      return color
+    }
+  }
 }
