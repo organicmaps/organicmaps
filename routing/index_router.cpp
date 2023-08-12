98b58093293e8175fdb5c2605286e043787c3879
Viktor Govako <viktor.govako@gmail.com>
2023-08-11 23:48:20 -0300

    [routing] Cross-MWM crutch to fix possible "Avoid options".
    Signed-off-by: Viktor Govako <viktor.govako@gmail.com>

--- routing/index_router.cpp
+++ routing/index_router.cpp
@@ -1458,6 +1458,8 @@ RouterResultCode IndexRouter::ProcessLeapsJoints(vector<Segment> const & input,
                                                  RoutesCalculator & calculator,
                                                  RoutingResultT & result)
 {
+  LOG(LDEBUG, ("Process input:", input));
+
   // { fake, mwmId1, mwmId2, mwmId2, mwmId3, .. pairs of ids .. , fake }.
   ASSERT_GREATER_OR_EQUAL(input.size(), 4, ());
   ASSERT(input.size() % 2 == 0, ());
@@ -1554,6 +1556,7 @@ RouterResultCode IndexRouter::ProcessLeapsJoints(vector<Segment> const & input,
 
     size_t lastPrev = 0;
     RouteWeight currentWeight = GetAStarWeightZero<RouteWeight>();
+    RouteWeight lastWeight = currentWeight;
 
     for (size_t i = startLeapEnd; i <= finishLeapStart; ++i)
     {
@@ -1577,49 +1580,42 @@ RouterResultCode IndexRouter::ProcessLeapsJoints(vector<Segment> const & input,
       RoutingResultT const * res = nullptr;
       if (res = tryBuildRoute(prev, next, WorldGraphMode::JointSingleMwm); !res)
       {
-        auto const prevPoint = starter.GetPoint(input[next], true);
-        // |next + 1| - is the twin of |next|
-        // |next + 2| - is the next exit.
-        while (next + 2 < finishLeapStart && next != finishLeapStart)
+        // As written above, it may happen when "Avoid option" is enabled.
+        // Possibile recover here is to shift |prev| and |next| to skip this "bad" MWM, and build a new
+        // (mode = Joints) route (via several MWMs).
+        // https://github.com/organicmaps/organicmaps/issues/5695
+
+        /// @todo Anyway, this _recovering_ is a crutch. Should compare result.m_distance on caller side
+        /// and call ProcessLeapsJoints for the next candidates (not for the "best" only).
+        /// "Cross MWMs" together with "Avoid options" doesn't guarantee a best route, because
+        /// cross-mwm graph keeps only _clean_ (no any avoids) weights.
+
+        if (prev > 0)
         {
-          auto const point = starter.GetPoint(input[next + 2], true);
-          double const distBetweenExistsMeters = ms::DistanceOnEarth(point, prevPoint);
-
-          static double constexpr kMinDistBetweenExitsM = 100000;  // 100 km
-          if (distBetweenExistsMeters > kMinDistBetweenExitsM)
-            break;
-
-          LOG(LINFO, ("Exit:", point, "too close(", distBetweenExistsMeters / 1000, "km ), try get next."));
-          next += 2;
+          prev = lastPrev;
+          currentWeight = lastWeight;
+          if (prev == 0)
+            dropFirstSegment = false;
         }
 
-        if (next + 2 > finishLeapStart || next == finishLeapStart)
+        if (next + 2 > finishLeapStart)
           next = input.size() - 1;
         else
           next += 2;
 
         if (res = tryBuildRoute(prev, next, WorldGraphMode::Joints); !res)
-        {
-          // Already in start
-          if (prev == 0)
-            return RouterResultCode::RouteNotFound;
-
-          prev = lastPrev;
-          if (prev == 0)
-            dropFirstSegment = false;
-
-          if (res = tryBuildRoute(prev, next, WorldGraphMode::Joints); !res)
-            return RouterResultCode::RouteNotFound;
-        }
+          return RouterResultCode::RouteNotFound;
       }
 
       lastPrev = prev;
+      lastWeight = currentWeight;
       i = next;
 
       currentWeight += res->m_distance;
     }
 
     // Update final result if new route is better.
+    LOG(LDEBUG, ("Got a route:", startLeapEnd, finishLeapStart, currentWeight));
     if (result.Empty() || currentWeight < result.m_distance)
     {
       if (!result.Empty())
