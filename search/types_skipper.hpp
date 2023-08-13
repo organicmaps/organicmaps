c285021022115a67fd83807e4136208d6ea25670
Viktor Govako <viktor.govako@gmail.com>
2023-08-13 09:32:05 -0300

    [search] Do not index entrance numbers (common for CIS countries).
    Signed-off-by: Viktor Govako <viktor.govako@gmail.com>

--- search/types_skipper.hpp
+++ search/types_skipper.hpp
@@ -20,6 +20,8 @@ public:
   // Useful for mixed features, sponsored objects.
   bool SkipAlways(feature::TypesHolder const & types) const;
 
+  bool SkipSpecialNames(feature::TypesHolder const & types, std::string_view defName) const;
+
 private:
   using Cont = buffer_vector<uint32_t, 16>;
 
@@ -27,6 +29,7 @@ private:
 
   // Array index (0, 1) means type level for checking (1, 2).
   Cont m_skipIfEmptyName[2];
-  Cont m_skipAlways[2];
+  Cont m_skipAlways[1];
+  Cont m_skipSpecialNames[1];
 };
 }  // namespace search
