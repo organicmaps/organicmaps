76016d6ce0be42bfb6ed967868b9bd7d16b79882
Artem Trukhanoff <trukhanovag@gmail.com>
2023-08-15 23:16:15 +0300

    [ios] Fixes wikipedia place description text in dark mode (#5564)
    [ios] Fixes wikipedia place description text in dark mode
    
    Added CSS color property in PlacePageDescriptionViewController.swift that is being substituted by particular hexCode of color depending on whether night mode is active or not.
    
    I have decided to retrieve colors from UIColor because in future theme accents might change and this would result in additional work for finding this controller and replacing colors in it.
    
    Fixes: #5364, #3042
    Signed-off-by: Artem Trukhanov <trukhanovartem@me.com>

---
+++ iphone/Maps/Core/Theme/Extensions/UIColor+hexString.swift
@@ -0,0 +1,41 @@
+extension UIColor {
+  private func convertToHEX(component: CGFloat) -> Int {
+    return lroundf(Float(component * 255));
+  }
+  
+  private var hexColorComponentTemplate: String {
+    get {
+      return "%02lX";
+    }
+  }
+  
+  private var hexColorTemplate: String {
+    get {
+      return "#\(hexColorComponentTemplate)\(hexColorComponentTemplate)\(hexColorComponentTemplate)";
+    }
+  }
+  
+  var hexString: String {
+    get {
+      let cgColorInRGB = cgColor.converted(to: CGColorSpace(name: CGColorSpace.sRGB)!, intent: .defaultIntent, options: nil)!
+      let colorRef = cgColorInRGB.components
+      let r = colorRef?[0] ?? 0
+      let g = colorRef?[1] ?? 0
+      let b = ((colorRef?.count ?? 0) > 2 ? colorRef?[2] : g) ?? 0
+      let alpha = cgColor.alpha
+      
+      var color = String(
+        format: hexColorTemplate,
+        convertToHEX(component: r),
+        convertToHEX(component: g),
+        convertToHEX(component: b)
+      )
+
+      if (alpha < 1) {
+        color += String(format: hexColorComponentTemplate, convertToHEX(component: alpha))
+      }
+
+      return color
+    }
+  }
+}
