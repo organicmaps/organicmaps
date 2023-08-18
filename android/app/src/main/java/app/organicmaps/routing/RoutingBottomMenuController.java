d2f0d103499cf986675e967bb31c5446f86d243e
S. Kozyr <s.trump@gmail.com>
2023-08-18 10:39:50 +0300

    [Android] Refactoring of routing details panel.
    Signed-off-by: S. Kozyr <s.trump@gmail.com>

--- android/app/src/main/java/app/organicmaps/routing/RoutingBottomMenuController.java
+++ android/app/src/main/java/app/organicmaps/routing/RoutingBottomMenuController.java
@@ -60,9 +60,11 @@ final class RoutingBottomMenuController implements View.OnClickListener
   @NonNull
   private final ImageView mAltitudeChart;
   @NonNull
-  private final TextView mAltitudeDifference;
+  private final TextView mTime;
   @NonNull
-  private final View mNumbersFrame;
+  private final TextView mAltitudeDifference;
+  @Nullable
+  private final TextView mArrival;
   @NonNull
   private final View mActionFrame;
   @NonNull
@@ -86,12 +88,13 @@ final class RoutingBottomMenuController implements View.OnClickListener
     TextView error = (TextView) getViewById(activity, frame, R.id.error);
     Button start = (Button) getViewById(activity, frame, R.id.start);
     ImageView altitudeChart = (ImageView) getViewById(activity, frame, R.id.altitude_chart);
+    TextView time = (TextView) getViewById(activity, frame, R.id.time);
     TextView altitudeDifference = (TextView) getViewById(activity, frame, R.id.altitude_difference);
-    View numbersFrame = getViewById(activity, frame, R.id.numbers);
+    TextView arrival = (TextView) getViewById(activity, frame, R.id.arrival);
     View actionFrame = getViewById(activity, frame, R.id.routing_action_frame);
 
-    return new RoutingBottomMenuController(activity, altitudeChartFrame, transitFrame, error, start, altitudeChart, altitudeDifference,
-                                           numbersFrame, actionFrame, listener);
+    return new RoutingBottomMenuController(activity, altitudeChartFrame, transitFrame, error, start, altitudeChart,
+                                           time, altitudeDifference, arrival, actionFrame, listener);
   }
 
   @NonNull
@@ -108,8 +111,9 @@ final class RoutingBottomMenuController implements View.OnClickListener
                                       @NonNull TextView error,
                                       @NonNull Button start,
                                       @NonNull ImageView altitudeChart,
+                                      @NonNull TextView time,
                                       @NonNull TextView altitudeDifference,
-                                      @NonNull View numbersFrame,
+                                      @Nullable TextView arrival,
                                       @NonNull View actionFrame,
                                       @Nullable RoutingBottomMenuListener listener)
   {
@@ -119,8 +123,9 @@ final class RoutingBottomMenuController implements View.OnClickListener
     mError = error;
     mStart = start;
     mAltitudeChart = altitudeChart;
+    mTime = time;
     mAltitudeDifference = altitudeDifference;
-    mNumbersFrame = numbersFrame;
+    mArrival = arrival;
     mActionFrame = actionFrame;
     mActionMessage = actionFrame.findViewById(R.id.tv__message);
     mActionButton = actionFrame.findViewById(R.id.btn__my_position_use);
@@ -155,7 +160,7 @@ final class RoutingBottomMenuController implements View.OnClickListener
   @SuppressLint("SetTextI18n")
   void showTransitInfo(@NonNull TransitRouteInfo info)
   {
-    UiUtils.hide(mError, mAltitudeChartFrame, mActionFrame, mAltitudeChartFrame);
+    UiUtils.hide(mError, mAltitudeChartFrame, mActionFrame);
     showStartButton(false);
     UiUtils.show(mTransitFrame);
     RecyclerView rv = mTransitFrame.findViewById(R.id.transit_recycler_view);
@@ -332,19 +337,18 @@ final class RoutingBottomMenuController implements View.OnClickListener
     final RoutingInfo rinfo = RoutingController.get().getCachedRoutingInfo();
     if (rinfo == null)
     {
-      UiUtils.hide(mNumbersFrame);
+      UiUtils.hide(mTime);
+      UiUtils.hide(mAltitudeDifference);
       return;
     }
 
     Spanned spanned = makeSpannedRoutingDetails(mContext, rinfo);
-    TextView numbersTime = mNumbersFrame.findViewById(R.id.time);
-    numbersTime.setText(spanned);
+    mTime.setText(spanned);
 
-    TextView numbersArrival = mNumbersFrame.findViewById(R.id.arrival);
-    if (numbersArrival != null)
+    if (mArrival != null)
     {
       String arrivalTime = RoutingController.formatArrivalTime(rinfo.totalTimeInSeconds);
-      numbersArrival.setText(arrivalTime);
+      mArrival.setText(arrivalTime);
     }
   }
 
