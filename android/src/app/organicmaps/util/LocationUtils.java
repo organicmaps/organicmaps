f3f9f7de41a17beaa0088409eda86b2c299047a2
Roman Tsisyk <roman@tsisyk.com>
2023-08-25 08:15:28 +0300

    [android] Remove UI from LocationHelper
    AlertDialogs are moved from LocationHelper back to parent MwmActivity.
    Initial idea of attaching LocationHelper to different activities failed
    and MwmActivity was the only one user of LocationHelper.attach().
    
    Location permission checks are extracted from LocationHelper.start() to
    callers of LocationHelper.start() to avoid unnecessary inverted callbacks.
    LocationHelper.start() now has @RequiresPermission compile-time annotation
    to validate that required location permissions are checked before the call.
    LocationUtils.isLocationGranted() is replaced with direct calls to
    ActitivyCompat.checkSelfPermission() because Android Studio and Android Lint
    are not smart enough to detect it for @RequiresPermission check.
    
    Remove harmful Listeners<> abstraction from LocationHelper. Make all things
    explicit. Listeners should be removed when the upper levels want to remove
    them, not sometime later in the future.
    
    See #4240
    Needed for #573 (#4611)
    
    Signed-off-by: Roman Tsisyk <roman@tsisyk.com>

--- android/src/app/organicmaps/util/LocationUtils.java
+++ android/src/app/organicmaps/util/LocationUtils.java
@@ -1,10 +1,6 @@
 package app.organicmaps.util;
 
-import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
-import static android.Manifest.permission.ACCESS_FINE_LOCATION;
-
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Build;
@@ -12,7 +8,6 @@ import android.provider.Settings;
 import android.view.Surface;
 
 import androidx.annotation.NonNull;
-import androidx.core.content.ContextCompat;
 
 public class LocationUtils
 {
@@ -115,15 +110,5 @@ public class LocationUtils
       e.printStackTrace();
       return false;
     }
-  }
-
-  public static boolean isFineLocationGranted(@NonNull Context context)
-  {
-    return ContextCompat.checkSelfPermission(context, ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
-  }
-
-  public static boolean isLocationGranted(@NonNull Context context)
-  {
-    return isFineLocationGranted(context) || ContextCompat.checkSelfPermission(context, ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
   }
 }
