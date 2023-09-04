0931844bd159a98177fb9ea8f6b143c1ebc7cd2c
Konstantin Pastbin <konstantin.pastbin@gmail.com>
2023-09-04 20:44:39 +0300

    [generator] More helpful logging of subprocess launch failures
    Signed-off-by: Konstantin Pastbin <konstantin.pastbin@gmail.com>

--- tools/python/maps_generator/generator/exceptions.py
+++ tools/python/maps_generator/generator/exceptions.py
@@ -35,4 +35,9 @@ class FailedTest(MapsGeneratorError):
 
 def wait_and_raise_if_fail(p):
     if p.wait() != os.EX_OK:
-        raise BadExitStatusError(f"The launch of {' '.join(p.args)} failed.")
+        args = p.args
+        logs = p.output.name
+        if p.error.name != logs:
+            logs += " and " + p.error.name
+        msg = f"The launch of {args.pop(0)} failed.\nArguments used: {' '.join(args)}\nSee details in {logs}"
+        raise BadExitStatusError(msg)
