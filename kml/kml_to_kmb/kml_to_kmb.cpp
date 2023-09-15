3611e35c9e65fd642a3d7283d3612141a89b1756
S. Kozyr <s.trump@gmail.com>
2023-09-15 10:58:12 +0300

    WIP. Versioning support in KML-to-KMB
    Signed-off-by: S. Kozyr <s.trump@gmail.com>

--- kml/kml_to_kmb/kml_to_kmb.cpp
+++ kml/kml_to_kmb/kml_to_kmb.cpp
@@ -13,13 +13,34 @@ int main(int argc, char** argv)
 {
   if (argc < 2) {
     std::cout << "Converts kml file to kmb\n";
-    std::cout << "Usage: " << argv[0] << " path_to_kml_file\n";
+    std::cout << "Usage: " << argv[0] << " path_to_kml_file [KMB_VERSION]\n";
+    std::cout << "    KMB_VERSION could be: ";
+    for(int i=0; i <= static_cast<int>(kml::binary::Version::Latest); i++)
+    {
+      if (i != 0)
+        std::cout << ", ";
+      std::cout << VersionToName(static_cast<kml::binary::Version>(i));
+    }
     return 1;
   }
+
+  //
   // TODO: Why bookmarks serialization requires classifier?
   classificator::Load();
 
   std::string filePath = argv[1];
+  kml::binary::Version ver = kml::binary::Version::Latest;
+  if (argc == 3)
+  {
+    std::optional<kml::binary::Version> maybeVer = kml::binary::NameToVersion(argv[2]);
+    if (!maybeVer.has_value())
+    {
+      std::cout << "Invalid KMB_VERSION: " << argv[2] << '\n';
+      return 2;
+    }
+    ver = maybeVer.value();
+  }
+
   kml::FileData kmlData;
   try
   {
@@ -39,7 +60,7 @@ int main(int argc, char** argv)
     filePath[filePath.size() - 1] = 'b';
     kml::binary::SerializerKml ser(kmlData);
     FileWriter kmlFile(filePath);
-    ser.Serialize(kmlFile);
+    ser.Serialize(kmlFile, ver);
   }
   catch (kml::SerializerKml::SerializeException const & ex)
   {
