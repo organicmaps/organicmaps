634a3182019157cf9ea556535d12c5e2cbe858c4
Roman Tsisyk <roman@tsisyk.com>
2023-09-24 11:46:04 +0300

    [android] Remove HttpUploader artifact
    It was used by tracking.
    
    Signed-off-by: Roman Tsisyk <roman@tsisyk.com>

--- android/app/src/main/cpp/app/organicmaps/util/HttpUploaderUtils.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "jni.h"
-#include "platform/http_payload.hpp"
-
-namespace platform
-{
-jobject MakeHttpUploader(JNIEnv * env, const platform::HttpPayload payload, jclass uploaderClass);
-}
--- android/app/src/main/java/app/organicmaps/util/KeyValue.java
+++ android/app/src/main/java/app/organicmaps/util/KeyValue.java
@@ -2,18 +2,14 @@ package app.organicmaps.util;
 
 import androidx.annotation.NonNull;
 
-import com.google.gson.annotations.SerializedName;
-
 import java.io.Serializable;
 
 public final class KeyValue implements Serializable
 {
   private static final long serialVersionUID = -3079360274128509979L;
   @NonNull
-  @SerializedName("key")
   private final String mKey;
   @NonNull
-  @SerializedName("value")
   private final String mValue;
 
   public KeyValue(@NonNull String key, @NonNull String value)
