fb5b17064381cbb7629e1e74c68a44edaf823f40
Jean-BaptisteC <jeanbaptiste.charron@outlook.fr>
2023-10-26 19:50:17 +0200

    Add about layout land
    Signed-off-by: Jean-BaptisteC <jeanbaptiste.charron@outlook.fr>

--- android/app/src/main/java/app/organicmaps/help/HelpFragment.java
+++ android/app/src/main/java/app/organicmaps/help/HelpFragment.java
@@ -50,9 +50,6 @@ public class HelpFragment extends BaseMwmFragment implements View.OnClickListene
     final boolean isLandscape = getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
 
     final String dataVersion = DateUtils.getShortDateFormatter().format(Framework.getDataVersion());
-    final TextView dataVersionView = (TextView) root.findViewById(R.id.data_version);
-    if (dataVersionView != null)
-      dataVersionView.setText(getString(R.string.data_version, dataVersion));
     final TextView osmPresentationView = (TextView) root.findViewById(R.id.osm_presentation);
     if (osmPresentationView != null)
       osmPresentationView.setText(getString(R.string.osm_presentation, dataVersion));
