882ca26e669838ec419969da8f74c0f287e6483c
Andrew Shkrob <andrew.shkrob.social@yandex.by>
2023-10-29 20:19:07 +0100

    [tests] Fix test server
    Signed-off-by: Andrew Shkrob <andrew.shkrob.social@yandex.by>

--- tools/python/test_server/start_server.py
+++ tools/python/test_server/start_server.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
+"""
+    Run from `$OMIM_ROOT/tools/python/test_server` dir
+"""
+
 import subprocess
 
 SERVER_RUNNABLE = 'server/testserver.py'
+LOG_FILE = 'server.log'
 
-subprocess.Popen(['python3', SERVER_RUNNABLE], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+with open(LOG_FILE, 'w') as log:
+    process = subprocess.Popen(['python3', SERVER_RUNNABLE], stdout=subprocess.DEVNULL, stderr=log, text=True)
+
+while True:
+    with open(LOG_FILE, 'r') as log:
+        data = log.read()
+        if data and "Started server with pid" in data:
+            break
