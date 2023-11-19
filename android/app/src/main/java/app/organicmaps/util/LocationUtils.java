organicmaps/organicmaps

commit 86bbe486431994403852b736801ccd8f2415ee78

[android] Add Android Auto support
Signed-off-by: Andrew Shkrob <andrew.shkrob.social@yandex.by>
Signed-off-by: Roman Tsisyk <roman@tsisyk.com>

android/app/src/main/java/app/organicmaps/util/LocationUtils.java
@@ -1,5 +1,8 @@
 package app.organicmaps.util;
 
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+
 import android.content.Context;
 import android.location.Location;
 import android.location.LocationManager;
@@ -8,6 +11,7 @@ import android.provider.Settings;
 import android.view.Surface;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 
 public class LocationUtils
 {
@@ -110,5 +114,20 @@ public class LocationUtils
       e.printStackTrace();
       return false;
     }
+  }
+
+  public static boolean checkFineLocationPermission(@NonNull Context context)
+  {
+    return ContextCompat.checkSelfPermission(context, ACCESS_FINE_LOCATION) == PERMISSION_GRANTED;
+  }
+
+  public static boolean checkCoarseLocationPermission(@NonNull Context context)
+  {
+    return ContextCompat.checkSelfPermission(context, ACCESS_FINE_LOCATION) == PERMISSION_GRANTED;
+  }
+
+  public static boolean checkLocationPermission(@NonNull Context context)
+  {
+    return checkFineLocationPermission(context) || checkCoarseLocationPermission(context);
   }
 }

commit e4a3380137f578c2e009da455bf9e90590240d0e

[android] Fix coding error in checkCoarseLocationPermission()
Regression by 86bbe48 "[android] Add Android Auto support"

Signed-off-by: Roman Tsisyk <roman@tsisyk.com>

android/app/src/main/java/app/organicmaps/util/LocationUtils.java
@@ -1,5 +1,6 @@
 package app.organicmaps.util;
 
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
@@ -123,7 +124,7 @@ public class LocationUtils
 
   public static boolean checkCoarseLocationPermission(@NonNull Context context)
   {
-    return ContextCompat.checkSelfPermission(context, ACCESS_FINE_LOCATION) == PERMISSION_GRANTED;
+    return ContextCompat.checkSelfPermission(context, ACCESS_COARSE_LOCATION) == PERMISSION_GRANTED;
   }
 
   public static boolean checkLocationPermission(@NonNull Context context)