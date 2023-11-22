organicmaps/organicmaps

commit 4cadc099bac698ab94389a2c4b53db838661b83f

[android] Switch to use BundleCompat
Signed-off-by: Roman Tsisyk <roman@tsisyk.com>

android/app/src/main/java/app/organicmaps/bookmarks/BookmarkCategorySettingsFragment.java
@@ -13,6 +13,7 @@ import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.os.BundleCompat;
 
 import app.organicmaps.R;
 import app.organicmaps.base.BaseMwmToolbarFragment;
@@ -43,10 +44,9 @@ public class BookmarkCategorySettingsFragment extends BaseMwmToolbarFragment
   public void onCreate(@Nullable Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
-    final Bundle args = getArguments();
-    if (args == null)
-      throw new IllegalArgumentException("Args must not be null");
-    mCategory = Objects.requireNonNull(Utils.getParcelable(args, BookmarkCategorySettingsActivity.EXTRA_BOOKMARK_CATEGORY, BookmarkCategory.class));
+    final Bundle args = requireArguments();
+    mCategory = Objects.requireNonNull(BundleCompat.getParcelable(args,
+        BookmarkCategorySettingsActivity.EXTRA_BOOKMARK_CATEGORY, BookmarkCategory.class));
   }
 
   @Nullable

android/app/src/main/java/app/organicmaps/bookmarks/BookmarksListFragment.java
@@ -16,6 +16,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.os.BundleCompat;
 import androidx.recyclerview.widget.ConcatAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.SimpleItemAnimator;
@@ -46,6 +47,7 @@ import app.organicmaps.util.bottomsheet.MenuBottomSheetItem;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class BookmarksListFragment extends BaseMwmRecyclerFragment<ConcatAdapter>
     implements BookmarkManager.BookmarksSharingListener,
@@ -107,13 +109,9 @@ public class BookmarksListFragment extends BaseMwmRecyclerFragment<ConcatAdapter
   @NonNull
   private BookmarkCategory getCategoryOrThrow()
   {
-    Bundle args = getArguments();
-    BookmarkCategory category;
-    if (args == null || (args.getBundle(EXTRA_BUNDLE) == null) ||
-        ((category = Utils.getParcelable(args.getBundle(EXTRA_BUNDLE), EXTRA_CATEGORY, BookmarkCategory.class))) == null)
-      throw new IllegalArgumentException("Category not exist in bundle");
-
-    return category;
+    final Bundle args = requireArguments();
+    final Bundle extra = Objects.requireNonNull(args.getBundle(EXTRA_BUNDLE));
+    return Objects.requireNonNull(BundleCompat.getParcelable(extra, EXTRA_CATEGORY, BookmarkCategory.class));
   }
 
   @NonNull

android/app/src/main/java/app/organicmaps/editor/FeatureCategoryFragment.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import androidx.annotation.CallSuper;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.os.BundleCompat;
 
 import app.organicmaps.R;
 import app.organicmaps.base.BaseMwmRecyclerFragment;
@@ -42,8 +43,11 @@ public class FeatureCategoryFragment extends BaseMwmRecyclerFragment<FeatureCate
     super.onViewCreated(view, savedInstanceState);
 
     final Bundle args = getArguments();
-    if (args != null && args.containsKey(FeatureCategoryActivity.EXTRA_FEATURE_CATEGORY))
-      mSelectedCategory = Utils.getParcelable(args, FeatureCategoryActivity.EXTRA_FEATURE_CATEGORY, FeatureCategory.class);
+    if (args != null)
+    {
+      mSelectedCategory = BundleCompat.getParcelable(args, FeatureCategoryActivity.EXTRA_FEATURE_CATEGORY,
+          FeatureCategory.class);
+    }
     mToolbarController = new SearchToolbarController(view, requireActivity())
     {
       @Override

android/app/src/main/java/app/organicmaps/editor/HoursMinutesPickerFragment.java
@@ -17,6 +17,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.StyleRes;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.content.res.AppCompatResources;
+import androidx.core.os.BundleCompat;
 import androidx.fragment.app.FragmentManager;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
@@ -115,8 +116,8 @@ public class HoursMinutesPickerFragment extends BaseMwmDialogFragment
     final Bundle args = getArguments();
     if (args == null)
       throw new IllegalArgumentException("Args must not be null");
-    mFrom = Utils.getParcelable(args, EXTRA_FROM, HoursMinutes.class);
-    mTo = Utils.getParcelable(args, EXTRA_TO, HoursMinutes.class);
+    mFrom = BundleCompat.getParcelable(args, EXTRA_FROM, HoursMinutes.class);
+    mTo = BundleCompat.getParcelable(args, EXTRA_TO, HoursMinutes.class);
     mSelectedTab = args.getInt(EXTRA_SELECT_FIRST);
     mId = args.getInt(EXTRA_ID);
   }

android/app/src/main/java/app/organicmaps/util/Utils.java
@@ -831,23 +831,6 @@ public class Utils
 
   @SuppressWarnings({"deprecation", "unchecked"})
   @Nullable
-  private static <T> T getParcelableOld(Bundle args, String key)
-  {
-    return (T) args.getParcelable(key);
-  }
-
-
-  @Nullable
-  public static <T> T getParcelable(@NonNull Bundle args, String key, Class<T> clazz)
-  {
-    args.setClassLoader(clazz.getClassLoader());
-    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU)
-      return getParcelableOld(args, key);
-    return args.getParcelable(key, clazz);
-  }
-
-  @SuppressWarnings({"deprecation", "unchecked"})
-  @Nullable
   private static <T extends Serializable> T getSerializableOld(Bundle args, String key)
   {
     return (T) args.getSerializable(key);
@@ -861,7 +844,6 @@ public class Utils
       return getSerializableOld(args, key);
     return args.getSerializable(key, clazz);
   }
-
 
   @SuppressWarnings("deprecation")
   private static Spanned fromHtmlOld(@NonNull String htmlDescription)

android/app/src/main/java/app/organicmaps/widget/placepage/DirectionFragment.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.os.BundleCompat;
 
 import app.organicmaps.Framework;
 import app.organicmaps.R;
@@ -52,7 +53,7 @@ public class DirectionFragment extends BaseMwmDialogFragment
     });
     initViews(root);
     if (savedInstanceState != null)
-      setMapObject(Utils.getParcelable(savedInstanceState, EXTRA_MAP_OBJECT, MapObject.class));
+      setMapObject(BundleCompat.getParcelable(savedInstanceState, EXTRA_MAP_OBJECT, MapObject.class));
 
     return root;
   }

android/app/build.gradle
@@ -109,6 +109,7 @@ dependencies {
   // > A failure occurred while executing com.android.build.gradle.internal.tasks.CheckDuplicatesRunnable
   // We don't use Kotlin, but some dependencies are actively using it.
   // See https://stackoverflow.com/a/75719642
+  implementation 'androidx.core:core:1.12.0'
   implementation(platform('org.jetbrains.kotlin:kotlin-bom:1.9.10'))
   implementation 'androidx.annotation:annotation:1.7.0'
   implementation 'androidx.appcompat:appcompat:1.6.1'

android/app/src/main/java/app/organicmaps/widget/placepage/ElevationProfileViewRenderer.java
@@ -133,7 +133,7 @@ public class ElevationProfileViewRenderer implements PlacePageStateListener
 
   public void onRestore(@NonNull Bundle inState)
   {
-//    mElevationInfo = Utils.getParcelable(inState, PlacePageUtils.EXTRA_PLACE_PAGE_DATA, ElevationInfo.class);
+//    mElevationInfo = BundleCompat.getParcelable(inState, PlacePageUtils.EXTRA_PLACE_PAGE_DATA, ElevationInfo.class);
 //    if (mElevationInfo != null)
 //      render(mElevationInfo);
   }

commit 9fa46fd9b230078bb5f8ba7a429ea348a99533ba

[android] Fix ClassNotFoundException .bookmarks.data.BookmarkCategory
Regression by 4cadc09

Related to #143 and #3757
Closes #6629

Signed-off-by: Roman Tsisyk <roman@tsisyk.com>

android/app/src/main/java/app/organicmaps/bookmarks/BookmarkCategorySettingsFragment.java
@@ -12,12 +12,12 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.os.BundleCompat;
 
 import app.organicmaps.R;
 import app.organicmaps.base.BaseMwmToolbarFragment;
 import app.organicmaps.bookmarks.data.BookmarkCategory;
 import app.organicmaps.bookmarks.data.BookmarkManager;
+import app.organicmaps.util.Utils;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.textfield.TextInputEditText;
@@ -45,7 +45,7 @@ public class BookmarkCategorySettingsFragment extends BaseMwmToolbarFragment
   {
     super.onCreate(savedInstanceState);
     final Bundle args = requireArguments();
-    mCategory = Objects.requireNonNull(BundleCompat.getParcelable(args,
+    mCategory = Objects.requireNonNull(Utils.getParcelable(args,
         BookmarkCategorySettingsActivity.EXTRA_BOOKMARK_CATEGORY, BookmarkCategory.class));
   }
 

android/app/src/main/java/app/organicmaps/bookmarks/BookmarkListActivity.java
@@ -1,7 +1,5 @@
 package app.organicmaps.bookmarks;
 
-import static app.organicmaps.bookmarks.BookmarksListFragment.EXTRA_BUNDLE;
-
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -62,9 +60,8 @@ public class BookmarkListActivity extends BaseToolbarActivity
   static void startForResult(@NonNull Fragment fragment, @NonNull BookmarkCategory category)
   {
     Bundle args = new Bundle();
-    args.putParcelable(BookmarksListFragment.EXTRA_CATEGORY, category);
     Intent intent = new Intent(fragment.requireActivity(), BookmarkListActivity.class);
-    intent.putExtra(EXTRA_BUNDLE, args);
+    intent.putExtra(BookmarksListFragment.EXTRA_CATEGORY, category);
     fragment.startActivityForResult(intent, BookmarkCategoriesFragment.REQ_CODE_DELETE_CATEGORY);
   }
 }

android/app/src/main/java/app/organicmaps/bookmarks/BookmarksListFragment.java
@@ -16,7 +16,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.os.BundleCompat;
 import androidx.recyclerview.widget.ConcatAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.SimpleItemAnimator;
@@ -36,6 +35,7 @@ import app.organicmaps.intent.Factory;
 import app.organicmaps.location.LocationHelper;
 import app.organicmaps.search.NativeBookmarkSearchListener;
 import app.organicmaps.search.SearchEngine;
+import app.organicmaps.util.Utils;
 import app.organicmaps.widget.SearchToolbarController;
 import app.organicmaps.widget.placepage.EditBookmarkFragment;
 import app.organicmaps.widget.recycler.DividerItemDecorationWithPadding;
@@ -58,7 +58,6 @@ public class BookmarksListFragment extends BaseMwmRecyclerFragment<ConcatAdapter
 {
   public static final String TAG = BookmarksListFragment.class.getSimpleName();
   public static final String EXTRA_CATEGORY = "bookmark_category";
-  public static final String EXTRA_BUNDLE = "bookmark_bundle";
   private static final int INDEX_BOOKMARKS_COLLECTION_ADAPTER = 0;
   private static final int INDEX_BOOKMARKS_LIST_ADAPTER = 1;
   private static final String BOOKMARKS_MENU_ID = "BOOKMARKS_MENU_BOTTOM_SHEET";
@@ -109,8 +108,7 @@ public class BookmarksListFragment extends BaseMwmRecyclerFragment<ConcatAdapter
   private BookmarkCategory getCategoryOrThrow()
   {
     final Bundle args = requireArguments();
-    final Bundle extra = Objects.requireNonNull(args.getBundle(EXTRA_BUNDLE));
-    return Objects.requireNonNull(BundleCompat.getParcelable(extra, EXTRA_CATEGORY, BookmarkCategory.class));
+    return Objects.requireNonNull(Utils.getParcelable(args, EXTRA_CATEGORY, BookmarkCategory.class));
   }
 
   @NonNull

android/app/src/main/java/app/organicmaps/editor/FeatureCategoryFragment.java
@@ -8,7 +8,6 @@ import android.view.ViewGroup;
 import androidx.annotation.CallSuper;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.os.BundleCompat;
 
 import app.organicmaps.R;
 import app.organicmaps.base.BaseMwmRecyclerFragment;
@@ -45,7 +44,7 @@ public class FeatureCategoryFragment extends BaseMwmRecyclerFragment<FeatureCate
     final Bundle args = getArguments();
     if (args != null)
     {
-      mSelectedCategory = BundleCompat.getParcelable(args, FeatureCategoryActivity.EXTRA_FEATURE_CATEGORY,
+      mSelectedCategory = Utils.getParcelable(args, FeatureCategoryActivity.EXTRA_FEATURE_CATEGORY,
           FeatureCategory.class);
     }
     mToolbarController = new SearchToolbarController(view, requireActivity())

android/app/src/main/java/app/organicmaps/editor/HoursMinutesPickerFragment.java
@@ -17,7 +17,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.StyleRes;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.content.res.AppCompatResources;
-import androidx.core.os.BundleCompat;
 import androidx.fragment.app.FragmentManager;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
@@ -27,6 +26,7 @@ import app.organicmaps.base.BaseMwmDialogFragment;
 import app.organicmaps.editor.data.HoursMinutes;
 import app.organicmaps.util.DateUtils;
 import app.organicmaps.util.ThemeUtils;
+import app.organicmaps.util.Utils;
 
 public class HoursMinutesPickerFragment extends BaseMwmDialogFragment
 {
@@ -115,8 +115,8 @@ public class HoursMinutesPickerFragment extends BaseMwmDialogFragment
     final Bundle args = getArguments();
     if (args == null)
       throw new IllegalArgumentException("Args must not be null");
-    mFrom = BundleCompat.getParcelable(args, EXTRA_FROM, HoursMinutes.class);
-    mTo = BundleCompat.getParcelable(args, EXTRA_TO, HoursMinutes.class);
+    mFrom = Utils.getParcelable(args, EXTRA_FROM, HoursMinutes.class);
+    mTo = Utils.getParcelable(args, EXTRA_TO, HoursMinutes.class);
     mSelectedTab = args.getInt(EXTRA_SELECT_FIRST);
     mId = args.getInt(EXTRA_ID);
   }

android/app/src/main/java/app/organicmaps/util/Utils.java
@@ -13,6 +13,7 @@ import android.content.pm.ResolveInfo;
 import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
 import android.text.Html;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
@@ -33,6 +34,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 import androidx.core.app.NavUtils;
+import androidx.core.os.BundleCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
@@ -720,6 +722,13 @@ public class Utils
         }
       });
     }
+  }
+
+  public static <T> T getParcelable(@NonNull Bundle in, @Nullable String key,
+                                    @NonNull Class<T> clazz)
+  {
+    in.setClassLoader(clazz.getClassLoader());
+    return BundleCompat.getParcelable(in, key, clazz);
   }
 
   @SuppressWarnings("deprecation")

android/app/src/main/java/app/organicmaps/widget/placepage/DirectionFragment.java
@@ -10,7 +10,6 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.os.BundleCompat;
 
 import app.organicmaps.Framework;
 import app.organicmaps.R;
@@ -21,6 +20,7 @@ import app.organicmaps.location.LocationHelper;
 import app.organicmaps.location.LocationListener;
 import app.organicmaps.location.SensorHelper;
 import app.organicmaps.location.SensorListener;
+import app.organicmaps.util.Utils;
 import app.organicmaps.widget.ArrowView;
 import app.organicmaps.util.UiUtils;
 
@@ -52,7 +52,7 @@ public class DirectionFragment extends BaseMwmDialogFragment
     });
     initViews(root);
     if (savedInstanceState != null)
-      setMapObject(BundleCompat.getParcelable(savedInstanceState, EXTRA_MAP_OBJECT, MapObject.class));
+      setMapObject(Utils.getParcelable(savedInstanceState, EXTRA_MAP_OBJECT, MapObject.class));
 
     return root;
   }