963d787876d14d5744d78e5dede62235c75bc2d3
Osyotr <Osyotr@users.noreply.github.com>
2023-12-24 18:06:07 +0300

    Fix MainWindow::nativeEvent
    winEvent is a relic from Qt4 era
    
    Signed-off-by: Osyotr <Osyotr@users.noreply.github.com>

--- qt/mainwindow.hpp
+++ qt/mainwindow.hpp
@@ -96,9 +96,9 @@ protected:
 
   void SetLayerEnabled(LayerType type, bool enable);
 
-#if defined(Q_WS_WIN)
+#if defined(OMIM_OS_WINDOWS)
   /// to handle menu messages
-  bool winEvent(MSG * msg, long * result) override;
+  bool nativeEvent(QByteArray const & eventType, void * message, qintptr * result) override;
 #endif
 
   void closeEvent(QCloseEvent * e) override;
