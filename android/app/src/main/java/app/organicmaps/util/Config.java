bc5a29739b94643c8051e66ff87c4b55959aa94c
Roman Tsisyk <roman@tsisyk.com>
2024-01-07 16:57:24 +0200

    [android] Disable OSM Login for Google Play
    See #7000
    
    Signed-off-by: Roman Tsisyk <roman@tsisyk.com>

--- android/app/src/main/java/app/organicmaps/util/Config.java
+++ android/app/src/main/java/app/organicmaps/util/Config.java
@@ -357,6 +357,13 @@ public final class Config
     return url;
   }
 
+  @NonNull
+  @SuppressWarnings("ConstantConditions") // BuildConfig
+  public static boolean isOsmLoginEnabled(@NonNull Context context)
+  {
+    return !BuildConfig.FLAVOR.equals("google");
+  }
+
   public static void init(@NonNull Context context)
   {
     PreferenceManager.setDefaultValues(context, R.xml.prefs_main, false);
