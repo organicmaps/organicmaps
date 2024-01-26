6653955bf1b3d29b75edfc66a22cad1728ba19b7
Harry Bond <hrbond@pm.me>
2024-01-26 12:11:48 +0000

    get local_ref
    Signed-off-by: Harry Bond <hrbond@pm.me>

--- android/app/src/main/java/app/organicmaps/bookmarks/data/Metadata.java
+++ android/app/src/main/java/app/organicmaps/bookmarks/data/Metadata.java
@@ -61,7 +61,8 @@ public class Metadata implements Parcelable
     FMD_BUILDING_MIN_LEVEL(40),
     FMD_WIKIMEDIA_COMMONS(41),
     FMD_CAPACITY(42),
-    FMD_WHEELCHAIR(43);
+    FMD_WHEELCHAIR(43),
+    FMD_LOCAL_REF(44);
     private final int mMetaType;
 
     MetadataType(int metadataType)
--- indexer/feature_meta.hpp
+++ indexer/feature_meta.hpp
@@ -149,6 +149,7 @@ public:
     FMD_WIKIMEDIA_COMMONS = 41,
     FMD_CAPACITY = 42,
     FMD_WHEELCHAIR = 43, // Value is runtime only, data is taken from the classificator types
+    FMD_LOCAL_REF = 44,
     FMD_COUNT
   };
 
