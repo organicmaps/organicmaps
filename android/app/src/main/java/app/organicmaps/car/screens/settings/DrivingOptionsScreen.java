d7999dff224912be0eb0a0b3b3ed618ebda73659
Andrew Shkrob <andrew.shkrob.social@yandex.by>
2024-01-28 00:31:47 +0100

    [android-auto] Use toggles instead of checkboxes
    Signed-off-by: Andrew Shkrob <andrew.shkrob.social@yandex.by>

--- android/app/src/main/java/app/organicmaps/car/screens/settings/DrivingOptionsScreen.java
+++ android/app/src/main/java/app/organicmaps/car/screens/settings/DrivingOptionsScreen.java
@@ -4,18 +4,18 @@ import androidx.annotation.NonNull;
 import androidx.annotation.StringRes;
 import androidx.car.app.CarContext;
 import androidx.car.app.model.Action;
-import androidx.car.app.model.CarIcon;
 import androidx.car.app.model.Header;
 import androidx.car.app.model.ItemList;
+import androidx.car.app.model.OnClickListener;
 import androidx.car.app.model.Row;
 import androidx.car.app.model.Template;
 import androidx.car.app.navigation.model.MapTemplate;
-import androidx.core.graphics.drawable.IconCompat;
 import androidx.lifecycle.LifecycleOwner;
 
 import app.organicmaps.R;
 import app.organicmaps.car.SurfaceRenderer;
 import app.organicmaps.car.screens.base.BaseMapScreen;
+import app.organicmaps.car.util.Toggle;
 import app.organicmaps.car.util.UiHelpers;
 import app.organicmaps.routing.RoutingOptions;
 import app.organicmaps.settings.RoadType;
@@ -49,18 +49,11 @@ public class DrivingOptionsScreen extends BaseMapScreen
   };
 
   @NonNull
-  private final CarIcon mCheckboxIcon;
-  @NonNull
-  private final CarIcon mCheckboxSelectedIcon;
-
-  @NonNull
   private final Map<RoadType, Boolean> mInitialDrivingOptionsState = new HashMap<>();
 
   public DrivingOptionsScreen(@NonNull CarContext carContext, @NonNull SurfaceRenderer surfaceRenderer)
   {
     super(carContext, surfaceRenderer);
-    mCheckboxIcon = new CarIcon.Builder(IconCompat.createWithResource(carContext, R.drawable.ic_check_box)).build();
-    mCheckboxSelectedIcon = new CarIcon.Builder(IconCompat.createWithResource(carContext, R.drawable.ic_check_box_checked)).build();
 
     initDrivingOptionsState();
   }
@@ -103,24 +96,21 @@ public class DrivingOptionsScreen extends BaseMapScreen
   {
     final ItemList.Builder builder = new ItemList.Builder();
     for (final DrivingOption drivingOption : mDrivingOptions)
-      builder.addItem(createDrivingOptionCheckbox(drivingOption.roadType, drivingOption.text));
+      builder.addItem(createDrivingOptionsToggle(drivingOption.roadType, drivingOption.text));
     return builder.build();
   }
 
   @NonNull
-  private Row createDrivingOptionCheckbox(RoadType roadType, @StringRes int titleRes)
+  private Row createDrivingOptionsToggle(RoadType roadType, @StringRes int title)
   {
-    final Row.Builder builder = new Row.Builder();
-    builder.setTitle(getCarContext().getString(titleRes));
-    builder.setOnClickListener(() -> {
+    final OnClickListener listener = () -> {
       if (RoutingOptions.hasOption(roadType))
         RoutingOptions.removeOption(roadType);
       else
         RoutingOptions.addOption(roadType);
       invalidate();
-    });
-    builder.setImage(RoutingOptions.hasOption(roadType) ? mCheckboxSelectedIcon : mCheckboxIcon);
-    return builder.build();
+    };
+    return Toggle.create(getCarContext(), title, listener, RoutingOptions.hasOption(roadType));
   }
 
   private void initDrivingOptionsState()
