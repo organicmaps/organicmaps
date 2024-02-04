fb4bb96abeb6025bbf820ab3011f4b074771d657
Kiryl Kaveryn <kirylkaveryn@gmail.com>
2024-02-04 14:12:35 +0400

    [ios] make the UIView's extension AddSeparator internal
    Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

--- iphone/Maps/UI/PlacePage/Components/PlacePageInfoViewController.swift
+++ iphone/Maps/UI/PlacePage/Components/PlacePageInfoViewController.swift
@@ -356,22 +356,3 @@ private extension UIStackView {
     addArrangedSubview(view)
   }
 }
-
-private extension UIView {
-  func addSeparator(thickness: CGFloat,
-                    color: UIColor?,
-                    insets: UIEdgeInsets) {
-    let lineView = UIView()
-    lineView.styleName = "Divider"
-    lineView.backgroundColor = color ?? .black
-    lineView.isUserInteractionEnabled = false
-    lineView.translatesAutoresizingMaskIntoConstraints = false
-    addSubview(lineView)
-    NSLayoutConstraint.activate([
-      lineView.heightAnchor.constraint(equalToConstant: thickness),
-      lineView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: insets.left),
-      lineView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -insets.right),
-      lineView.topAnchor.constraint(equalTo: topAnchor, constant: insets.top),
-    ])
-  }
-}
