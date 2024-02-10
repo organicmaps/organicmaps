e5a852f6808ee5ede40bfd660fbf03415d1dc18e
Harry Bond <hrbond@pm.me>
2024-02-10 00:19:27 +0000

    [android] poi name in wikipedia page header
    Signed-off-by: Harry Bond <hrbond@pm.me>

--- android/app/src/main/java/app/organicmaps/widget/placepage/PlaceDescriptionActivity.java
+++ android/app/src/main/java/app/organicmaps/widget/placepage/PlaceDescriptionActivity.java
@@ -10,16 +10,26 @@ import app.organicmaps.base.BaseToolbarActivity;
 
 public class PlaceDescriptionActivity extends BaseToolbarActivity
 {
+  private static final String EXTRA_TITLE = "title";
+
   @Override
   protected Class<? extends Fragment> getFragmentClass()
   {
     return PlaceDescriptionFragment.class;
   }
 
-  public static void start(@NonNull Context context, @NonNull String description)
+  public static void start(@NonNull Context context, @NonNull String title, @NonNull String description)
   {
     Intent intent = new Intent(context, PlaceDescriptionActivity.class)
-        .putExtra(PlaceDescriptionFragment.EXTRA_DESCRIPTION, description);
+        .putExtra(PlaceDescriptionFragment.EXTRA_DESCRIPTION, description)
+        .putExtra(EXTRA_TITLE, title);
     context.startActivity(intent);
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    String toolbarTitle = getIntent().getStringExtra(EXTRA_TITLE);
+    this.getToolbar().setTitle(toolbarTitle);
   }
 }
