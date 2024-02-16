24504ebad611e283b9d1e1bc79d2e41d43475588
S. Kozyr <s.trump@gmail.com>
2024-02-16 13:07:37 +0200

    [Android] Hide "Wikipedia" button if link is not available
    Signed-off-by: S. Kozyr <s.trump@gmail.com>

--- android/app/src/main/java/app/organicmaps/widget/placepage/sections/PlacePageWikipediaFragment.java
+++ android/app/src/main/java/app/organicmaps/widget/placepage/sections/PlacePageWikipediaFragment.java
@@ -105,15 +105,21 @@ public class PlacePageWikipediaFragment extends Fragment implements Observer<Map
     }
 
     final String wikipediaLink = mMapObject.getMetadata(Metadata.MetadataType.FMD_WIKIPEDIA);
-    mWiki.setOnClickListener((v) -> {
-      if (!TextUtils.isEmpty(wikipediaLink))
-        Utils.openUrl(requireContext(), wikipediaLink);
-    });
-    mWiki.setOnLongClickListener((v) -> {
-      if (!TextUtils.isEmpty(wikipediaLink))
-        PlacePageUtils.copyToClipboard(requireContext(), mFrame, wikipediaLink);
-      return true;
-    });
+    if (TextUtils.isEmpty(wikipediaLink))
+      UiUtils.hide(mWiki);
+    else
+    {
+      UiUtils.show(mWiki);
+      mWiki.setOnClickListener((v) -> {
+        if (!TextUtils.isEmpty(wikipediaLink))
+          Utils.openUrl(requireContext(), wikipediaLink);
+      });
+      mWiki.setOnLongClickListener((v) -> {
+        if (!TextUtils.isEmpty(wikipediaLink))
+          PlacePageUtils.copyToClipboard(requireContext(), mFrame, wikipediaLink);
+        return true;
+      });
+    }
   }
 
   @Override
