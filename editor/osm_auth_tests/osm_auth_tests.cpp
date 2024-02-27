4e5c0a494d4ef7f788dda4b83230ce7ef5c05892
S. Kozyr <s.trump@gmail.com>
2024-02-27 11:20:41 +0200

    [MacOS] Fix HttpSessionManager main queue issue
    Signed-off-by: S. Kozyr <s.trump@gmail.com>

--- editor/osm_auth_tests/osm_auth_tests.cpp
+++ editor/osm_auth_tests/osm_auth_tests.cpp
@@ -14,33 +14,17 @@ constexpr char const * kInvalidOsmPassword = "123";
 constexpr char const * kForgotPasswordEmail = "osmtest1@organicmaps.app";
 }  // namespace
 
-#if defined(OMIM_OS_MAC)
-  extern void runMainQueue(void func());
-#endif
-
 UNIT_TEST(OSM_Auth_InvalidLogin)
 {
-#if defined(OMIM_OS_MAC)
-  runMainQueue([]() {
-#endif
-
   OsmOAuth auth = OsmOAuth::DevServerAuth();
   bool result;
   TEST_NO_THROW(result = auth.AuthorizePassword(kValidOsmUser, kInvalidOsmPassword), ());
   TEST_EQUAL(result, false, ("invalid password"));
   TEST(!auth.IsAuthorized(), ("Should not be authorized."));
-
-#if defined(OMIM_OS_MAC)
-  });
-#endif
 }
 
 UNIT_TEST(OSM_Auth_Login)
 {
-#if defined(OMIM_OS_MAC)
-  runMainQueue([]() {
-#endif
-
   OsmOAuth auth = OsmOAuth::DevServerAuth();
   bool result;
   TEST_NO_THROW(result = auth.AuthorizePassword(kValidOsmUser, kValidOsmPassword), ());
@@ -49,29 +33,16 @@ UNIT_TEST(OSM_Auth_Login)
   OsmOAuth::Response const perm = auth.Request("/permissions");
   TEST_EQUAL(perm.first, OsmOAuth::HTTP::OK, ("permission request ok"));
   TEST_NOT_EQUAL(perm.second.find("write_api"), std::string::npos, ("can write to api"));
-
-#if defined(OMIM_OS_MAC)
-  });
-#endif
 }
 
 /*
 UNIT_TEST(OSM_Auth_ForgotPassword)
 {
-#if defined(OMIM_OS_MAC)
-  runMainQueue([]() {
-#endif
-
   OsmOAuth auth = OsmOAuth::DevServerAuth();
   bool result;
   TEST_NO_THROW(result = auth.ResetPassword(kForgotPasswordEmail), ());
   TEST_EQUAL(result, true, ("Correct email"));
   TEST_NO_THROW(result = auth.ResetPassword("not@registered.email"), ());
   TEST_EQUAL(result, false, ("Incorrect email"));
-
-
-#if defined(OMIM_OS_MAC)
-  });
-#endif
 }
 */
