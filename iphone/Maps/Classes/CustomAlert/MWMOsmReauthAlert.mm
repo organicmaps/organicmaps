053c845dd3932b85d477b5e3a8d177b1f01e01e5
S. Kozyr <s.trump@gmail.com>
2024-03-19 23:16:02 +0200

    [ios] Added blue color for link inside MWMOsmReauthAlert
    Signed-off-by: S. Kozyr <s.trump@gmail.com>

--- iphone/Maps/Classes/CustomAlert/MWMOsmReauthAlert.mm
+++ iphone/Maps/Classes/CustomAlert/MWMOsmReauthAlert.mm
@@ -12,7 +12,26 @@ static NSString * const kMap2OsmLoginSegue = @"Map2OsmLogin";
 {
   MWMOsmReauthAlert * alert =
       [NSBundle.mainBundle loadNibNamed:[self className] owner:nil options:nil].firstObject;
+
+  //[alert.messageLabel setScrollEnabled:NO];
+  alert.messageLabel.attributedText = [self buildAlertMessage];
+  UITapGestureRecognizer *tapGesture =
+        [[UITapGestureRecognizer alloc] initWithTarget:alert
+                                                action:@selector(showMoreInfo)];
+  [alert.messageLabel addGestureRecognizer:tapGesture];
+
   return alert;
+}
+
++ (NSMutableAttributedString*)buildAlertMessage
+{
+  auto linkAttrs = @{NSForegroundColorAttributeName : UIColor.linkBlue};
+
+  NSMutableAttributedString *alertMessage = [[NSMutableAttributedString alloc] initWithString:L(@"alert_reauth_message_ios")];
+  [alertMessage appendAttributedString:([[NSAttributedString alloc] initWithString:@" "])];
+  NSAttributedString *alertLinkText = [[NSAttributedString alloc] initWithString:L(@"alert_reauth_link_text_ios") attributes:linkAttrs];
+  [alertMessage appendAttributedString:alertLinkText];
+  return alertMessage;
 }
 
 - (IBAction)osmTap
@@ -21,6 +40,11 @@ static NSString * const kMap2OsmLoginSegue = @"Map2OsmLogin";
     [self.alertController.ownerViewController performSegueWithIdentifier:kMap2OsmLoginSegue
                                                                   sender:nil];
   }];
+}
+
+- (IBAction)showMoreInfo
+{
+  NSLog(@"Navigate to URL: ");
 }
 
 - (IBAction)closeTap
