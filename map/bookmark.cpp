02e6f0f7bb947897c99ac2583fafcd5354db0d43
Alexander Borsuk <me@alex.bio>
2024-03-22 19:58:33 +0100

    [bookmarks] Do not change bookmarks list modification date when hiding/showing it
    It introduces discrepancy as the file modification date will be newer than
    modification date stored inside the KML.

--- map/bookmark.cpp
+++ map/bookmark.cpp
@@ -324,13 +324,13 @@ void BookmarkCategory::SetIsVisible(bool isVisible)
 
 void BookmarkCategory::SetName(std::string const & name)
 {
-  SetDirty();
+  SetDirty(true /* updateModificationTime */);
   kml::SetDefaultStr(m_data.m_name, name);
 }
 
 void BookmarkCategory::SetDescription(std::string const & desc)
 {
-  SetDirty();
+  SetDirty(true /* updateModificationTime */);
   kml::SetDefaultStr(m_data.m_description, desc);
 }
 
@@ -339,7 +339,7 @@ void BookmarkCategory::SetServerId(std::string const & serverId)
   if (m_serverId == serverId)
     return;
 
-  SetDirty();
+  SetDirty(true /* updateModificationTime */);
   m_serverId = serverId;
 }
 
@@ -348,7 +348,7 @@ void BookmarkCategory::SetTags(std::vector<std::string> const & tags)
   if (m_data.m_tags == tags)
     return;
 
-  SetDirty();
+  SetDirty(true /* updateModificationTime */);
   m_data.m_tags = tags;
 }
 
@@ -358,7 +358,7 @@ void BookmarkCategory::SetCustomProperty(std::string const & key, std::string co
   if (it != m_data.m_properties.end() && it->second == value)
     return;
 
-  SetDirty();
+  SetDirty(true /* updateModificationTime */);
   m_data.m_properties[key] = value;
 }
 
@@ -378,7 +378,7 @@ void BookmarkCategory::SetAuthor(std::string const & name, std::string const & i
   if (m_data.m_authorName == name && m_data.m_authorId == id)
     return;
 
-  SetDirty();
+  SetDirty(true /* updateModificationTime */);
   m_data.m_authorName = name;
   m_data.m_authorId = id;
 }
@@ -388,7 +388,7 @@ void BookmarkCategory::SetAccessRules(kml::AccessRules accessRules)
   if (m_data.m_accessRules == accessRules)
     return;
 
-  SetDirty();
+  SetDirty(true /* updateModificationTime */);
   m_data.m_accessRules = accessRules;
 }
 
@@ -398,8 +398,9 @@ kml::PredefinedColor BookmarkCategory::GetDefaultColor()
   return kml::PredefinedColor::Red;
 }
 
-void BookmarkCategory::SetDirty()
+void BookmarkCategory::SetDirty(bool updateModificationDate)
 {
-  Base::SetDirty();
-  m_data.m_lastModified = kml::TimestampClock::now();
+  Base::SetDirty(updateModificationDate);
+  if (updateModificationDate)
+    m_data.m_lastModified = kml::TimestampClock::now();
 }
