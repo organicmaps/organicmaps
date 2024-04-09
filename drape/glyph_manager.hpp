2973ec364b868ac95fd2ed0b3b40fd468ff22fd2
Alexander Borsuk <me@alex.bio>
2024-04-09 23:23:49 +0200

    [drape] Split text into segments needed for proper text shaping with Harfbuzz
    - Added a font_tool to experiment and test runs algorithm
    - Updated existing test to show/compare different lang strings rendered in different ways
    
    Harfbuzz shaping requires splitting each string into segments (runs). Each run should have the same:
    - font
    - direction
    - script
    - language
    
    The base idea of getting runs is based on the Chromium source code, with some changes and improvements.
    
    There are many TODOs that can be handled later.
    
    Signed-off-by: Alexander Borsuk <me@alex.bio>

--- drape/glyph_manager.hpp
+++ drape/glyph_manager.hpp
@@ -14,6 +14,36 @@ namespace dp
 {
 struct UnicodeBlock;
 
+// TODO(AB): Move to a separate file?
+namespace text
+{
+struct GlyphMetrics
+{
+  int16_t m_font;
+  uint16_t m_glyphId;
+  // TODO(AB): Store original font units or floats?
+  int32_t m_xOffset;
+  int32_t m_yOffset;
+  int32_t m_xAdvance;
+  // yAdvance is used only in vertical text layouts.
+};
+
+// TODO(AB): Move to a separate file?
+struct TextMetrics
+{
+  int32_t m_lineWidthInPixels {0};
+  int32_t m_maxLineHeightInPixels {0};
+  std::vector<GlyphMetrics> m_glyphs;
+
+  void AddGlyphMetrics(int16_t font, uint16_t glyphId, int32_t xOffset, int32_t yOffset, int32_t xAdvance, int32_t height)
+  {
+    m_glyphs.push_back({font, glyphId, xOffset, yOffset, xAdvance});
+    m_lineWidthInPixels += xAdvance;
+    m_maxLineHeightInPixels = std::max(m_maxLineHeightInPixels, height);
+  }
+};
+}  // namespace text
+
 class GlyphManager
 {
 public:
@@ -36,13 +66,19 @@ public:
 
   Glyph const & GetInvalidGlyph() const;
 
-private:
   int GetFontIndex(strings::UniChar unicodePoint);
+  int GetFontIndex(std::u16string_view sv);
+
+  text::TextMetrics ShapeText(std::string_view utf8, int fontPixelHeight, int8_t lang);
+  text::TextMetrics ShapeText(std::string_view utf8, int fontPixelHeight, char const * lang);
+
+  GlyphImage GetGlyphImage(int fontIndex, uint16_t glyphId, int pixelHeight, bool sdf);
+
+private:
   // Immutable version can be called from any thread and doesn't require internal synchronization.
   int GetFontIndexImmutable(strings::UniChar unicodePoint) const;
   int FindFontIndexInBlock(UnicodeBlock const & block, strings::UniChar unicodePoint) const;
 
-private:
   struct Impl;
   std::unique_ptr<Impl> m_impl;
 };
---
+++ drape/harfbuzz_shaping.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "std/target_os.hpp"
+
+#include <string>
+#include <vector>
+
+#if defined(OMIM_OS_LINUX) || defined(OMIM_OS_WINDOWS)
+#include <harfbuzz/hb.h>
+#else
+#include <hb.h>
+#endif
+
+namespace harfbuzz_shaping
+{
+struct TextSegment
+{
+  // TODO(AB): Use 1 byte or 2 bytes for memory-efficient caching.
+  int32_t m_start;  // Offset to the segment start in the string.
+  int32_t m_length;
+  hb_script_t m_script;
+  hb_direction_t m_direction;
+
+  TextSegment(int32_t start, int32_t length, hb_script_t script, hb_direction_t direction)
+      : m_start(start), m_length(length), m_script(script), m_direction(direction) {}
+};
+
+struct TextSegments
+{
+  std::u16string m_text;
+  std::vector<TextSegment> m_segments;
+  // TODO(AB): Reverse indexes to order segments instead of moving them.
+  //std::vector<size_t> m_segmentsOrder;
+};
+
+// Finds segments with common properties suitable for harfbuzz in a single line of text without newlines.
+// Any line breaking/trimming should be done by the caller.
+TextSegments GetTextSegments(std::string_view utf8);
+}  // namespace harfbuzz_shaping
