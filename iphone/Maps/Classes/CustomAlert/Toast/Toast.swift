91b3e09a384264cd74cb84cd2b551c56dc7fe190
Kiryl Kaveryn <kirylkaveryn@gmail.com>
2024-05-23 19:45:40 +0400

    [ios] refactor the `Toast` to support Undo
    - Now Toast is a facade to hide the subclass implementations from the callers
    - New `UndoToast` class
    
    Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

--- iphone/Maps/Classes/CustomAlert/Toast/Toast.swift
+++ iphone/Maps/Classes/CustomAlert/Toast/Toast.swift
@@ -1,62 +1,53 @@
 @objc(MWMToast)
-final class Toast: NSObject {
+class Toast: NSObject {
   @objc(MWMToastAlignment)
   enum Alignment: Int {
     case bottom
     case top
   }
 
-  private var blurView = UIVisualEffectView(effect: UIBlurEffect(style: .dark))
-  private var timer: Timer?
-
+  private static let duration: TimeInterval = 3.0
   private static var toasts: [Toast] = []
 
-  @objc static func toast(withText text: String) -> Toast {
-    let toast = Toast(text)
-    toasts.append(toast)
-    return toast
+  private let blurView = UIVisualEffectView(effect: UIBlurEffect(style: .dark))
+  private var timer: Timer?
+
+  var contentView: UIView { blurView.contentView }
+  var onHideCompletion: (() -> Void)?
+
+  override init() {
+    super.init()
+    setupBlurView()
+    Self.toasts.append(self)
   }
-  
-  @objc static func hideAll() {
-    toasts.forEach { $0.hide() }
-  }
-  
-  private init(_ text: String) {
+
+  private func setupBlurView() {
     blurView.layer.setCorner(radius: 8)
     blurView.clipsToBounds = true
     blurView.alpha = 0
-
-    let label = UILabel()
-    label.text = text
-    label.textAlignment = .center
-    label.numberOfLines = 0
-    label.font = .regular14()
-    label.textColor = .white
-    label.translatesAutoresizingMaskIntoConstraints = false
-    blurView.contentView.addSubview(label)
-    blurView.isUserInteractionEnabled = false
-
-    NSLayoutConstraint.activate([
-        label.leadingAnchor.constraint(equalTo: blurView.contentView.leadingAnchor, constant: 8),
-        label.trailingAnchor.constraint(equalTo: blurView.contentView.trailingAnchor, constant: -8),
-        label.topAnchor.constraint(equalTo: blurView.contentView.topAnchor, constant: 8),
-        label.bottomAnchor.constraint(equalTo: blurView.contentView.bottomAnchor, constant: -8)
-    ])
   }
-  
+
   deinit {
     timer?.invalidate()
   }
+}
+
+// MARK: - Public methods
+extension Toast {
+  @objc static func hideAll() {
+    toasts.forEach { $0.hide() }
+  }
 
   @objc func show() {
-    show(in: UIApplication.shared.keyWindow, alignment: .bottom)
+    Self.hideAll()
+    show(in: UIApplication.shared.keyWindow, alignment: .bottom, duration: Toast.duration)
   }
 
-  @objc func show(withAlignment alignment: Alignment, pinToSafeArea: Bool = true) {
-    show(in: UIApplication.shared.keyWindow, alignment: alignment, pinToSafeArea: pinToSafeArea)
+  @objc func show(withAlignment alignment: Alignment, pinToSafeArea: Bool = true, duration: TimeInterval = Toast.duration) {
+    show(in: UIApplication.shared.keyWindow, alignment: alignment, pinToSafeArea: pinToSafeArea, duration: duration)
   }
 
-  @objc func show(in view: UIView?, alignment: Alignment, pinToSafeArea: Bool = true) {
+  @objc func show(in view: UIView?, alignment: Alignment, pinToSafeArea: Bool = true, duration: TimeInterval = Toast.duration) {
     guard let view = view else { return }
     blurView.translatesAutoresizingMaskIntoConstraints = false
     view.addSubview(blurView)
@@ -85,7 +76,7 @@ final class Toast: NSObject {
       self.blurView.alpha = 1
     }
 
-    timer = Timer.scheduledTimer(timeInterval: 3,
+    timer = Timer.scheduledTimer(timeInterval: duration,
                                  target: self,
                                  selector: #selector(hide),
                                  userInfo: nil,
@@ -96,9 +87,124 @@ final class Toast: NSObject {
     timer?.invalidate()
     if self.blurView.superview != nil {
       UIView.animate(withDuration: kDefaultAnimationDuration,
-                     animations: { self.blurView.alpha = 0 }) { [self] _ in
+                     animations: { self.blurView.alpha = 0 },
+                     completion: { [self] _ in
+        self.onHideCompletion?()
         self.blurView.removeFromSuperview()
-        Self.toasts.removeAll(where: { $0 === self }) }
+        Self.toasts.removeAll(where: { $0 === self })
+      })
     }
+  }
+}
+
+extension Toast {
+  @objc static func toast(withText text: String) -> Toast {
+    toast(withText: text, onHideCompletion: nil)
+  }
+
+  @objc static func toast(withText text: String, onHideCompletion: (() -> Void)? = nil) -> Toast {
+    DefaultToast(text, onHideCompletion: onHideCompletion)
+  }
+
+  @objc static func undoToast(withText text: String, undoAction: @escaping () -> Void, onHideCompletion: (() -> Void)?) -> Toast {
+    UndoToast(text, undoAction: undoAction, onHideCompletion: onHideCompletion)
+  }
+
+  @objc static func undoToast(deletedObject: String, undoAction: @escaping () -> Void, onHideCompletion: (() -> Void)? = nil) -> Toast {
+    // TODO: localize text
+    undoToast(withText: "The \(deletedObject) was deleted", undoAction: undoAction, onHideCompletion: onHideCompletion)
+  }
+}
+
+private final class DefaultToast: Toast {
+
+  private let messageLabel = UILabel()
+
+  fileprivate convenience init(_ text: String, onHideCompletion: (() -> Void)?) {
+    self.init()
+    self.onHideCompletion = onHideCompletion
+    setupMessageLabel(text)
+    layoutViews()
+  }
+
+  private func setupMessageLabel(_ text: String) {
+    messageLabel.text = text
+    messageLabel.textAlignment = .center
+    messageLabel.numberOfLines = 0
+    messageLabel.font = .regular14()
+    messageLabel.textColor = .white
+    messageLabel.isUserInteractionEnabled = false
+  }
+
+  private func layoutViews() {
+    contentView.addSubview(messageLabel)
+    
+    messageLabel.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      messageLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+      messageLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+      messageLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+      messageLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8)
+    ])
+  }
+}
+
+private final class UndoToast: Toast {
+
+  private let messageLabel = UILabel()
+  private let undoButton = UIButton()
+  private var undoAction: (() -> Void)?
+
+  fileprivate convenience init(_ text: String, undoAction: @escaping () -> Void, onHideCompletion: (() -> Void)?) {
+    self.init()
+    self.undoAction = undoAction
+    self.onHideCompletion = onHideCompletion
+    setupMessageLabel(text)
+    setupUndoButton()
+    layoutViews()
+  }
+
+  private func setupMessageLabel(_ text: String) {
+    messageLabel.text = text
+    messageLabel.textAlignment = .center
+    messageLabel.numberOfLines = 0
+    messageLabel.font = .regular14()
+    messageLabel.textColor = .white
+    messageLabel.isUserInteractionEnabled = false
+  }
+
+  private func setupUndoButton() {
+    undoButton.setTitle(L("undo"), for: .normal)
+    undoButton.setTitleColor(.white, for: .normal)
+    undoButton.setTitleColor(.lightGray, for: .highlighted)
+    undoButton.titleLabel?.font = .bold17()
+    undoButton.backgroundColor = .clear
+    undoButton.addTarget(self, action: #selector(undoButtonDidTap), for: .touchUpInside)
+  }
+
+  @objc private func undoButtonDidTap() {
+    undoAction?()
+    hide()
+  }
+
+  private func layoutViews() {
+    contentView.addSubview(messageLabel)
+    contentView.addSubview(undoButton)
+
+    messageLabel.translatesAutoresizingMaskIntoConstraints = false
+    undoButton.translatesAutoresizingMaskIntoConstraints = false
+    messageLabel.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+    NSLayoutConstraint.activate([
+      contentView.heightAnchor.constraint(greaterThanOrEqualToConstant: 40),
+
+      messageLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12),
+      messageLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+      messageLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8),
+
+      undoButton.leadingAnchor.constraint(equalTo: messageLabel.trailingAnchor, constant: 20),
+      undoButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -25),
+      undoButton.centerYAnchor.constraint(equalTo: messageLabel.centerYAnchor),
+      undoButton.heightAnchor.constraint(equalToConstant: 30),
+    ])
   }
 }
