7187d247e37b8524fc50bbfcecda72b607548e7c
Kiryl Kaveryn <kirylkaveryn@gmail.com>
2024-05-24 17:57:57 +0400

    [ios] add assert on second `LoadBookmarks` call
    Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

--- map/bookmark_manager.cpp
+++ map/bookmark_manager.cpp
@@ -1951,10 +1951,11 @@ BookmarkManager::KMLDataCollectionPtr BookmarkManager::LoadBookmarks(
 void BookmarkManager::LoadBookmarks()
 {
   CHECK_THREAD_CHECKER(m_threadChecker, ());
-  ClearCategories();
+  CHECK(!m_loadBookmarksCalled, ("LoadBookmarks should be called only once."));
+  m_loadBookmarksCalled = true;
+
   LoadMetadata();
 
-  m_loadBookmarksFinished = false;
   NotifyAboutStartAsyncLoading();
   GetPlatform().RunTask(Platform::Thread::File, [this]()
   {
