214b0a21f8f210743d2c2c236657b7ba910ad33c
Alexander Borsuk <me@alex.bio>
2024-05-25 00:36:38 +0200

    [drape][gui] Removed unused size
    Signed-off-by: Alexander Borsuk <me@alex.bio>

--- drape_frontend/gui/copyright_label.hpp
+++ drape_frontend/gui/copyright_label.hpp
@@ -9,8 +9,7 @@ class CopyrightLabel : public Shape
   using TBase = Shape;
 
 public:
-  explicit CopyrightLabel(gui::Position const & position);
-  drape_ptr<ShapeRenderer> Draw(ref_ptr<dp::GraphicsContext> context, m2::PointF & size,
-                                ref_ptr<dp::TextureManager> tex) const;
+  explicit CopyrightLabel(Position const & position);
+  drape_ptr<ShapeRenderer> Draw(ref_ptr<dp::GraphicsContext> context, ref_ptr<dp::TextureManager> tex) const;
 };
 }  // namespace gui
--- drape_frontend/gui/gui_text.hpp
+++ drape_frontend/gui/gui_text.hpp
@@ -118,7 +118,6 @@ public:
                 ref_ptr<dp::TextureManager> mng);
 
   void SetText(LabelResult & result, std::string text) const;
-  m2::PointF GetAverageSize() const;
 
   using TAlphabetNode = std::pair<strings::UniChar, dp::TextureManager::GlyphRegion>;
   using TAlphabet = std::vector<TAlphabetNode>;
@@ -152,7 +151,6 @@ public:
   bool Update(ScreenBase const & screen) override;
 
   ref_ptr<MutableLabel> GetTextView() const;
-  void UpdateSize(m2::PointF const & size);
 
 protected:
   void SetContent(std::string && content);
@@ -194,7 +192,7 @@ class StaticLabelHandle : public Handle
 
 public:
   StaticLabelHandle(uint32_t id, ref_ptr<dp::TextureManager> textureManager, dp::Anchor anchor,
-                    m2::PointF const & pivot, m2::PointF const & size, TAlphabet const & alphabet);
+                    m2::PointF const & pivot, TAlphabet const & alphabet);
 
   bool Update(ScreenBase const & screen) override;
 
