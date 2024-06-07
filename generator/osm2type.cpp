3c7884a1418ebffadf1abe8bd2ed5ae22707862c
Alexander Borsuk <me@alex.bio>
2024-06-07 22:03:29 +0200

    [C++20] Fixed warning
    warning: arithmetic between enumeration type 'feature::Layer' and floating-point type 'const double' is deprecated [-Wdeprecated-enum-float-conversion]
    
    Signed-off-by: Alexander Borsuk <me@alex.bio>

--- generator/osm2type.cpp
+++ generator/osm2type.cpp
@@ -795,7 +795,7 @@ string DeterminePathGrade(OsmElement * p)
 
   string scale = p->GetTag("sac_scale");
   string visibility = p->GetTag("trail_visibility");
-  
+
   if (scale.empty() && visibility.empty())
     return {};
 
@@ -1391,7 +1391,7 @@ void GetNameAndType(OsmElement * p, FeatureBuilderParams & params,
           // atoi error value (0) should match empty layer constant.
           static_assert(feature::LAYER_EMPTY == 0);
           params.layer = atoi(v.c_str());
-          params.layer = base::Clamp(params.layer, int8_t(feature::LAYER_LOW), int8_t(feature::LAYER_HIGH));
+          params.layer = base::Clamp(params.layer, int8_t{feature::LAYER_LOW}, int8_t{feature::LAYER_HIGH});
         }
       }},
   });
