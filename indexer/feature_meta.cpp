91b2542fe0612564399f09b8530e0eb1896b5dfa
David Martinez <47610359+dvdmrtnz@users.noreply.github.com>
2024-06-22 14:40:00 +0200

    [core] Add FMD_NETWORK metadata [strings] Add "network"
    Signed-off-by: David Martinez <47610359+dvdmrtnz@users.noreply.github.com>
    
    network

--- indexer/feature_meta.cpp
+++ indexer/feature_meta.cpp
@@ -140,6 +140,8 @@ bool Metadata::TypeFromString(string_view k, Metadata::EType & outType)
     outType = Metadata::FMD_SELF_SERVICE;
   else if (k == "outdoor_seating")
     outType = Metadata::FMD_OUTDOOR_SEATING;
+  else if (k == "network")
+    outType = Metadata::FMD_NETWORK;
   else
     return false;
 
@@ -262,6 +264,7 @@ string ToString(Metadata::EType type)
   case Metadata::FMD_WEBSITE_MENU: return "website:menu";
   case Metadata::FMD_SELF_SERVICE: return "self_service";
   case Metadata::FMD_OUTDOOR_SEATING: return "outdoor_seating";
+  case Metadata::FMD_NETWORK: return "network";
   case Metadata::FMD_COUNT: CHECK(false, ("FMD_COUNT can not be used as a type."));
   };
 
