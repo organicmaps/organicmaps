d0a5911fd17731cc06168a60e581e78c812cd05b
Emin <Ohpleaseman@tutanota.com>
2024-07-31 11:06:02 +0500

    ios - remove swiftui stuff, decrease ios min to 12

---
+++ iphone/Maps/Tourism/Presentation/Auth/Screens/WelcomeViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class WelcomeViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+}
---
+++ iphone/Maps/Tourism/Presentation/Home/Screens/HomeViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class HomeViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+  
+}
