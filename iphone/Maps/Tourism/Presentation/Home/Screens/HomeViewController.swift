09d92aec73517ea04e4aff9760933afb87d3f441
Emin <Ohpleaseman@tutanota.com>
2024-08-01 17:11:45 +0500

    tried some programmatic UI

--- iphone/Maps/Tourism/Presentation/Home/Screens/HomeViewController.swift
+++ iphone/Maps/Tourism/Presentation/Home/Screens/HomeViewController.swift
@@ -2,9 +2,24 @@ import UIKit
 
 class HomeViewController: UIViewController {
   
+  private let label: UILabel = {
+    let label = UILabel()
+    label.text = "Hello, World!"
+    label.textAlignment = .center
+    label.translatesAutoresizingMaskIntoConstraints = false
+    return label
+  }()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    view.backgroundColor = .white
+    
+    view.addSubview(label)
+    
+    NSLayoutConstraint.activate([
+      label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+    ])
   }
-  
 }
