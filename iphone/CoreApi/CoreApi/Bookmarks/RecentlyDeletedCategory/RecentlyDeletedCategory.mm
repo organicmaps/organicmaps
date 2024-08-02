f3d1cc63c298b663fb6410516cf3af90f02c5587
Kiryl Kaveryn <kirylkaveryn@gmail.com>
2024-08-02 18:25:41 +0400

    [ios] add support for the `recently deleted` feature to the bookmark manager
    - add and implement @protocol RecentlyDeletedCategoriesManager
    - move the bmmanager observing method to the @protocol BookmarksObservable to make manager mokable
    - add RecentlyDeletedCategory class as a lightweight version of the `CategoryData` to pass to it the swift
    Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

---
+++ iphone/CoreApi/CoreApi/Bookmarks/RecentlyDeletedCategory/RecentlyDeletedCategory.mm
@@ -0,0 +1,35 @@
+#import "RecentlyDeletedCategory+Core.h"
+
+#include <map/bookmark_helpers.hpp>
+#include <platform/platform_ios.h>
+
+@implementation RecentlyDeletedCategory
+
+- (instancetype)initTitle:(NSString *)title fileURL:(NSURL *)fileURL deletionDate:(NSDate *)deletionDate {
+  self = [super init];
+  if (self) {
+    _title = title;
+    _fileURL = fileURL;
+    _deletionDate = deletionDate;
+  }
+  return self;
+}
+
+@end
+
+@implementation RecentlyDeletedCategory (Core)
+
+- (instancetype)initWithCategoryData:(kml::CategoryData)data filePath:(std::string const &)filePath {
+  self = [super init];
+  if (self) {
+    auto const name = GetPreferredBookmarkStr(data.m_name);
+    _title = [NSString stringWithCString:name.c_str() encoding:NSUTF8StringEncoding];
+    auto const pathString = [NSString stringWithCString:filePath.c_str() encoding:NSUTF8StringEncoding];
+    _fileURL = [NSURL fileURLWithPath:pathString];
+    NSTimeInterval creationTime = Platform::GetFileCreationTime(filePath);
+    _deletionDate = [NSDate dateWithTimeIntervalSince1970:creationTime];
+  }
+  return self;
+}
+
+@end
