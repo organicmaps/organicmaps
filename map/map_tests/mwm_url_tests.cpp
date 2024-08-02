8edf155970c5679e0910532f634efae91296cb59
Sergiy Kozyr <s.trump@gmail.com>
2024-08-02 16:27:26 +0300

    Improved tests for OSM OAuth2 URI parsing
    Signed-off-by: Sergiy Kozyr <s.trump@gmail.com>

--- map/map_tests/mwm_url_tests.cpp
+++ map/map_tests/mwm_url_tests.cpp
@@ -476,9 +476,19 @@ UNIT_TEST(AppNameTest)
 
 UNIT_TEST(OAuth2Test)
 {
-  ParsedMapApi api("om://oauth2/osm/callback?code=THE_MEGA_CODE");
-  TEST_EQUAL(api.GetRequestType(), UrlType::OAuth2, ());
-  TEST_EQUAL(api.GetOAuth2Code(), "THE_MEGA_CODE", ());
+  {
+    ParsedMapApi api("om://oauth2/osm/callback?code=THE_MEGA_CODE");
+    TEST_EQUAL(api.GetRequestType(), UrlType::OAuth2, ());
+    TEST_EQUAL(api.GetOAuth2Code(), "THE_MEGA_CODE", ());
+  }
+  {
+    ParsedMapApi api("om://oauth2/google/callback?code=THE_MEGA_CODE");
+    TEST_EQUAL(api.GetRequestType(), UrlType::Incorrect, ());
+  }
+  {
+    ParsedMapApi api("om://oauth2/osm/callback?code=");
+    TEST_EQUAL(api.GetRequestType(), UrlType::Incorrect, ());
+  }
 }
 
 namespace
