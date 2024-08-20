7b2fedb93a2a960e55a1510d05f52978d851ca2b
Sergiy Kozyr <s.trump@gmail.com>
2024-08-20 09:45:33 +0300

    Code formatting
    Signed-off-by: Sergiy Kozyr <s.trump@gmail.com>

--- android/app/src/main/java/app/organicmaps/editor/OsmLoginBottomFragment.java
+++ android/app/src/main/java/app/organicmaps/editor/OsmLoginBottomFragment.java
@@ -26,91 +26,99 @@ import app.organicmaps.util.concurrency.UiThread;
 
 public class OsmLoginBottomFragment extends BottomSheetDialogFragment
 {
-    final private OsmLoginFragment parentFragment;
-    private TextInputEditText mLoginInput;
-    private TextInputEditText mPasswordInput;
-    private Button mLoginButton;
-    private Button mLostPasswordButton;
-    private Button mRegisterButton;
-    private ProgressBar mProgress;
+  final private OsmLoginFragment parentFragment;
+  private TextInputEditText mLoginInput;
+  private TextInputEditText mPasswordInput;
+  private Button mLoginButton;
+  private Button mLostPasswordButton;
+  private Button mRegisterButton;
+  private ProgressBar mProgress;
 
-    public OsmLoginBottomFragment(OsmLoginFragment parentFragment) {
-        super();
-        this.parentFragment = parentFragment;
-    }
+  public OsmLoginBottomFragment(OsmLoginFragment parentFragment)
+  {
+    super();
+    this.parentFragment = parentFragment;
+  }
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+  @Override
+  public void onCreate(@Nullable Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+  }
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_osm_login_bottom, container, false);
-    }
+  @Nullable
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+  {
+    return inflater.inflate(R.layout.fragment_osm_login_bottom, container, false);
+  }
 
-    @NonNull
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        BottomSheetDialog dialog = (BottomSheetDialog) super.onCreateDialog(savedInstanceState);
-        dialog.getBehavior().setState(BottomSheetBehavior.STATE_EXPANDED);
-        return dialog;
-    }
+  @NonNull
+  @Override
+  public Dialog onCreateDialog(Bundle savedInstanceState)
+  {
+    BottomSheetDialog dialog = (BottomSheetDialog) super.onCreateDialog(savedInstanceState);
+    dialog.getBehavior().setState(BottomSheetBehavior.STATE_EXPANDED);
+    return dialog;
+  }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        mLoginInput = view.findViewById(R.id.osm_username);
-        mPasswordInput = view.findViewById(R.id.osm_password);
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+  {
+    mLoginInput = view.findViewById(R.id.osm_username);
+    mPasswordInput = view.findViewById(R.id.osm_password);
 
-        mLoginButton = view.findViewById(R.id.login);
-        mLoginButton.setOnClickListener((v) -> doLogin());
+    mLoginButton = view.findViewById(R.id.login);
+    mLoginButton.setOnClickListener((v) -> doLogin());
 
-        mLostPasswordButton = view.findViewById(R.id.lost_password);
-        mLostPasswordButton.setOnClickListener((v) -> Utils.openUrl(requireActivity(), Constants.Url.OSM_RECOVER_PASSWORD));
-        mRegisterButton = view.findViewById(R.id.register);
-        mRegisterButton.setOnClickListener((v) -> Utils.openUrl(requireActivity(), Constants.Url.OSM_REGISTER));
-        mProgress = view.findViewById(R.id.osm_login_progress);
-    }
+    mLostPasswordButton = view.findViewById(R.id.lost_password);
+    mLostPasswordButton.setOnClickListener((v) -> Utils.openUrl(requireActivity(), Constants.Url.OSM_RECOVER_PASSWORD));
+    mRegisterButton = view.findViewById(R.id.register);
+    mRegisterButton.setOnClickListener((v) -> Utils.openUrl(requireActivity(), Constants.Url.OSM_REGISTER));
+    mProgress = view.findViewById(R.id.osm_login_progress);
+  }
 
-    private void doLogin() {
-        InputUtils.hideKeyboard(mLoginInput);
-        final String username = mLoginInput.getText().toString().trim();
-        final String password = mPasswordInput.getText().toString();
-        enableInput(false);
-        UiUtils.show(mProgress);
-        mLoginButton.setText("");
+  private void doLogin()
+  {
+    InputUtils.hideKeyboard(mLoginInput);
+    final String username = mLoginInput.getText().toString().trim();
+    final String password = mPasswordInput.getText().toString();
+    enableInput(false);
+    UiUtils.show(mProgress);
+    mLoginButton.setText("");
 
-        ThreadPool.getWorker().execute(() -> {
-          final String oauthToken = OsmOAuth.nativeAuthWithPassword(username, password);
-          final String username1 = (oauthToken == null) ? null : OsmOAuth.nativeGetOsmUsername(oauthToken);
-          UiThread.run(() -> processAuth(oauthToken, username1));
-        });
-    }
-
-    private void processAuth(String oauthToken, String username)
+    ThreadPool.getWorker().execute(() ->
     {
-        if (!isAdded())
-            return;
+      final String oauthToken = OsmOAuth.nativeAuthWithPassword(username, password);
+      final String username1 = (oauthToken == null) ? null : OsmOAuth.nativeGetOsmUsername(oauthToken);
+      UiThread.run(() -> processAuth(oauthToken, username1));
+    });
+  }
 
-        enableInput(true);
-        UiUtils.hide(mProgress);
-        mLoginButton.setText(R.string.login_osm);
+  private void processAuth(String oauthToken, String username)
+  {
+    if (!isAdded())
+      return;
 
-        if (oauthToken == null)
-            parentFragment.onAuthFail();
-        else {
-            this.dismiss();
-            parentFragment.onAuthSuccess(oauthToken, username);
-        }
-    }
+    enableInput(true);
+    UiUtils.hide(mProgress);
+    mLoginButton.setText(R.string.login_osm);
 
-    private void enableInput(boolean enable)
+    if (oauthToken == null)
+      parentFragment.onAuthFail();
+    else
     {
-        mPasswordInput.setEnabled(enable);
-        mLoginInput.setEnabled(enable);
-        mLoginButton.setEnabled(enable);
-        mLostPasswordButton.setEnabled(enable);
-        mRegisterButton.setEnabled(enable);
+      this.dismiss();
+      parentFragment.onAuthSuccess(oauthToken, username);
     }
+  }
+
+  private void enableInput(boolean enable)
+  {
+    mPasswordInput.setEnabled(enable);
+    mLoginInput.setEnabled(enable);
+    mLoginButton.setEnabled(enable);
+    mLostPasswordButton.setEnabled(enable);
+    mRegisterButton.setEnabled(enable);
+  }
 }
