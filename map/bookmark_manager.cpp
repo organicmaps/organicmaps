7741ec4f12f23f64b23ee482ea16a8ec5e6bdc96
Kiryl Kaveryn <kirylkaveryn@gmail.com>
2024-08-25 21:05:44 +0400

    [gps] reserve space for the recorded track points and timestamps
    Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

--- map/bookmark_manager.cpp
+++ map/bookmark_manager.cpp
@@ -1148,10 +1148,15 @@ void BookmarkManager::SaveTrackRecording(std::string trackName)
   auto const & tracker = GpsTracker::Instance();
   CHECK(!tracker.IsEmpty(), ("Track recording should be not be empty"));
 
+  auto const trackSize = tracker.GetTrackSize();
   kml::MultiGeometry::LineT line;
-  tracker.ForEachTrackPoint([&line](location::GpsInfo const & pt, size_t id)->bool
+  kml::MultiGeometry::TimeT timestamps;
+  line.reserve(trackSize);
+  timestamps.reserve(trackSize);
+
+  tracker.ForEachTrackPoint([&line, &timestamps](location::GpsInfo const & pt, size_t id)->bool
   {
-    line.emplace_back(mercator::FromLatLon(pt.m_latitude, pt.m_longitude));
+    line.emplace_back(mercator::FromLatLon(pt.m_latitude, pt.m_longitude), pt.m_altitude);
     return true;
   });
 
