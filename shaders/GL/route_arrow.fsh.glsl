76c9df706a90f6f0a9459757e71f03a0dee955df
Dominik Piasecki <dominikpiasecki2002@gmail.com>
2024-10-18 23:13:13 +0200

    [shaders] Make route arrows outline white
    Signed-off-by: Dominik Piasecki <dominikpiasecki2002@gmail.com>

--- shaders/GL/route_arrow.fsh.glsl
+++ shaders/GL/route_arrow.fsh.glsl
@@ -11,7 +11,10 @@ varying vec2 v_colorTexCoords;
 void main()
 {
   vec4 finalColor = texture2D(u_colorTex, v_colorTexCoords);
-  finalColor=vec4(vec3(u_maskColor.rgb * finalColor.rgb), finalColor.a);
+  if (finalColor.r == 1.0 && finalColor.g == 1.0 && finalColor.b == 1.0)
+    finalColor=vec4(vec3(u_maskColor.rgb * finalColor.rgb), finalColor.a);
+  else
+    finalColor=vec4(1.0,1.0,1.0,finalColor.a);
   if (finalColor.a < 0.01)
     discard;
   finalColor = finalColor * u_opacity;
