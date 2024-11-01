32a1945100cf697e915d2d9f0ce358e6b0166674
Kiryl Kaveryn <kirylkaveryn@gmail.com>
2024-11-01 17:27:19 +0400

    [ios] add `AltitudeFormatter`
    Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

---
+++ iphone/CoreApi/CoreApi/Formatting/AltitudeFormatter.mm
@@ -0,0 +1,12 @@
+#import "AltitudeFormatter.h"
+
+#include "platform/distance.hpp"
+
+@implementation AltitudeFormatter
+
++ (NSString *)altitudeStringFromMeters:(double)meters {
+  auto const altitude = platform::Distance::FormatAltitude(meters);
+  return [NSString stringWithUTF8String:altitude.c_str()];
+}
+
+@end
