e612ac08b20f9720885699b672682944a1277fe6
Kiryl Kaveryn <kirylkaveryn@gmail.com>
2024-11-01 17:20:37 +0400

    [ios] add `DistanceFormatter`
    Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

---
+++ iphone/CoreApi/CoreApi/Formatting/DistanceFormatter.mm
@@ -0,0 +1,12 @@
+#import "DistanceFormatter.h"
+
+#include "platform/distance.hpp"
+
+@implementation DistanceFormatter
+
++ (NSString *)distanceStringFromMeters:(double)meters {
+  auto const coreDistance = platform::Distance::CreateFormatted(meters);
+  return [NSString stringWithUTF8String:coreDistance.ToString().c_str()];
+}
+
+@end
