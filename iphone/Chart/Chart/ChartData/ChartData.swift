df2674abc4c559f43f1da4b4e227a75b15b23407
Kiryl Kaveryn <kirylkaveryn@gmail.com>
2024-11-05 15:32:12 +0400

    [ios] refactor Chart framework
    1. Fixed big issue when the track point's array indexes are used for the xAxis values instead of the actual distances. Old approach cause wrong elevation profile shape and wrong slope ratio.
    2. Track start/end clipper (chartPreviewView) set to hidden because it overload the screen
    3. Fix code formatting, naming and style
    4. Added TapGesture to handle single taps on the chart
    
    Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

--- iphone/Chart/Chart/ChartData/ChartData.swift
+++ iphone/Chart/Chart/ChartData/ChartData.swift
@@ -10,20 +10,42 @@ public enum ChartLineType: String {
   case lineArea = "lineArea"
 }
 
-public protocol IFormatter {
-  func distanceString(from value: Double) -> String
-  func altitudeString(from value: Double) -> String
+public protocol ChartFormatter {
+  func xAxisString(from value: Double) -> String
+  func yAxisString(from value: Double) -> String
+  func yAxisLowerBound(from value: CGFloat) -> CGFloat
+  func yAxisUpperBound(from value: CGFloat) -> CGFloat
+  func yAxisSteps(lowerBound: CGFloat, upperBound: CGFloat) -> [CGFloat]
 }
 
-public protocol IChartData {
+public protocol ChartData {
   var xAxisValues: [Double] { get }
-  var lines: [IChartLine] { get }
+  var lines: [ChartLine] { get }
   var type: ChartType { get }
 }
 
-public protocol IChartLine {
-  var values: [Int] { get }
+public protocol ChartLine {
+  var values: [ChartValue] { get }
   var name: String { get }
   var color: UIColor { get }
   var type: ChartLineType { get }
+}
+
+public struct ChartValue {
+  let x: CGFloat
+  let y: CGFloat
+
+  public init(xValues: CGFloat, y: CGFloat) {
+    self.x = xValues
+    self.y = y
+  }
+}
+
+extension Array where Element == ChartValue {
+  var maxDistance: CGFloat { return map { $0.x }.max() ?? 0 }
+
+  func altitude(at relativeDistance: CGFloat) -> CGFloat {
+    guard let distance = last?.x else { return 0 }
+    return first { $0.x >= distance * relativeDistance }?.y ?? 0
+  }
 }
