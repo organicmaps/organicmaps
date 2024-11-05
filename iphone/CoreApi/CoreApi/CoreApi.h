39d1c4d08b99ce1f076ff43399edd681475fad22
Kiryl Kaveryn <kirylkaveryn@gmail.com>
2024-11-05 14:48:23 +0400

    [ios] add PlacePageTrackData to pass the track to the place page
    Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

--- iphone/CoreApi/CoreApi/CoreApi.h
+++ iphone/CoreApi/CoreApi/CoreApi.h
@@ -38,6 +38,7 @@ FOUNDATION_EXPORT const unsigned char CoreApiVersionString[];
 #import <CoreApi/IOpeningHoursLocalization.h>
 #import <CoreApi/OpeningHours.h>
 #import <CoreApi/PlacePageBookmarkData.h>
+#import <CoreApi/PlacePageTrackData.h>
 #import <CoreApi/PlacePageButtonsData.h>
 #import <CoreApi/PlacePageData.h>
 #import <CoreApi/PlacePageInfoData.h>
