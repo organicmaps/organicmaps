omapsapp/omapsapp

[ios] fix UIView+AddSeparator to support the top and bottom positions
Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

iphone/Maps/Categories/UIView+AddSeparator.swift
@@ -1,5 +1,11 @@
 extension UIView {
-  func addSeparator(thickness: CGFloat = 1.0,
+  enum SeparatorPosition {
+    case top
+    case bottom
+  }
+
+  func addSeparator(_ position: SeparatorPosition = .bottom,
+                    thickness: CGFloat = 1.0,
                     color: UIColor? = StyleManager.shared.theme?.colors.blackDividers,
                     insets: UIEdgeInsets = .zero) {
     let lineView = UIView()
@@ -7,11 +13,21 @@ extension UIView {
     lineView.isUserInteractionEnabled = false
     lineView.translatesAutoresizingMaskIntoConstraints = false
     addSubview(lineView)
-    NSLayoutConstraint.activate([
-      lineView.heightAnchor.constraint(equalToConstant: thickness),
-      lineView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: insets.left),
-      lineView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -insets.right),
-      lineView.topAnchor.constraint(equalTo: topAnchor, constant: insets.top),
-    ])
+    switch position {
+    case .top:
+      NSLayoutConstraint.activate([
+        lineView.heightAnchor.constraint(equalToConstant: thickness),
+        lineView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: insets.left),
+        lineView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -insets.right),
+        lineView.topAnchor.constraint(equalTo: topAnchor, constant: insets.top),
+      ])
+    case .bottom:
+      NSLayoutConstraint.activate([
+        lineView.heightAnchor.constraint(equalToConstant: thickness),
+        lineView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: insets.left),
+        lineView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -insets.right),
+        lineView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -insets.bottom),
+      ])
+    }
   }
 }

organicmaps/organicmaps

iphone/Maps/Categories/UIView+AddSeparator.swift
@@ -1,17 +1,28 @@
 extension UIView {
-  func addSeparator(thickness: CGFloat = 1.0,
-                    color: UIColor? = StyleManager.shared.theme?.colors.blackDividers,
+  enum SeparatorPosition {
+    case top
+    case bottom
+  }
+
+  func addSeparator(_ position: SeparatorPosition = .top,
+                    thickness: CGFloat = 1.0,
                     insets: UIEdgeInsets = .zero) {
     let lineView = UIView()
-    lineView.backgroundColor = color
+    lineView.setStyleAndApply("Divider")
     lineView.isUserInteractionEnabled = false
     lineView.translatesAutoresizingMaskIntoConstraints = false
     addSubview(lineView)
+
     NSLayoutConstraint.activate([
       lineView.heightAnchor.constraint(equalToConstant: thickness),
       lineView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: insets.left),
       lineView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -insets.right),
-      lineView.topAnchor.constraint(equalTo: topAnchor, constant: insets.top),
     ])
+    switch position {
+    case .top:
+      lineView.topAnchor.constraint(equalTo: topAnchor, constant: insets.top).isActive = true
+    case .bottom:
+      lineView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -insets.bottom).isActive = true
+    }
   }
 }