df2541e1bf12abca329becdac8de7c92f0893b03
Ferenc Géczi <ferenc.gm@gmail.com>
2024-12-16 00:00:00 +0000

    [qt] Remove defunct Traffic layer button
    Signed-off-by: Ferenc Géczi <ferenc.gm@gmail.com>

--- qt/mainwindow.cpp
+++ qt/mainwindow.cpp
@@ -282,9 +282,10 @@ void MainWindow::CreateNavigationBar()
 
     m_layers = new PopupMenuHolder(this);
 
-    m_layers->addAction(QIcon(":/navig64/traffic.png"), tr("Traffic"),
-                        std::bind(&MainWindow::OnLayerEnabled, this, LayerType::TRAFFIC), true);
-    m_layers->setChecked(LayerType::TRAFFIC, m_pDrawWidget->GetFramework().LoadTrafficEnabled());
+    /// @todo Uncomment when we will integrate a traffic provider.
+    // m_layers->addAction(QIcon(":/navig64/traffic.png"), tr("Traffic"),
+    //                     std::bind(&MainWindow::OnLayerEnabled, this, LayerType::TRAFFIC), true);
+    // m_layers->setChecked(LayerType::TRAFFIC, m_pDrawWidget->GetFramework().LoadTrafficEnabled());
 
     m_layers->addAction(QIcon(":/navig64/subway.png"), tr("Public transport"),
                         std::bind(&MainWindow::OnLayerEnabled, this, LayerType::TRANSIT), true);
@@ -875,11 +876,11 @@ void MainWindow::SetLayerEnabled(LayerType type, bool enable)
   auto & frm = m_pDrawWidget->GetFramework();
   switch (type)
   {
-  case LayerType::TRAFFIC:
-    /// @todo Uncomment when we will integrate a traffic provider.
-    // frm.GetTrafficManager().SetEnabled(enable);
-    // frm.SaveTrafficEnabled(enable);
-    break;
+  // @todo Uncomment when we will integrate a traffic provider.
+  // case LayerType::TRAFFIC:
+  //   frm.GetTrafficManager().SetEnabled(enable);
+  //   frm.SaveTrafficEnabled(enable);
+  //   break;
   case LayerType::TRANSIT:
     frm.GetTransitManager().EnableTransitSchemeMode(enable);
     frm.SaveTransitSchemeEnabled(enable);
