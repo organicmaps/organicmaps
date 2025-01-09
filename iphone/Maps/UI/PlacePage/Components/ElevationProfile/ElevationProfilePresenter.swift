294cf803b4f8e39e96ac222b884c1b342ce44466
Kiryl Kaveryn <kirylkaveryn@gmail.com>
2025-01-09 16:57:11 +0400

    [ios] refactor ElevationProfileViewController
    1. remove a stroryboard and implement VC and ElevationProfileDescriptionCell programmatically
    2. move the description collection view over the chart
    3. remove some unused code
    4. add isChartViewInfoHidden to show/hide the info view and enable/disable user interation (will be used for the track recording)
    
    Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

--- iphone/Maps/UI/PlacePage/Components/ElevationProfile/ElevationProfilePresenter.swift
+++ iphone/Maps/UI/PlacePage/Components/ElevationProfile/ElevationProfilePresenter.swift
@@ -3,6 +3,7 @@ import CoreApi
 
 protocol ElevationProfilePresenterProtocol: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
   func configure()
+  func update(trackInfo: TrackInfo, profileData: ElevationProfileData?)
 
   func onDifficultyButtonPressed()
   func onSelectedPointChanged(_ point: CGFloat)
@@ -21,13 +22,14 @@ fileprivate struct DescriptionsViewModel {
 
 final class ElevationProfilePresenter: NSObject {
   private weak var view: ElevationProfileViewProtocol?
-  private let trackInfo: TrackInfo
-  private let profileData: ElevationProfileData?
+  private var trackInfo: TrackInfo
+  private var profileData: ElevationProfileData?
   private let delegate: ElevationProfileViewControllerDelegate?
+  private let bookmarkManager: BookmarksManager = .shared()
 
   private let cellSpacing: CGFloat = 8
-  private let descriptionModels: [DescriptionsViewModel]
-  private let chartData: ElevationProfileChartData?
+  private var descriptionModels: [DescriptionsViewModel]
+  private var chartData: ElevationProfileChartData?
   private let formatter: ElevationProfileFormatter
 
   init(view: ElevationProfileViewProtocol,
@@ -36,17 +38,18 @@ final class ElevationProfilePresenter: NSObject {
        formatter: ElevationProfileFormatter = ElevationProfileFormatter(),
        delegate: ElevationProfileViewControllerDelegate?) {
     self.view = view
+    self.delegate = delegate
+    self.formatter = formatter
     self.trackInfo = trackInfo
     self.profileData = profileData
-    self.delegate = delegate
     if let profileData {
       self.chartData = ElevationProfileChartData(profileData)
-    } else {
-      self.chartData = nil
     }
-    self.formatter = formatter
+    self.descriptionModels = Self.descriptionModels(for: trackInfo)
+  }
 
-    descriptionModels = [
+  private static func descriptionModels(for trackInfo: TrackInfo) -> [DescriptionsViewModel] {
+    [
       DescriptionsViewModel(title: L("elevation_profile_ascent"), value: trackInfo.ascent, imageName: "ic_em_ascent_24"),
       DescriptionsViewModel(title: L("elevation_profile_descent"), value: trackInfo.descent, imageName: "ic_em_descent_24"),
       DescriptionsViewModel(title: L("elevation_profile_max_elevation"), value: trackInfo.maxElevation, imageName: "ic_em_max_attitude_24"),
@@ -55,41 +58,39 @@ final class ElevationProfilePresenter: NSObject {
   }
 
   deinit {
-    BookmarksManager.shared().resetElevationActivePointChanged()
-    BookmarksManager.shared().resetElevationMyPositionChanged()
+    bookmarkManager.resetElevationActivePointChanged()
+    bookmarkManager.resetElevationMyPositionChanged()
   }
 }
 
 extension ElevationProfilePresenter: ElevationProfilePresenterProtocol {
+  func update(trackInfo: TrackInfo, profileData: ElevationProfileData?) {
+    self.profileData = profileData
+    if let profileData {
+      self.chartData = ElevationProfileChartData(profileData)
+    } else {
+      self.chartData = nil
+    }
+    descriptionModels = Self.descriptionModels(for: trackInfo)
+    configure()
+  }
+
   func configure() {
-    guard let profileData, let chartData else {
+    let kMinPointsToDraw = 3
+    guard let profileData, let chartData, chartData.points.count >= kMinPointsToDraw else {
       view?.isChartViewHidden = true
-      view?.isDifficultyHidden = true
-      view?.isExtendedDifficultyLabelHidden = true
-      view?.isBottomPanelHidden = true
       return
     }
     view?.isChartViewHidden = false
+    view?.setChartData(ChartPresentationData(chartData, formatter: formatter))
+    view?.reloadDescription()
 
-    if profileData.difficulty != .disabled {
-      view?.isDifficultyHidden = false
-      view?.setDifficulty(profileData.difficulty)
-    } else {
-      view?.isDifficultyHidden = true
-    }
-
-    view?.isBottomPanelHidden = profileData.difficulty == .disabled
-    view?.isExtendedDifficultyLabelHidden = true
-
-    let presentationData = ChartPresentationData(chartData, formatter: formatter)
-    view?.setChartData(presentationData)
     view?.setActivePoint(profileData.activePoint)
     view?.setMyPosition(profileData.myPosition)
-
-    BookmarksManager.shared().setElevationActivePointChanged(profileData.trackId) { [weak self] distance in
+    bookmarkManager.setElevationActivePointChanged(profileData.trackId) { [weak self] distance in
       self?.view?.setActivePoint(distance)
     }
-    BookmarksManager.shared().setElevationMyPositionChanged(profileData.trackId) { [weak self] distance in
+    bookmarkManager.setElevationMyPositionChanged(profileData.trackId) { [weak self] distance in
       self?.view?.setMyPosition(distance)
     }
   }
@@ -114,9 +115,9 @@ extension ElevationProfilePresenter {
   }
 
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ElevationProfileDescriptionCell", for: indexPath) as! ElevationProfileDescriptionCell
+    let cell = collectionView.dequeueReusableCell(cell: ElevationProfileDescriptionCell.self, indexPath: indexPath)
     let model = descriptionModels[indexPath.row]
-    cell.configure(title: model.title, value: formatter.yAxisString(from: Double(model.value)), imageName: model.imageName)
+    cell.configure(subtitle: model.title, value: formatter.yAxisString(from: Double(model.value)), imageName: model.imageName)
     return cell
   }
 }
@@ -128,7 +129,7 @@ extension ElevationProfilePresenter {
     let width = collectionView.width
     let cellHeight = collectionView.height
     let modelsCount = CGFloat(descriptionModels.count)
-    let cellWidth = (width - cellSpacing * (modelsCount - 1)) / modelsCount
+    let cellWidth = (width - cellSpacing * (modelsCount - 1) - collectionView.contentInset.right) / modelsCount
     return CGSize(width: cellWidth, height: cellHeight)
   }
 
@@ -141,7 +142,6 @@ fileprivate struct ElevationProfileChartData {
 
   struct Line: ChartLine {
     var values: [ChartValue]
-    var name: String
     var color: UIColor
     var type: ChartLineType
   }
@@ -159,8 +159,8 @@ fileprivate struct ElevationProfileChartData {
     self.maxDistance = distances.last ?? 0
     let lineColor = StyleManager.shared.theme?.colors.chartLine ?? .blue
     let lineShadowColor = StyleManager.shared.theme?.colors.chartShadow ?? .lightGray
-    let l1 = Line(values: chartValues, name: "Altitude", color: lineColor, type: .line)
-    let l2 = Line(values: chartValues, name: "Altitude", color: lineShadowColor, type: .lineArea)
+    let l1 = Line(values: chartValues, color: lineColor, type: .line)
+    let l2 = Line(values: chartValues, color: lineShadowColor, type: .lineArea)
     chartLines = [l1, l2]
   }
 
@@ -168,7 +168,6 @@ fileprivate struct ElevationProfileChartData {
                                        _ p2: ElevationHeightPoint,
                                        at distance: Double) -> Double {
     assert(distance > p1.distance && distance < p2.distance, "distance must be between points")
-
     let d = (distance - p1.distance) / (p2.distance - p1.distance)
     return p1.altitude + round(Double(p2.altitude - p1.altitude) * d)
   }
