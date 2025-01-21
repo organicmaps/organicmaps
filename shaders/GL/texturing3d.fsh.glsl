e5e9fc69ae652a4b8e4c138c535b693c5b2c5711
renderexpert <expert@renderconsulting.co.uk>
2025-01-21 14:39:31 +0000

    Refactor shaders to use GLES3 syntax
    Signed-off-by: renderexpert <expert@renderconsulting.co.uk>

--- shaders/GL/texturing3d.fsh.glsl
+++ shaders/GL/texturing3d.fsh.glsl
@@ -1,11 +1,13 @@
 uniform sampler2D u_colorTex;
 uniform float u_opacity;
 
-varying vec2 v_colorTexCoords;
-varying float v_intensity;
+in vec2 v_colorTexCoords;
+in float v_intensity;
+
+out vec4 v_FragColor;
 
 void main()
 {
-  vec4 finalColor = vec4(texture2D(u_colorTex, v_colorTexCoords).rgb, u_opacity);
-  gl_FragColor = vec4((v_intensity * 0.2 + 0.8) * finalColor.rgb, finalColor.a);
+  vec4 finalColor = vec4(texture(u_colorTex, v_colorTexCoords).rgb, u_opacity);
+  v_FragColor = vec4((v_intensity * 0.2 + 0.8) * finalColor.rgb, finalColor.a);
 }
