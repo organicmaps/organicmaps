cb7a557aa45ff714b8bd2551135c9c7121854738
renderexpert <expert@renderconsulting.co.uk>
2025-01-24 14:30:29 +0000

    Remove Tegra support hacks
    Signed-off-by: renderexpert <expert@renderconsulting.co.uk>

--- drape/support_manager.hpp
+++ drape/support_manager.hpp
@@ -26,7 +26,6 @@ public:
   // reinitialization.
   void Init(ref_ptr<GraphicsContext> context);
 
-  bool IsTegraDevice() const { return m_isTegra; }
   bool IsAntialiasingEnabledByDefault() const { return m_isAntialiasingEnabledByDefault; }
 
   float GetMaxLineWidth() const { return m_maxLineWidth; }
@@ -48,7 +47,6 @@ private:
 
   std::string m_rendererName;
   std::string m_rendererVersion;
-  bool m_isTegra = false;
   bool m_isAntialiasingEnabledByDefault = false;
 
   float m_maxLineWidth = 1;
