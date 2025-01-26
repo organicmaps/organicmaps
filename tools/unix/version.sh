organicmaps/organicmaps

commit ee36eb8d125c9a4535964e2b50642a66b74aca9b

[ios] Use today's commits count and two numbers from commit hash in the build number
Now it looks like this:

  ios_version    2024.03.27
  ios_build      1.44211.31960

And allows deploying different versions from branches into the TestFlight without clashing

Signed-off-by: Alexander Borsuk <me@alex.bio>

tools/unix/version.sh
@@ -31,7 +31,9 @@ function ios_version {
 }
 
 function ios_build {
-  echo "$COUNT"
+  MINOR=$((16#${GIT_HASH:0:4}))
+  PATCH=$((16#${GIT_HASH:4:4}))
+  echo "$COUNT.$MINOR.$PATCH"
 }
 
 function  android_name {

commit 25a875483e2058cfa415f449ce864d7b18fc2c62

[build] Fix Android versioning
Caused by ee36eb8d1 "Use today's commits count and two numbers"

Signed-off-by: Roman Tsisyk <roman@tsisyk.com>

.github/workflows/android-release.yaml
@@ -29,7 +29,7 @@ jobs:
           version=$(tools/unix/version.sh ios_version)
           # +1 because below a "Bump versions" commit is created.
           # TODO: Find a way to refactor FDroid versioning without that additional commit.
-          build=$(($(tools/unix/version.sh ios_build) + 1))
+          build=$(($(tools/unix/version.sh count) + 1))
           code=$(($(tools/unix/version.sh android_code) + 1))
           tag=$version-$build-android
           echo "::set-output name=version::$version"

tools/unix/version.sh
@@ -36,6 +36,10 @@ function ios_build {
   echo "$COUNT.$MINOR.$PATCH"
 }
 
+function count {
+  echo "$COUNT"
+}
+
 function  android_name {
   echo "$DATE-$COUNT"
 }
@@ -79,6 +83,7 @@ Where format is one of the following arguments (shows current values):
   android_code   $(android_code)
   qt_version     $(qt_version)
   qt_int_version $(qt_int_version)
+  count          $(count)
 EOF
 }
 