d8a594855a5109cff4caff36090eba44ad3a07e4
Kiryl Kaveryn <kirylkaveryn@gmail.com>
2025-02-10 19:13:36 +0400

    [ios] fix UIDatePicker style in the Editor
    Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

--- iphone/Maps/Core/Theme/Renderers/UITableViewRenderer.swift
+++ iphone/Maps/Core/Theme/Renderers/UITableViewRenderer.swift
@@ -1,7 +1,7 @@
 extension UITableView {
   @objc override func applyTheme() {
     if styleName.isEmpty {
-      setStyle(.tabView)
+      setStyle(.tableView)
     }
     for style in StyleManager.shared.getStyle(styleName)
       where !style.isEmpty && !style.hasExclusion(view: self) {
