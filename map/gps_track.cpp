omapsapp/omapsapp

[map] return the ElevationInfo for the current track recording
Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

map/gps_track.cpp
@@ -84,6 +84,11 @@ GpsTrackInfo GpsTrack::GetTrackInfo() const
   return m_collection ? m_collection->GetTrackInfo() : GpsTrackInfo();
 }
 
+const ElevationInfo & GpsTrack::GetElevationInfo() const
+{
+  return m_collection->GetElevationInfo();
+}
+
 void GpsTrack::Clear()
 {
   {

organicmaps/organicmaps

map/gps_track.cpp
@@ -84,6 +84,11 @@ TrackStatistics GpsTrack::GetTrackStatistics() const
   return m_collection ? m_collection->GetTrackStatistics() : TrackStatistics();
 }
 
+const ElevationInfo & GpsTrack::GetElevationInfo() const
+{
+  return m_collection->UpdateAndGetElevationInfo();
+}
+
 void GpsTrack::Clear()
 {
   {