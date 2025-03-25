7c268f34eb1c619172fdc32889197bf79953e66d
Kiryl Kaveryn <kirylkaveryn@gmail.com>
2025-03-25 22:32:09 +0400

    [ios] move out UI from the MWMNavigationDashboardManager to RoutePreviewView
    The MWMNavigationDashboardManager's old impl contains both the business logic and UI.
    Now this responsibilities are separated: the MWMNavigationDashboardManager controls the previewing logic and NavigationDashboardView - UI
    
    Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

---
+++ iphone/Maps/Classes/CustomViews/NavigationDashboard/Views/RoutePreview/RoutePreviewView.h
@@ -0,0 +1,39 @@
+#import "MWMCircularProgressState.h"
+#import "MWMRouterType.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+typedef NS_ENUM(NSInteger, MWMDrivingOptionsState) {
+  MWMDrivingOptionsStateNone,
+  MWMDrivingOptionsStateDefine,
+  MWMDrivingOptionsStateChange
+};
+
+@protocol MWMRoutePreviewDelegate;
+
+@protocol RoutePreviewView <NSObject>
+
+@property(nonatomic) MWMDrivingOptionsState drivingOptionsState;
+@property(weak, nonatomic) id<MWMRoutePreviewDelegate> delegate;
+
+- (void)addToView:(UIView *)superview;
+- (void)remove;
+
+- (void)statePrepare;
+- (void)selectRouter:(MWMRouterType)routerType;
+- (void)router:(MWMRouterType)routerType setState:(MWMCircularProgressState)state;
+- (void)router:(MWMRouterType)routerType setProgress:(CGFloat)progress;
+
+@end
+
+@protocol MWMRoutePreviewDelegate <NSObject>
+
+- (void)routePreviewDidPressDrivingOptions:(id<RoutePreviewView>)routePreview;
+- (void)ttsButtonDidTap;
+- (void)settingsButtonDidTap;
+- (void)stopRoutingButtonDidTap;
+- (void)routingStartButtonDidTap;
+
+@end
+
+NS_ASSUME_NONNULL_END
