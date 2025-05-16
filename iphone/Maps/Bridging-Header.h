alessandro-pierro/worth_navigator_ios

[ios] move out UI from the MWMNavigationDashboardManager to RoutePreviewView
The MWMNavigationDashboardManager's old impl contains both the business logic and UI.
Now this responsibilities are separated: the MWMNavigationDashboardManager controls the previewing logic and NavigationDashboardView - UI

Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

iphone/Maps/Bridging-Header.h
@@ -79,3 +79,4 @@
 #import "MWMSearchSuggestionCell.h"
 #import "MWMSearch.h"
 #import "SearchResult.h"
+#import "RoutePreview.h"

organicmaps/organicmaps

iphone/Maps/Bridging-Header.h
@@ -42,6 +42,7 @@
 #import "MWMNavigationController.h"
 #import "MWMNavigationDashboardEntity.h"
 #import "MWMNavigationDashboardManager.h"
+#import "MWMNavigationDashboardView.h"
 #import "MWMNetworkPolicy+UI.h"
 #import "MWMNoMapsViewController.h"
 #import "MWMNoteCell.h"
@@ -79,3 +80,5 @@
 #import "MWMSearchSuggestionCell.h"
 #import "MWMSearch.h"
 #import "SearchResult.h"
+#import "SearchOnMapState.h"
+#import "RoutePreviewView.h"