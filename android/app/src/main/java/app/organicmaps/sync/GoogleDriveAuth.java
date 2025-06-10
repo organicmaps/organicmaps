96452ad200e9cace6918f61a182c15ee85d18207
savsch <119003089+savsch@users.noreply.github.com>
2025-06-10 13:21:35 +0530

    [android] Add Google Drive auth
    Signed-off-by: savsch <119003089+savsch@users.noreply.github.com>

--- android/app/src/main/java/app/organicmaps/sync/GoogleDriveAuth.java
+++ android/app/src/main/java/app/organicmaps/sync/GoogleDriveAuth.java
@@ -2,45 +2,69 @@ package app.organicmaps.sync;
 
 import android.content.Context;
 import androidx.annotation.Nullable;
+import app.organicmaps.R;
+import java.util.Objects;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+// {"rt": string, "em": string}
+
 public class GoogleDriveAuth extends AuthState
 {
+  private static final String KEY_REFRESH_TOKEN = "rt";
+  private static final String KEY_EMAIL = "em";
+
+  private final String mRefreshToken;
+  private final String mEmail;
+
   public GoogleDriveAuth(JSONObject authStateJson) throws JSONException
   {
     super(authStateJson);
-    throw new RuntimeException("TODO implement");
+    mRefreshToken = authStateJson.getString(KEY_REFRESH_TOKEN);
+    mEmail = authStateJson.getString(KEY_EMAIL);
   }
 
   @Override
   public JSONObject toJson() throws JSONException
   {
-    throw new RuntimeException("TODO implement");
+    JSONObject json = new JSONObject();
+    json.put(KEY_REFRESH_TOKEN, mRefreshToken);
+    json.put(KEY_EMAIL, mEmail);
+    return json;
   }
 
   @Override
   public boolean equals(AuthState other)
   {
-    throw new RuntimeException("TODO implement");
+    if (other instanceof GoogleDriveAuth)
+      return Objects.equals(mEmail, ((GoogleDriveAuth) other).mEmail);
+    return false;
   }
 
   @Override
   public String getUsername()
   {
-    throw new RuntimeException("TODO implement");
+    return mEmail;
   }
 
   @Override
   public String getBackendInfo(Context context)
   {
-    throw new RuntimeException("TODO implement");
+    return context.getString(R.string.google_drive);
   }
 
   @Nullable
   @Override
   public Long getExpiryTimestamp()
   {
-    throw new RuntimeException("TODO implement");
+    return null;
+  }
+
+  public static GoogleDriveAuth fromTokenAndEmail(String refreshToken, String email) throws JSONException
+  {
+    JSONObject json = new JSONObject();
+    json.put(KEY_REFRESH_TOKEN, refreshToken);
+    json.put(KEY_EMAIL, email);
+    return new GoogleDriveAuth(json);
   }
 }
