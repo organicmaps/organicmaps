6f3555e27193f2a01f49fa83440c41e7c7923cc8
hemanggs <hemangmanhas@gmail.com>
2025-07-06 23:25:14 +0530

    [drape] Fixes add business  button crosshair placement
    -Point crosshair to optionalPosition instead of a random point in BoundArea
    
    Signed-off-by: hemanggs <hemangmanhas@gmail.com>

--- android/app/src/main/java/app/organicmaps/widget/placepage/PlacePageView.java
+++ android/app/src/main/java/app/organicmaps/widget/placepage/PlacePageView.java
@@ -565,7 +565,7 @@ public class PlacePageView extends Fragment
 
   private void addOrganisation()
   {
-    ((MwmActivity) requireActivity()).showPositionChooserForEditor(true, false);
+    ((MwmActivity) requireActivity()).showPositionChooserForEditor(true, true);
   }
 
   private void addPlace()
