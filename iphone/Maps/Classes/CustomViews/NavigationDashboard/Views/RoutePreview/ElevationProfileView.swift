87630f074b717e75ac5d245b4d6ee623e50b9a2e
Kiryl Kaveryn <kirylkaveryn@gmail.com>
2025-07-07 13:14:41 +0400

    [ios] Remove title from the Elevation profile view
    Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

--- iphone/Maps/Classes/CustomViews/NavigationDashboard/Views/RoutePreview/ElevationProfileView.swift
+++ iphone/Maps/Classes/CustomViews/NavigationDashboard/Views/RoutePreview/ElevationProfileView.swift
@@ -1,12 +1,9 @@
 final class ElevationProfileView: UIView {
 
   private enum Constants {
-    static let titleInsets = UIEdgeInsets(top: 4, left: 0, bottom: -8, right: 0)
     static let imageHeight: CGFloat = 60
-    static let imageBottomOffset: CGFloat = -8
   }
 
-  private let elevationTitleLabel = UILabel()
   private let imageView = UIImageView()
 
   init() {
@@ -23,29 +20,20 @@ final class ElevationProfileView: UIView {
   private func setupView() {
     backgroundColor = .clear
 
-    elevationTitleLabel.text = L("elevation_profile")
-    elevationTitleLabel.setFontStyleAndApply(.regular14, color: .blackSecondary)
-
     imageView.backgroundColor = .clear
     imageView.contentMode = .scaleToFill
   }
 
   private func layout() {
-    addSubview(elevationTitleLabel)
     addSubview(imageView)
 
-    elevationTitleLabel.translatesAutoresizingMaskIntoConstraints = false
     imageView.translatesAutoresizingMaskIntoConstraints = false
 
     NSLayoutConstraint.activate([
-      elevationTitleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: Constants.titleInsets.left),
-      elevationTitleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: Constants.titleInsets.right),
-      elevationTitleLabel.topAnchor.constraint(equalTo: topAnchor, constant: Constants.titleInsets.top),
-      elevationTitleLabel.bottomAnchor.constraint(equalTo: imageView.topAnchor, constant: Constants.titleInsets.bottom),
-
       imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
       imageView.trailingAnchor.constraint(equalTo: trailingAnchor),
-      imageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: Constants.imageBottomOffset),
+      imageView.topAnchor.constraint(equalTo: topAnchor),
+      imageView.bottomAnchor.constraint(equalTo: bottomAnchor),
       imageView.heightAnchor.constraint(equalToConstant: Constants.imageHeight)
     ])
   }
