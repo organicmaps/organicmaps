3381212b365a76c95d4f82cc825ac9dbad563f62
Kiryl Kaveryn <kirylkaveryn@gmail.com>
2025-07-09 17:30:40 +0400

    [ios] [map] Show `Add Place` for business
    Closes https://github.com/organicmaps/organicmaps/issues/10849
    
    Related PRs: https://github.com/organicmaps/organicmaps/pull/9002 and https://github.com/organicmaps/organicmaps/pull/8158
    
    Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

--- iphone/CoreApi/CoreApi/PlacePageData/Common/PlacePageButtonsData.mm
+++ iphone/CoreApi/CoreApi/PlacePageData/Common/PlacePageButtonsData.mm
@@ -9,7 +9,7 @@
 - (instancetype)initWithRawData:(place_page::Info const &)rawData {
   self = [super init];
   if (self) {
-    _showAddPlace = rawData.ShouldShowAddPlace();
+    _showAddPlace = rawData.ShouldShowAddPlace() || rawData.ShouldShowAddBusiness();
     _showEditPlace = rawData.ShouldShowEditPlace();
     _enableAddPlace = rawData.ShouldEnableAddPlace();
     _enableEditPlace = rawData.ShouldEnableEditPlace();
