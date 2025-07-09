6fe19399720fa38acccaab0f31febaa6be8b0c6f
S. Kozyr <s.trump@gmail.com>
2025-07-09 17:29:14 +0300

    Changed priority of Gradle repos
    Signed-off-by: S. Kozyr <s.trump@gmail.com>

--- android/settings.gradle
+++ android/settings.gradle
@@ -6,7 +6,7 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
