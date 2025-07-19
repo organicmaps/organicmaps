770fd092767a4a55902a3dae5128bb04157f00f2
David Martinez <47610359+dvdmrtnz@users.noreply.github.com>
2025-07-19 16:54:06 +0200

    [styles] Add fast_food search result icons & bookmark icons
    Signed-off-by: David Martinez <47610359+dvdmrtnz@users.noreply.github.com>

--- map/bookmark.cpp
+++ map/bookmark.cpp
@@ -51,6 +51,7 @@ std::string GetBookmarkIconType(kml::BookmarkIcon const & icon)
   case kml::BookmarkIcon::BicycleParking: return "bicycle_parking";
   case kml::BookmarkIcon::BicycleParkingCovered: return "bicycle_parking_covered";
   case kml::BookmarkIcon::BicycleRental: return "bicycle_rental";
+  case kml::BookmarkIcon::FastFood: return "fast_food";
   case kml::BookmarkIcon::Count:
     ASSERT(false, ("Invalid bookmark icon type"));
     return {};
