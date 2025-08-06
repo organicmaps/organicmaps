d1baf45af6098e4a72d1f3a92f61507ecb40284b
savsch <119003089+savsch@users.noreply.github.com>
2025-08-06 14:51:11 +0530

    [android] Support (de)serializing SyncOpException
    Signed-off-by: savsch <119003089+savsch@users.noreply.github.com>

--- android/app/src/main/java/app/organicmaps/sync/SyncOpException.java
+++ android/app/src/main/java/app/organicmaps/sync/SyncOpException.java
@@ -1,25 +1,90 @@
 package app.organicmaps.sync;
 
+import app.organicmaps.sdk.util.log.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
 public sealed abstract class SyncOpException extends Exception permits SyncOpException.NetworkException,
                                                      SyncOpException.AuthExpiredException,
                                                      // TODO(savsch) add storage quota exceeded UI and exception type
                                                      SyncOpException.UnexpectedException
 {
+  private static final String TAG = SyncOpException.class.getSimpleName();
+  private static final String JSON_KEY_TYPE = "typ";
+  private static final String JSON_KEY_TIMESTAMP = "tim";
+  private static final String JSON_KEY_MESSAGE = "msg";
+
+  private final long timestamp; // milliseconds
+
   protected SyncOpException()
   {
-    super();
+    this(System.currentTimeMillis());
   }
 
   protected SyncOpException(String message)
   {
-    super(message);
+    this(message, System.currentTimeMillis());
+  }
+
+  protected SyncOpException(long timestamp)
+  {
+    super();
+    this.timestamp = timestamp;
+  }
+
+  protected SyncOpException(String message, long timestamp)
+  {
+    super(truncateIfNeeded(message));
+    this.timestamp = timestamp;
+  }
+
+  /// Serialize to JSON
+  public JSONObject toJson() throws JSONException
+  {
+    JSONObject json = new JSONObject();
+    json.put(JSON_KEY_TYPE, getClass().getSimpleName());
+    json.put(JSON_KEY_TIMESTAMP, timestamp);
+    json.put(JSON_KEY_MESSAGE, getMessage());
+    return json;
+  }
+
+  public static SyncOpException fromJson(JSONObject json) throws JSONException
+  {
+    int typeId = json.getInt(JSON_KEY_TYPE);
+    long timestamp = json.getLong(JSON_KEY_TIMESTAMP);
+    if (typeId == Types.NETWORK.typeId)
+      return new NetworkException(timestamp);
+    else if (typeId == Types.AUTH_EXPIRED.typeId)
+      return new AuthExpiredException(timestamp);
+    else if (typeId == Types.UNEXPECTED.typeId)
+    {
+      String message = json.optString(JSON_KEY_MESSAGE, "");
+      return message.isEmpty() ? new UnexpectedException(timestamp) : new UnexpectedException(message, timestamp);
+    }
+    else
+    {
+      // Should be impossible
+      Logger.e(TAG, "Unexpected serialized exception type: " + typeId);
+      return new UnexpectedException();
+    }
   }
 
   static final class NetworkException extends SyncOpException
-  {}
+  {
+    public NetworkException() {}
+    private NetworkException(long timestamp)
+    {
+      super(timestamp);
+    }
+  }
 
   static final class AuthExpiredException extends SyncOpException
-  {}
+  {
+    public AuthExpiredException() {}
+    private AuthExpiredException(long timestamp)
+    {
+      super(timestamp);
+    }
+  }
 
   static final class UnexpectedException extends SyncOpException
   {
@@ -28,5 +93,34 @@ public sealed abstract class SyncOpException extends Exception permits SyncOpExc
       super(message);
     }
     public UnexpectedException() {}
+    private UnexpectedException(String message, long timestamp)
+    {
+      super(message, timestamp);
+    }
+    private UnexpectedException(long timestamp)
+    {
+      super(timestamp);
+    }
+  }
+
+  private static String truncateIfNeeded(String message)
+  {
+    if (message == null)
+      return null;
+    if (message.length() <= 200)
+      return message;
+    return message.substring(0, 200) + "...";
+  }
+
+  private enum Types
+  {
+    NETWORK(0),
+    AUTH_EXPIRED(1),
+    UNEXPECTED(1000);
+    final int typeId; // persisted to disk, so must be the same across releases
+    Types(int typeId)
+    {
+      this.typeId = typeId;
+    }
   }
 }
