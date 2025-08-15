f19678b2c47ea7d91925cd611e5d12827b31ab07
Andrei Shkrob <github@shkrob.dev>
2025-08-16 00:22:42 +0200

    [base] stl_helpers cleanup
    Signed-off-by: Andrei Shkrob <github@shkrob.dev>

--- libs/base/base_tests/stl_helpers_tests.cpp
+++ libs/base/base_tests/stl_helpers_tests.cpp
@@ -126,30 +126,6 @@ UNIT_TEST(SortUnique_VectorTest)
   TestSortUnique<std::deque>();
 }
 
-UNIT_TEST(IgnoreFirstArgument)
-{
-  {
-    int s = 0;
-    auto f1 = [&](int a, int b) { s += a + b; };
-    auto f2 = [&](int a, int b) { s -= a + b; };
-    auto f3 = MakeIgnoreFirstArgument(f2);
-
-    f1(2, 3);
-    TEST_EQUAL(s, 5, ());
-    f3(1, 2, 3);
-    TEST_EQUAL(s, 0, ());
-  }
-
-  {
-    auto f1 = [](int a, int b) -> int { return a + b; };
-    auto f2 = MakeIgnoreFirstArgument(f1);
-
-    auto const x = f1(2, 3);
-    auto const y = f2("ignored", 2, 3);
-    TEST_EQUAL(x, y, ());
-  }
-}
-
 namespace
 {
 struct EqualZero
@@ -215,105 +191,6 @@ UNIT_TEST(RemoveIfKeepValid)
     v.push_back(1);
     CheckNoZero(v, RemoveIfKeepValid(v.begin(), v.end(), EqualZero()));
     TEST_EQUAL(v.size(), 4, ());
-  }
-}
-
-namespace
-{
-template <class T, size_t N1, size_t N2, size_t N3>
-void CheckAccumulateIntervals(size_t & idTest, std::pair<T, T> (&arr1)[N1], std::pair<T, T> (&arr2)[N2],
-                              std::pair<T, T> (&arr3)[N3])
-{
-  std::vector<std::pair<T, T>> res;
-  AccumulateIntervals1With2(arr1, arr1 + N1, arr2, arr2 + N2, back_inserter(res));
-
-  ++idTest;
-  TEST_EQUAL(N3, res.size(), ("Test", idTest, res));
-  TEST(equal(res.begin(), res.end(), arr3), ("Test", idTest, res));
-}
-}  // namespace
-
-UNIT_TEST(AccumulateIntervals)
-{
-  typedef std::pair<int, int> T;
-  size_t idTest = 0;
-
-  // bound cases
-  {
-    std::vector<T> res;
-    T arr[] = {T(10, 20)};
-
-    res.clear();
-    AccumulateIntervals1With2(arr, arr + 1, arr, arr, back_inserter(res));
-    TEST_EQUAL(res.size(), 1, ());
-
-    res.clear();
-    AccumulateIntervals1With2(arr, arr, arr, arr + 1, back_inserter(res));
-    TEST_EQUAL(res.size(), 0, ());
-  }
-
-  // check splice overlapped
-  {
-    T arr1[] = {T(10, 20), T(30, 40)};
-    T arr2[] = {T(19, 31)};
-    T res[] = {T(10, 40)};
-    CheckAccumulateIntervals(idTest, arr1, arr2, res);
-  }
-
-  // check skip not overlapped
-  {
-    T arr1[] = {T(10, 20), T(30, 40)};
-    T arr2[] = {T(0, 9), T(21, 29), T(41, 50)};
-    T res[2] = {T(10, 20), T(30, 40)};
-    CheckAccumulateIntervals(idTest, arr1, arr2, res);
-  }
-  {
-    T arr1[] = {T(10, 20), T(30, 40)};
-    T arr2[] = {T(1, 2), T(3, 4), T(5, 6), T(21, 22), T(23, 24), T(25, 26), T(41, 42), T(43, 44), T(45, 46)};
-    T res[] = {T(10, 20), T(30, 40)};
-    CheckAccumulateIntervals(idTest, arr1, arr2, res);
-  }
-
-  // check equal bounds
-  {
-    T arr1[] = {T(20, 30)};
-    T arr2[] = {T(10, 20), T(30, 40)};
-    T res[] = {T(20, 30)};
-    CheckAccumulateIntervals(idTest, arr1, arr2, res);
-  }
-  {
-    T arr1[] = {T(10, 20), T(30, 40)};
-    T arr2[] = {T(20, 30)};
-    T res[] = {T(10, 20), T(30, 40)};
-    CheckAccumulateIntervals(idTest, arr1, arr2, res);
-  }
-
-  // check large overlap interval
-  {
-    T arr1[] = {T(10, 20), T(30, 40), T(50, 60)};
-    T arr2[] = {T(0, 100)};
-    T res[] = {T(0, 100)};
-    CheckAccumulateIntervals(idTest, arr1, arr2, res);
-  }
-  {
-    T arr1[] = {T(0, 100)};
-    T arr2[] = {T(10, 20), T(30, 40), T(50, 60)};
-    T res[] = {T(0, 100)};
-    CheckAccumulateIntervals(idTest, arr1, arr2, res);
-  }
-
-  // check splice overlapped
-  {
-    T arr1[] = {T(10, 20), T(30, 40)};
-    T arr2[] = {T(5, 15), T(35, 45)};
-    T res[] = {T(5, 20), T(30, 45)};
-    CheckAccumulateIntervals(idTest, arr1, arr2, res);
-  }
-  {
-    T arr1[] = {T(10, 20), T(30, 40)};
-    T arr2[] = {T(1, 2), T(3, 4), T(5, 15), T(17, 25), T(26, 27), T(28, 32), T(38, 45), T(46, 50)};
-    T res[] = {T(5, 25), T(28, 45)};
-    CheckAccumulateIntervals(idTest, arr1, arr2, res);
   }
 }
 
--- libs/base/stl_helpers.hpp
+++ libs/base/stl_helpers.hpp
@@ -72,23 +72,6 @@ struct Equals<false, T, C>
 
   T (C::*m_p)() const;
 };
-
-template <typename Container, typename Deletor>
-class DeleteRangeFunctor
-{
-public:
-  DeleteRangeFunctor(Container & cont, Deletor const & deletor) : m_cont(cont), m_deletor(deletor) {}
-
-  void operator()()
-  {
-    for_each(m_cont.begin(), m_cont.end(), m_deletor);
-    m_cont.clear();
-  }
-
-private:
-  Container & m_cont;
-  Deletor m_deletor;
-};
 }  // namespace impl
 
 // Sorts and removes duplicate entries from |c|.
@@ -99,51 +82,48 @@ void SortUnique(Cont & c)
   c.erase(std::unique(c.begin(), c.end()), c.end());
 }
 
-/// @name Use std::ref to pass functors into std, since all algorithm functions here get forwarding reference.
-/// @{
 template <typename Cont, typename Equals>
 void Unique(Cont & c, Equals && equals)
 {
-  c.erase(std::unique(c.begin(), c.end(), std::ref(equals)), c.end());
+  c.erase(std::unique(c.begin(), c.end(), std::forward<Equals>(equals)), c.end());
 }
 
 template <typename Cont, typename Less, typename Equals>
 void SortUnique(Cont & c, Less && less, Equals && equals)
 {
-  std::sort(c.begin(), c.end(), std::ref(less));
+  std::sort(c.begin(), c.end(), std::forward<Less>(less));
   Unique(c, equals);
 }
 
 template <typename Cont, typename Fn>
 void EraseIf(Cont & c, Fn && fn)
 {
-  c.erase(std::remove_if(c.begin(), c.end(), std::ref(fn)), c.end());
+  c.erase(std::remove_if(c.begin(), c.end(), std::forward<Fn>(fn)), c.end());
 }
 
 template <typename Cont, typename Fn>
 bool AllOf(Cont const & c, Fn && fn)
 {
-  return std::all_of(std::cbegin(c), std::cend(c), std::ref(fn));
+  return std::all_of(std::cbegin(c), std::cend(c), std::forward<Fn>(fn));
 }
 
 template <typename Cont, typename Fn>
 bool AnyOf(Cont const & c, Fn && fn)
 {
-  return std::any_of(std::cbegin(c), std::cend(c), std::ref(fn));
+  return std::any_of(std::cbegin(c), std::cend(c), std::forward<Fn>(fn));
 }
 
 template <typename Cont, typename OutIt, typename Fn>
 decltype(auto) Transform(Cont const & c, OutIt it, Fn && fn)
 {
-  return std::transform(std::cbegin(c), std::cend(c), it, std::ref(fn));
+  return std::transform(std::cbegin(c), std::cend(c), it, std::forward<Fn>(fn));
 }
 
 template <typename Cont, typename Fn>
 decltype(auto) FindIf(Cont const & c, Fn && fn)
 {
-  return std::find_if(std::cbegin(c), std::cend(c), std::ref(fn));
+  return std::find_if(std::cbegin(c), std::cend(c), std::forward<Fn>(fn));
 }
-/// @}
 
 template <typename Cont, typename T>
 bool IsExist(Cont const & c, T const & t)
@@ -206,54 +186,6 @@ std::underlying_type_t<T> constexpr E2I(T value)
   return Underlying(value);
 }
 
-// Use this if you want to make a functor whose first
-// argument is ignored and the rest are forwarded to |fn|.
-template <typename Fn>
-class IgnoreFirstArgument
-{
-public:
-  template <typename Gn>
-  explicit IgnoreFirstArgument(Gn && gn) : m_fn(std::forward<Gn>(gn))
-  {}
-
-  template <typename Arg, typename... Args>
-  std::invoke_result_t<Fn, Args &&...> operator()(Arg &&, Args &&... args)
-  {
-    return m_fn(std::forward<Args>(args)...);
-  }
-
-private:
-  Fn m_fn;
-};
-
-template <typename Fn>
-IgnoreFirstArgument<Fn> MakeIgnoreFirstArgument(Fn && fn)
-{
-  return IgnoreFirstArgument<Fn>(std::forward<Fn>(fn));
-}
-
-template <size_t I = 0, typename Fn, typename... Tp>
-std::enable_if_t<I == sizeof...(Tp), void> for_each_in_tuple(std::tuple<Tp...> &, Fn &&)
-{}
-
-template <size_t I = 0, typename Fn, typename... Tp>
-std::enable_if_t<I != sizeof...(Tp), void> for_each_in_tuple(std::tuple<Tp...> & t, Fn && fn)
-{
-  fn(I, std::get<I>(t));
-  for_each_in_tuple<I + 1, Fn, Tp...>(t, std::forward<Fn>(fn));
-}
-
-template <size_t I = 0, typename Fn, typename... Tp>
-std::enable_if_t<I == sizeof...(Tp), void> for_each_in_tuple_const(std::tuple<Tp...> const &, Fn &&)
-{}
-
-template <size_t I = 0, typename Fn, typename... Tp>
-std::enable_if_t<I != sizeof...(Tp), void> for_each_in_tuple_const(std::tuple<Tp...> const & t, Fn && fn)
-{
-  fn(I, std::get<I>(t));
-  for_each_in_tuple_const<I + 1, Fn, Tp...>(t, std::forward<Fn>(fn));
-}
-
 template <typename Container>
 class BackInsertFunctor
 {
@@ -307,7 +239,7 @@ bool IsSortedAndUnique(Iter beg, Iter end, Compare comp)
 template <typename ContT>
 bool IsSortedAndUnique(ContT const & cont)
 {
-  return IsSortedAndUnique(cont.begin(), cont.end(), std::less<>());
+  return IsSortedAndUnique(cont.begin(), cont.end(), std::less{});
 }
 
 template <typename Iter, typename Compare>
@@ -343,18 +275,6 @@ struct DeleteFunctor
   }
 };
 
-template <typename Container, typename Deletor>
-impl::DeleteRangeFunctor<Container, Deletor> GetRangeDeletor(Container & cont, Deletor const & deletor)
-{
-  return impl::DeleteRangeFunctor<Container, Deletor>(cont, deletor);
-}
-
-template <typename Container, typename Deletor>
-void DeleteRange(Container & cont, Deletor const & deletor)
-{
-  (void)GetRangeDeletor(cont, deletor)();
-}
-
 struct IdFunctor
 {
   template <typename T>
@@ -387,86 +307,6 @@ Iter PrevIterInCycle(Iter it, Iter beg, Iter end)
     it = end;
   return --it;
 }
-
-template <typename Iter1, typename Iter2, typename InsertIter>
-void AccumulateIntervals1With2(Iter1 b1, Iter1 e1, Iter2 b2, Iter2 e2, InsertIter res)
-{
-  using T = typename std::iterator_traits<Iter1>::value_type;
-
-  T prev;
-  bool validPrev = false;
-
-  while (b1 != e1 || b2 != e2)
-  {
-    // Try to continue previous range.
-    if (validPrev)
-    {
-      // add b1 range to prev if needed
-      if (b1 != e1 && b1->first < prev.second)
-      {
-        // correct only second if needed
-        if (prev.second < b1->second)
-          prev.second = b1->second;
-        ++b1;
-        continue;
-      }
-
-      // add b2 range to prev if needed
-      if (b2 != e2 && b2->first < prev.second)
-      {
-        // check that intervals are overlapped
-        if (prev.first < b2->second)
-        {
-          // correct first and second if needed
-          if (b2->first < prev.first)
-            prev.first = b2->first;
-          if (prev.second < b2->second)
-            prev.second = b2->second;
-        }
-
-        ++b2;
-        continue;
-      }
-
-      // if nothing to add - push to results
-      *res++ = prev;
-      validPrev = false;
-    }
-
-    if (b1 != e1)
-    {
-      // start new range
-      prev = *b1++;
-      validPrev = true;
-    }
-    else
-    {
-      // go to exit
-      break;
-    }
-  }
-
-  if (validPrev)
-    *res++ = prev;
-}
-
-struct EnumClassHash
-{
-  template <typename T, std::enable_if_t<std::is_enum<T>::value> * = nullptr>
-  size_t operator()(T const & t) const noexcept
-  {
-    return static_cast<size_t>(t);
-  }
-};
-
-struct RetrieveFirst
-{
-  template <typename T>
-  typename T::first_type const & operator()(T const & pair) const
-  {
-    return pair.first;
-  }
-};
 
 struct RetrieveSecond
 {
