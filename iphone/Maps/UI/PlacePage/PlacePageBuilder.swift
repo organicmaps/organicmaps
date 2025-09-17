7c8aa41bac1b216bef756834d9d93047179a23c8
Kiryl Kaveryn <kirylkaveryn@gmail.com>
2025-09-17 19:22:23 +0400

    [ios] Fully reload the PP on bookmark update
    In the previous solution the PlacePage data was reloaded partially (see `updateBookmarkStatus` that triggers `updateTrackRelatedSections` and `updateBookmarkRelatedSections`) during updating.
    The full reload is clearer and easier than partial, because:
    1. require less code
    2. avoid stale pp data (the UI should not check all updated values)
    3. allows to automatically show the bm when save it from the track point
    
    The only cons is the bookmark section show/hide animation is lost.
    
    Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

--- iphone/Maps/UI/PlacePage/PlacePageBuilder.swift
+++ iphone/Maps/UI/PlacePage/PlacePageBuilder.swift
@@ -29,7 +29,7 @@
     return viewController
 	}
 
-  @objc static func update(_ viewController: PlacePageViewController, with data: PlacePageData) {
+  @objc static func update(_ viewController: PlacePageViewController, with data: PlacePageData, isNewSelection: Bool) {
     viewController.isPreviewPlus = data.isPreviewPlus
     let interactor = PlacePageInteractor(data: data)
     let layout: IPlacePageLayout
@@ -53,6 +53,6 @@
     interactor.presenter = presenter
     layout.presenter = presenter
     viewController.updateWithLayout(layout)
-    viewController.updatePreviewOffset()
+    viewController.updatePreviewOffset(reset: isNewSelection)
   }
 }
