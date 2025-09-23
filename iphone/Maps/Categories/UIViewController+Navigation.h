47bd54d9341b7e3898414f14347c12bd1676f4e7
Kiryl Kaveryn <kirylkaveryn@gmail.com>
2025-09-23 14:03:42 +0400

    [ios] Remove dead code
    Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

--- iphone/Maps/Categories/UIViewController+Navigation.h
+++ iphone/Maps/Categories/UIViewController+Navigation.h
@@ -2,7 +2,4 @@
 
 - (void)goBack;
 
-- (UIBarButtonItem *)buttonWithImage:(UIImage *)image action:(SEL)action;
-- (NSArray<UIBarButtonItem *> *)alignedNavBarButtonItems:(NSArray<UIBarButtonItem *> *)items;
-
 @end
