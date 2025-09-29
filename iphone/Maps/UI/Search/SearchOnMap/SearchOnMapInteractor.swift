organicmaps/organicmaps

commit 5e98dfb630260a6ebe8429ef8c81c36f7f9505fa

[ios] Remove unused navigation toast and related tooltip code
This code was used before route builder redesign to show the bottom toast view with search. Not used anymore.
Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

iphone/Maps/UI/Search/SearchOnMap/SearchOnMapInteractor.swift
@@ -2,17 +2,12 @@ final class SearchOnMapInteractor: NSObject {
 
   private let presenter: SearchOnMapPresenter
   private let searchManager: SearchManager.Type
-  private let routeManager: MWMRouter.Type
   private var isUpdatesDisabled = false
 
-  var routingTooltipSearch: SearchOnMapRoutingTooltipSearch = .none
-
   init(presenter: SearchOnMapPresenter,
-       searchManager: SearchManager.Type = Search.self,
-       routeManager: MWMRouter.Type = MWMRouter.self) {
+       searchManager: SearchManager.Type = Search.self) {
     self.presenter = presenter
     self.searchManager = searchManager
-    self.routeManager = routeManager
     super.init()
     searchManager.add(self)
   }
@@ -106,26 +101,6 @@ final class SearchOnMapInteractor: NSObject {
     switch result.itemType {
     case .regular:
       searchManager.save(query)
-      switch routingTooltipSearch {
-      case .none:
-        searchManager.showResult(at: result.index)
-      case .start:
-        let point = MWMRoutePoint(cgPoint: result.point,
-                                  title: result.titleText,
-                                  subtitle: result.addressText,
-                                  type: .start,
-                                  intermediateIndex: 0)
-        routeManager.build(from: point, bestRouter: false)
-      case .finish:
-        let point = MWMRoutePoint(cgPoint: result.point,
-                                  title: result.titleText,
-                                  subtitle: result.addressText,
-                                  type: .finish,
-                                  intermediateIndex: 0)
-        routeManager.build(to: point, bestRouter: false)
-      @unknown default:
-        fatalError("Unsupported routingTooltipSearch")
-      }
       return isiPad ? .none : .setSearchScreenHidden(true)
     case .suggestion:
       let suggestionQuery = SearchQuery(result.suggestion,
@@ -139,12 +114,10 @@ final class SearchOnMapInteractor: NSObject {
   }
 
   private func deselectPlaceOnMap() -> SearchOnMap.Response {
-    routingTooltipSearch = .none
     return .setSearchScreenHidden(false)
   }
 
   private func closeSearch() -> SearchOnMap.Response {
-    routingTooltipSearch = .none
     isUpdatesDisabled = true
     searchManager.clear()
     return .close

iphone/Maps/Classes/CustomViews/NavigationDashboard/Views/Navigation/MWMNavigationInfoView.h
@@ -33,8 +33,6 @@ NS_SWIFT_NAME(NavigationInfoView)
 
 - (void)onNavigationInfoUpdated:(MWMNavigationDashboardEntity *)info;
 
-- (void)updateToastView;
-
 - (void)updateSideButtonsAvailableArea:(CGRect)frame animated:(BOOL)animated;
 
 @end

iphone/Maps/Classes/CustomViews/NavigationDashboard/Views/Navigation/MWMNavigationInfoView.mm
@@ -3,7 +3,6 @@
 #import "MWMButton.h"
 #import "MWMLocationHelpers.h"
 #import "MWMLocationManager.h"
-#import "MWMLocationObserver.h"
 #import "MWMMapViewControlsCommon.h"
 #import "MWMSearch.h"
 #import "MapViewController.h"
@@ -53,7 +52,7 @@ BOOL defaultOrientation(CGSize const & size)
 }
 }  // namespace
 
-@interface MWMNavigationInfoView () <MWMLocationObserver>
+@interface MWMNavigationInfoView ()
 
 @property(weak, nonatomic) IBOutlet UIView * streetNameView;
 @property(weak, nonatomic) IBOutlet NSLayoutConstraint * streetNameTopOffsetConstraint;
@@ -82,9 +81,6 @@ BOOL defaultOrientation(CGSize const & size)
 @property(weak, nonatomic) IBOutlet MWMButton * searchFoodButton;
 @property(weak, nonatomic) IBOutlet MWMButton * searchATMButton;
 @property(weak, nonatomic) IBOutlet NSLayoutConstraint * turnsTopOffset;
-
-@property(weak, nonatomic) IBOutlet MWMNavigationAddPointToastView * toastView;
-@property(weak, nonatomic) IBOutlet NSLayoutConstraint * toastViewHideOffset;
 @property(weak, nonatomic) IBOutlet NSLayoutConstraint * searchMainButtonBottomConstraint;
 
 @property(nonatomic, readwrite) NavigationSearchState searchState;
@@ -108,50 +104,7 @@ BOOL defaultOrientation(CGSize const & size)
   [self setSearchState:NavigationSearchStateMinimizedSearch animated:YES];
 }
 
-- (void)updateToastView
-{
-  // -S-F-L -> Start
-  // -S-F+L -> Finish
-  // -S+F-L -> Start
-  // -S+F+L -> Start + Use
-  // +S-F-L -> Finish
-  // +S-F+L -> Finish
-  // +S+F-L -> Hide
-  // +S+F+L -> Hide
-
-  BOOL const hasStart = ([MWMRouter startPoint] != nil);
-  BOOL const hasFinish = ([MWMRouter finishPoint] != nil);
-  self.hasLocation = ([MWMLocationManager lastLocation] != nil);
-
-  if (hasStart && hasFinish)
-  {
-    [self setToastViewHidden:YES];
-    return;
-  }
-
-  [self setToastViewHidden:NO];
-
-  auto toastView = self.toastView;
-
-  if (hasStart)
-  {
-    [toastView configWithIsStart:NO withLocationButton:NO];
-    return;
-  }
-
-  if (hasFinish)
-  {
-    [toastView configWithIsStart:YES withLocationButton:self.hasLocation];
-    return;
-  }
-
-  if (self.hasLocation)
-    [toastView configWithIsStart:NO withLocationButton:NO];
-  else
-    [toastView configWithIsStart:YES withLocationButton:NO];
-}
-
-- (void)updateSideButtonsAvailableArea:(CGRect)frame
+- (void)updateSideButtonsAvailableArea:(CGRect)frame animated:(BOOL)animated
 {
   CGFloat const height = frame.size.height;
   if (height == 0)
@@ -161,7 +114,7 @@ BOOL defaultOrientation(CGSize const & size)
 
   self.searchMainButtonBottomConstraint.constant = bottomOffset;
   BOOL isOutOfBounds = height < kSearchButtonsViewHeightPortrait;
-  [UIView animateWithDuration:kDefaultAnimationDuration
+  [UIView animateWithDuration:animated ? 0 : kDefaultAnimationDuration
       animations:^{
         self.searchMainButton.alpha = isOutOfBounds ? 0.0 : 1.0;
         self.bookmarksButton.alpha = isOutOfBounds ? 0.0 : 1.0;
@@ -175,25 +128,6 @@ BOOL defaultOrientation(CGSize const & size)
 - (SearchOnMapManager *)searchManager
 {
   return [MapViewController sharedController].searchManager;
-}
-
-- (IBAction)openSearch
-{
-  BOOL const isStart = self.toastView.isStart;
-
-  [self.searchManager
-      setRoutingTooltip:isStart ? SearchOnMapRoutingTooltipSearchStart : SearchOnMapRoutingTooltipSearchFinish];
-  [self.searchManager startSearchingWithIsRouting:YES];
-}
-
-- (IBAction)addLocationRoutePoint
-{
-  NSAssert(![MWMRouter startPoint], @"Action button is active while start point is available");
-  NSAssert([MWMLocationManager lastLocation], @"Action button is active while my location is not available");
-
-  [MWMRouter buildFromPoint:[[MWMRoutePoint alloc] initWithLastLocationAndType:MWMRoutePointTypeStart
-                                                             intermediateIndex:0]
-                 bestRouter:NO];
 }
 
 - (IBAction)ttsButtonAction
@@ -357,15 +291,6 @@ BOOL defaultOrientation(CGSize const & size)
   [self setNeedsLayout];
 }
 
-#pragma mark - MWMLocationObserver
-
-- (void)onLocationUpdate:(CLLocation *)location
-{
-  BOOL const hasLocation = ([MWMLocationManager lastLocation] != nil);
-  if (self.hasLocation != hasLocation)
-    [self updateToastView];
-}
-
 #pragma mark - SolidTouchView
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
@@ -509,18 +434,8 @@ BOOL defaultOrientation(CGSize const & size)
   _state = state;
   switch (state)
   {
-  case MWMNavigationInfoViewStateHidden:
-    self.isVisible = NO;
-    [self setToastViewHidden:YES];
-    [MWMLocationManager removeObserver:self];
-    break;
-  case MWMNavigationInfoViewStateNavigation:
-    self.isVisible = YES;
-    if ([MWMRouter type] == MWMRouterTypePedestrian)
-      [MWMLocationManager addObserver:self];
-    else
-      [MWMLocationManager removeObserver:self];
-    break;
+  case MWMNavigationInfoViewStateHidden: self.isVisible = NO; break;
+  case MWMNavigationInfoViewStateNavigation: self.isVisible = YES; break;
   }
 }
 
@@ -562,20 +477,6 @@ BOOL defaultOrientation(CGSize const & size)
       completion:^(BOOL finished) {
         if (!isVisible)
           self.hidden = isVisible;
-      }];
-}
-
-- (void)setToastViewHidden:(BOOL)hidden
-{
-  if (!hidden)
-    self.toastView.hidden = NO;
-  [self setNeedsLayout];
-  self.toastViewHideOffset.priority = (hidden ? UILayoutPriorityDefaultHigh : UILayoutPriorityDefaultLow);
-  [UIView animateWithDuration:kDefaultAnimationDuration
-      animations:^{ [self layoutIfNeeded]; }
-      completion:^(BOOL finished) {
-        if (hidden)
-          self.toastView.hidden = YES;
       }];
 }
 

iphone/Maps/Classes/CustomViews/NavigationDashboard/Views/Navigation/MWMNavigationInfoView.xib
@@ -1,9 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<document type="com.apple.InterfaceBuilder3.CocoaTouch.XIB" version="3.0" toolsVersion="19529" targetRuntime="iOS.CocoaTouch" propertyAccessControl="none" useAutolayout="YES" useTraitCollections="YES" useSafeAreas="YES" colorMatched="YES">
+<document type="com.apple.InterfaceBuilder3.CocoaTouch.XIB" version="3.0" toolsVersion="24127" targetRuntime="iOS.CocoaTouch" propertyAccessControl="none" useAutolayout="YES" useTraitCollections="YES" useSafeAreas="YES" colorMatched="YES">
     <device id="retina6_1" orientation="portrait" appearance="light"/>
     <dependencies>
         <deployment identifier="iOS"/>
-        <plugIn identifier="com.apple.InterfaceBuilder.IBCocoaTouchPlugin" version="19519"/>
+        <plugIn identifier="com.apple.InterfaceBuilder.IBCocoaTouchPlugin" version="24063"/>
         <capability name="Safe area layout guides" minToolsVersion="9.0"/>
         <capability name="documents saved in the Xcode 8 format" minToolsVersion="8.0"/>
     </dependencies>
@@ -14,96 +14,18 @@
             <rect key="frame" x="0.0" y="0.0" width="1024" height="1366"/>
             <autoresizingMask key="autoresizingMask" widthSizable="YES" heightSizable="YES"/>
             <subviews>
-                <view opaque="NO" multipleTouchEnabled="YES" contentMode="center" translatesAutoresizingMaskIntoConstraints="NO" id="Tzc-l6-Vvt" customClass="MWMNavigationAddPointToastView">
-                    <rect key="frame" x="0.0" y="1366" width="1024" height="48"/>
-                    <subviews>
-                        <view contentMode="scaleToFill" translatesAutoresizingMaskIntoConstraints="NO" id="jmn-hv-OlG">
-                            <rect key="frame" x="-100" y="0.0" width="1224" height="100"/>
-                            <color key="backgroundColor" white="1" alpha="1" colorSpace="custom" customColorSpace="genericGamma22GrayColorSpace"/>
-                            <constraints>
-                                <constraint firstAttribute="height" constant="100" id="6em-0e-zym"/>
-                            </constraints>
-                            <userDefinedRuntimeAttributes>
-                                <userDefinedRuntimeAttribute type="string" keyPath="styleName" value="Background"/>
-                            </userDefinedRuntimeAttributes>
-                        </view>
-                        <button opaque="NO" contentMode="scaleToFill" horizontalHuggingPriority="240" verticalHuggingPriority="240" contentHorizontalAlignment="left" contentVerticalAlignment="center" lineBreakMode="wordWrap" translatesAutoresizingMaskIntoConstraints="NO" id="OJ3-VB-hqA">
-                            <rect key="frame" x="0.0" y="0.0" width="971" height="48"/>
-                            <constraints>
-                                <constraint firstAttribute="height" constant="48" id="oPW-KZ-iX3"/>
-                            </constraints>
-                            <inset key="contentEdgeInsets" minX="12" minY="0.0" maxX="0.0" maxY="0.0"/>
-                            <inset key="titleEdgeInsets" minX="12" minY="0.0" maxX="0.0" maxY="0.0"/>
-                            <state key="normal" title="Button" image="ic_search_toast"/>
-                            <userDefinedRuntimeAttributes>
-                                <userDefinedRuntimeAttribute type="string" keyPath="styleName" value="FlatNormalTransButton:MWMBlue:regular16"/>
-                            </userDefinedRuntimeAttributes>
-                            <connections>
-                                <action selector="openSearch" destination="iN0-l3-epB" eventType="touchUpInside" id="rRN-4C-luO"/>
-                            </connections>
-                        </button>
-                        <view contentMode="scaleToFill" translatesAutoresizingMaskIntoConstraints="NO" id="pWX-NL-dy3">
-                            <rect key="frame" x="971" y="8" width="1" height="32"/>
-                            <color key="backgroundColor" white="1" alpha="1" colorSpace="calibratedWhite"/>
-                            <constraints>
-                                <constraint firstAttribute="width" constant="1" id="5sH-RH-vPb"/>
-                            </constraints>
-                            <userDefinedRuntimeAttributes>
-                                <userDefinedRuntimeAttribute type="string" keyPath="styleName" value="Divider"/>
-                            </userDefinedRuntimeAttributes>
-                        </view>
-                        <button opaque="NO" contentMode="scaleToFill" horizontalCompressionResistancePriority="900" contentHorizontalAlignment="center" contentVerticalAlignment="center" buttonType="system" lineBreakMode="wordWrap" translatesAutoresizingMaskIntoConstraints="NO" id="fh6-Nu-aYU">
-                            <rect key="frame" x="972" y="0.0" width="52" height="48"/>
-                            <accessibility key="accessibilityConfiguration" identifier="routePlaningButtomPanelUseLocation"/>
-                            <constraints>
-                                <constraint firstAttribute="height" constant="48" id="1d3-V3-ab6"/>
-                                <constraint firstAttribute="width" secondItem="fh6-Nu-aYU" secondAttribute="height" multiplier="1:1" priority="750" id="Y41-YU-X4s"/>
-                            </constraints>
-                            <inset key="contentEdgeInsets" minX="16" minY="0.0" maxX="16" maxY="0.0"/>
-                            <state key="normal" image="ic_get_position"/>
-                            <userDefinedRuntimeAttributes>
-                                <userDefinedRuntimeAttribute type="string" keyPath="styleName" value="MWMBlue"/>
-                            </userDefinedRuntimeAttributes>
-                            <connections>
-                                <action selector="addLocationRoutePoint" destination="iN0-l3-epB" eventType="touchUpInside" id="4bf-b1-JqQ"/>
-                            </connections>
-                        </button>
-                    </subviews>
-                    <color key="backgroundColor" white="0.0" alpha="0.0" colorSpace="custom" customColorSpace="genericGamma22GrayColorSpace"/>
-                    <accessibility key="accessibilityConfiguration" identifier="routePlaningButtomPanel"/>
-                    <constraints>
-                        <constraint firstItem="OJ3-VB-hqA" firstAttribute="leading" secondItem="Tzc-l6-Vvt" secondAttribute="leading" id="Dhd-ED-QZD"/>
-                        <constraint firstAttribute="height" constant="48" id="E4J-Rq-3CZ"/>
-                        <constraint firstItem="fh6-Nu-aYU" firstAttribute="leading" secondItem="pWX-NL-dy3" secondAttribute="trailing" id="GWY-Hv-xMd"/>
-                        <constraint firstAttribute="trailing" secondItem="jmn-hv-OlG" secondAttribute="trailing" constant="-100" id="IwI-ua-fZg"/>
-                        <constraint firstItem="pWX-NL-dy3" firstAttribute="top" secondItem="fh6-Nu-aYU" secondAttribute="top" constant="8" id="QuG-5X-6WM"/>
-                        <constraint firstItem="jmn-hv-OlG" firstAttribute="leading" secondItem="Tzc-l6-Vvt" secondAttribute="leading" constant="-100" id="Vc5-xh-8nV"/>
-                        <constraint firstItem="OJ3-VB-hqA" firstAttribute="top" secondItem="Tzc-l6-Vvt" secondAttribute="top" id="dYq-JZ-UgV"/>
-                        <constraint firstItem="fh6-Nu-aYU" firstAttribute="top" secondItem="Tzc-l6-Vvt" secondAttribute="top" id="kaA-3q-cFB"/>
-                        <constraint firstItem="pWX-NL-dy3" firstAttribute="leading" secondItem="OJ3-VB-hqA" secondAttribute="trailing" priority="999" id="ngH-HH-FLo"/>
-                        <constraint firstAttribute="trailing" secondItem="fh6-Nu-aYU" secondAttribute="trailing" id="o3O-CY-AKb"/>
-                        <constraint firstItem="jmn-hv-OlG" firstAttribute="top" secondItem="Tzc-l6-Vvt" secondAttribute="top" id="oKf-hI-i5g"/>
-                        <constraint firstItem="pWX-NL-dy3" firstAttribute="bottom" secondItem="fh6-Nu-aYU" secondAttribute="bottom" constant="-8" id="whl-Po-qqC"/>
-                    </constraints>
-                    <connections>
-                        <outlet property="actionButton" destination="OJ3-VB-hqA" id="MW0-KG-h24"/>
-                        <outlet property="extraBottomBackground" destination="jmn-hv-OlG" id="D8x-Ph-Pm5"/>
-                        <outlet property="locationButton" destination="fh6-Nu-aYU" id="2dI-Ab-cCH"/>
-                        <outlet property="separator" destination="pWX-NL-dy3" id="DC7-YE-KjK"/>
-                    </connections>
-                </view>
                 <view hidden="YES" contentMode="scaleToFill" translatesAutoresizingMaskIntoConstraints="NO" id="YYv-pG-Wkw" userLabel="Street name" customClass="NavigationStreetNameView" customModule="Organic_Maps" customModuleProvider="target">
-                    <rect key="frame" x="-100" y="0.0" width="1224" height="65"/>
+                    <rect key="frame" x="-100" y="0.0" width="1224" height="39"/>
                     <subviews>
                         <view contentMode="scaleToFill" translatesAutoresizingMaskIntoConstraints="NO" id="WID-Um-Va6" userLabel="BackgroundView">
-                            <rect key="frame" x="-100" y="-100" width="1424" height="165"/>
+                            <rect key="frame" x="-100" y="-100" width="1424" height="139"/>
                             <color key="backgroundColor" white="1" alpha="1" colorSpace="custom" customColorSpace="genericGamma22GrayColorSpace"/>
                             <userDefinedRuntimeAttributes>
                                 <userDefinedRuntimeAttribute type="string" keyPath="styleName" value="StreetNameBackgroundView"/>
                             </userDefinedRuntimeAttributes>
                         </view>
                         <label opaque="NO" userInteractionEnabled="NO" contentMode="left" horizontalHuggingPriority="251" verticalHuggingPriority="251" horizontalCompressionResistancePriority="600" verticalCompressionResistancePriority="600" text="Ленинградский проспект" textAlignment="center" lineBreakMode="tailTruncation" numberOfLines="0" baselineAdjustment="alignBaselines" minimumScaleFactor="0.5" translatesAutoresizingMaskIntoConstraints="NO" id="ShI-bz-5g8">
-                            <rect key="frame" x="204" y="24" width="900" height="29"/>
+                            <rect key="frame" x="204" y="0.0" width="900" height="29"/>
                             <accessibility key="accessibilityConfiguration">
                                 <accessibilityTraits key="traits" staticText="YES" notEnabled="YES"/>
                             </accessibility>
@@ -358,15 +280,12 @@
             <color key="backgroundColor" white="0.0" alpha="0.0" colorSpace="custom" customColorSpace="genericGamma22GrayColorSpace"/>
             <constraints>
                 <constraint firstAttribute="trailing" secondItem="YYv-pG-Wkw" secondAttribute="trailing" constant="-100" id="15D-kN-gda"/>
-                <constraint firstAttribute="trailing" secondItem="Tzc-l6-Vvt" secondAttribute="trailing" id="26j-tD-P9y"/>
                 <constraint firstItem="TMW-aw-1RT" firstAttribute="centerY" secondItem="Xna-Q1-7zW" secondAttribute="centerY" id="652-PO-iav"/>
                 <constraint firstItem="YYv-pG-Wkw" firstAttribute="leading" secondItem="iN0-l3-epB" secondAttribute="leading" constant="-100" id="IBQ-gK-Cpe"/>
                 <constraint firstItem="Xna-Q1-7zW" firstAttribute="leading" secondItem="iN0-l3-epB" secondAttribute="leading" id="J7x-54-WT5"/>
-                <constraint firstItem="Tzc-l6-Vvt" firstAttribute="top" secondItem="iN0-l3-epB" secondAttribute="bottom" priority="770" id="JqF-da-c1k"/>
                 <constraint firstItem="Xna-Q1-7zW" firstAttribute="centerY" secondItem="iN0-l3-epB" secondAttribute="centerY" priority="500" constant="32" id="KQb-ju-1bf"/>
                 <constraint firstItem="YYv-pG-Wkw" firstAttribute="top" secondItem="iN0-l3-epB" secondAttribute="top" priority="500" id="Rd5-Hl-fSF"/>
                 <constraint firstAttribute="trailingMargin" secondItem="ShI-bz-5g8" secondAttribute="trailing" id="RsT-Ct-FiQ"/>
-                <constraint firstItem="Tzc-l6-Vvt" firstAttribute="leading" secondItem="iN0-l3-epB" secondAttribute="leading" id="TvX-b3-3Sa"/>
                 <constraint firstItem="XMv-au-OYf" firstAttribute="top" relation="greaterThanOrEqual" secondItem="wt8-EO-MKz" secondAttribute="bottom" priority="800" constant="8" id="Vtn-PC-puN"/>
                 <constraint firstItem="XMv-au-OYf" firstAttribute="centerX" secondItem="Xna-Q1-7zW" secondAttribute="centerX" id="XwW-6c-UzJ"/>
                 <constraint firstItem="TMW-aw-1RT" firstAttribute="leading" secondItem="Xna-Q1-7zW" secondAttribute="leading" priority="250" constant="-2" id="Y9b-UG-buF"/>
@@ -376,12 +295,10 @@
                 <constraint firstItem="Aa6-N8-acP" firstAttribute="top" secondItem="iN0-l3-epB" secondAttribute="top" constant="28" id="goU-O7-rpS"/>
                 <constraint firstItem="XMv-au-OYf" firstAttribute="top" relation="greaterThanOrEqual" secondItem="Aa6-N8-acP" secondAttribute="bottom" priority="800" id="hiU-Sn-hG1"/>
                 <constraint firstItem="ShI-bz-5g8" firstAttribute="leading" relation="greaterThanOrEqual" secondItem="Aa6-N8-acP" secondAttribute="trailing" priority="999" constant="8" id="mdA-B9-tvQ"/>
-                <constraint firstItem="Tzc-l6-Vvt" firstAttribute="top" relation="greaterThanOrEqual" secondItem="Xna-Q1-7zW" secondAttribute="bottom" priority="740" constant="12" id="pFP-r6-dGK"/>
                 <constraint firstItem="Aa6-N8-acP" firstAttribute="leading" secondItem="iN0-l3-epB" secondAttribute="leading" id="pXD-dk-ku2"/>
                 <constraint firstItem="Xna-Q1-7zW" firstAttribute="top" secondItem="XMv-au-OYf" secondAttribute="bottom" constant="8" id="tEP-Qf-uBZ"/>
                 <constraint firstAttribute="top" secondItem="YYv-pG-Wkw" secondAttribute="bottom" priority="250" id="xMz-Tm-KEn"/>
                 <constraint firstAttribute="leading" secondItem="Aa6-N8-acP" secondAttribute="trailing" priority="250" id="xO2-jM-2bk"/>
-                <constraint firstAttribute="bottom" secondItem="Tzc-l6-Vvt" secondAttribute="bottom" priority="760" id="yP1-ip-l24"/>
             </constraints>
             <freeformSimulatedSizeMetrics key="simulatedDestinationMetrics"/>
             <connections>
@@ -406,8 +323,6 @@
                 <outlet property="streetNameTopOffsetConstraint" destination="KK1-dA-hII" id="d2I-37-Guw"/>
                 <outlet property="streetNameView" destination="YYv-pG-Wkw" id="gbk-SH-idq"/>
                 <outlet property="streetNameViewHideOffset" destination="xMz-Tm-KEn" id="Lwv-ep-zvo"/>
-                <outlet property="toastView" destination="Tzc-l6-Vvt" id="gTw-kd-UAJ"/>
-                <outlet property="toastViewHideOffset" destination="JqF-da-c1k" id="WtX-JJ-0lS"/>
                 <outlet property="turnsTopOffset" destination="goU-O7-rpS" id="MEy-6X-7Qq"/>
                 <outlet property="turnsView" destination="Aa6-N8-acP" id="daB-uQ-UFM"/>
                 <outlet property="turnsViewHideOffset" destination="xO2-jM-2bk" id="U11-pU-2ka"/>
@@ -434,13 +349,11 @@
         </view>
     </objects>
     <resources>
-        <image name="ic_get_position" width="20" height="20"/>
         <image name="ic_routing_atm_on" width="24" height="24"/>
         <image name="ic_routing_eat_on" width="24" height="24"/>
         <image name="ic_routing_food_on" width="24" height="24"/>
         <image name="ic_routing_fuel_on" width="24" height="24"/>
         <image name="ic_routing_parking_on" width="24" height="24"/>
         <image name="ic_routing_search_light" width="56" height="56"/>
-        <image name="ic_search_toast" width="24" height="24"/>
     </resources>
 </document>

iphone/Maps/Classes/CustomViews/NavigationDashboard/Views/Navigation/NavigationAddPointToastView.swift
@@ -1,26 +1,0 @@
-@objc(MWMNavigationAddPointToastView)
-final class NavigationAddPointToastView: UIView {
-  @IBOutlet private var actionButton: UIButton!
-  @IBOutlet private var locationButton: UIButton!
-  @IBOutlet private var separator: UIView!
-  @IBOutlet private weak var extraBottomBackground: UIView!
-  @objc private(set) var isStart = true
-
-  @objc func config(isStart: Bool, withLocationButton: Bool) {
-    self.isStart = isStart
-    let text = isStart ? L("routing_add_start_point") : L("routing_add_finish_point")
-    actionButton.setTitle(text, for: .normal)
-
-    if withLocationButton {
-      locationButton.isHidden = false
-      separator.isHidden = false
-    } else {
-      locationButton.isHidden = true
-      separator.isHidden = true
-    }
-  }
-
-  override var widgetsAreaAffectDirections: MWMAvailableAreaAffectDirections { return .bottom }
-
-  override var sideButtonsAreaAffectDirections: MWMAvailableAreaAffectDirections { return .bottom }
-}

iphone/Maps/Classes/CustomViews/NavigationDashboard/Views/NavigationDashboardViewController.swift
@@ -435,7 +435,6 @@ extension NavigationDashboardViewController {
 
     case .navigation:
       navigationInfoView.onNavigationInfoUpdated(viewModel.entity)
-      navigationInfoView.updateToastView()
       if let navigationSearchState = viewModel.navigationSearchState {
         navigationInfoView.setSearchState(navigationSearchState, animated: true)
       }

iphone/Maps/Classes/CustomViews/NavigationDashboard/Views/NavigationDashboardViewModel.swift
@@ -24,7 +24,6 @@ enum NavigationDashboard {
   struct NavigationInfo: Equatable {
     var state: MWMNavigationInfoViewState
     var availableArea: CGRect
-    var shouldUpdateToastView: Bool
   }
 }
 
@@ -82,7 +81,6 @@ extension NavigationDashboard.NavigationInfo {
   static let hidden = NavigationDashboard.NavigationInfo(
     state: .hidden,
     availableArea: MapViewController.shared()?.navigationInfoArea.areaFrame ?? .screenBounds,
-    shouldUpdateToastView: false
   )
 }
 

commit 1059f0427b3e214979235c5d84c5c897f34b554c

[ios] Fix search result selection issue
Caused by the 5e98dfb630260a6ebe8429ef8c81c36f7f9505fa

Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

iphone/Maps/UI/Search/SearchOnMap/SearchOnMapInteractor.swift
@@ -101,6 +101,7 @@ final class SearchOnMapInteractor: NSObject {
     switch result.itemType {
     case .regular:
       searchManager.save(query)
+      searchManager.showResult(at: result.index)
       return isiPad ? .none : .setSearchScreenHidden(true)
     case .suggestion:
       let suggestionQuery = SearchQuery(result.suggestion,