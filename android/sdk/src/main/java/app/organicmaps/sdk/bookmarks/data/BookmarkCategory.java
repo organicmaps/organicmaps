1b84e990b8cfd9b9b9558d7fe18e3f02839b3b1b
Andrei Shkrob <github@shkrob.dev>
2025-10-22 18:10:16 +0200

    [android] Fix BookmarkCategory toggleVisibility method
    Signed-off-by: Andrei Shkrob <github@shkrob.dev>

--- android/sdk/src/main/java/app/organicmaps/sdk/bookmarks/data/BookmarkCategory.java
+++ android/sdk/src/main/java/app/organicmaps/sdk/bookmarks/data/BookmarkCategory.java
@@ -105,7 +105,7 @@ public final class BookmarkCategory implements Parcelable
 
   public void toggleVisibility()
   {
-    setVisibility(isVisible());
+    setVisibility(!isVisible());
   }
 
   public int size()
