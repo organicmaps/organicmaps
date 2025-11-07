6327f8946ef02595f291807a3d3ae11a23a887fb
Raghad Dahi <raghaddahi27@gmail.com>
2025-11-07 21:55:12 +0200

    [3party] Add support for public holidays in opening_hours lib
    Signed-off-by: Raghad Dahi <raghaddahi27@gmail.com>

--- 3party/opening_hours/rules_evaluation_private.hpp
+++ 3party/opening_hours/rules_evaluation_private.hpp
@@ -8,8 +8,8 @@ namespace osmoh
 {
 bool IsActive(Timespan span, std::tm const & date);
 bool IsActive(WeekdayRange const & range, std::tm const & date);
-bool IsActive(Holiday const & holiday, std::tm const & date);
-bool IsActive(Weekdays const & weekdays, std::tm const & date);
+bool IsActive(Holiday const & holiday, std::tm const & date, THolidayDates const & holidays);
+bool IsActive(Weekdays const & weekdays, std::tm const & date, THolidayDates const & holidays = {});
 bool IsActive(MonthdayRange const & range, std::tm const & date);
 bool IsActive(YearRange const & range, std::tm const & date);
 bool IsActive(WeekRange const & range, std::tm const & date);
