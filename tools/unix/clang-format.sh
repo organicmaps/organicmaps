4143f7fbf54634d74e0faeac49a85a818736debf
Alexander Borsuk <me@alex.bio>
2025-11-25 22:39:13 +0100

    [github] Use clang-format-21 on CI
    Signed-off-by: Alexander Borsuk <me@alex.bio>

--- tools/unix/clang-format.sh
+++ tools/unix/clang-format.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+for binary in clang-format clang-format-21; do
+  if command -v "$binary" >/dev/null 2>&1; then
+    CLANG_FORMAT="$binary"
+  fi
+done
+
+"$CLANG_FORMAT" --version
+
 echo "Running clang-format on all repository files..."
 
-XARGS_COMMAND='xargs -n1 -0 -P0 clang-format -i'
+XARGS_COMMAND="xargs -n1 -0 -P0 $CLANG_FORMAT -i"
 
 # Android
 find android/{app,sdk}/src -type f -name '*.java' -print0 | $XARGS_COMMAND
