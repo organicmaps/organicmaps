282e782f3ae1537bc88e23972b54e832314e0721
skirm-tech <244209000+skirm-tech@users.noreply.github.com>
2025-12-02 00:10:44 +0300

    Update android/app/src/main/java/app/organicmaps/car/util/IntentUtils.java
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Signed-off-by: skirm-tech <244209000+skirm-tech@users.noreply.github.com>

--- android/app/src/main/java/app/organicmaps/car/util/IntentUtils.java
+++ android/app/src/main/java/app/organicmaps/car/util/IntentUtils.java
@@ -55,8 +55,7 @@ public final class IntentUtils
   }
 
   private record CarSearchHandler(CarContext mCarContext, Renderer mSurfaceRenderer)
-      implements GoogleAssistantIntentHandler.SearchHandler
-  {
+      implements GoogleAssistantIntentHandler.SearchHandler {
     private CarSearchHandler(@NonNull CarContext mCarContext, @NonNull Renderer mSurfaceRenderer)
     {
       this.mCarContext = mCarContext;
