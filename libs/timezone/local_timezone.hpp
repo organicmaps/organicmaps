8c2f82ebad3bf2b9bce17543919cb04347f5defc
Andrei Shkrob <github@shkrob.dev>
2026-01-05 00:39:58 +0100

    Remove LocalTimeZone struct
    Signed-off-by: Andrei Shkrob <github@shkrob.dev>

--- libs/timezone/local_timezone.hpp
+++ libs/timezone/local_timezone.hpp
@@ -4,5 +4,5 @@
 
 namespace om::tz
 {
-LocalTimeZone GetLocalTimeZone();
+TimeZone GetLocalTimeZone();
 }
--- libs/timezone/timezone_tests/local_timezone_tests.cpp
+++ libs/timezone/timezone_tests/local_timezone_tests.cpp
@@ -7,18 +7,23 @@ using namespace om::tz;
 
 TEST(TimeZoneLocal, ShouldCreateLocalTimeZone)
 {
-  LocalTimeZone const localTz = GetLocalTimeZone();
+  TimeZone const localTz = GetLocalTimeZone();
 
   // Just check that base offset is in reasonable range
-  EXPECT_GE(localTz.base_offset, -12 * 60);
-  EXPECT_LE(localTz.base_offset, 14 * 60);
+  EXPECT_GE(localTz.base_offset, 0);
+  EXPECT_LE(localTz.base_offset, 127);
 
   time_t const now = std::time(nullptr);
   std::tm local{};
   localtime_r(&now, &local);
-  EXPECT_EQ(local.tm_isdst, localTz.dst_delta);
-  if (localTz.dst_delta > 0)
+  if (local.tm_isdst > 0)
+  {
     EXPECT_THAT(localTz.dst_delta, testing::Not(testing::Eq(0)));
+    EXPECT_THAT(localTz.transitions, testing::Not(testing::IsEmpty()));
+  }
   else
+  {
     EXPECT_THAT(localTz.dst_delta, testing::Eq(0));
+    EXPECT_THAT(localTz.transitions, testing::IsEmpty());
+  }
 }
