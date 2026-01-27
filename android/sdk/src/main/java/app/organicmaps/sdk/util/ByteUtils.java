1a62cb5f3042d2552ab1e07b0710fdc101db0e68
S. Kozyr <s.trump@gmail.com>
2026-01-27 09:50:16 +0200

    Removed decoding of UTF16 encoding. Only UTF8 BOM is supported.
    Signed-off-by: S. Kozyr <s.trump@gmail.com>

--- android/sdk/src/main/java/app/organicmaps/sdk/util/ByteUtils.java
+++ android/sdk/src/main/java/app/organicmaps/sdk/util/ByteUtils.java
@@ -1,14 +1,7 @@
 package app.organicmaps.sdk.util;
 
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-
 public class ByteUtils
 {
-  static byte[] utf8Bom = {(byte) 0xEF, (byte) 0xBB, (byte) 0xBF};
-  static byte[] utf16BeBom = {(byte) 0xFE, (byte) 0xFF};
-  static byte[] utf16LeBom = {(byte) 0xFF, (byte) 0xFE};
-
   public static boolean startsWith(byte[] source, byte[] target)
   {
     // Check if first bytes of 'source' array match 'target' array
@@ -27,29 +20,20 @@ public class ByteUtils
     return true;
   }
 
-  public static boolean contains(byte[] source, byte[] target)
+  public static boolean contains(byte[] source, byte[] target) {
+    return contains(source, target, 0);
+  }
+
+  public static boolean contains(byte[] source, byte[] target, int source_offset)
   {
     // Check if 'source' array contains bytes from 'target' at some offset.
-    if (target.length > source.length)
+    if (target.length > source.length + source_offset)
       return false;
 
-    for (int i = 0; i < source.length; i++)
+    for (int i = source_offset; i < source.length; i++)
       if (startsWith(source, target, i))
         return true;
 
     return false;
-  }
-
-  public static Charset guessEncodingByBom(byte[] buffer)
-  {
-    // Look for UTF_8, UTF_16BE and UTF_16LE markers on the start of the buffer.
-    if (startsWith(buffer, utf8Bom))
-      return StandardCharsets.UTF_8;
-    if (startsWith(buffer, utf16BeBom))
-      return StandardCharsets.UTF_16BE;
-    if (startsWith(buffer, utf16LeBom))
-      return StandardCharsets.UTF_16LE;
-
-    return null;
   }
 }
