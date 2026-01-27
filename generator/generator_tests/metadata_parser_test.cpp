e7781675c9f1e458e41bd2c2468e1c772ef7e53c
alnzrv <7187657+alnzrv@users.noreply.github.com>
2026-01-27 12:10:27 +0000

    Fix code style
    Signed-off-by: alnzrv <7187657+alnzrv@users.noreply.github.com>

--- generator/generator_tests/metadata_parser_test.cpp
+++ generator/generator_tests/metadata_parser_test.cpp
@@ -213,7 +213,8 @@ UNIT_TEST(Metadata_ValidateAndFormat_wikimedia_commons)
   p(kWikiKey, "Category:Bosphorus");
   TEST_EQUAL(md.Get(Metadata::FMD_WIKIMEDIA_COMMONS), "Category:Bosphorus", ());
 
-  TEST_EQUAL(md.ToWikimediaCommonsURL("Category:What If? (Bonn)"), "https://commons.wikimedia.org/wiki/Category:What_If%3F_(Bonn)", ());
+  TEST_EQUAL(md.ToWikimediaCommonsURL("Category:What If? (Bonn)"),
+             "https://commons.wikimedia.org/wiki/Category:What_If%3F_(Bonn)", ());
 
   md.Drop(Metadata::FMD_WIKIMEDIA_COMMONS);
   p(kWikiKey, "incorrect_wikimedia_content");
