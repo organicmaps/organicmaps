0ea0b823678346a4f621a548b5f6b1df10e67116
Hitanshi Goklani <hitanshigoklani33@gmail.com>
2026-02-07 02:47:39 +0530

    Add track description editing feature for iOS
    - Add SetDescription() method to Track class
    - Update MWMBookmarksManager to handle track descriptions
    - Add description text field to EditTrackViewController
    - Update MWMPlacePageManager to pass description parameter
    
    Fixes #12124
    
    Signed-off-by: Hitanshi Goklani <hitanshigoklani33@gmail.com>

--- libs/map/track.cpp
+++ libs/map/track.cpp
@@ -83,6 +83,10 @@ std::string Track::GetDescription() const
 {
   return GetPreferredBookmarkStr(m_data.m_description);
 }
+void Track::SetDescription(std::string const & description)
+{
+  kml::SetDefaultStr(m_data.m_description, description);
+}
 
 void Track::setData(kml::TrackData const & data)
 {
