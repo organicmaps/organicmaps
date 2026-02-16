16d9c5f20dfd3db1c3b56010652e7e5cf9ac4c61
S. Kozyr <s.trump@gmail.com>
2026-02-16 11:34:12 +0200

    Added `m_editTimestamp` field to BookmarkData struct. Added serialization to/from KML.
    Field is updated on every bookmark modification.
    Updated unit-tests.
    
    Signed-off-by: S. Kozyr <s.trump@gmail.com>

--- libs/kml/serdes.cpp
+++ libs/kml/serdes.cpp
@@ -40,9 +40,14 @@ bool IsCoord(std::string const & s)
   return s == "coord" || s == "gx:coord";
 }
 
-bool IsTimestamp(std::string const & s)
+bool IsCreatedTimestamp(std::string const & s)
 {
   return s == "when";
+}
+
+bool IsModifiedTimestamp(std::string const & s)
+{
+  return s == "modified";
 }
 
 std::string_view constexpr kKmlHeader =
@@ -442,7 +447,12 @@ void SaveBookmarkData(Writer & writer, BookmarkData const & bookmarkData)
   }
 
   if (bookmarkData.m_timestamp != Timestamp())
-    writer << kIndent4 << "<TimeStamp><when>" << TimestampToString(bookmarkData.m_timestamp) << "</when></TimeStamp>\n";
+  {
+    writer << kIndent4 << "<TimeStamp><when>" << TimestampToString(bookmarkData.m_timestamp) << "</when>";
+    if (bookmarkData.m_editTimestamp != Timestamp())
+      writer << "<modified>" << TimestampToString(bookmarkData.m_editTimestamp) << "</modified>";
+    writer << "</TimeStamp>\n";
+  }
 
   // Custom colors reference a shared #placemark-<rgbahex> style; presets keep #placemark-<name> so
   // old OM still matches by name. A bookmark that slipped through unnormalized as {None, 0} falls
@@ -991,6 +1001,7 @@ void KmlParser::Pop(std::string_view tag)
         data.m_icon = m_icon;
         data.m_viewportScale = m_viewportScale;
         data.m_timestamp = m_timestamp;
+        data.m_editTimestamp = m_editTimestamp;
         data.m_point = m_org;
         data.m_featureTypes = std::move(m_featureTypes);
         data.m_customName = std::move(m_customName);
@@ -1139,7 +1150,7 @@ void KmlParser::CharData(std::string & value)
       if (!IsTrack(prevTag))
         return false;
 
-      if (IsTimestamp(currTag))
+      if (IsCreatedTimestamp(currTag))
       {
         auto & timestamps = m_geometry.m_timestamps;
         ASSERT(!timestamps.empty(), ());
@@ -1419,11 +1430,17 @@ void KmlParser::CharData(std::string & value)
       }
       else if (prevTag == "TimeStamp")
       {
-        if (IsTimestamp(currTag))
+        if (IsCreatedTimestamp(currTag))
         {
           auto const ts = base::StringToTimestamp(value);
           if (ts != base::INVALID_TIME_STAMP)
             m_timestamp = TimestampClock::from_time_t(ts);
+        }
+        else if (IsModifiedTimestamp(currTag))
+        {
+          auto const ts = base::StringToTimestamp(value);
+          if (ts != base::INVALID_TIME_STAMP)
+            m_editTimestamp = TimestampClock::from_time_t(ts);
         }
       }
       else if (currTag == kStyleUrl)
--- libs/map/bookmark_manager.cpp
+++ libs/map/bookmark_manager.cpp
@@ -2360,6 +2360,8 @@ void BookmarkManager::UpdateBookmark(kml::MarkId bmID, kml::BookmarkData const &
   auto const & newColor = bookmark->GetData().m_color;
   if (prevColor != newColor)
     SetLastEditedBmColor(newColor);
+
+  bookmark->SetEditTimeStamp(kml::TimestampClock::now());
 }
 
 void BookmarkManager::ChangeTrackColor(kml::TrackId trackId, dp::Color color)
