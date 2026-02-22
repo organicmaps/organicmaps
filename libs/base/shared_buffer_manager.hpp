1dab8b45abd544712c360ec1f151be9bd1dd810d
Alexander Borsuk <me@alex.bio>
2026-02-22 02:00:06 +0100

    [base] Optimize SharedBufferManager pool data structures and access patterns
    - Normalize all sizes to power-of-2 via std::bit_ceil, so all callers
      benefit from pooling (glyphs, mutations previously missed the cache)
    - Replace std::map with std::unordered_map for O(1) lookup and contiguous inner storage
    - Switch from FIFO (front/pop_front) to LIFO (back/pop_back) for
      better cache warmth and to avoid the overhead of std::list
    - Use std::move on shared_ptr to eliminate redundant atomic refcount ops
    
    Signed-off-by: Alexander Borsuk <me@alex.bio>

--- libs/base/shared_buffer_manager.hpp
+++ libs/base/shared_buffer_manager.hpp
@@ -1,22 +1,19 @@
 #pragma once
 
-#include <list>
-#include <map>
 #include <memory>
 #include <mutex>
+#include <unordered_map>
 #include <vector>
 
 class SharedBufferManager
 {
 public:
-  typedef std::vector<uint8_t> shared_buffer_t;
-  typedef std::shared_ptr<shared_buffer_t> shared_buffer_ptr_t;
-  typedef std::list<shared_buffer_ptr_t> shared_buffer_ptr_list_t;
-  typedef std::map<size_t, shared_buffer_ptr_list_t> shared_buffers_t;
+  using shared_buffer_t = std::vector<uint8_t>;
+  using shared_buffer_ptr_t = std::shared_ptr<shared_buffer_t>;
 
 private:
   std::mutex m_mutex;
-  shared_buffers_t m_sharedBuffers;
+  std::unordered_map<size_t, std::vector<shared_buffer_ptr_t>> m_pool;
 
 public:
   static SharedBufferManager & instance();
@@ -26,5 +23,5 @@ public:
   shared_buffer_ptr_t reserveSharedBuffer(size_t s);
   void freeSharedBuffer(size_t s, shared_buffer_ptr_t buf);
 
-  static uint8_t * GetRawPointer(shared_buffer_ptr_t ptr);
+  static uint8_t * GetRawPointer(shared_buffer_ptr_t const & ptr);
 };
