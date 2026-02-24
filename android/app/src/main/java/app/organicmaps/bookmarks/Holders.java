827142ece006c4405fe899b4599b193aa2b06aac
Alexander Borsuk <me@alex.bio>
2026-02-25 00:31:10 +0100

    [android] Eye visibility icon is gray when not visible
    Also it correctly displays on Android 5
    
    Signed-off-by: Alexander Borsuk <me@alex.bio>

--- android/app/src/main/java/app/organicmaps/bookmarks/Holders.java
+++ android/app/src/main/java/app/organicmaps/bookmarks/Holders.java
@@ -258,7 +258,7 @@ public class Holders
     @NonNull
     private final TextView mName;
     @NonNull
-    ImageView mVisibilityMarker;
+    CheckBox mVisibilityMarker;
     @NonNull
     ImageView mMoreButton;
 
@@ -272,10 +272,7 @@ public class Holders
 
     void setVisibilityState(boolean visible)
     {
-      if (visible)
-        mVisibilityMarker.setImageResource(R.drawable.ic_show);
-      else
-        mVisibilityMarker.setImageResource(R.drawable.ic_hide);
+      mVisibilityMarker.setChecked(visible);
     }
 
     void setVisibilityListener(@Nullable View.OnClickListener listener)
