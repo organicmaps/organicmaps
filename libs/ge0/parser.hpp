5b4abebcfb6ceece9b133a6d25a16efbce203842
Alexander Borsuk <me@alex.bio>
2026-02-28 21:52:38 +0100

    Fixed ge0
    Signed-off-by: Alexander Borsuk <me@alex.bio>

--- libs/ge0/parser.hpp
+++ libs/ge0/parser.hpp
@@ -4,6 +4,7 @@
 #include <cstddef>
 #include <cstdint>
 #include <string>
+#include <string_view>
 
 namespace ge0
 {
@@ -22,25 +23,31 @@ public:
     std::string m_name;
   };
 
-  Ge0Parser();
-
   bool Parse(std::string const & url, Result & result);
   bool ParseAfterPrefix(std::string const & url, size_t from, Result & result);
 
 protected:
-  uint8_t DecodeBase64Char(char const c);
+  static uint8_t DecodeBase64Char(char c);
   static double DecodeZoom(uint8_t const zoomByte);
-  bool DecodeLatLon(std::string const & s, double & lat, double & lon);
-  bool DecodeLatLonToInt(std::string const & s, int & lat, int & lon);
-  double DecodeLatFromInt(int const lat, int const maxValue);
-  double DecodeLonFromInt(int const lon, int const maxValue);
-  std::string DecodeName(std::string name);
-  void SpacesToUnderscore(std::string & name);
-  void ValidateName(std::string & name);
+  static bool DecodeLatLon(std::string_view s, double & lat, double & lon);
+  static bool DecodeLatLonToInt(std::string_view s, int & lat, int & lon);
+  static double DecodeLatFromInt(int const lat, int const maxValue);
+  static double DecodeLonFromInt(int const lon, int const maxValue);
+  static std::string DecodeName(std::string name);
+  static void SpacesToUnderscore(std::string & name);
+  static void ValidateName(std::string & name);
   static bool IsHexChar(char const a);
 
 private:
-  uint8_t m_base64ReverseCharTable[256];
+  static constexpr std::array<uint8_t, 256> kBase64ReverseCharTable = []
+  {
+    std::array<uint8_t, 256> t;
+    t.fill(255);
+    constexpr char kAlphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+    for (uint8_t i = 0; i < 64; ++i)
+      t[static_cast<uint8_t>(kAlphabet[i])] = i;
+    return t;
+  }();
 };
 
 std::string DebugPrint(Ge0Parser::Result const & r);
