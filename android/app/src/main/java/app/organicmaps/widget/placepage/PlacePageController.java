organicmaps/organicmaps

commit 0c639965b1c2a9cc29108222234301e656886572

expand pp collapsed height to show necessary details
Signed-off-by: Rover track <rishan.pgowda@gmail.com>

android/app/src/main/java/app/organicmaps/widget/placepage/PlacePageController.java
@@ -13,7 +13,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.content.res.ResourcesCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -218,6 +217,11 @@ public class PlacePageController
       });
     }
     mPlacePage.addOnLayoutChangeListener((v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom) -> {
+      final int topInset = mCurrentWindowInsets.getInsets(WindowInsetsCompat.Type.systemBars()).top;
+      if (mPlacePage.getHeight() >= mCoordinator.getHeight() - topInset)
+      {
+        mPlacePageDistanceToTopObserver.onChanged(oldTop);
+      }
       if (top != oldTop)
       {
         mDistanceToTop = oldTop;
@@ -394,20 +398,21 @@ public class PlacePageController
     mCustomPeekHeightAnimator.start();
   }
 
-  private float getHeightRatio()
-  {
-    return ResourcesCompat.getFloat(getResources(), R.dimen.place_page_bottom_sheet_height_ratio);
-  }
-
   private int calculatePeekHeight()
   {
     final int bottomInsets = (mCurrentWindowInsets != null)
                                ? mCurrentWindowInsets.getInsets(WindowInsetsCompat.Type.systemBars()).bottom
                                : 0;
     final boolean isLandscape = getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
+    final int bottomMargins = getResources().getDimensionPixelSize(R.dimen.margin_double);
+    final View plusDetailsContainer = mPlacePage.findViewById(R.id.plus_details);
+    int peekHeight = mPreviewHeight + mButtonsHeight + bottomMargins;
     if (mMapObject != null && mMapObject.getOpeningMode() == MapObject.OPENING_MODE_PREVIEW_PLUS)
-      return (int) (mCoordinator.getHeight() * getHeightRatio());
-    return mPreviewHeight + mButtonsHeight + (isLandscape ? bottomInsets : 0);
+    {
+      peekHeight += plusDetailsContainer.getHeight();
+    }
+    return Math.min(peekHeight + (isLandscape ? bottomInsets : 0),
+                    (mCoordinator.getHeight() - (mPlacePageStatusBarBackground.getHeight())));
   }
 
   @Override

android/app/src/main/java/app/organicmaps/widget/placepage/sections/PlacePageOpeningHoursFragment.java
@@ -40,6 +40,7 @@ public class PlacePageOpeningHoursFragment extends Fragment implements Observer<
   private RecyclerView mFullWeekOpeningHours;
   private PlaceOpeningHoursAdapter mOpeningHoursAdapter;
   private View dropDownIcon;
+  private View mOhContainer;
   private boolean isOhExpanded;
 
   private PlacePageViewModel mViewModel;
@@ -68,6 +69,7 @@ public class PlacePageOpeningHoursFragment extends Fragment implements Observer<
     mFullWeekOpeningHours.getLayoutParams().height = 0;
     UiUtils.hide(dropDownIcon);
     isOhExpanded = false;
+    mOhContainer = mFrame.findViewById(R.id.oh_container);
   }
 
   private void refreshTodayNonBusinessTime(Timespan[] closedTimespans)
@@ -151,7 +153,7 @@ public class PlacePageOpeningHoursFragment extends Fragment implements Observer<
         mOpeningHoursAdapter.setTimetables(timetables, firstDayOfWeek);
         final View iconView = mFrame.findViewById(R.id.dropdown_icon);
         UiUtils.show(iconView);
-        mFrame.setOnClickListener((v) -> expandOpeningHours());
+        mOhContainer.setOnClickListener((v) -> expandOpeningHours());
         mFullWeekOpeningHours.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
           @Override
           public boolean onInterceptTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e)

android/app/src/main/res/layout/place_page_bookmark_fragment.xml
@@ -18,6 +18,4 @@
     android:textAppearance="?android:attr/textAppearance"
     tools:text="Long, long text Long, long text Long, long text Long, long text Long, long text Long, long text "/>
 
-  <include
-    layout="@layout/divider_horizontal"/>
 </LinearLayout>

android/app/src/main/res/layout/place_page_details.xml
@@ -16,24 +16,20 @@
     android:background="?ppBackground"
     android:orientation="vertical">
 
-    <androidx.fragment.app.FragmentContainerView
-      android:id="@+id/place_page_bookmark_fragment"
+    <LinearLayout
+      android:id="@+id/plus_details"
       android:layout_width="match_parent"
-      android:layout_height="wrap_content"
-      tools:layout="@layout/place_page_bookmark_fragment" />
-    <androidx.fragment.app.FragmentContainerView
-      android:id="@+id/place_page_wikipedia_fragment"
-      android:layout_width="match_parent"
-      android:layout_height="wrap_content"
-      tools:layout="@layout/place_page_wikipedia_fragment" />
+      android:layout_height="match_parent"
+      android:orientation="vertical">
+
+      <androidx.fragment.app.FragmentContainerView
+        android:id="@+id/place_page_wikipedia_fragment"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        tools:layout="@layout/place_page_wikipedia_fragment" />
+    </LinearLayout>
 
     <include layout="@layout/place_page_placename"/>
-
-    <androidx.fragment.app.FragmentContainerView
-      android:id="@+id/place_page_opening_hours_fragment"
-      android:layout_width="match_parent"
-      android:layout_height="wrap_content"
-      tools:layout="@layout/place_page_opening_hours_fragment" />
 
     <include layout="@layout/place_page_cuisine"/>
 
@@ -65,7 +61,6 @@
 
     <include layout="@layout/place_page_outdoor_seating"/>
 
-    <include layout="@layout/place_page_route_ref"/>
 
     <!-- ToDo: Address is missing compared with iOS. It's shown in title but anyway .. -->
 

android/app/src/main/res/layout/place_page_opening_hours_fragment.xml
@@ -1,77 +1,95 @@
 <?xml version="1.0" encoding="utf-8"?>
-<RelativeLayout
+<LinearLayout
   xmlns:android="http://schemas.android.com/apk/res/android"
   xmlns:app="http://schemas.android.com/apk/res-auto"
   xmlns:tools="http://schemas.android.com/tools"
-  style="@style/PlacePageItemFrame">
+  android:layout_width="match_parent"
+  android:orientation="vertical"
+  android:layout_height="wrap_content">
 
-  <ImageView
-    android:id="@+id/icon"
-    android:layout_width="wrap_content"
-    android:layout_height="wrap_content"
-    android:layout_marginEnd="@dimen/margin_base"
-    android:layout_marginTop="@dimen/margin_quarter"
-    app:srcCompat="@drawable/ic_operating_hours"
-    app:tint="?iconTint"/>
+  <include layout="@layout/place_page_shadow_gap" />
 
-  <TextView
-    android:id="@+id/oh_today_label"
-    android:layout_width="wrap_content"
-    android:layout_height="wrap_content"
-    android:layout_marginTop="6dp"
-    android:layout_marginBottom="0dp"
-    android:layout_toEndOf="@id/icon"
-    android:textAppearance="@style/MwmTextAppearance.PlacePage"
-    tools:text="@string/today"
-    android:text="@string/today"
-    tools:visibility="visible"/>
+  <RelativeLayout
+    android:id="@+id/oh_container"
+    android:visibility="visible"
+    style="@style/PlacePageItemFrame">
 
-  <TextView
-    android:id="@+id/oh_today_open_time"
-    android:layout_width="wrap_content"
-    android:layout_height="wrap_content"
-    android:layout_marginTop="6dp"
-    android:layout_marginEnd="4dp"
-    android:layout_toStartOf="@+id/dropdown_icon"
-    android:textAppearance="@style/MwmTextAppearance.PlacePage"
-    android:layout_toEndOf="@id/oh_today_label"
-    tools:text="@string/twentyfour_seven"
-    android:text="@string/twentyfour_seven"
-    android:textAlignment="viewEnd"
-    tools:visibility="visible" />
+    <ImageView
+      android:id="@+id/icon"
+      android:layout_width="wrap_content"
+      android:layout_height="wrap_content"
+      android:layout_marginEnd="@dimen/margin_base"
+      android:layout_marginTop="@dimen/margin_quarter"
+      app:srcCompat="@drawable/ic_operating_hours"
+      app:tint="?iconTint" />
 
-  <ImageView
-    android:id="@+id/dropdown_icon"
-    android:layout_width="30dp"
-    android:layout_height="30dp"
-    android:layout_alignParentEnd="true"
-    android:layout_marginTop="1dp"
-    android:layout_marginEnd="@dimen/neg_margin_quarter"
-    app:srcCompat="@drawable/ic_down"
-    app:tint="?iconTint"
-    tools:visibility="visible" />
+    <TextView
+      android:id="@+id/oh_today_label"
+      android:layout_width="wrap_content"
+      android:layout_height="wrap_content"
+      android:layout_marginTop="6dp"
+      android:layout_marginBottom="0dp"
+      android:layout_toEndOf="@id/icon"
+      android:textAppearance="@style/MwmTextAppearance.PlacePage"
+      tools:text="@string/today"
+      android:text="@string/today"
+      tools:visibility="visible" />
 
-  <TextView
-    android:id="@+id/oh_nonbusiness_time"
-    android:layout_height="wrap_content"
-    android:layout_width="wrap_content"
-    android:layout_below="@id/oh_today_label"
-    android:layout_alignStart="@id/oh_today_label"
-    android:layout_alignEnd="@id/oh_today_open_time"
-    android:layout_alignParentEnd="false"
-    android:textAlignment="viewEnd"
-    android:textAppearance="@style/MwmTextAppearance.Body4"
-    android:visibility="gone"
-    tools:visibility="visible" />
+    <TextView
+      android:id="@+id/oh_today_open_time"
+      android:layout_width="wrap_content"
+      android:layout_height="wrap_content"
+      android:layout_marginTop="6dp"
+      android:layout_marginEnd="4dp"
+      android:layout_toStartOf="@+id/dropdown_icon"
+      android:textAppearance="@style/MwmTextAppearance.PlacePage"
+      android:layout_toEndOf="@id/oh_today_label"
+      tools:text="@string/twentyfour_seven"
+      android:text="@string/twentyfour_seven"
+      android:textAlignment="viewEnd"
+      tools:visibility="visible" />
 
-  <androidx.recyclerview.widget.RecyclerView
-    android:id="@+id/rw__full_opening_hours"
-    android:layout_width="wrap_content"
-    android:layout_height="wrap_content"
-    android:layout_alignParentEnd="true"
-    android:layout_alignStart="@id/oh_nonbusiness_time"
-    android:layout_below="@id/oh_nonbusiness_time"
-    app:layoutManager="androidx.recyclerview.widget.LinearLayoutManager"
-    android:layout_marginTop="8dp"/>
+    <ImageView
+      android:id="@+id/dropdown_icon"
+      android:layout_width="30dp"
+      android:layout_height="30dp"
+      android:layout_alignParentEnd="true"
+      android:layout_marginTop="1dp"
+      android:layout_marginEnd="@dimen/neg_margin_quarter"
+      app:srcCompat="@drawable/ic_down"
+      app:tint="?iconTint"
+      tools:visibility="visible" />
 
-</RelativeLayout>
+    <TextView
+      android:id="@+id/oh_nonbusiness_time"
+      android:layout_height="wrap_content"
+      android:layout_width="wrap_content"
+      android:layout_below="@id/oh_today_label"
+      android:layout_alignStart="@id/oh_today_label"
+      android:layout_alignEnd="@id/oh_today_open_time"
+      android:layout_alignParentEnd="false"
+      android:textAlignment="viewEnd"
+      android:textAppearance="@style/MwmTextAppearance.Body4"
+      android:visibility="gone"
+      tools:visibility="visible" />
+
+    <androidx.recyclerview.widget.RecyclerView
+      android:id="@+id/rw__full_opening_hours"
+      android:layout_width="wrap_content"
+      android:layout_height="wrap_content"
+      android:layout_alignParentEnd="true"
+      android:layout_alignStart="@id/oh_nonbusiness_time"
+      android:layout_below="@id/oh_nonbusiness_time"
+      app:layoutManager="androidx.recyclerview.widget.LinearLayoutManager"
+      android:layout_marginTop="8dp"/>
+
+  </RelativeLayout>
+
+  <LinearLayout
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    android:orientation="vertical">
+
+  </LinearLayout>
+
+</LinearLayout>

android/app/src/main/res/layout/place_page_preview.xml
@@ -244,7 +244,15 @@
         android:clickable="true"
         android:focusable="true"
         android:src="@drawable/ic_edit" />
+
     </LinearLayout>
+
+    <androidx.fragment.app.FragmentContainerView
+      android:id="@+id/place_page_bookmark_fragment"
+      android:layout_width="match_parent"
+      android:layout_height="wrap_content"
+      tools:layout="@layout/place_page_bookmark_fragment" />
+
   </LinearLayout>
 
   <LinearLayout
@@ -275,6 +283,21 @@
         tools:text="Some very long text for a very very very very very very very very very very very very very very very very very very very very very very very very looooooong descriptions that doesn't fit into the cell. Interesting, how will it look for even longer text" />
     </LinearLayout>
 
+  </LinearLayout>
+
+  <include layout="@layout/place_page_route_ref" />
+
+  <LinearLayout
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    android:orientation="vertical">
+
+    <androidx.fragment.app.FragmentContainerView
+      android:id="@+id/place_page_opening_hours_fragment"
+      android:layout_width="match_parent"
+      android:layout_height="wrap_content"
+      tools:layout="@layout/place_page_opening_hours_fragment"
+      tools:visibility="visible" />
   </LinearLayout>
 
   <androidx.fragment.app.FragmentContainerView

android/app/src/main/res/layout/place_page_route_ref.xml
@@ -4,22 +4,30 @@
   xmlns:tools="http://schemas.android.com/tools"
   xmlns:app="http://schemas.android.com/apk/res-auto"
   android:id="@+id/ll__place_route_ref"
-  style="@style/PlacePageItemFrame"
-  android:tag="route_ref"
-  tools:background="#40FF00FF"
-  tools:visibility="visible">
+  android:layout_width="match_parent"
+  android:layout_height="wrap_content"
+  android:orientation="vertical">
 
-  <ImageView
-    android:id="@+id/iv__place_route_ref"
-    style="@style/PlacePageMetadataIcon"
-    app:srcCompat="@drawable/ic_category_bus" />
+  <include layout="@layout/place_page_shadow_gap" />
 
-  <TextView
-    android:id="@+id/tv__place_route_ref"
-    android:textAlignment="viewStart"
-    android:layout_width="match_parent"
-    android:layout_height="wrap_content"
-    android:textAppearance="@style/MwmTextAppearance.PlacePage"
-    tools:text="Route references"/>
+  <LinearLayout
+    style="@style/PlacePageItemFrame"
+    android:tag="route_ref"
+    android:visibility="visible"
+    tools:background="#40FF00FF"
+    tools:visibility="visible">
 
-</LinearLayout> 
+    <ImageView
+      android:id="@+id/iv__place_route_ref"
+      style="@style/PlacePageMetadataIcon"
+      app:srcCompat="@drawable/ic_category_bus" />
+
+    <TextView
+      android:id="@+id/tv__place_route_ref"
+      android:textAlignment="viewStart"
+      android:layout_width="match_parent"
+      android:layout_height="wrap_content"
+      android:textAppearance="@style/MwmTextAppearance.PlacePage"
+      tools:text="Route references" />
+  </LinearLayout>
+</LinearLayout>

android/app/src/main/res/values-land/dimens.xml
@@ -17,5 +17,4 @@
   <!--About Fragment -->
   <dimen name="about_max_button_width">310dp</dimen>
   <item name="place_page_bottom_sheet_width_ratio" type="fraction">0.65</item>
-  <item name="place_page_bottom_sheet_height_ratio" type="dimen" format="float">0.70</item>
 </resources>

commit cc54101ce48669eae7e555241013b7a432a2e228

[android] Fixed NPE with uninitialized mCurrentWindowInsets
The crash was introduced in 0c639965b1

Signed-off-by: Alexander Borsuk <me@alex.bio>

android/app/src/main/java/app/organicmaps/widget/placepage/PlacePageController.java
@@ -91,6 +91,7 @@ public class PlacePageController
       // This callback may be called before insets are updated when resuming the app
       if (mCurrentWindowInsets == null)
         return;
+
       final int topInset = mCurrentWindowInsets.getInsets(WindowInsetsCompat.Type.systemBars()).top;
       // Only animate the status bar background if the place page can reach it
       if (mCoordinator.getHeight() - mPlacePageContainer.getHeight() < topInset)
@@ -226,11 +227,14 @@ public class PlacePageController
       });
     }
     mPlacePage.addOnLayoutChangeListener((v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom) -> {
+      // This callback may be called before insets are updated when resuming the app
+      if (mCurrentWindowInsets == null)
+        return;
+
       final int topInset = mCurrentWindowInsets.getInsets(WindowInsetsCompat.Type.systemBars()).top;
       if (mPlacePage.getHeight() >= mCoordinator.getHeight() - topInset)
-      {
         mPlacePageDistanceToTopObserver.onChanged(oldTop);
-      }
+
       if (top != oldTop)
       {
         mDistanceToTop = oldTop;
@@ -383,10 +387,10 @@ public class PlacePageController
    */
   private void animatePeekHeight(int peekHeight)
   {
+    // This callback may be called before insets are updated when resuming the app
     if (mCurrentWindowInsets == null)
-    {
       return;
-    }
+
     final int bottomInsets = mCurrentWindowInsets.getInsets(WindowInsetsCompat.Type.systemBars()).bottom;
     // Make sure to start from the current height of the place page
     final int parentHeight = ((View) mPlacePage.getParent()).getHeight();