a21a0e35cb5e67fe82c6fcd6af71cffd4c249728
kavikhalique <kavikhalique3@gmail.com>
2026-03-17 01:37:14 +0530

    Navigation mode fixes
    Signed-off-by: kavikhalique <kavikhalique3@gmail.com>

--- android/app/src/main/java/app/organicmaps/MwmActivity.java
+++ android/app/src/main/java/app/organicmaps/MwmActivity.java
@@ -752,6 +752,7 @@ public class MwmActivity extends BaseMwmFragmentActivity
   @Override
   public void onSearchCanceled()
   {
+    mMapButtonsViewModel.setSearchOption(null);
     forceCloseSearchFragment();
   }
 
