388de199263e56f9135c85b8fad0eac8f61d42ba
Viktor Govako <viktor.govako@gmail.com>
2026-03-31 20:52:01 -0300

    [map] Correct PP update when Route Track selection.
    Signed-off-by: Viktor Govako <viktor.govako@gmail.com>

--- libs/map/framework.cpp
+++ libs/map/framework.cpp
@@ -2088,9 +2088,12 @@ void Framework::OnTapEvent(place_page::BuildInfo const & buildInfo)
 
     m_currentPlacePageInfo = placePageInfo;
 
-    if (m_currentPlacePageInfo->GetTrackId() != kml::kInvalidTrackId)
+    auto const newTrackId = m_currentPlacePageInfo->GetTrackId();
+    if (newTrackId != kml::kInvalidTrackId)
     {
-      if (m_currentPlacePageInfo->GetTrackId() == prevTrackId)
+      // For user tracks: tapping the same track at a different point just moves the selection circle.
+      // Temp relation tracks always reuse the same ID, so always update the PlacePage for them.
+      if (newTrackId == prevTrackId && newTrackId != BookmarkManager::kTempRelationTrackId)
       {
         if (m_drapeEngine)
         {
@@ -2100,7 +2103,7 @@ void Framework::OnTapEvent(place_page::BuildInfo const & buildInfo)
         }
         return;
       }
-      GetBookmarkManager().UpdateElevationMyPosition(m_currentPlacePageInfo->GetTrackId());
+      GetBookmarkManager().UpdateElevationMyPosition(newTrackId);
     }
 
     ActivateMapSelection();
