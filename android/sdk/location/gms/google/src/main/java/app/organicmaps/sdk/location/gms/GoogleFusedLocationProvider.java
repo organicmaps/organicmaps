11c67aae15d373f0072117bf3270b31e883bb4e7
strider-dunadan <emerald.dunadan@gmail.com>
2026-04-03 13:06:20 +0200

    [android] Move mActive flag before start block (refactoring)
    Signed-off-by: strider-dunadan <emerald.dunadan@gmail.com>

--- android/sdk/location/gms/google/src/main/java/app/organicmaps/sdk/location/gms/GoogleFusedLocationProvider.java
+++ android/sdk/location/gms/google/src/main/java/app/organicmaps/sdk/location/gms/GoogleFusedLocationProvider.java
@@ -71,7 +71,6 @@ class GoogleFusedLocationProvider extends BaseLocationProvider
   public void start(long interval)
   {
     Logger.d(TAG);
-    mActive = true;
 
     final LocationRequest locationRequest =
         new LocationRequest
@@ -93,6 +92,7 @@ class GoogleFusedLocationProvider extends BaseLocationProvider
     builder.setAlwaysShow(true); // improves the wording/appearance of the dialog
     final LocationSettingsRequest locationSettingsRequest = builder.build();
 
+    mActive = true;
     mSettingsClient.checkLocationSettings(locationSettingsRequest)
         .addOnSuccessListener(locationSettingsResponse -> {
           if (!mActive)
