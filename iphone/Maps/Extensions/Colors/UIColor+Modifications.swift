2ef17ea1292393a9bea0de9d0742ecdf5f723025
Kiryl Kaveryn <kirylkaveryn@gmail.com>
2026-04-15 15:33:33 +0200

    [ios] Rename `Categories` to `Extensions` and add intermediate dirs
    1. The `extension` is more convenient naming for iOS because there are not categories in swift. Also `Categories` conflicts with widely used `bookmarks categories` entities.
    2. Some files are grouped into the subdirectories to simplify usage
    
    Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

--- iphone/Maps/Extensions/Colors/UIColor+Modifications.swift
+++ iphone/Maps/Extensions/Colors/UIColor+Modifications.swift
@@ -41,10 +41,3 @@ extension UIColor {
     }
   }
 }
-
-extension UIColor {
-  func components() -> (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat)? {
-    var r: CGFloat = 0, g: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 0
-    return getRed(&r, green: &g, blue: &b, alpha: &a) ? (r, g, b, a) : nil
-  }
-}
