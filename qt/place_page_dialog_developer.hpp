773fadc39b024d15a98b96bd1a476dc0d5d5b8d9
Viktor Govako <viktor.govako@gmail.com>
2026-04-21 12:39:34 -0300

    [desktop] Show route buttons in PP.
    Signed-off-by: Viktor Govako <viktor.govako@gmail.com>

--- qt/place_page_dialog_developer.hpp
+++ qt/place_page_dialog_developer.hpp
@@ -2,6 +2,8 @@
 
 #include <QtWidgets/QDialog>
 
+class Framework;
+
 namespace place_page
 {
 class Info;
@@ -12,5 +14,8 @@ class PlacePageDialogDeveloper : public QDialog
   Q_OBJECT
 
 public:
-  PlacePageDialogDeveloper(QWidget * parent, place_page::Info const & info);
+  PlacePageDialogDeveloper(QWidget * parent, place_page::Info const & info, Framework & framework);
+
+private:
+  Framework & m_framework;
 };
