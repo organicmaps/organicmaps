c40fa48ae8451c1f2167da109d2839e3b060e446
Viktor Govako <viktor.govako@gmail.com>
2026-05-07 12:19:59 -0300

    Skip invalid classifier types.
    Signed-off-by: Viktor Govako <viktor.govako@gmail.com>

--- libs/search/categories_set.hpp
+++ libs/search/categories_set.hpp
@@ -1,13 +1,8 @@
 #pragma once
 
-#include "indexer/classificator.hpp"
-#include "indexer/search_string_utils.hpp"
-
 #include "base/macros.hpp"
-#include "base/string_utils.hpp"
 
 #include <algorithm>
-#include <cstdint>
 #include <unordered_set>
 
 namespace search
@@ -15,9 +10,13 @@ namespace search
 class CategoriesSet
 {
 public:
-  CategoriesSet() : m_classificator(classif()) {}
+  CategoriesSet() = default;
 
-  inline void Add(uint32_t type) { m_categories.insert(type); }
+  void Add(uint32_t type)
+  {
+    ASSERT(type != 0, ());  // Classificator::INVALID_TYPE
+    m_categories.insert(type);
+  }
 
   template <typename Fn>
   void ForEach(Fn && fn) const
@@ -26,7 +25,6 @@ public:
   }
 
 private:
-  Classificator const & m_classificator;
   std::unordered_set<uint32_t> m_categories;
 
   DISALLOW_COPY_AND_MOVE(CategoriesSet);
