143999ae706322a888dd3ea23514646ba0164896
Viktor Govako <viktor.govako@gmail.com>
2026-05-24 18:45:58 -0300

    [iOS] Fix and pass lat/lon coordinates when opening Editor view.
    Signed-off-by: Viktor Govako <viktor.govako@gmail.com>

--- iphone/Maps/UI/Editor/MWMObjectsCategorySelectorController.h
+++ iphone/Maps/UI/Editor/MWMObjectsCategorySelectorController.h
@@ -1,5 +1,7 @@
 #import "MWMViewController.h"
 
+#include "geometry/point2d.hpp"
+
 #include <string>
 
 namespace osm
@@ -18,5 +20,9 @@ class EditableMapObject;
 @property(weak, nonatomic) id<MWMObjectsCategorySelectorDelegate> delegate;
 
 - (void)setSelectedCategory:(std::string const &)type;
+// Position captured when the user confirms placement. Must be set before the controller is shown;
+// the picked category is created at this exact point instead of the live viewport center
+// (which can drift while the user browses categories).
+- (void)setCreatedPosition:(m2::PointD const &)position;
 
 @end
