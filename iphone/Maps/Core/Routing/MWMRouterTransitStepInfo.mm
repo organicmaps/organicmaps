d05d9a7beba2cbbad44f870b8908ea3aa8c9aaf8
Viktor Govako <viktor.govako@gmail.com>
2026-06-12 10:24:47 -0300

    [transit] Update transit display with new types.
    Signed-off-by: Viktor Govako <viktor.govako@gmail.com>

--- iphone/Maps/Core/Routing/MWMRouterTransitStepInfo.mm
+++ iphone/Maps/Core/Routing/MWMRouterTransitStepInfo.mm
@@ -14,6 +14,8 @@ MWMRouterTransitType convertType(TransitType type)
   case TransitType::Train: return MWMRouterTransitTypeTrain;
   case TransitType::LightRail: return MWMRouterTransitTypeLightRail;
   case TransitType::Monorail: return MWMRouterTransitTypeMonorail;
+  case TransitType::Tram: return MWMRouterTransitTypeTram;
+  case TransitType::Bus: return MWMRouterTransitTypeBus;
   // This is temporary solution for compiling iOS project after adding new
   // TransitType values. When these values will be approved we'll add them
   // above in switch(type) and remove this line.
