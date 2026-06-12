dfbb473ce3fa89c9f804148e4d670cd4ebb747c2
Alexander Borsuk <me@alex.bio>
2026-06-12 21:04:43 +0200

    [data][3party] Remove protobuf
    Signed-off-by: Alexander Borsuk <me@alex.bio>

--- qt/build_style/build_statistics.cpp
+++ qt/build_style/build_statistics.cpp
@@ -22,9 +22,7 @@ QString GetStyleStatistics(QString const & mapcssMappingFile, QString const & dr
   if (!QFile(drulesFile).exists())
     throw std::runtime_error("drawing-rules file does not exist at " + drulesFile.toStdString());
 
-  // Add path to the protobuf EGG in the PROTOBUF_EGG_PATH environment variable.
   QProcessEnvironment env{QProcessEnvironment::systemEnvironment()};
-  env.insert("PROTOBUF_EGG_PATH", GetProtobufEggPath());
 
   // Run the script.
   return ExecProcess("python",
