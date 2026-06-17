f604a7800b95d5d8941730b3abc94b220888f6a8
Kiryl Kaveryn <kirylkaveryn@gmail.com>
2026-06-17 11:23:56 +0200

    [ios] Update Tiles setting screen: use slider for mb; clamp values.
    Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

--- iphone/Maps/UI/Settings/MWMMapTilesSettingsViewController.h
+++ iphone/Maps/UI/Settings/MWMMapTilesSettingsViewController.h
@@ -2,8 +2,6 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-// Settings sub-screen for the custom raster background tiles (XYZ {z}/{x}/{y} source). Built
-// programmatically; push it from the main Settings list (see MWMSettingsViewController).
 @interface MWMMapTilesSettingsViewController : MWMTableViewController
 
 @end
