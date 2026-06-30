d14aee32673a1120ddeef8b1dc582f5c4a6df969
Kiryl Kaveryn <kirylkaveryn@gmail.com>
2026-06-30 11:52:30 +0200

    [ios] Display elevation info chart placeholder on track recording start instead of empty space
    Also a label to explain the user why the profile is still empty.
    Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

--- iphone/Maps/UI/PlacePage/Components/ElevationProfile/ElevationProfileBuilder.swift
+++ iphone/Maps/UI/PlacePage/Components/ElevationProfile/ElevationProfileBuilder.swift
@@ -1,13 +1,16 @@
 import CoreApi
 
 class ElevationProfileBuilder {
-  static func build(routeElevationPreviewData: RouteElevationPreviewData,
+  private enum Constants {
+    static let minPointsToDraw = 2
+  }
+
+  static func build(state: ElevationProfileState,
                     delegate: ElevationProfileViewControllerDelegate?,
                     presentationStyle: ElevationProfileViewController.PresentationStyle) -> ElevationProfileViewController {
     let viewController = ElevationProfileViewController(presentationStyle: presentationStyle)
     let presenter = ElevationProfilePresenter(view: viewController,
-                                              trackInfo: routeElevationPreviewData.trackInfo,
-                                              elevationProfileData: routeElevationPreviewData.elevationProfileData,
+                                              state: state,
                                               delegate: delegate)
     viewController.presenter = presenter
     return viewController
@@ -15,15 +18,72 @@ class ElevationProfileBuilder {
 
   static func build(trackData: PlacePageTrackData,
                     delegate: ElevationProfileViewControllerDelegate?,
+                    presentationStyle: ElevationProfileViewController.PresentationStyle) -> ElevationProfileViewController? {
+    guard let state = makeTrackState(trackData: trackData) else {
+      return nil
+    }
+    return build(state: state, delegate: delegate, presentationStyle: presentationStyle)
+  }
+
+  static func build(trackRecordingData trackData: PlacePageTrackData,
+                    delegate: ElevationProfileViewControllerDelegate?,
                     presentationStyle: ElevationProfileViewController.PresentationStyle) -> ElevationProfileViewController {
-    let viewController = ElevationProfileViewController(presentationStyle: presentationStyle)
-    let presenter = ElevationProfilePresenter(view: viewController,
-                                              trackInfo: trackData.trackInfo,
-                                              elevationProfileData: trackData.elevationProfileData,
-                                              activePointDistance: trackData.activePointDistance,
-                                              myPositionDistance: trackData.myPositionDistance,
-                                              delegate: delegate)
-    viewController.presenter = presenter
-    return viewController
+    build(state: makeTrackRecordingState(trackData: trackData), delegate: delegate, presentationStyle: presentationStyle)
+  }
+
+  static func makeRoutePreviewState(routeElevationPreviewData: RouteElevationPreviewData) -> ElevationProfileState? {
+    guard let chartData = makeChartData(elevationProfileData: routeElevationPreviewData.elevationProfileData) else {
+      return nil
+    }
+    let data = ElevationProfileDisplayData(trackInfo: routeElevationPreviewData.trackInfo,
+                                           chartData: chartData,
+                                           activePointDistance: 0,
+                                           myPositionDistance: 0)
+    return .routePreview(data)
+  }
+
+  static func makeTrackRecordingState(trackData: PlacePageTrackData) -> ElevationProfileState {
+    let chartData = makeChartData(elevationProfileData: trackData.elevationProfileData)
+      ?? makePlaceholderChartData(elevationProfileData: trackData.elevationProfileData)
+    let data = ElevationProfileDisplayData(trackInfo: trackData.trackInfo,
+                                           chartData: chartData,
+                                           activePointDistance: trackData.activePointDistance,
+                                           myPositionDistance: trackData.myPositionDistance)
+    return .trackRecording(data)
+  }
+
+  private static func makeTrackState(trackData: PlacePageTrackData) -> ElevationProfileState? {
+    guard trackData.trackInfo.hasElevationInfo,
+          let chartData = makeChartData(elevationProfileData: trackData.elevationProfileData) else {
+      return nil
+    }
+    let data = ElevationProfileDisplayData(trackInfo: trackData.trackInfo,
+                                           chartData: chartData,
+                                           activePointDistance: trackData.activePointDistance,
+                                           myPositionDistance: trackData.myPositionDistance)
+    return .track(data)
+  }
+
+  private static func makeChartData(elevationProfileData: ElevationProfileData?) -> ElevationProfileChartData? {
+    guard let elevationProfileData else {
+      return nil
+    }
+    let chartData = ElevationProfileChartData(elevationProfileData)
+    guard chartData.pointsCount >= Constants.minPointsToDraw else {
+      return nil
+    }
+    return chartData
+  }
+
+  private static func makePlaceholderChartData(elevationProfileData: ElevationProfileData?) -> ElevationProfileChartData {
+    let altitude = elevationProfileData?.points.first?.altitude ?? currentAltitude ?? 0
+    return .placeholder(altitude: altitude)
+  }
+
+  private static var currentAltitude: Double? {
+    guard let location = LocationManager.lastLocation(), location.verticalAccuracy >= 0 else {
+      return nil
+    }
+    return location.altitude
   }
 }
