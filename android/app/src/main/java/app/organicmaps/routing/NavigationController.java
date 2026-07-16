3597a087d4787253a8f8a327846ef31de96170d2
strider-dunadan <emerald.dunadan@gmail.com>
2026-07-16 13:53:28 +0200

    [android] Removed usused method
    Signed-off-by: strider-dunadan <emerald.dunadan@gmail.com>

--- android/app/src/main/java/app/organicmaps/routing/NavigationController.java
+++ android/app/src/main/java/app/organicmaps/routing/NavigationController.java
@@ -15,7 +15,6 @@ import androidx.lifecycle.ViewModelProvider;
 import app.organicmaps.MwmApplication;
 import app.organicmaps.R;
 import app.organicmaps.maplayer.MapButtonsViewModel;
-import app.organicmaps.sdk.Framework;
 import app.organicmaps.sdk.Router;
 import app.organicmaps.sdk.maplayer.traffic.TrafficManager;
 import app.organicmaps.sdk.routing.RoutingController;
@@ -137,14 +136,6 @@ public class NavigationController implements TrafficManager.TrafficCallback, Nav
       updateSpeedLimit(info);
     }
     mNavMenu.update(info);
-  }
-
-  public void updateNorth()
-  {
-    if (!RoutingController.get().isNavigating())
-      return;
-
-    update(Framework.nativeGetRouteFollowingInfo());
   }
 
   public void show(boolean show)
