26a2bd61ae567f9590dc65cac5e3701180ba6f5c
Kuzey Bilgin <kuzeybilgin@proton.me>
2026-07-22 14:16:57 -0400

    Draw spinner programmatically instead of images
    Signed-off-by: Kuzey Bilgin <kuzeybilgin@proton.me>

--- qt/qt_common/spinner.cpp
+++ qt/qt_common/spinner.cpp
@@ -1,46 +1,63 @@
 #include "qt/qt_common/spinner.hpp"
 
-#include <QtCore/QString>
-#include <QtCore/QTimer>
-#include <QtCore/Qt>
+#include <QPainter>
+#include <QTimer>
 
 namespace
 {
-int constexpr kMinSpinnerPixmap = 1;
-int constexpr kMaxSpinnerPixmap = 12;
+int constexpr kSegmentCount = 12;
+int constexpr kSegmentStep = 360 / kSegmentCount;
 int constexpr kTimeoutMs = 100;
+int constexpr kWidgetSize = 20;
+int constexpr kOuterRadius = 8;
+int constexpr kSegmentWidth = 2;
+int constexpr kSegmentLength = 4;
 }  // namespace
 
 Spinner::Spinner()
 {
-  for (int i = kMinSpinnerPixmap; i <= kMaxSpinnerPixmap; ++i)
-  {
-    auto const path = ":common/spinner" + QString::number(i) + ".png";
-    m_pixmaps.emplace_back(path);
-  }
-
+  setFixedSize(kWidgetSize, kWidgetSize);
   setEnabled(false);
-  setAlignment(Qt::AlignCenter);
 
   QSizePolicy policy(QSizePolicy::Fixed, QSizePolicy::Fixed);
   policy.setRetainSizeWhenHidden(true);
   setSizePolicy(policy);
 
-  setMinimumSize(m_pixmaps.front().size());
-  setPixmap(m_pixmaps.front());
-
-  m_timer = new QTimer(this /* parent */);
+  m_timer = new QTimer(this);
   connect(m_timer, &QTimer::timeout, [this]()
   {
-    m_progress = (m_progress + 1) % m_pixmaps.size();
-    setPixmap(m_pixmaps[m_progress]);
+    m_angle = (m_angle + kSegmentStep) % 360;
+    update();
   });
+}
+
+void Spinner::paintEvent(QPaintEvent *)
+{
+  QPainter painter(this);
+  painter.setRenderHint(QPainter::Antialiasing);
+
+  auto const center = QPointF(width() / 2.0, height() / 2.0);
+
+  for (int i = 0; i < kSegmentCount; ++i)
+  {
+    int const alpha = 255 * (kSegmentCount - i) / kSegmentCount;
+    painter.setPen(Qt::NoPen);
+    painter.setBrush(QColor(0, 0, 0, alpha));
+
+    painter.save();
+    painter.translate(center);
+    painter.rotate(m_angle + i * kSegmentStep);
+
+    painter.drawRoundedRect(-kSegmentWidth / 2, -kOuterRadius, kSegmentWidth, kSegmentLength, kSegmentWidth / 2.0,
+                            kSegmentWidth / 2.0);
+    painter.restore();
+  }
 }
 
 void Spinner::Show()
 {
-  m_progress = 0;
-  setPixmap(m_pixmaps[m_progress]);
+  m_angle = 0;
+  update();
   m_timer->start(kTimeoutMs);
   show();
 }
