25e62454277a969a2cb0297c8b9c3fd8ea32212c
Viktor Govako <viktor.govako@gmail.com>
2026-07-29 10:33:33 -0300

    [classifier] Added leisure=bird_hide.
    Signed-off-by: Viktor Govako <viktor.govako@gmail.com>

--- generator/generator_tests/osm_type_test.cpp
+++ generator/generator_tests/osm_type_test.cpp
@@ -2399,6 +2399,7 @@ UNIT_CLASS_TEST(TestWithClassificator, OsmType_SimpleTypesSmoke)
       {"landuse", "salt_pond"},
       {"landuse", "village_green"},
       {"landuse", "vineyard"},
+      {"leisure", "bird_hide"},
       {"leisure", "common"},
       {"leisure", "dog_park"},
       {"leisure", "fitness_centre"},
