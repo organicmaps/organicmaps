7ea9820d09a651a89d02dee4339fdbf14af36027
Alexander Borsuk <me@alex.bio>
2026-08-18 11:26:29 +0200

    [base] Fix flaky benchmark
    SmallMap_Benchmark1 asserted that SmallMap is not slower than unordered_map on
    a 26 entries dataset. The margin is too small to survive a loaded CI runner,
    where t2 > t1 happens from time to time. Only log both timings now, with a
    comment explaining why nothing is asserted.
    
    SmallMap_Benchmark2 keeps its assertion: hashing a string beats comparing it
    by a stable ~2x, so it is a cheap guard against using SmallMap for string keys.
    
    Benchmarks are compiled in Release only, and they were the sole place where
    SmallMap was verified at all, hence a new SmallMap_Smoke test.
    
    Signed-off-by: Alexander Borsuk <me@alex.bio>

--- libs/base/base_tests/small_set_test.cpp
+++ libs/base/base_tests/small_set_test.cpp
@@ -72,18 +72,37 @@ UNIT_TEST(SmallSet_Smoke)
   TEST_EQUAL(set.Size(), std::distance(set.begin(), set.end()), ());
 }
 
-bool BenchmarkTimeLessOrNear(uint64_t l, uint64_t r, double relativeTolerance)
+UNIT_TEST(SmallMap_Smoke)
 {
-  return (l < r) || ((l - r) / static_cast<double>(l) < relativeTolerance);
+  // Unsorted init to also cover FinishBuilding().
+  SmallMap<uint32_t, int> map = {{5, 50}, {1, 10}, {3, 30}};
+  TEST_EQUAL(map.size(), 3, ());
+  TEST(!map.Find(0), ());
+  TEST(!map.Find(6), ());
+  TEST_EQUAL(*map.Find(1), 10, ());
+  TEST_EQUAL(map.Get(5), 50, ());
+
+  map.Replace(3, 33);
+  TEST_EQUAL(*map.Find(3), 33, ());
+  SmallMap<uint32_t, int> const expected = {{1, 10}, {3, 33}, {5, 50}};
+  TEST(map == expected, ());
+
+  SmallMapBase<uint32_t, int> baseMap;
+  baseMap.Insert(7, 70);
+  TEST_EQUAL(*baseMap.Find(7), 70, ());
+  TEST(!baseMap.Find(8), ());
 }
 
 #ifndef DEBUG
 std::vector<uint32_t> GenerateIndices(uint32_t min, uint32_t max)
 {
+  size_t constexpr kCount = 10000000;
+
   std::vector<uint32_t> res;
+  res.reserve(kCount);
 
   base::UniformRandom<uint32_t> randDist(min, max);
-  for (size_t i = 0; i < 10000000; ++i)
+  for (size_t i = 0; i < kCount; ++i)
     res.push_back(randDist());
 
   return res;
@@ -124,8 +143,7 @@ UNIT_TEST(SmallMap_Benchmark1)
   }
 
   TEST_EQUAL(sum1, sum2, ());
-  // At this moment, we have rare t2 > t1 on Linux CI.
-  TEST(BenchmarkTimeLessOrNear(t2, t1, 0.3), (t2, t1));
+  // Expect t2 < t1 here, but the margin is too small to assert: it flakes on CI.
   LOG(LINFO, ("unordered_map time =", t1, "SmallMap time =", t2));
 }
 
@@ -204,63 +222,10 @@ UNIT_TEST(SmallMap_Benchmark2)
   }
 
   TEST_EQUAL(sum1, sum2, ());
-  // std::hash(std::string) is better than std::less(std::string)
+  // std::hash(std::string) is better than std::less(std::string), the margin is ~2x.
   TEST_LESS(t1, t2, ());
   LOG(LINFO, ("unordered_map time =", t1, "SmallMap time =", t2));
 }
-
-// Small 4 elements sample doesn't work for new (gcc11+, clang14+) toolchain.
-/*
-UNIT_TEST(SmallMap_Benchmark3)
-{
-  // Dataset is similar to routing::VehicleModel.m_surfaceFactors.
-  std::unordered_map<int, int> uMap = {
-    {1, 0}, {10, 1}, {100, 2}, {1000, 3},
-  };
-
-  base::SmallMap<int, int> sMap(uMap.begin(), uMap.end());
-  base::SmallMapBase<int, int> sbMap(uMap.begin(), uMap.end());
-
-  std::vector<uint32_t> indices = GenerateIndices(0, 3);
-  // Missing key queries are even worse for the std map.
-  std::vector<int> keys;
-  for (auto const & e : uMap)
-    keys.push_back(e.first);
-
-  uint64_t t1, t2, t3;
-  uint32_t sum1 = 0, sum2 = 0, sum3 = 0;
-
-  // 3. Run unordered_map.
-  {
-    base::HighResTimer timer;
-    for (auto i : indices)
-      sum1 += uMap.find(keys[i])->second;
-    t1 = timer.ElapsedMilliseconds();
-  }
-
-  // 4. Run SmallMap.
-  {
-    base::HighResTimer timer;
-    for (auto i : indices)
-      sum2 += *sMap.Find(keys[i]);
-    t2 = timer.ElapsedMilliseconds();
-  }
-
-  // 5. Run SmallMapBase.
-  {
-    base::HighResTimer timer;
-    for (auto i : indices)
-      sum3 += *sbMap.Find(keys[i]);
-    t3 = timer.ElapsedMilliseconds();
-  }
-
-  TEST_EQUAL(sum1, sum2, ());
-  TEST_EQUAL(sum1, sum3, ());
-  TEST_LESS(t2, t1, ());
-  TEST(BenchmarkTimeLessOrNear(t3, t2, 0.05), (t3, t2));
-  LOG(LINFO, ("unordered_map time =", t1, "SmallMap time =", t2, "SmallMapBase time =", t3));
-}
-*/
 #endif
 
 }  // namespace small_set_test
