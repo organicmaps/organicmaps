19bccef9390835b79f3b97d18cf64d11059f8c09
BelKed <66956532+BelKed@users.noreply.github.com>
2026-09-04 01:09:13 +0200

    [ios] Fix HTML descriptions with mismatched appearance
    Signed-off-by: BelKed <66956532+BelKed@users.noreply.github.com>

--- iphone/Maps/Tests/UI/PlacePageTests/PlacePageUserDescriptionWebViewTests.swift
+++ iphone/Maps/Tests/UI/PlacePageTests/PlacePageUserDescriptionWebViewTests.swift
@@ -26,28 +26,67 @@ final class PlacePageUserDescriptionWebViewTests: XCTestCase {
     XCTAssertTrue(measuredHeights.allSatisfy { $0 > 0 && $0 < 70 }, "Measured heights: \(measuredHeights)")
   }
 
+  func test_GivenDifferentCreationAndHostAppearances_WhenHTMLIsLoaded_ThenMeasuresContent() {
+    let (_, lightHeights) = loadHTML("<div style=\"height: 20px\"></div>",
+                                     creationStyle: .dark,
+                                     hostStyle: .light) { $0 < 70 }
+    XCTAssertTrue(lightHeights.allSatisfy { $0 > 0 && $0 < 70 })
+
+    let (_, darkHeights) = loadHTML("<div style=\"height: 20px\"></div>",
+                                    creationStyle: .light,
+                                    hostStyle: .dark) { $0 < 70 }
+    XCTAssertTrue(darkHeights.allSatisfy { $0 > 0 && $0 < 70 })
+  }
+
+  func test_GivenExplicitAppearance_WhenFragmentIsBuilt_ThenUsesMatchingTextColor() {
+    let builder = UserDescriptionHTMLDocumentBuilder()
+    let lightTraits = UITraitCollection(userInterfaceStyle: .light)
+    let darkTraits = UITraitCollection(userInterfaceStyle: .dark)
+
+    let lightHTML = builder.buildHTML(with: "Text", compatibleWith: lightTraits)
+    let darkHTML = builder.buildHTML(with: "Text", compatibleWith: darkTraits)
+    let lightColor = UIColor.blackPrimaryText.resolvedColor(with: lightTraits).hexString
+    let darkColor = UIColor.blackPrimaryText.resolvedColor(with: darkTraits).hexString
+
+    XCTAssertNotEqual(lightColor, darkColor)
+    XCTAssertTrue(lightHTML.contains("color: \(lightColor)"))
+    XCTAssertTrue(darkHTML.contains("color: \(darkColor)"))
+  }
+
   func test_GivenAlternatingPooledDocuments_WhenHeightIsMeasured_ThenIgnoresPreviousDocument() {
     var retainedWebView: PlacePageUserDescriptionWebView?
     var measuredHeights: [CGFloat]
 
-    (retainedWebView, measuredHeights) = loadHTML("<div style=\"height: 240px\"></div>") { $0 > 200 }
+    (retainedWebView, measuredHeights) = loadHTML("<div style=\"height: 240px\"></div>",
+                                                  creationStyle: .light,
+                                                  hostStyle: .dark) { $0 > 200 }
     XCTAssertTrue(measuredHeights.allSatisfy { $0 > 200 })
     retainedWebView = nil
 
-    (retainedWebView, measuredHeights) = loadHTML("<div style=\"height: 20px\"></div>") { $0 < 70 }
+    (retainedWebView, measuredHeights) = loadHTML("<div style=\"height: 20px\"></div>",
+                                                  creationStyle: .dark,
+                                                  hostStyle: .light) { $0 < 70 }
     XCTAssertTrue(measuredHeights.allSatisfy { $0 > 0 && $0 < 70 })
     retainedWebView = nil
 
-    (retainedWebView, measuredHeights) = loadHTML("<div style=\"height: 240px\"></div>") { $0 > 200 }
+    (retainedWebView, measuredHeights) = loadHTML("<div style=\"height: 240px\"></div>",
+                                                  creationStyle: .light,
+                                                  hostStyle: .dark) { $0 > 200 }
     XCTAssertTrue(measuredHeights.allSatisfy { $0 > 200 })
     withExtendedLifetime(retainedWebView) {}
   }
 
   private func loadHTML(_ html: String,
+                        creationStyle: UIUserInterfaceStyle = .unspecified,
+                        hostStyle: UIUserInterfaceStyle = .unspecified,
                         until isExpectedHeight: @escaping (CGFloat) -> Bool) ->
     (PlacePageUserDescriptionWebView, [CGFloat]) {
     let expectation = expectation(description: "HTML height measured")
-    let webView = PlacePageUserDescriptionWebView(htmlString: html)
+    let creationTraits = UITraitCollection(userInterfaceStyle: creationStyle)
+    var webView: PlacePageUserDescriptionWebView!
+    creationTraits.performAsCurrent {
+      webView = PlacePageUserDescriptionWebView(htmlString: html)
+    }
     var measuredHeights = [CGFloat]()
     var isFulfilled = false
     webView.onContentHeightChanged = { [weak webView] in
@@ -58,11 +97,20 @@ final class PlacePageUserDescriptionWebViewTests: XCTestCase {
       isFulfilled = true
       expectation.fulfill()
     }
+    let viewController = UIViewController()
+    viewController.overrideUserInterfaceStyle = hostStyle
+    let window = UIWindow(frame: CGRect(x: 0, y: 0, width: width, height: 640))
+    window.rootViewController = viewController
+    window.isHidden = false
     webView.frame = CGRect(x: 0, y: 0, width: width, height: webView.collapsedHeight(for: width))
+    viewController.view.addSubview(webView)
     webView.layoutIfNeeded()
 
-    wait(for: [expectation], timeout: 5)
+    withExtendedLifetime(window) {
+      wait(for: [expectation], timeout: 5)
+    }
     webView.onContentHeightChanged = nil
+    window.isHidden = true
     return (webView, measuredHeights)
   }
 }
