73a98e883081ede283fee79f8619db4183a18a82
Alexander Borsuk <me@alex.bio>
2026-09-03 23:33:01 +0200

    [ios] Select a custom voice from voiceover
    Signed-off-by: Alexander Borsuk <me@alex.bio>
    Signed-off-by: Kiryl Kaveryn <kirylkaveryn@gmail.com>

--- iphone/Maps/Core/TextToSpeech/LocaleTranslator.h
+++ iphone/Maps/Core/TextToSpeech/LocaleTranslator.h
@@ -5,6 +5,6 @@
 namespace locale_translator
 {
 
-std::string bcp47ToTwineLanguage(NSString const * bcp47LangName);
+std::string bcp47ToTwineLanguage(NSString * bcp47LangName);
 
 }  // namespace locale_translator
