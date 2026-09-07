dc84a98fa2973bd070a764ad312c1ef4de13543b
Alexander Borsuk <me@alex.bio>
2026-09-07 08:18:53 +0200

    [coding] Replace existing files in RenameFileX on Windows
    rename() fails with EEXIST on Windows when the destination exists, so
    every "write to a temporary file, then rename over the old one" save
    (bookmarks, editor notes, feature offsets tables, ...) failed there.
    MoveFileEx with MOVEFILE_REPLACE_EXISTING gives the POSIX semantics;
    MoveFileX reuses it, so a move over an existing file no longer degrades
    to a full copy on Windows.
    
    Signed-off-by: Alexander Borsuk <me@alex.bio>

--- libs/coding/internal/file_data.cpp
+++ libs/coding/internal/file_data.cpp
@@ -19,6 +19,8 @@
 #include <vector>
 
 #ifdef OMIM_OS_WINDOWS
+#include "std/windows.hpp"
+
 #include <io.h>
 #else
 #include <unistd.h>  // ftruncate
@@ -210,15 +212,22 @@ bool DeleteFileX(std::string const & fName)
 
 bool RenameFileX(std::string const & fOld, std::string const & fNew)
 {
+#ifdef OMIM_OS_WINDOWS
+  // rename() does not replace an existing destination on Windows.
+  if (::MoveFileExA(fOld.c_str(), fNew.c_str(), MOVEFILE_REPLACE_EXISTING))
+    return true;
+  LOG(LWARNING, ("Can't rename file", fOld, "to", fNew, "- error", ::GetLastError()));
+  return false;
+#else
   int res = rename(fOld.c_str(), fNew.c_str());
   return CheckFileOperationResult(res, fOld);
+#endif
 }
 
 bool MoveFileX(std::string const & fOld, std::string const & fNew)
 {
   // Try to rename the file first.
-  int res = rename(fOld.c_str(), fNew.c_str());
-  if (res == 0)
+  if (RenameFileX(fOld, fNew))
     return true;
 
   // Otherwise perform the full move.
