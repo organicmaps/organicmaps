ced04f2a368c0cf7f33d6ddcbf2428a09e1a3d53
Alexander Borsuk <me@alex.bio>
2026-09-12 22:04:13 +0200

    [routing] Remove unused IndexRouter::SetTransitAltFactors()
    The test hook has no callers, the alternative sets the factors on the
    estimator directly.
    
    Signed-off-by: Alexander Borsuk <me@alex.bio>

--- libs/routing/index_router.hpp
+++ libs/routing/index_router.hpp
@@ -94,13 +94,6 @@ public:
   VehicleType GetVehicleType() const { return m_vehicleType; }
   std::shared_ptr<NumMwmIds> const & GetNumMwmIds() const { return m_numMwmIds; }
 
-  // Test/tuning hook: bias the transit routing weight of walking and transfers.
-  // Used by tests to force a bus over a short walk.
-  void SetTransitAltFactors(double walkFactor, double transferFactor)
-  {
-    m_estimator->SetTransitAltFactors(walkFactor, transferFactor);
-  }
-
   template <class T>
   void SetCurrentTimeGetter(T && getter)
   {
